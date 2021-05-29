fa2cab07ee6eae070c20ad06c499598e8463f30a
naeunkim <knae11@naver.com>
2021-05-29 19:18:38 +0900

    feat: 카드를 빋는 기능

--- src/test/kotlin/blackjackgame/model/PlayerTest.kt
+++ src/test/kotlin/blackjackgame/model/PlayerTest.kt
@@ -1,5 +1,8 @@
 package blackjackgame.model
 
+import blackjackgame.model.card.Card
+import blackjackgame.model.card.Denomination
+import blackjackgame.model.card.Suit
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
@@ -9,5 +12,26 @@ internal class PlayerTest {
         val rocki = Player("rocki")
 
         assertThat(rocki.name).isEqualTo("rocki")
+    }
+
+    @Test
+    internal fun drawCard() {
+        val better = Player("better")
+        val card = Card(Suit.HEART, Denomination.ACE)
+        better.drawCard(card)
+
+        assertThat(better.cards).hasSize(1)
+        assertThat(better.cards[0]).isEqualTo(card)
+    }
+
+    @Test
+    internal fun drawCards() {
+        val better = Player("better")
+        val card1 = Card(Suit.HEART, Denomination.ACE)
+        val card2 = Card(Suit.CLOVER, Denomination.TWO)
+        better.drawCard(listOf(card1,card2))
+
+        assertThat(better.cards).hasSize(2)
+        assertThat(better.cards).containsExactly(card1, card2)
     }
 }
