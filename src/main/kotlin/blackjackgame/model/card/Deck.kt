9a3a7608da8149bd0fdc6f32976aff7ca7cec1df
naeunkim <knae11@naver.com>
2021-05-29 19:05:55 +0900

    feat: 카드를 나눠주는 기능

--- src/main/kotlin/blackjackgame/model/card/Deck.kt
+++ src/main/kotlin/blackjackgame/model/card/Deck.kt
@@ -2,6 +2,16 @@ package blackjackgame.model.card
 
 class Deck(val cards: MutableList<Card> = shuffledCards()) {
 
+    fun handOutInitialCards(): List<Card> {
+        require(cards.size >= 2) { "패가 부족합니다." }
+        return listOf(cards.removeLast(), cards.removeLast())
+    }
+
+    fun handOutCard(): Card {
+        require(cards.size >= 1) { "패가 부족합니다." }
+        return cards.removeLast()
+    }
+
     companion object {
         fun shuffledCards(): MutableList<Card> {
             return CARDS.shuffled().toMutableList()
