996634dc39a3a5d1c9b06aec7280c9cea570bc44
naeunkim <knae11@naver.com>
2021-05-29 19:05:55 +0900

    feat: 카드를 나눠주는 기능

--- src/test/kotlin/blackjackgame/model/card/DeckTest.kt
+++ src/test/kotlin/blackjackgame/model/card/DeckTest.kt
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 internal class DeckTest{
+
     @Test
     internal fun createDeck() {
         val deck = Deck()
@@ -11,5 +12,23 @@ internal class DeckTest{
 
         assertThat(deck.cards).hasSize(52)
         assertThat(deck.cards).hasSize(distinctCards.size)
+    }
+
+    @Test
+    internal fun handOutInitialCards() {
+        val deck = Deck()
+        val cards = deck.handOutInitialCards()
+
+        assertThat(deck.cards).hasSize(50)
+        assertThat(cards).hasSize(2)
+    }
+
+    @Test
+    internal fun handOutCard() {
+        val deck = Deck()
+        val card = deck.handOutCard()
+
+        assertThat(deck.cards).hasSize(51)
+        assertThat(card).isNotNull
     }
 }
