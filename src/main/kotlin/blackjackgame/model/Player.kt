e9d7930b285a858154f122f9c9cdc8814a037be7
naeunkim <knae11@naver.com>
2021-05-29 19:18:38 +0900

    feat: 카드를 빋는 기능

--- src/main/kotlin/blackjackgame/model/Player.kt
+++ src/main/kotlin/blackjackgame/model/Player.kt
@@ -5,6 +5,12 @@ import blackjackgame.model.card.Card
 class Player(val name: String) {
     val cards = mutableListOf<Card>()
 
+    fun drawCard(card: Card) {
+        cards.add(card)
+    }
 
+    fun drawCard(cards: List<Card>) {
+        this.cards.addAll(cards)
+    }
 
 }
