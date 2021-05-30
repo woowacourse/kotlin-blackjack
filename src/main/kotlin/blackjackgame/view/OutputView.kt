99635e2be41af3d45ca84d92b03686e05db198f7
naeunkim <knae11@naver.com>
2021-05-30 17:39:31 +0900

    feat: 딜러의 점수가 16 이하이면 카드를 추가로 한 장 받는 기능 구현

--- src/main/kotlin/blackjackgame/view/OutputView.kt
+++ src/main/kotlin/blackjackgame/view/OutputView.kt
@@ -3,23 +3,31 @@ package blackjackgame.view
 import blackjackgame.model.card.Card
 
 fun printStatus(players: List<Pair<String, List<Card>>>) {
+    println()
     println("${players.joinToString(",") { it.first }}에게 2장의 카드를 나누었습니다.")
     players.forEach {
         println("${it.first}카드: ${printCard(it.second)} ")
     }
+    println()
 }
 
 fun printStatus(player: Pair<String, List<Card>>) {
     println("${player.first}카드: ${printCard(player.second)} ")
 }
 
-fun printCard(cards: List<Card>): String {
+private fun printCard(cards: List<Card>): String {
     val result = cards.map { "${it.denomination.value}${it.suit.value}" }
     return result.joinToString(",")
 }
 
 fun printFinalResult(results: List<Triple<String, List<Card>, Int>>) {
+    println()
     results.forEach {
         println("${it.first}카드: ${printCard(it.second)} - 결과: ${it.third}")
     }
+    println()
+}
+
+fun printDealerTurnResult() {
+    println("딜러는 16이하라 한장의 카드를 더 받았습니다.")
 }
