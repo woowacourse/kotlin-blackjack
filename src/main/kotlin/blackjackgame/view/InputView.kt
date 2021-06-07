114bf9e812b921d3e7c42579644337f7030abb96
naeunkim <knae11@naver.com>
2021-06-07 13:32:39 +0900

    refactor: input 입력받는 방식 재귀형식으로 수정

--- src/main/kotlin/blackjackgame/view/InputView.kt
+++ src/main/kotlin/blackjackgame/view/InputView.kt
@@ -2,25 +2,28 @@ package blackjackgame.view
 
 fun inputPlayerNames(): List<String> {
     println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)")
-    val names = readLine() ?: throw IllegalArgumentException()
-    return names.split(",")
+    return readLine()?.split(",") ?: inputPlayerNames()
 }
 
-fun inputPlayerBetting(name:String):Int{
+fun inputPlayerBetting(name: String): Int {
     println("${name}의 배팅 금액은?")
-   return readLine() ?.toInt() ?: inputPlayerBetting(name)
+    return readLine()?.toInt() ?: inputPlayerBetting(name)
 }
 
 fun inputAskDrawCard(name: String): Boolean {
     println("${name}는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)")
-    val answer = readLine() ?: throw IllegalArgumentException()
-    if (answer.toLowerCase() == "y") {
+    return readLine()?.convertYesOrNo() ?: inputAskDrawCard(name)
+
+}
+
+private fun String.convertYesOrNo(): Boolean? {
+    if (this.toLowerCase() == "y") {
         return true
     }
 
-    if (answer.toLowerCase() == "n") {
+    if (this.toLowerCase() == "n") {
         return false
     }
 
-    throw IllegalArgumentException()
+    return null
 }
