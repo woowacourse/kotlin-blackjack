f4ca670ce3cd712d4a350e079490fb4c37cb9faf
rhkrwngud445 <rhkrwngud445@gmail.com>
2023-02-28 20:23:29 +0900

    feat: 플레이어의 카드를 뽑는 기능 구현
    Co-authored-by: pingu244 <pingu244@naver.com>
    Co-authored-by: rhkrwngud445 <rhkrwngud445@gmail.com>

---
+++ src/test/kotlin/domain/gamer/PlayerTest.kt
@@ -0,0 +1,31 @@
+package domain.gamer
+
+import domain.card.Card
+import domain.card.Shape
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+class PlayerTest {
+    @ParameterizedTest
+    @MethodSource("generateCard")
+    fun `뽑은 카드를 저장한다`(cards: List<Card>) {
+        val player = Player()
+        for (card in cards) {
+            player.pick(card)
+        }
+        Assertions.assertThat(player.cards).isEqualTo(cards)
+    }
+
+    companion object {
+        @JvmStatic
+        private fun generateCard(): List<Arguments> =
+            listOf(
+                Arguments.of(
+                    listOf(Card(Shape.SPADE, "A"), Card(Shape.HEART, "2"), Card(Shape.CLOVER, "3")),
+                    listOf(Card(Shape.SPADE, "Q"), Card(Shape.SPADE, "3"))
+                )
+            )
+    }
+}
