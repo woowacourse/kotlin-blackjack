7bebf1efccaeb3230d0f96db13d66c7d6cdc1ec5
re4rk <37167652+re4rk@users.noreply.github.com>
2023-02-28 14:13:24 +0900

    feat(CardValue) : 카드들의 숫자를 구현한다.

---
+++ src/main/kotlin/blackjack/domain/CardValue.kt
@@ -0,0 +1,17 @@
+package blackjack.domain
+
+enum class CardValue(val value: Int) {
+    ACE(11),
+    KING(10),
+    QUEEN(10),
+    JACK(10),
+    TEN(10),
+    NINE(9),
+    EIGHT(8),
+    SEVEN(7),
+    SIX(6),
+    FIVE(5),
+    FOUR(4),
+    THREE(3),
+    TWO(2),
+}
---
+++ src/test/kotlin/blackjack/domain/CardValueTest.kt
@@ -0,0 +1,72 @@
+package blackjack.domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class CardValueTest {
+
+    @Test
+    fun `A의 값은 11이다`() {
+        assertThat(CardValue.ACE.value).isEqualTo(11)
+    }
+
+    @Test
+    fun `K의 값은 10이다`() {
+        assertThat(CardValue.KING.value).isEqualTo(10)
+    }
+
+    @Test
+    fun `Q의 값은 10이다`() {
+        assertThat(CardValue.QUEEN.value).isEqualTo(10)
+    }
+
+    @Test
+    fun `J의 값은 10이다`() {
+        assertThat(CardValue.JACK.value).isEqualTo(10)
+    }
+
+    @Test
+    fun `TEN의 값은 10이다`() {
+        assertThat(CardValue.TEN.value).isEqualTo(10)
+    }
+
+    @Test
+    fun `NINE의 값은 9이다`() {
+        assertThat(CardValue.NINE.value).isEqualTo(9)
+    }
+
+    @Test
+    fun `EIGHT의 값은 8이다`() {
+        assertThat(CardValue.EIGHT.value).isEqualTo(8)
+    }
+
+    @Test
+    fun `SEVEN의 값은 7이다`() {
+        assertThat(CardValue.SEVEN.value).isEqualTo(7)
+    }
+
+    @Test
+    fun `SIX의 값은 6이다`() {
+        assertThat(CardValue.SIX.value).isEqualTo(6)
+    }
+
+    @Test
+    fun `FIVE의 값은 5이다`() {
+        assertThat(CardValue.FIVE.value).isEqualTo(5)
+    }
+
+    @Test
+    fun `FOUR의 값은 4이다`() {
+        assertThat(CardValue.FOUR.value).isEqualTo(4)
+    }
+
+    @Test
+    fun `THREE의 값은 3이다`() {
+        assertThat(CardValue.THREE.value).isEqualTo(3)
+    }
+
+    @Test
+    fun `TWO의 값은 2이다`() {
+        assertThat(CardValue.TWO.value).isEqualTo(2)
+    }
+}
