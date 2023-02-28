7d7c588f51c8e17488541926c2133dd4e7142c36
chws0508 <chws0508@naver.com>
2023-02-28 17:49:05 +0900

    feat(Name): Name 클래스 구현

---
+++ src/main/kotlin/domain/Name.kt
@@ -0,0 +1,13 @@
+package domain
+
+@JvmInline
+value class Name(val name: String) {
+    init {
+        check(name.matches(regex)) { ERROR_NAME_FORMAT }
+    }
+
+    companion object {
+        private val regex = Regex("^[a-zA-Z가-힣]*\$")
+        private const val ERROR_NAME_FORMAT = "[ERROR] 이름은 문자열만 허용합니다."
+    }
+}
