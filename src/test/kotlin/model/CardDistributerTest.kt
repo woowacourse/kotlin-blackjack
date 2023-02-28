352defcfb0846071a1d8a5ceb4dbde30e1526792
Doyeop Kim <dygames34@gmail.com>
2023-02-28 15:46:12 +0900

    refactor: 카드 여러 장 배분에 대한 테스트 리팩토링

--- src/test/kotlin/model/CardDistributerTest.kt
+++ src/test/kotlin/model/CardDistributerTest.kt
@@ -9,15 +9,17 @@ import org.junit.jupiter.api.Test
 
 class CardDistributerTest() {
     @Test
-    fun `플레이어들에게 카드 1장 씩 나누어 주었을 때 정상적으로 분배됐다`() {
-        val manualCardFactory = ManualCardFactory(listOf(CardType.CLUB), listOf(CardNumber.THREE))
+    fun `플레이어들에게 카드 2장 씩 나누어 주었을 때 정상적으로 분배됐다`() {
+        // given
+        val manualCardFactory = ManualCardFactory(listOf(CardType.CLUB, CardType.SPADE), listOf(CardNumber.THREE, CardNumber.QUEEN))
         val cardDistributer = CardDistributer(manualCardFactory)
         val player = Player(Cards(mutableListOf()))
 
         // when
-        val except = Card(CardType.CLUB, CardNumber.THREE)
+        val except = listOf(Card(CardType.CLUB, CardNumber.THREE), Card(CardType.SPADE, CardNumber.QUEEN))
+        cardDistributer.distribute(player, 2)
 
-        cardDistributer.distribute(player, 1)
-        assertThat(player.cards.value[0]).isEqualTo(except)
+        // then
+        assertThat(player.cards.value).isEqualTo(except)
     }
 }
--- src/test/kotlin/model/ManualCardFactory.kt
+++ src/test/kotlin/model/ManualCardFactory.kt
@@ -5,7 +5,7 @@ import entity.CardNumber
 import entity.CardType
 
 class ManualCardFactory(private val cardTypes: List<CardType>, private val cardNumbers: List<CardNumber>) : CardFactory {
-    var index = 0
+    private var index = 0
 
     override fun generate(): Card {
         return Card(cardTypes[index], cardNumbers[index++])
