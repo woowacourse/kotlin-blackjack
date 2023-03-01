2b0181e601ceeb721fa0d948cf94532e90cfe9c1
re4rk <37167652+re4rk@users.noreply.github.com>
2023-03-01 20:29:49 +0900

    refactor(CardDeck) : 상수처리

--- src/main/kotlin/blackjack/domain/CardDeck.kt
+++ src/main/kotlin/blackjack/domain/CardDeck.kt
@@ -15,7 +15,7 @@ class CardDeck(cards: List<Card>) {
 
     companion object {
         private const val CARDS_SIZE = 52
-        private const val ERROR_INVALID_CARDS_SIZE = "카드덱 초기 사이즈는 52장이어야 합니다."
+        private const val ERROR_INVALID_CARDS_SIZE = "카드덱 초기 사이즈는 ${CARDS_SIZE}장이어야 합니다."
         private const val ERROR_EXIST_DUPLICATE_CARDS = "카드덱에는 중복이 없어야 합니다."
     }
 }
