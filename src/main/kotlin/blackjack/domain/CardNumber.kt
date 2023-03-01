e485c715f4258f1dd307d955b52307f6a8748446
tmdgh1592 <tmdgh1592@naver.com>
2023-03-01 14:10:46 +0900

    feat: 카드 숫자를 문자로 반환하는 기능 구현

--- src/main/kotlin/blackjack/domain/CardNumber.kt
+++ src/main/kotlin/blackjack/domain/CardNumber.kt
@@ -6,9 +6,22 @@ value class CardNumber(private val number: Int) {
         require(number in MIN_CARD_NUMBER..MAX_CARD_NUMBER) { NUMBER_RANGE_ERROR }
     }
 
+    fun toLetter() = when (number) {
+        ACE_NUMBER -> "A"
+        JACK_NUMBER -> "J"
+        QUEEN_NUMBER -> "Q"
+        KING_NUMBER -> "K"
+        else -> number.toString()
+    }
+
     companion object {
         private const val MIN_CARD_NUMBER = 1
         private const val MAX_CARD_NUMBER = 13
         private const val NUMBER_RANGE_ERROR = "카드 숫자의 범위는 1부터 13이어야 합니다."
+
+        private const val ACE_NUMBER = 1
+        private const val JACK_NUMBER = 11
+        private const val QUEEN_NUMBER = 12
+        private const val KING_NUMBER = 13
     }
 }
--- src/test/kotlin/blackjack/domain/CardNumberTest.kt
+++ src/test/kotlin/blackjack/domain/CardNumberTest.kt
@@ -1,8 +1,10 @@
 package blackjack.domain
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import org.junit.jupiter.params.provider.ValueSource
 
 class CardNumberTest {
@@ -16,5 +18,15 @@ class CardNumberTest {
     @ValueSource(ints = [0, 14])
     fun `카드 숫자의 범위는 1부터 13을 벗어날 수 없다`(number: Int) {
         assertThrows<IllegalArgumentException> { CardNumber(number) }
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "1, A", "2, 2", "3, 3", "4, 4", "5, 5", "6, 6",
+        "7, 7", "8, 8", "9, 9", "10, 10", "11, J", "12, Q", "13, K",
+    )
+    fun `카드 숫자를 문자로 반환한다`(number: Int, expected: String) {
+        val cardNumber = CardNumber(number)
+        assertThat(cardNumber.toLetter()).isEqualTo(expected)
     }
 }
