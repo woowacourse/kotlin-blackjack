8dd81dcde2647cd86d48ca47ddeefc2f0f496151
vrexpert <vrexpert2302@gmail.com>
2023-03-02 20:55:12 +0900

    feat(Card): override Card toString

--- src/main/kotlin/domain/card/Card.kt
+++ src/main/kotlin/domain/card/Card.kt
@@ -1,3 +1,7 @@
 package domain.card
 
-data class Card(val shape: CardShape, val number: CardNumber)
+data class Card(val shape: CardShape, val number: CardNumber) {
+    override fun toString(): String {
+        return number.label + shape.label
+    }
+}
