9d18c8c6e086ed305e748a9697944b0d7ca515e1
whk06061 <gju06061@naver.com>
2023-03-02 17:18:03 +0900

    feat: 플레이어 이름의 길이가 2글자 이상이고 10글자 이하인지 확인하는 기능 구현

--- src/main/kotlin/blackjack/domain/Player.kt
+++ src/main/kotlin/blackjack/domain/Player.kt
@@ -1,3 +1,13 @@
 package blackjack.domain
 
-class Player(val name: String)
+interface Player {
+    val name: String
+
+    fun checkNameLength() {
+        require(name.length in 2..10) { ERROR_NAME_LENGTH }
+    }
+
+    companion object {
+        const val ERROR_NAME_LENGTH = "이름은 2글자 이상 10글자 이하여야 합니다."
+    }
+}
--- src/test/kotlin/blackjack/domain/PlayerTest.kt
+++ src/test/kotlin/blackjack/domain/PlayerTest.kt
@@ -2,11 +2,31 @@ package blackjack.domain
 
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class PlayerTest {
 
     @Test
     fun `이름을 생성할 수 있다`() {
-        assertDoesNotThrow { Player("수달") }
+        assertDoesNotThrow { TestPlayer("수달") }
+    }
+
+    @Test
+    fun `이름은 2글자 이상 10글자 이하여야 한다`() {
+        assertDoesNotThrow { TestPlayer("berry") }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["b", "berryyyyyyy"])
+    fun `이름은 2글자 미만이거나 10글자 초과면 오류가 발생한다`(expected: String) {
+        assertThrows<IllegalArgumentException> { TestPlayer(expected) }
+    }
+
+    class TestPlayer(override val name: String) : Player {
+        init {
+            checkNameLength()
+        }
     }
 }
