f07fa503f278bb20e8eb785287e5000701534252
tmdgh1592 <tmdgh1592@naver.com>
2023-03-02 15:17:03 +0900

    feat: 플레이어가 가진 카드 목록에 카드를 추가하는 기능 구현

--- src/main/kotlin/blackjack/domain/Player.kt
+++ src/main/kotlin/blackjack/domain/Player.kt
@@ -1,3 +1,9 @@
 package blackjack.domain
 
-class Player(val name: String)
+class Player(val name: String) {
+    val hand = PlayerHand()
+
+    fun addCard(card: Card) {
+        hand.add(card)
+    }
+}
--- src/test/kotlin/blackjack/domain/PlayerTest.kt
+++ src/test/kotlin/blackjack/domain/PlayerTest.kt
@@ -9,4 +9,12 @@ class PlayerTest {
         val player = Player("pobi")
         assertThat(player.name).isEqualTo("pobi")
     }
+
+    @Test
+    fun `플레이어는 카드 목록에 카드를 추가한다`() {
+        val player = Player("pobi")
+        player.addCard(Card.of(2))
+
+        assertThat(player.hand.cards).containsExactly(Card.of(2))
+    }
 }
