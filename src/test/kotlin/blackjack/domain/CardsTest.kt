5f7b5c961a70cf66934733aed8e7c6ace004cdc0
whk06061 <gju06061@naver.com>
2023-03-02 20:49:07 +0900

    fix: CardsTest 오류 해결
    - Cards 클래스에서 생성자 파라미터를 제거하였으나 테스트코드에서 반영을 하지 않아 발생한 오류 해결

--- src/test/kotlin/blackjack/domain/CardsTest.kt
+++ src/test/kotlin/blackjack/domain/CardsTest.kt
@@ -2,34 +2,21 @@ package blackjack.domain
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertDoesNotThrow
 
 class CardsTest {
-    @Test
-    fun `카드들을 생성할 수 있다`() {
-        val cards = listOf(Card(CardNumber.KING, CardShape.CLOVER), Card(CardNumber.TWO, CardShape.HEART))
-        assertDoesNotThrow { Cards(cards) }
-    }
 
     @Test
     fun `새로운 카드를 받아 가진 카드에 추가한다`() {
-        val cards = Cards(
-            listOf(
-                Card(CardNumber.FOUR, CardShape.DIAMOND), Card(CardNumber.JACK, CardShape.CLOVER)
-            )
-        )
+        val cards = Cards()
         cards.addCard(Card(CardNumber.KING, CardShape.HEART))
-        assertThat(cards.values.size).isEqualTo(3)
+        assertThat(cards.values.size).isEqualTo(1)
     }
 
     @Test
     fun `갖고 있는 카드 숫자의 합을 계산해 반환한다`() {
-        val cards = Cards(
-            listOf(
-                Card(CardNumber.FOUR, CardShape.DIAMOND),
-                Card(CardNumber.JACK, CardShape.CLOVER)
-            )
-        )
+        val cards = Cards()
+        cards.addCard(Card(CardNumber.FOUR, CardShape.DIAMOND))
+        cards.addCard(Card(CardNumber.JACK, CardShape.CLOVER))
         val actual = cards.sumCardsNumber()
         assertThat(actual).isEqualTo(14)
     }
