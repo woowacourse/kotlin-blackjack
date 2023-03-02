f024fbb6e145b5703206f70587cf38e44e8fb66e
rhkrwngud445 <rhkrwngud445@gmail.com>
2023-03-02 14:49:51 +0900

    refactor: 매개변수 네이밍 수정
    Co-authored-by: pingu244 <pingu244@naver.com>
    Co-authored-by: rhkrwngud445 <rhkrwngud445@gmail.com>

--- src/main/kotlin/domain/gamer/Dealer.kt
+++ src/main/kotlin/domain/gamer/Dealer.kt
@@ -2,8 +2,8 @@ package domain.gamer
 
 import domain.card.Card
 
-class Dealer(private val _card: MutableList<Card> = mutableListOf()) : Participant(_card) {
-    fun checkAvailableForPick() = _card.sumOf { it.cardValue.value } > CARD_PICK_CONDITION
+class Dealer(private val _cards: MutableList<Card> = mutableListOf()) : Participant(_cards) {
+    fun checkAvailableForPick() = _cards.sumOf { it.cardValue.value } > CARD_PICK_CONDITION
 
     companion object {
         private const val CARD_PICK_CONDITION = 16
--- src/main/kotlin/domain/gamer/Player.kt
+++ src/main/kotlin/domain/gamer/Player.kt
@@ -2,9 +2,9 @@ package domain.gamer
 
 import domain.card.Card
 
-class Player(private val _card: MutableList<Card> = mutableListOf()) : Participant(_card) {
+class Player(private val _cards: MutableList<Card> = mutableListOf()) : Participant(_cards) {
 
-    fun checkBurst(): Boolean = _card.sumOf { it.cardValue.value } > CARD_SUM_MAX_VALUE
+    fun checkBurst(): Boolean = _cards.sumOf { it.cardValue.value } > CARD_SUM_MAX_VALUE
 
     companion object {
         private const val CARD_SUM_MAX_VALUE = 21
