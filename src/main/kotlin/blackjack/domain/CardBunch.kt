c0ad0941782ca091b59311c58867b79f9594cb32
changhwan <chang5hustle@gmail.com>
2023-03-02 17:06:22 +0900

    feat(CardBunch): 카드의 총합을 계산하는 기능 구현

--- src/main/kotlin/blackjack/domain/CardBunch.kt
+++ src/main/kotlin/blackjack/domain/CardBunch.kt
@@ -18,6 +18,23 @@ class CardBunch private constructor(cards: MutableSet<Card>) {
         require(originSize != _cards.size) { DUPLICATE_ERROR }
     }
 
+    fun getTotalScore(): Int {
+        var result = 0
+        val sortedCards = cards.sortedBy { it.cardNumber.value }.reversed()
+
+        sortedCards.forEach {
+            result += it.cardNumber.value ?: checkAceValue(result)
+        }
+        return result
+    }
+
+    private fun checkAceValue(result: Int): Int {
+        return when (result + 11 > 21) {
+            true -> 1
+            false -> 11
+        }
+    }
+
     companion object {
         private const val INITIAL_SIZE_ERROR = "초기 카드는 2장이 배정되어야 합니다."
         private const val DUPLICATE_ERROR = "중복된 카드는 추가할 수 없습니다."
--- src/test/kotlin/blackjack/domain/CardBunchTest.kt
+++ src/test/kotlin/blackjack/domain/CardBunchTest.kt
@@ -54,4 +54,43 @@ class CardBunchTest {
 
         assertThat(cardBunch.cards.size).isEqualTo(3)
     }
+
+    @Test
+    fun `6,7,9 카드의 총합을 계산한다`() {
+        val card1 = Card(Shape.HEART, CardNumber.SEVEN)
+        val card2 = Card(Shape.HEART, CardNumber.SIX)
+        val card3 = Card(Shape.HEART, CardNumber.NINE)
+
+        val cardBunch = CardBunch(card1, card2)
+        cardBunch.addCard(card3)
+
+        val totalScore = cardBunch.getTotalScore()
+        assertThat(totalScore).isEqualTo(22)
+    }
+
+    @Test
+    fun `Ace,2,3 카드의 총합을 계산한다`() {
+        val card1 = Card(Shape.HEART, CardNumber.ACE)
+        val card2 = Card(Shape.HEART, CardNumber.TWO)
+        val card3 = Card(Shape.HEART, CardNumber.THREE)
+
+        val cardBunch = CardBunch(card1, card2)
+        cardBunch.addCard(card3)
+
+        val totalScore = cardBunch.getTotalScore()
+        assertThat(totalScore).isEqualTo(16)
+    }
+
+    @Test
+    fun `Ace,Jack,King 카드의 총합을 계산한다`() {
+        val card1 = Card(Shape.HEART, CardNumber.ACE)
+        val card2 = Card(Shape.HEART, CardNumber.JACK)
+        val card3 = Card(Shape.HEART, CardNumber.KING)
+
+        val cardBunch = CardBunch(card1, card2)
+        cardBunch.addCard(card3)
+
+        val totalScore = cardBunch.getTotalScore()
+        assertThat(totalScore).isEqualTo(21)
+    }
 }
