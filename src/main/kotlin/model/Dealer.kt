25ee7ff44da118c509c2c4d53a5516b026be5ca3
hyemdooly <hyemdooly@gmail.com>
2023-03-02 17:22:09 +0900

    refactor(Dealer): 딜러 이름 추가

--- src/main/kotlin/model/Dealer.kt
+++ src/main/kotlin/model/Dealer.kt
@@ -1,8 +1,12 @@
 package model
 
-class Dealer(name: Name, cards: Cards) : Participant(name, cards) {
+class Dealer(name: Name = Name(DEALER), cards: Cards) : Participant(name, cards) {
     override fun isHit(): Boolean {
         if (cards.sum() < 17) return true
         return false
+    }
+
+    companion object {
+        private const val DEALER = "딜러"
     }
 }
