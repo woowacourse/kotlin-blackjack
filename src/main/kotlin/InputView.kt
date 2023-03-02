4a93a0d792e21a9f1d3f468a6ae0f1d1b3e77637
Yerin <myheaven_b@naver.com>
2023-03-02 16:00:34 +0900

    feat: 카드를 더 받을지에 대한 여부를 확인

--- src/main/kotlin/InputView.kt
+++ src/main/kotlin/InputView.kt
@@ -5,4 +5,10 @@ object InputView {
 
         return readln().split(",")
     }
+
+    fun requestAdditionalDraw(player: Player): String {
+        println("%s는 한장의 카드를 더 받겠습니까?".format(player.name))
+
+        return readln()
+    }
 }
--- src/main/kotlin/OutputView.kt
+++ src/main/kotlin/OutputView.kt
@@ -1,4 +1,4 @@
-class OutputView {
+object OutputView {
 
     fun printCardDividingMessage(dealer: Dealer, players: List<Player>) {
         println("딜러와${players.joinToString(",") { it.name }}에게 2장의 카드를 나누었습니다.")
