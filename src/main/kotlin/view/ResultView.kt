254a0bc6d1288bf22461fdda6b662358c24fad06
hyunji1203 <hj27720@gmail.com>
2023-03-02 20:57:04 +0900

    feat(ResultView): 게임 결과 화면

---
+++ src/main/kotlin/view/ResultView.kt
@@ -0,0 +1,47 @@
+package view
+
+import entity.Dealer
+import entity.DealerGameResult
+import entity.GameResultType
+import entity.Players
+import entity.PlayersGameResult
+
+class ResultView {
+    fun printGameStatus(dealer: Dealer, players: Players) {
+        println(MESSAGE_DEALER_GAME_STATUS.format(ViewUtils.cardsToString(dealer.cards), dealer.cardsNumberSum()))
+        players.value.forEach {
+            println(MESSAGE_PLAYERS_GAME_STATUS.format(it.name, ViewUtils.cardsToString(it.cards), it.cardsNumberSum()))
+        }
+    }
+
+    fun printGameResult(dealerGameResult: DealerGameResult, playersGameResult: PlayersGameResult) {
+        println(MESSAGE_GAME_RESULT)
+        println(MESSAGE_DEALER_GAME_RESULT.format(formatDealerGameResult(dealerGameResult)))
+        playersGameResult.value.forEach {
+            println(MESSAGE_PLAYERS_GAME_RESULT.format(it.key.name, gameResultTypeToString(it.value)))
+        }
+    }
+
+    private fun formatDealerGameResult(dealerGameResult: DealerGameResult) {
+        dealerGameResult.value.asSequence().joinToString(" ") {
+            MESSAGE_GAME_RESULT_TYPE.format(it.value, gameResultTypeToString(it.key))
+        }
+    }
+
+    private fun gameResultTypeToString(gameResultType: GameResultType): String {
+        return when (gameResultType) {
+            GameResultType.WIN -> "승"
+            GameResultType.LOSE -> "패"
+            GameResultType.DRAW -> "무"
+        }
+    }
+
+    companion object {
+        private const val MESSAGE_DEALER_GAME_STATUS = "딜러 카드: %s - 결과: %d"
+        private const val MESSAGE_PLAYERS_GAME_STATUS = "%s카드: %s - 결과: %d"
+        private const val MESSAGE_GAME_RESULT = "## 최종 승패"
+        private const val MESSAGE_DEALER_GAME_RESULT = "딜러: %s"
+        private const val MESSAGE_PLAYERS_GAME_RESULT = "%s: %s"
+        private const val MESSAGE_GAME_RESULT_TYPE = "%d%s"
+    }
+}
