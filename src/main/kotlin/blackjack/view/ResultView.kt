40ae2d239682f1c2e50975901bdc031f50398b6c
Sujin Kim <sujinkim919@gmail.com>
2023-03-02 21:36:43 +0900

    fix: 딜러가 처음에만 카드 한 장만 출력함을 반영하여 수정

--- src/main/kotlin/blackjack/view/ResultView.kt
+++ src/main/kotlin/blackjack/view/ResultView.kt
@@ -2,6 +2,7 @@ package blackjack.view
 
 import blackjack.domain.Card
 import blackjack.domain.Dealer
+import blackjack.domain.Participant
 import blackjack.domain.Participant.Companion.INIT_CARD_SIZE
 import blackjack.domain.Player
 
@@ -12,15 +13,16 @@ object ResultView {
     fun printSetUp(dealer: Dealer, players: List<Player>) {
         val names = players.joinToString(", ") { it.name }
         println(SET_UP_MESSAGE.format(dealer.name, names))
-        println(dealer.faceUp())
+        println(dealer.faceUpOnlyOne())
         players.forEach { println(it.faceUp()) }
+        println()
     }
 
     fun printPlayerCards(player: Player) {
         println(player.faceUp())
     }
 
-    private fun Dealer.faceUp() = FACE_UP_CARDS.format(this.name, this.cards[0].name())
-    private fun Player.faceUp() = FACE_UP_CARDS.format(this.name, this.cards.joinToString(", ") { it.name() })
+    private fun Dealer.faceUpOnlyOne() = FACE_UP_CARDS.format(this.name, this.cards[0].name())
+    private fun Participant.faceUp() = FACE_UP_CARDS.format(this.name, this.cards.joinToString(", ") { it.name() })
     private fun Card.name() = "${this.number.mark}${this.shape.mark}"
 }
