26511655ed64ab74fac43cc567a8fbda5ed18b2e
hyemdooly <hyemdooly@gmail.com>
2023-03-02 21:01:00 +0900

    refactor(Participant): 상수 선언

--- src/main/kotlin/model/Participant.kt
+++ src/main/kotlin/model/Participant.kt
@@ -3,7 +3,10 @@ package model
 abstract class Participant(val cards: Cards, val name: Name) {
     abstract fun isHit(): Boolean
     fun isBust(): Boolean {
-        if (cards.sum() > 21) return true
+        if (cards.sum() > BUST_POINT) return true
         return false
+    }
+    companion object {
+        private const val BUST_POINT = 21
     }
 }
