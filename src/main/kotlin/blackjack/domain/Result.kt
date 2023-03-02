91d48a88750badc93bcada912a0958624df34d12
whk06061 <gju06061@naver.com>
2023-03-02 15:22:33 +0900

    feat: 결과 단어를 가지는 Result Enum Class 구현

---
+++ src/main/kotlin/blackjack/domain/Result.kt
@@ -0,0 +1,7 @@
+package blackjack.domain
+
+enum class Result(val word: String) {
+    WIN("승"),
+    LOSE("패"),
+    DRAW("무")
+}
---
+++ src/test/kotlin/blackjack/domain/ResultTest.kt
@@ -0,0 +1,15 @@
+package blackjack.domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+class ResultTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = ["승", "패", "무"])
+    fun `승패무 결과를 가진다`(expected: String) {
+        val resultWord: List<String> = Result.values().map { it.word }
+        assertThat(resultWord).contains(expected)
+    }
+}
