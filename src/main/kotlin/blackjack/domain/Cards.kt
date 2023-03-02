b25ba0d60df5194858f08e337b9bf53da485b5b5
whk06061 <gju06061@naver.com>
2023-03-02 19:59:27 +0900

    feat: 처음에 받는 카드가 두장인지 확인하는 기능 구현

--- src/main/kotlin/blackjack/domain/Cards.kt
+++ src/main/kotlin/blackjack/domain/Cards.kt
@@ -1,3 +1,17 @@
 package blackjack.domain
 
-class Cards(private val cards: MutableList<Card>)
+class Cards(private val cards: MutableList<Card>) {
+
+    init {
+        checkCardInitSize()
+    }
+
+    private fun checkCardInitSize() {
+        require(cards.size == CARDS_INITIAL_SIZE) { ERROR_CARDS_INITIAL_SIZE }
+    }
+
+    companion object {
+        const val CARDS_INITIAL_SIZE = 2
+        const val ERROR_CARDS_INITIAL_SIZE = "처음 받는 카드는 두 장이어야 합니다"
+    }
+}
--- src/test/kotlin/blackjack/domain/CardsTest.kt
+++ src/test/kotlin/blackjack/domain/CardsTest.kt
@@ -2,11 +2,28 @@ package blackjack.domain
 
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
 
 class CardsTest {
     @Test
     fun `카드들을 생성할 수 있다`() {
         val cards = mutableListOf(Card(CardNumber.KING, CardShape.CLOVER), Card(CardNumber.TWO, CardShape.HEART))
         assertDoesNotThrow { Cards(cards) }
+    }
+
+    @Test
+    fun `처음으로 받은 카드가 두장인지 확인한다`() {
+        val cards = mutableListOf(Card(CardNumber.FOUR, CardShape.DIAMOND), Card(CardNumber.JACK, CardShape.CLOVER))
+        assertDoesNotThrow { Cards(cards) }
+    }
+
+    @Test
+    fun `처음으로 받은 카드가 두장이 아니라면 에러가 발생한다`() {
+        val cards = mutableListOf(
+            Card(CardNumber.FOUR, CardShape.DIAMOND),
+            Card(CardNumber.JACK, CardShape.CLOVER),
+            Card(CardNumber.KING, CardShape.HEART)
+        )
+        assertThrows<IllegalArgumentException> { Cards(cards) }
     }
 }
