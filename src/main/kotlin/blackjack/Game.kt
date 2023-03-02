7bd924aa63e2a0661f4e0b9bd9b2569b1bf96321
Sujin Kim <sujinkim919@gmail.com>
2023-03-02 20:18:02 +0900

    feat: 게임 시작 시, 딜러와 플레이어에게 2 장씩 카드 분배 구현

--- src/main/kotlin/blackjack/Game.kt
+++ src/main/kotlin/blackjack/Game.kt
@@ -1,9 +1,21 @@
 package blackjack
 
+import blackjack.domain.Dealer
+import blackjack.domain.Deck
+import blackjack.domain.Participant
+import blackjack.domain.Participant.Companion.INIT_CARD_SIZE
 import blackjack.domain.Player
 import blackjack.view.InputView
 
 fun main() {
     val playerNames = InputView.getNames()
     val players = playerNames.map(::Player)
+    val dealer = Dealer()
+    dealCards(players + dealer)
+}
+
+private fun dealCards(participants: List<Participant>) {
+    repeat(INIT_CARD_SIZE) {
+        participants.forEach { it.receive(Deck.draw()) }
+    }
 }
