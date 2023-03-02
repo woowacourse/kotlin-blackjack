5ac413c39af7a063fb3666f65305b91423df2cb9
re4rk <37167652+re4rk@users.noreply.github.com>
2023-03-02 20:29:15 +0900

    fix(BlackJackGame) : 취소 안되는 버그 수정

--- src/main/kotlin/blackjack/domain/BlackJackGame.kt
+++ src/main/kotlin/blackjack/domain/BlackJackGame.kt
@@ -5,14 +5,8 @@ class BlackJackGame {
 
     fun input(func: (String) -> String) { input = func }
 
-    fun BlackJack.guestsTurn(output: (User) -> Unit) {
-        guests.forEach { guest ->
-            while (guest.isContinue) {
-                command(guest, cardDeck)
-                output(guest)
-            }
-        }
-    }
+    fun BlackJack.guestsTurn(output: (User) -> Unit) =
+        guests.forEach { guest -> guestTurn(guest, output) }
 
     fun BlackJack.dealerTurn(output: () -> Unit) {
         if (dealer.isContinue) {
@@ -21,12 +15,22 @@ class BlackJackGame {
         }
     }
 
-    private fun command(guest: Guest, cardDeck: CardDeck) {
-        if (guest.isDrawCommand()) return
-        guest.draw(cardDeck.nextCard())
+    private fun BlackJack.guestTurn(guest: Guest, output: (User) -> Unit) {
+        when (guest.getCommand()) {
+            in DRAW_COMMANDS -> draw(guest, output)
+            else -> output(guest)
+        }
     }
 
-    private fun Guest.isDrawCommand() = input(this.name.toString()) !in DRAW_COMMANDS
+    private fun BlackJack.draw(guest: Guest, output: (User) -> Unit) {
+        guest.draw(cardDeck.nextCard())
+        output(guest)
+        if (guest.isContinue) {
+            guestTurn(guest, output)
+        }
+    }
+
+    private fun Guest.getCommand() = input(this.name.toString())
 
     companion object {
         const val BLACKJACK_NUMBER = 21
