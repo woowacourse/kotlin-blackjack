82399ffd7fa282a6d1effca0e9dcd200b27154c1
changhwan <chang5hustle@gmail.com>
2023-03-02 21:07:57 +0900

    feat(InputView): 카드 지급여부를 입력받는 기능 구현

--- src/main/kotlin/blackjack/view/InputView.kt
+++ src/main/kotlin/blackjack/view/InputView.kt
@@ -1,5 +1,7 @@
 package blackjack.view
 
+import blackjack.domain.Player
+
 class InputView {
     fun getPlayerNames(): List<String> {
         return runCatching {
@@ -11,7 +13,23 @@ class InputView {
         }
     }
 
+    fun getDecision(player: Player): Boolean {
+        return runCatching {
+            println(GET_DECISION_SCRIPT.format(player.name))
+            return when (readln().lowercase()) {
+                "y" -> true
+                "n" -> false
+                else -> throw IllegalArgumentException(INCORRECT_DECISION_ERROR)
+            }
+        }.getOrElse {
+            println(it.message)
+            getDecision(player)
+        }
+    }
+
     companion object {
         private const val GET_PLAYER_NAME_SCRIPT = "게임에 참가할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)"
+        private const val GET_DECISION_SCRIPT = "%d는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)"
+        private const val INCORRECT_DECISION_ERROR = "올바른 입력이 아닙니다(y,n으로 입력해 주세요"
     }
 }
