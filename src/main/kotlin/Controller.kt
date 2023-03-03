895acc443e06d59ddaf7915842383759b4101eee
s9hn <tnghqn@naver.com>
2023-03-03 15:07:35 +0900

    [REFACTOR(Controller)] run() 메서드 분리

--- src/main/kotlin/Controller.kt
+++ src/main/kotlin/Controller.kt
@@ -1,5 +1,4 @@
 import domain.*
-import domain.Answer.Companion.NO
 import domain.Answer.Companion.YES
 import view.GameResultView
 import view.LoginView
@@ -8,58 +7,89 @@ import view.PlayGameView
 class Controller(
     private val loginView: LoginView = LoginView(),
     private val playGameView: PlayGameView = PlayGameView(),
-    private val cardMachine: CardMachine = CardMachine(),
     private val gameResultView: GameResultView = GameResultView(),
+    private val cardMachine: CardMachine = CardMachine(),
 
 ) {
 
     fun run() {
-        val userNameContainer = repeatWithRunCatching { getUserNames() }
+        val userNames = initUserName()
         val dealerCards = cardMachine.getCardPair()
-        val userCards = cardMachine.getCardPairs(userNameContainer.names.size)
-        val userNames = userNameContainer.names
-
-        val userNamesAndCards = userNames.zip(userCards)
-        val users = userNamesAndCards.map { userNameAndCard ->
-            User.create(userNameAndCard)
-        }
+        val userCards = cardMachine.getCardPairs(userNames.size)
+        val users = initUsers(userNames, userCards)
         val dealer = Dealer.create(dealerCards)
+
+        playGame(userNames, dealer, users)
+        gameEnd(dealer, users)
+    }
+
+    private fun playGame(userNames: List<String>, dealer: Dealer, users: List<User>) {
         playGameView.printNoticeSplitCard(userNames)
         playGameView.printPlayerCard(dealer, users)
 
-        users.forEach { user ->
-            repeatGetCommand(user)
-        }
+        requestGetCommand(users)
+        dealerPickNewCardIfNeeded(dealer)
+    }
 
+    private fun gameEnd(dealer: Dealer, users: List<User>) {
+        gameResultView.printCardResult(dealer, users)
+        gameResultView.printFinalResult(getGameResult(dealer, users), users)
+    }
+
+    private fun getGameResult(dealer: Dealer, users: List<User>): List<GameResult> {
+        val referee: Referee = Referee(
+            dealer.validDealerSum(),
+            users.map { user ->
+                user.validUserSum()
+            },
+        )
+        return referee.getResult()
+    }
+
+    private fun dealerPickNewCardIfNeeded(dealer: Dealer) {
         if (!dealer.isOverSumCondition()) {
             playGameView.printDealerPickNewCard()
             val newCard = cardMachine.getNewCard()
             dealer.addCard(newCard)
         }
+    }
 
-        gameResultView.printCardResult(dealer, users)
-        val referee: Referee = Referee(dealer.validDealerSum(), users.map { it.validUserSum() })
-        val gameResult = referee.getResult()
+    private fun initUserName(): List<String> {
+        val userNameContainer = repeatWithRunCatching { getUserNames() }
+        return userNameContainer.names
+    }
 
-        gameResultView.printFinalResult(gameResult,users)
+    private fun initUsers(userNames: List<String>, userCards: List<List<Card>>): List<User> {
+        val userNamesAndCards = userNames.zip(userCards)
+        return userNamesAndCards.map { userNameAndCard ->
+            User.create(userNameAndCard)
+        }
+    }
+
+    private fun requestGetCommand(users: List<User>) {
+        users.forEach { user ->
+            repeatGetCommand(user)
+        }
     }
 
     private fun repeatGetCommand(user: User) {
         val answer = getAnswer(user)
-        if (answer.value == YES) {
-            user.addCard(cardMachine.getNewCard())
-            playGameView.printUserCard(user)
-            repeatGetCommand(user)
-        }
-
-        if (answer.value == NO) {
-            playGameView.printUserCard(user)
-        }
+        if (answer.value == YES) return userPickNewCard(user)
+        return userPickNoCard(user)
     }
 
-    private fun getAnswer(user: User): Answer {
-        return repeatWithRunCatching { Answer(playGameView.requestOneMoreCard(user)) }
+    private fun userPickNewCard(user: User) {
+        user.addCard(cardMachine.getNewCard())
+        playGameView.printUserCard(user)
+        repeatGetCommand(user)
     }
+
+    private fun userPickNoCard(user: User) {
+        playGameView.printUserCard(user)
+    }
+
+    private fun getAnswer(user: User): Answer =
+        repeatWithRunCatching { Answer(playGameView.requestOneMoreCard(user)) }
 
     private fun getUserNames(): UserNameContainer = UserNameContainer(loginView.requestPlayerName())
 
@@ -68,9 +98,5 @@ class Controller(
             println(error.message.toString())
             repeatWithRunCatching(action)
         }
-    }
-
-    companion object {
-        private const val DEALER = 1
     }
 }
