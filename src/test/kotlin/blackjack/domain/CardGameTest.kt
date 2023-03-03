dc7374c1eab2e08c58a14d09b3f62e81e715a780
hyemdooly <hyemdooly@gmail.com>
2023-03-03 15:05:37 +0900

    test(CardGameTest): 플레이어 및 딜러 생성 테스트 작성

--- src/test/kotlin/blackjack/domain/CardGameTest.kt
+++ src/test/kotlin/blackjack/domain/CardGameTest.kt
@@ -14,7 +14,25 @@ class CardGameTest {
         val game = CardGame(CardPicker(CardPackGenerator().createCards()))
         val player1 = Player(game.pickTwice(), Name("jason"))
         val player2 = Player(game.pickTwice(), Name("pobi"))
-        assertThat(player1.cards.cards.size).isEqualTo(2)
-        assertThat(player2.cards.cards.size).isEqualTo(2)
+        assertThat(player1.cards.size).isEqualTo(2)
+        assertThat(player2.cards.size).isEqualTo(2)
+    }
+
+    @Test
+    fun `플레이어 두 명의 정보를 생성한다`() {
+        val game = CardGame(CardPicker(CardPackGenerator().createCards()))
+        val players = game.initPlayers(listOf(Name("jason"), Name("pobi")))
+        assertThat(players.size).isEqualTo(2)
+        assertThat(players[0].name.value).isEqualTo("jason")
+        assertThat(players[0].cards.size).isEqualTo(2)
+        assertThat(players[1].name.value).isEqualTo("pobi")
+        assertThat(players[1].cards.size).isEqualTo(2)
+    }
+
+    @Test
+    fun `딜러의 정보를 생성한다`() {
+        val game = CardGame(CardPicker(CardPackGenerator().createCards()))
+        val dealer = game.initDealer()
+        assertThat(dealer.cards.size).isEqualTo(2)
     }
 }
