5ed3275d5d8ede66c98a8d66ccdc9cf4697e91c7
vrexpert <vrexpert2302@gmail.com>
2023-03-03 16:12:34 +0900

    refactor(OnboardingView): OnboardingView script 상수화

--- src/main/kotlin/view/OnboardingView.kt
+++ src/main/kotlin/view/OnboardingView.kt
@@ -5,8 +5,12 @@ import domain.person.Player
 
 object OnboardingView {
     private const val ERROR_INPUT_BLACK = "공백은 입력할 수 없습니다."
+    private const val NAME_INPUT_SCRIPT = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)"
+    private const val SHARE_TWO_CARDS_SCRIPT = "%s와 %s에게 2장의 카드를 나누었습니다."
+    private const val INITIAL_CARDS_SCRIPT = "%s 카드: %s"
+
     fun requestInputNames(): List<String> {
-        println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)")
+        println(NAME_INPUT_SCRIPT)
         val input = readln()
         if (input.isBlank()) {
             println(ERROR_INPUT_BLACK)
@@ -17,17 +21,13 @@ object OnboardingView {
 
     fun printInitialSetting(players: List<Player>, dealer: Dealer) {
         println()
-        println(
-            "${dealer.name}와 " +
-                players.joinToString(", ") { it.name } +
-                "에게 2장의 카드를 나누었습니다."
-        )
-        println("${dealer.name}: ${dealer.showOneCard().joinToString { it.toString() }}")
+        println(SHARE_TWO_CARDS_SCRIPT.format(dealer.name, players.joinToString(", ") { it.name }))
+        println(INITIAL_CARDS_SCRIPT.format(dealer.name, dealer.showOneCard().joinToString { it.toString() }))
         players.forEach { printInitialCards(it) }
         println()
     }
 
     private fun printInitialCards(player: Player) {
-        println("${player.name}카드: " + player.cards.joinToString(",") { it.toString() })
+        println(INITIAL_CARDS_SCRIPT.format(player.name, player.cards.joinToString(",") { it.toString() }))
     }
 }
