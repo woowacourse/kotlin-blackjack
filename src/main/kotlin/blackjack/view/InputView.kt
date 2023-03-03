e49beed10417450f04e6fb8362da19540cc85d81
whk06061 <gju06061@naver.com>
2023-03-03 15:05:26 +0900

    feat: InputView 구현
    - 참가자 이름 입력 기능
    - 카드 추가 발행 여부 입력 기능

---
+++ src/main/kotlin/blackjack/view/InputView.kt
@@ -0,0 +1,19 @@
+package blackjack.view
+
+class InputView {
+
+    fun readParticipantsName(): List<String> {
+        println(PROMPT_PARTICIPANTS_NAME)
+        return readln().split(",")
+    }
+
+    fun readMoreCard(name: String): String {
+        println(ASK_MORE_CARD.format(name))
+        return readln()
+    }
+
+    companion object {
+        private const val ASK_MORE_CARD = "%s은(는) 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)"
+        private const val PROMPT_PARTICIPANTS_NAME = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)"
+    }
+}
