5da3d2053d7417b060e90908b2c756758efa0fd9
hyemdooly <hyemdooly@gmail.com>
2023-03-03 13:42:41 +0900

    refactor(CardPicker): 카드를 모두 다 썼을 경우 예외 추가

--- src/main/kotlin/domain/CardPicker.kt
+++ src/main/kotlin/domain/CardPicker.kt
@@ -6,6 +6,11 @@ import model.Cards
 class CardPicker(private val cards: Cards) {
     private var cursor = 0
     fun pick(): Card {
+        require(cursor < cards.cards.size) { OUT_OF_INDEX_CARDS_CURSOR }
         return cards.cards[cursor++]
+    }
+
+    companion object {
+        private const val OUT_OF_INDEX_CARDS_CURSOR = "카드를 모두 사용했습니다."
     }
 }
