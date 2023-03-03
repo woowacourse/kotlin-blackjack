10f78e12735d8801f111a28fc77492aeb8fbcc1c
chws0508 <chws0508@naver.com>
2023-03-03 17:30:38 +0900

    feat(LambdaTest): 람다 스터디 테스트 구현

---
+++ src/test/kotlin/study/LambdaTest2.kt
@@ -0,0 +1,50 @@
+package study
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class LambdaTest2 {
+    val numbers: List<Int> = listOf(1, 2, 3, 4, 5, 6)
+
+    fun sumAllEven(numbers: List<Int>): Int {
+        var total = 0
+        for (number in numbers) {
+            if (number % 2 == 0) {
+                total += number
+            }
+        }
+        return total
+    }
+
+    fun sumAllOverThree(numbers: List<Int>): Int {
+        var total = 0
+        for (number in numbers) {
+            if (number > 3) {
+                total += number
+            }
+        }
+        return total
+    }
+
+    interface Calculate {
+        fun condition(abc: Int): Boolean
+    }
+
+    fun sum(numbers: List<Int>, block: (Int) -> Boolean): Int {
+        var total = 0
+        for (number in numbers) {
+            if (block(number))
+                total += number
+        }
+        return total
+    }
+
+    @Test
+    fun `lamda`() {
+        val actual = sum(numbers) { it % 2 == 0 }
+        assertThat(actual).isEqualTo(sumAllEven(numbers))
+    }
+}
+
+
+
