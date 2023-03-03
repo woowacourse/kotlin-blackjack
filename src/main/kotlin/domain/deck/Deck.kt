820aaedca01438b1de653483456729acb70ae80b
rhkrwngud445 <rhkrwngud445@gmail.com>
2023-03-03 15:14:24 +0900

    refactor: deck에서 카드가 없을 시 에러를 던지는 로직 삭제
    Co-authored-by: pingu244 <pingu244@naver.com>
    Co-authored-by: rhkrwngud445 <rhkrwngud445@gmail.com>

--- src/main/kotlin/domain/deck/Deck.kt
+++ src/main/kotlin/domain/deck/Deck.kt
@@ -5,5 +5,5 @@ import domain.card.Card
 class Deck(private val deck: List<Card>) {
     private val _deck = deck.toMutableList()
 
-    fun giveCard() = _deck.removeLastOrNull() ?: check(false) { "[ERROR] 카드덱을 모두 사용했습니다." }
+    fun giveCard() = _deck.removeLast()
 }
