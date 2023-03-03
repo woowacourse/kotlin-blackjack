7300126ad360c1c4aee0869351a32e9b82b361aa
re4rk <37167652+re4rk@users.noreply.github.com>
2023-03-03 15:20:33 +0900

    refactor(InputView) : 입력 안내 문구 수정

--- src/main/kotlin/blackjack/view/InputView.kt
+++ src/main/kotlin/blackjack/view/InputView.kt
@@ -7,7 +7,7 @@ class InputView {
     }
 
     fun inputDrawMore(name: String): String {
-        println("\n${name}는 한장의 카드를 더 받겠습니까?")
+        println("\n${name}는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)")
         return readln()
     }
 }
