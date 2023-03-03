8eb2258a917b3cbcd955f43fde1ab2e5b482ec3e
s9hn <tnghqn@naver.com>
2023-03-03 16:28:49 +0900

    [REFACTOR(GameResultView)] 반복되는 문자열 출력 로직 수정 및 상수화

--- src/main/kotlin/view/GameResultView.kt
+++ src/main/kotlin/view/GameResultView.kt
@@ -7,10 +7,16 @@ import domain.User
 class GameResultView {
 
     fun printCardResult(dealer: Dealer, users: List<User>) {
-        println(DEALER_CARD.format(dealer.cards.map { it.toString() }.joinToString(SEPARATOR), dealer.validPlayerSum()))
+        println(
+            PLAYER_CARD.format(
+                dealer.name,
+                dealer.cards.map { it.toString() }.joinToString(SEPARATOR),
+                dealer.validPlayerSum(),
+            ),
+        )
         users.forEach { user ->
             println(
-                USERS_CARD.format(
+                PLAYER_CARD.format(
                     user.name,
                     (user.cards.map { it.toString() }).joinToString(
                         SEPARATOR,
@@ -24,22 +30,25 @@ class GameResultView {
     fun printFinalResult(gameResult: List<GameResult>, users: List<User>) {
         println(FINAL_RESULT)
         println(
-            "딜러: ${gameResult.count { it == GameResult.LOSE }}승 " +
-                "${gameResult.count { it == GameResult.DRAW }}무 " +
-                "${gameResult.count { it == GameResult.WIN }}패",
+            DEALER_RESULT_FORMAT.format(
+                gameResult.count { it == GameResult.LOSE },
+                GameResult.WIN.label,
+                gameResult.count { it == GameResult.DRAW },
+                GameResult.DRAW.label,
+                gameResult.count { it == GameResult.WIN },
+                GameResult.LOSE.label,
+            ),
         )
-        users.forEachIndexed { result, user ->
-            println("${user.name}: ${gameResult[result].label}")
+        users.forEachIndexed { index, user ->
+            println(USER_RESULT_FORMAT.format(user.name, gameResult[index].label))
         }
     }
 
     companion object {
-        private const val DEALER_CARD = "딜러: %s - 결과: %d"
-        private const val USERS_CARD = "%s: %s - 결과: %d"
+        private const val DEALER_RESULT_FORMAT = "딜러: %d%s %d%s %d%s"
+        private const val USER_RESULT_FORMAT = "%s: %s"
+        private const val PLAYER_CARD = "%s: %s - 결과: %d"
         private const val SEPARATOR = ", "
         private const val FINAL_RESULT = "## 최종 승패"
-        private const val WIN = "승"
-        private const val LOSE = "패"
-        private const val DRAW = "무"
     }
 }
