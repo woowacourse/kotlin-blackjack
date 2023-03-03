d2761733ae9787d206a1dfb69d82bf41d502a203
hyemdooly <hyemdooly@gmail.com>
2023-03-03 13:56:15 +0900

    refactor(CardGame): 초기화 구문 CardGame으로 책임 위임

--- src/main/kotlin/domain/CardGame.kt
+++ src/main/kotlin/domain/CardGame.kt
@@ -1,8 +1,19 @@
 package domain
 
 import model.Cards
+import model.Dealer
+import model.Name
+import model.Player
 
 class CardGame(private val cardPicker: CardPicker) {
+    fun initPlayers(names: List<Name>): List<Player> {
+        return names.map { Player(pickTwice(), it) }
+    }
+
+    fun initDealer(): Dealer {
+        return Dealer(pickTwice())
+    }
+
     fun pickTwice(): Cards = Cards(
         buildList {
             add(cardPicker.pick())
