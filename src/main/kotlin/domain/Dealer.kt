e0649828c86443f51de50cccda51ef60fcfa6762
chws0508 <chws0508@naver.com>
2023-03-03 15:25:28 +0900

    feat(Dealer): Dealer 승패 결과 반환 기능 구현

--- src/main/kotlin/domain/Dealer.kt
+++ src/main/kotlin/domain/Dealer.kt
@@ -9,4 +9,16 @@ class Dealer(cards: Cards) : Participant(Name("딜러"), cards) {
         if (cards.maxSumState().sum <= 16) return true
         return false
     }
+
+    fun getResult(players: Players): Map<GameResult, Int> {
+        val result = GameResult.values().associateWith { 0 }.toMutableMap()
+        players.players.forEach { player ->
+            val playerResult = player.getGameResult(getSumStateResult())
+            result[playerResult] = (result[playerResult] ?: 0) + 1
+        }
+        val winCount = result[GameResult.WIN]
+        result[GameResult.WIN] = result[GameResult.LOSE] ?: 0
+        result[GameResult.LOSE] = winCount ?: 0
+        return result
+    }
 }
