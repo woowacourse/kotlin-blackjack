dbc172859728721906bff13ec3a96f15e2a18532
re4rk <37167652+re4rk@users.noreply.github.com>
2023-03-03 15:27:05 +0900

    refactor(Name) : 이름의 글자수 제한 추가

--- src/main/kotlin/blackjack/domain/Name.kt
+++ src/main/kotlin/blackjack/domain/Name.kt
@@ -3,13 +3,14 @@ package blackjack.domain
 @JvmInline
 value class Name(private val value: String) {
     init {
-        require(value.length <= NAME_LENGTH_MAX) { ERROR_NAME_LENGTH_MAX }
+        require(value.length in NAME_LENGTH_MIN..NAME_LENGTH_MAX) { ERROR_NAME_LENGTH_MAX }
     }
 
     override fun toString(): String = value
 
     companion object {
+        private const val NAME_LENGTH_MIN = 1
         private const val NAME_LENGTH_MAX = 20
-        private const val ERROR_NAME_LENGTH_MAX = "이름의 최대 글자수는 ${NAME_LENGTH_MAX}글자 입니다."
+        private const val ERROR_NAME_LENGTH_MAX = "이름의 글자수는 $NAME_LENGTH_MIN~${NAME_LENGTH_MAX}사이 입니다."
     }
 }
--- src/test/kotlin/blackjack/domain/NameTest.kt
+++ src/test/kotlin/blackjack/domain/NameTest.kt
@@ -1,22 +1,25 @@
 package blackjack.domain
 
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class NameTest {
 
-    @Test
-    fun `이름은 20자 이하다`() {
+    @ParameterizedTest
+    @ValueSource(strings = ["아", "아크,로피,아크,로피,아크,로피,아크"])
+    fun `이름은 1~20자 이하다`(name: String) {
         assertDoesNotThrow {
-            Name("아크,로피,아크,로피,아크,로피,아크")
+            Name(name)
         }
     }
 
-    @Test
-    fun `이름이 21자 이상이면 에러난다`() {
+    @ParameterizedTest
+    @ValueSource(strings = ["", "가나다라마바사아자차카타파하abcdefjhijklmnopqrstuvwxyz"])
+    fun `이름은 1~20자 이외면 에러난다`(name: String) {
         assertThrows<IllegalArgumentException> {
-            Name("가나다라마바사아자차카타파하abcdefjhijklmnopqrstuvwxyz")
+            Name(name)
         }
     }
 }
