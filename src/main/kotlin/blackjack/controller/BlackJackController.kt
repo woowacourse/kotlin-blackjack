f9ea704ac1b1519bc7a9f0adcbf8f4b238cfdae4
SangHyun Moon <no1msh1217@gmail.com>
2023-03-03 12:50:51 +0900

    feat: 사용자의 입력을 받는 기능 구현
    게임에 참여하는 플레이어의 이름을 받는다.
    카드를 더 받을지 알려주는 명령어를 받는다.

---
+++ src/main/kotlin/blackjack/controller/BlackJackController.kt
@@ -0,0 +1,4 @@
+package blackjack.controller
+
+class BlackJackController {
+}
---
+++ src/main/kotlin/blackjack/view/InputView.kt
@@ -0,0 +1,16 @@
+package blackjack.view
+
+object InputView {
+    private const val REQUEST_PLAYER_NAMES = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)"
+    private const val REQUEST_DRAW_COMMAND = "%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)"
+
+    fun inputNames(): List<String> {
+        println(REQUEST_PLAYER_NAMES)
+        return readln().split(",")
+    }
+
+    fun inputDrawCommand(name: String): String {
+        println(REQUEST_DRAW_COMMAND.format(name))
+        return readln()
+    }
+}
