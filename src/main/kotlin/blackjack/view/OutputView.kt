b1b0e37b63f4b640f3e592f65b484b22b325c8dc
changhwan <chang5hustle@gmail.com>
2023-03-03 14:47:28 +0900

    feat(OutputView): 최초 카드 분배 대사 출력 기능 구현

--- src/main/kotlin/blackjack/view/OutputView.kt
+++ src/main/kotlin/blackjack/view/OutputView.kt
@@ -20,7 +20,12 @@ class OutputView {
         print("${player.name}카드 : $bunchString")
     }
 
+    fun printDistributeScript(players: List<Player>) {
+        println(DISTRIBUTE_SCRIPT.format(players.joinToString(separator = ", ") { it.name }))
+    }
+
     companion object {
-        private const val DEALER_INITIAL_CARD_SCRIPT = "딜러: %d"
+        private const val DEALER_INITIAL_CARD_SCRIPT = "딜러: %s"
+        private const val DISTRIBUTE_SCRIPT = "딜러와 %s에게 2장의 카드를 나누었습니다."
     }
 }
