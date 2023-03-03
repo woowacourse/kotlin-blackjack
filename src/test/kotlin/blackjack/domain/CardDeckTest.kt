c410c87bf307302e9aea5d3daa933bf46efe511f
re4rk <37167652+re4rk@users.noreply.github.com>
2023-03-03 16:38:42 +0900

    refactor(CardTest) : 테스트 케이스 가독성 개선

--- src/test/kotlin/blackjack/domain/CardDeckTest.kt
+++ src/test/kotlin/blackjack/domain/CardDeckTest.kt
@@ -7,29 +7,29 @@ import org.junit.jupiter.api.assertThrows
 class CardDeckTest {
 
     @Test
-    fun `카드덱 잘 있는지 확인`() {
+    fun `초기 카드 덱은 52장이다`() {
         val cardDeck = CardDeck(Cards.all())
         assertThat(cardDeck.size).isEqualTo(52)
     }
 
     @Test
-    fun `카드 draw가 잘 되었는지 확인`() {
+    fun `카드를 뽑으면 숫자가 줄어든다`() {
         val cardDeck = CardDeck(Cards.all())
         cardDeck.nextCard()
         assertThat(cardDeck.size).isEqualTo(51)
     }
 
     @Test
-    fun `초기 카드는 52장이다`() {
+    fun `초기 카드가 52장이 아니면 에러난다`() {
         assertThrows<IllegalArgumentException> {
             CardDeck(Cards.all().take(51))
         }
     }
 
     @Test
-    fun `카드는 중복이 없어야 합니다`() {
+    fun `초기 카드에 중복이 있으면 에러난다`() {
         assertThrows<IllegalArgumentException> {
-            CardDeck(Cards.all() + Cards.all())
+            CardDeck(Cards.all().take(26) + Cards.all().take(26))
         }
     }
 }
--- src/test/kotlin/blackjack/domain/CardValueTest.kt
+++ src/test/kotlin/blackjack/domain/CardValueTest.kt
@@ -1,72 +1,14 @@
 package blackjack.domain
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class CardValueTest {
 
-    @Test
-    fun `A의 값은 1이다`() {
-        assertThat(CardValue.ACE.value).isEqualTo(1)
-    }
-
-    @Test
-    fun `K의 값은 10이다`() {
-        assertThat(CardValue.KING.value).isEqualTo(10)
-    }
-
-    @Test
-    fun `Q의 값은 10이다`() {
-        assertThat(CardValue.QUEEN.value).isEqualTo(10)
-    }
-
-    @Test
-    fun `J의 값은 10이다`() {
-        assertThat(CardValue.JACK.value).isEqualTo(10)
-    }
-
-    @Test
-    fun `TEN의 값은 10이다`() {
-        assertThat(CardValue.TEN.value).isEqualTo(10)
-    }
-
-    @Test
-    fun `NINE의 값은 9이다`() {
-        assertThat(CardValue.NINE.value).isEqualTo(9)
-    }
-
-    @Test
-    fun `EIGHT의 값은 8이다`() {
-        assertThat(CardValue.EIGHT.value).isEqualTo(8)
-    }
-
-    @Test
-    fun `SEVEN의 값은 7이다`() {
-        assertThat(CardValue.SEVEN.value).isEqualTo(7)
-    }
-
-    @Test
-    fun `SIX의 값은 6이다`() {
-        assertThat(CardValue.SIX.value).isEqualTo(6)
-    }
-
-    @Test
-    fun `FIVE의 값은 5이다`() {
-        assertThat(CardValue.FIVE.value).isEqualTo(5)
-    }
-
-    @Test
-    fun `FOUR의 값은 4이다`() {
-        assertThat(CardValue.FOUR.value).isEqualTo(4)
-    }
-
-    @Test
-    fun `THREE의 값은 3이다`() {
-        assertThat(CardValue.THREE.value).isEqualTo(3)
-    }
-
-    @Test
-    fun `TWO의 값은 2이다`() {
-        assertThat(CardValue.TWO.value).isEqualTo(2)
+    @ParameterizedTest
+    @CsvSource(value = ["ACE,1", "KING,10", "QUEEN,10", "JACK,10", "TEN,10", "NINE,9", "EIGHT,8", "SEVEN,7", "SIX,6", "FIVE,5", "FOUR,4", "THREE,3", "TWO,2"])
+    fun `카드의 값들이 나온다`(cardValue: CardValue, value: Int) {
+        assertThat(cardValue.value).isEqualTo(value)
     }
 }
