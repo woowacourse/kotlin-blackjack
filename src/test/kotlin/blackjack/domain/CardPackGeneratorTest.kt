114fcac5d6c06a1ffda208f147644d9d8739d78d
hyemdooly <hyemdooly@gmail.com>
2023-03-03 14:58:56 +0900

    test(CardPackGeneratorTest): 메서드명 변경

--- src/test/kotlin/blackjack/domain/CardPackGeneratorTest.kt
+++ src/test/kotlin/blackjack/domain/CardPackGeneratorTest.kt
@@ -6,8 +6,8 @@ import org.junit.jupiter.api.Test
 
 class CardPackGeneratorTest {
     @Test
-    fun `카드팩을 중복 없이 48장 생성한다`() {
+    fun `카드팩을 중복 없이 52장 생성한다`() {
         val cards = CardPackGenerator().createCards()
-        assertThat(cards.cards.distinct().size).isEqualTo(48)
+        assertThat(cards.cards.distinct().size).isEqualTo(52)
     }
 }
