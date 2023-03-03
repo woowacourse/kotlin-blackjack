03f3ada3e7ca1bc68cff31bb8855fe5fec953b5f
vrexpert <vrexpert2302@gmail.com>
2023-03-03 16:20:23 +0900

    refactor(ResultView): ResultView 스크립트 상수화

--- src/main/kotlin/view/ResultView.kt
+++ src/main/kotlin/view/ResultView.kt
@@ -7,6 +7,9 @@ import domain.result.GameResult
 import domain.result.OutCome
 
 object ResultView {
+    private const val INITIAL_CARDS_SCRIPT = "%s 카드: %s - 결과: %s"
+    private const val FINAL_OUTCOME_SCRIPT = "## 최종 승패"
+    private const val DEALER_SCRIPT = "딜러: "
     fun printPersonsCards(dealer: Dealer, players: List<Player>) {
         printPersonCards(dealer)
         players.forEach { printPersonCards(it) }
@@ -15,20 +18,22 @@ object ResultView {
 
     private fun printPersonCards(person: Person) {
         println(
-            "${person.name}카드: " +
-                person.cards.joinToString(",") { it.toString() } +
-                " - 결과: ${person.getTotalCardNumber()}"
+            INITIAL_CARDS_SCRIPT.format(
+                person.name,
+                person.cards.joinToString(",") { it.toString() },
+                person.getTotalCardNumber()
+            )
         )
     }
 
     fun printFinalResult(gameResult: GameResult) {
-        println("## 최종 승패")
+        println(FINAL_OUTCOME_SCRIPT)
         printDealerResult(gameResult.getDealerResult())
         printPlayerResult(gameResult.getPlayerResult())
     }
 
     private fun printDealerResult(dealerResult: Map<OutCome, Int>) {
-        print("딜러:")
+        print(DEALER_SCRIPT)
         dealerResult.entries.forEach { print(" ${it.value}${it.key.text}") }
         println()
     }
