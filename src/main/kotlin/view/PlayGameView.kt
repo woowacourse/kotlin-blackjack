15f36e3de9a57c76a7f54adc857e629ebe82f7ef
s9hn <tnghqn@naver.com>
2023-03-03 16:28:24 +0900

    [REFACTOR(PlayGameView)] PLAYER 카드로 수정

--- src/main/kotlin/view/PlayGameView.kt
+++ src/main/kotlin/view/PlayGameView.kt
@@ -11,14 +11,14 @@ class PlayGameView {
     }
 
     fun printPlayerCard(dealer: Dealer, users: List<User>) {
-        println(DEALER_CARD.format(dealer.cards.first()))
+        println(PLAYER_CARD.format(dealer.name, dealer.cards.first()))
         users.forEach { user ->
-            println(USERS_CARD.format(user.name, (user.cards.map { it.toString() }).joinToString(SEPARATOR)))
+            println(PLAYER_CARD.format(user.name, (user.cards.map { it.toString() }).joinToString(SEPARATOR)))
         }
     }
 
     fun printUserCard(user: User) {
-        println(USERS_CARD.format(user.name, (user.cards.map { it.toString() }).joinToString(SEPARATOR)))
+        println(PLAYER_CARD.format(user.name, (user.cards.map { it.toString() }).joinToString(SEPARATOR)))
     }
 
     fun requestOneMoreCard(user: User): String {
@@ -31,8 +31,7 @@ class PlayGameView {
     companion object {
         private const val NOTICE_DEALER_PICK_NEW_CARD = "딜러는 16이하라 한장의 카드를 더 받았습니다."
         private const val NOTICE_SPLIT_CARD = "딜러와 %s에게 2장의 카드를 나누었습니다."
-        private const val DEALER_CARD = "딜러: %s"
-        private const val USERS_CARD = "%s: %s"
+        private const val PLAYER_CARD = "%s: %s"
         private const val SEPARATOR = ", "
         private const val REQUEST_MORE_CARD = "%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)"
     }
