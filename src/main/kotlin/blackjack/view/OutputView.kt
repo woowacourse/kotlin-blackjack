d94d7c4016aace24cb92c084374699d80ec2802f
changhwan <chang5hustle@gmail.com>
2023-03-03 14:38:06 +0900

    feat(OutputView): 플레이어의 카드를 보여주는 기능 구현

--- src/main/kotlin/blackjack/view/OutputView.kt
+++ src/main/kotlin/blackjack/view/OutputView.kt
@@ -3,12 +3,21 @@ package blackjack.view
 import blackjack.domain.Card
 import blackjack.domain.CardBunch
 import blackjack.domain.CardNumber
+import blackjack.domain.Player
 
 class OutputView {
     private fun makeCardString(card: Card): String = CardNumber.valueOf(card.cardNumber) + card.shape.korean
 
+    private fun makeBunchString(bunch: CardBunch): String =
+        bunch.cards.joinToString(separator = ", ") { makeCardString(it) }
+
     fun printDealerInitialCard(cardBunch: CardBunch) {
         println(DEALER_INITIAL_CARD_SCRIPT.format(makeCardString(cardBunch.cards.first())))
+    }
+
+    fun printPlayerCards(player: Player) {
+        val bunchString = makeBunchString(player.cardBunch)
+        print("${player.name}카드 : $bunchString")
     }
 
     companion object {
