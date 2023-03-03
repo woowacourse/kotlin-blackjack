c3ef1563e8b839930e348aca5896c9b72efe4bdb
changhwan <chang5hustle@gmail.com>
2023-03-03 14:54:46 +0900

    feat(OutputView): 딜러의 카드 합에 따른 카드 지급여부 출력하는 기능 구현

--- src/main/kotlin/blackjack/view/OutputView.kt
+++ src/main/kotlin/blackjack/view/OutputView.kt
@@ -24,8 +24,17 @@ class OutputView {
         println(DISTRIBUTE_SCRIPT.format(players.joinToString(separator = ", ") { it.name }))
     }
 
+    fun printDealerOverCondition(condition: Boolean) {
+        when (condition) {
+            true -> println(CAN_GET_CARD_SCRIPT)
+            false -> println(CANNOT_GET_CARD_SCRIPT)
+        }
+    }
+
     companion object {
         private const val DEALER_INITIAL_CARD_SCRIPT = "딜러: %s"
         private const val DISTRIBUTE_SCRIPT = "딜러와 %s에게 2장의 카드를 나누었습니다."
+        private const val CAN_GET_CARD_SCRIPT = "딜러는 16이하라 한 장의 카드를 더 받았습니다."
+        private const val CANNOT_GET_CARD_SCRIPT = "딜러는 17이상이라 카드를 더 받지 못합니다."
     }
 }
