bf0c973712063650f162671139d1307b3f4b8a3e
re4rk <37167652+re4rk@users.noreply.github.com>
2023-03-03 16:41:18 +0900

    refactor(CardTest) : 테스트 케이스 가독성 개선

--- src/test/kotlin/blackjack/domain/CardTest.kt
+++ src/test/kotlin/blackjack/domain/CardTest.kt
@@ -1,34 +1,15 @@
 package blackjack.domain
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class CardTest {
-    @Test
-    fun `카드의 문양과 숫자를 가져올 수 있다`() {
-        val card = Card(CardMark.CLOVER, CardValue.ACE)
-        assertThat(card.mark).isEqualTo(CardMark.CLOVER)
-        assertThat(card.value).isEqualTo(CardValue.ACE)
-    }
-
-    @Test
-    fun `카드의 문양과 숫자를 가져올 수 있다2`() {
-        val card = Card(CardMark.HEART, CardValue.TEN)
-        assertThat(card.mark).isEqualTo(CardMark.HEART)
-        assertThat(card.value).isEqualTo(CardValue.TEN)
-    }
-
-    @Test
-    fun `카드의 문양과 숫자를 가져올 수 있다3`() {
-        val card = Card(CardMark.SPADE, CardValue.TWO)
-        assertThat(card.mark).isEqualTo(CardMark.SPADE)
-        assertThat(card.value).isEqualTo(CardValue.TWO)
-    }
-
-    @Test
-    fun `카드의 문양과 숫자를 가져올 수 있다4`() {
-        val card = Card(CardMark.DIA, CardValue.JACK)
-        assertThat(card.mark).isEqualTo(CardMark.DIA)
-        assertThat(card.value).isEqualTo(CardValue.JACK)
+    @ParameterizedTest
+    @CsvSource(value = ["CLOVER, ACE", "HEART, TEN", "SPADE, TWO", "DIA, JACK"])
+    fun `카드의 문양과 숫자를 가져올 수 있다`(mark: CardMark, value: CardValue) {
+        val card = Card(mark, value)
+        assertThat(card.mark).isEqualTo(mark)
+        assertThat(card.value).isEqualTo(value)
     }
 }
