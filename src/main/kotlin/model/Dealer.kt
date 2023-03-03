ae43aa61231214a1d97c5c8efff1849037a72093
hyemdooly <hyemdooly@gmail.com>
2023-03-03 13:56:45 +0900

    refactor(Dealer): 딜러 상수 public

--- src/main/kotlin/model/Dealer.kt
+++ src/main/kotlin/model/Dealer.kt
@@ -4,7 +4,7 @@ class Dealer(cards: Cards, name: Name = Name(DEALER)) : Participant(cards, name)
     override fun isHit(): Boolean = cards.sum() < HIT_STANDARD_POINT
 
     companion object {
-        private const val DEALER = "딜러"
+        const val DEALER = "딜러"
         private const val HIT_STANDARD_POINT = 17
     }
 }
