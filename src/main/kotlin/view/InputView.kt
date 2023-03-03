b5a2e17d2ae533174d31c137148ac2828433f009
hyemdooly <hyemdooly@gmail.com>
2023-03-03 16:32:38 +0900

    refactor(CardTest, InputView): 상수 선언

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -5,21 +5,27 @@ class InputView {
         val input = readln().replace(" ", "")
         require(input.isNotBlank()) { NULL_ERROR }
         val names = input.split(",")
-        require(names.size <= 8) { PLAYER_COUNT_ERROR }
+        require(names.size <= PLAYER_MAX) { PLAYER_COUNT_ERROR }
         return names
     }
 
     fun readYesOrNo(): Boolean {
         val input = readln()
         require(input.isNotBlank()) { NULL_ERROR }
-        require(Regex("[yYnN]").matches(input)) { IS_NOT_YES_OR_NO_ERROR }
-        if (input == "y" || input == "Y") {
+        require(Regex("[$Y$y$N$n]").matches(input)) { IS_NOT_YES_OR_NO_ERROR }
+        if (input == Y || input == y) {
             return true
         }
         return false
     }
 
     companion object {
+        private const val Y = "Y"
+        private const val y = "y"
+        private const val N = "N"
+        private const val n = "n"
+
+        private const val PLAYER_MAX = 8
         private const val NULL_ERROR = "입력 값이 비었습니다"
         private const val IS_NOT_YES_OR_NO_ERROR = "입력 값은 y 혹은 n이 아닙니다"
         private const val PLAYER_COUNT_ERROR = "플레이어는 8명까지 가능합니다"
