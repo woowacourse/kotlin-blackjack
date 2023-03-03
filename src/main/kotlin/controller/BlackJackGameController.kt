7007fd7db0fc8fb0f79e60fab7609209d711f315
chws0508 <chws0508@naver.com>
2023-03-03 15:42:42 +0900

    feat(BlackJackGameController): BlackJackGameController 클래스 구현

---
+++ src/main/kotlin/controller/BlackJackGameController.kt
@@ -0,0 +1,30 @@
+package domain
+
+import view.InputView
+import view.ResultView
+
+class BlackJackGameController(private val inputView: InputView, private val resultView: ResultView) {
+    fun run() {
+        val blackJackGame = initGame()
+        mainGame(blackJackGame)
+        gameResult(blackJackGame)
+    }
+
+    private fun initGame(): BlackJackGame {
+        val names = inputView.readNames()
+        val blackJackGame = BlackJackGame(names)
+        resultView.printGameInit(blackJackGame.participants.players)
+        resultView.printInitCards(blackJackGame.participants)
+        return blackJackGame
+    }
+
+    private fun mainGame(blackJackGame: BlackJackGame) {
+        blackJackGame.playersSelectAddPhase(inputView::readChoiceOfAddCard, resultView::printPlayerCard)
+        blackJackGame.dealerSelectPhase(resultView::printDealerAddCard)
+    }
+
+    private fun gameResult(blackJackGame: BlackJackGame) {
+        resultView.printScore(blackJackGame.participants)
+        resultView.printGameResult(blackJackGame.participants.players, blackJackGame.participants.dealer)
+    }
+}
