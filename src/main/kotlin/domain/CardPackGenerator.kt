8952355ab9b242e84845eddcd9692adf8f2a0e79
inseonyun <yuninseon@naver.com>
2023-03-05 16:35:32 +0900

    refactor(CardPackGenerator): 카드팩 생성 코드 수정

--- src/main/kotlin/domain/CardPackGenerator.kt
+++ src/main/kotlin/domain/CardPackGenerator.kt
@@ -8,9 +8,7 @@ import model.Suit
 class CardPackGenerator {
     fun createCards(): Cards {
         return Cards(
-            buildList {
-                Rank.values().forEach { rank -> addAll(Suit.values().map { suit -> Card(rank, suit) }) }
-            },
+            Rank.values().flatMap { rank -> Suit.values().map { suit -> Card(rank, suit) } }
         )
     }
 }
