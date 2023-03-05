3377757c32b9f2cf6e377cd3acd7dba6c209dab5
hyemdooly <hyemdooly@gmail.com>
2023-03-05 17:04:29 +0900

    refactor(Participants): 일급컬렉션 해제 및 toList() 구현으로 인한 수정

--- src/main/kotlin/model/Participants.kt
+++ src/main/kotlin/model/Participants.kt
@@ -2,6 +2,6 @@ package model
 data class Participants(private val participants: List<Participant>) {
     fun toList(): List<Participant> = participants.toList()
     companion object {
-        fun of(dealer: Dealer, players: Players) = Participants(listOf(dealer as Participant) + players as List<Participant>)
+        fun of(dealer: Dealer, players: Players) = Participants(listOf(dealer as Participant) + players.toList() as List<Participant>)
     }
 }
