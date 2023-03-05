eda158cef7cb6da8f78a07913c8758ffa20acdd3
hyemdooly <hyemdooly@gmail.com>
2023-03-05 16:29:59 +0900

    refactor(DealerTest): Dealer 생성 함수 분리

--- src/test/kotlin/blackjack/model/DealerTest.kt
+++ src/test/kotlin/blackjack/model/DealerTest.kt
@@ -11,27 +11,31 @@ import org.junit.jupiter.api.Test
 class DealerTest {
     @Test
     fun `카드의 합이 16을 초과하지 않으면 hit 한다`() {
-        val dealer = Dealer(
-            Cards(
-                listOf(
-                    Card(Rank.JACK, Suit.DIAMOND),
-                    Card(Rank.SIX, Suit.CLOVER),
-                ),
-            ),
-        )
+        val dealer = hitDealer()
         assertThat(dealer.isHit()).isTrue
     }
 
     @Test
     fun `카드의 합이 16을 초과하면 stay 한다`() {
-        val dealer = Dealer(
-            Cards(
-                listOf(
-                    Card(Rank.JACK, Suit.DIAMOND),
-                    Card(Rank.SEVEN, Suit.CLOVER),
-                ),
-            ),
-        )
+        val dealer = stayDealer()
         assertThat(dealer.isHit()).isFalse
     }
+
+    private fun hitDealer(): Dealer = Dealer(
+        Cards(
+            listOf(
+                Card(Rank.JACK, Suit.DIAMOND),
+                Card(Rank.SIX, Suit.CLOVER),
+            )
+        )
+    )
+
+    private fun stayDealer(): Dealer = Dealer(
+        Cards(
+            listOf(
+                Card(Rank.JACK, Suit.DIAMOND),
+                Card(Rank.SEVEN, Suit.CLOVER),
+            )
+        )
+    )
 }
