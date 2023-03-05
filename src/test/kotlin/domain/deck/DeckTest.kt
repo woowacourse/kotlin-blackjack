456961694ac022388984084ae0fdb47bbbb0b314
rhkrwngud445 <rhkrwngud445@gmail.com>
2023-03-05 17:03:28 +0900

    refactor: Deck 멤버변수 제거

--- src/test/kotlin/domain/deck/DeckTest.kt
+++ src/test/kotlin/domain/deck/DeckTest.kt
@@ -5,12 +5,11 @@ import domain.card.CardValue
 import domain.card.Shape
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 
 class DeckTest {
     @Test
     fun `덱에서 마지막 카드를 준다`() {
-        val deck = Deck(listOf(Card(Shape.SPADE, CardValue.TWO), Card(Shape.SPADE, CardValue.THREE)))
+        val deck = Deck(mutableListOf(Card(Shape.SPADE, CardValue.TWO), Card(Shape.SPADE, CardValue.THREE)))
         assertThat(deck.giveCard()).isEqualTo(Card(Shape.SPADE, CardValue.THREE))
     }
 
