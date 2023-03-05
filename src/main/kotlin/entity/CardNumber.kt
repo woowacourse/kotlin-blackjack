43b9ed0b93081410a8019164cccb3846e5dbf540
hyunji1203 <hj27720@gmail.com>
2023-03-05 23:48:30 +0900

    style: 불필요한 주석 삭제

--- src/main/kotlin/entity/CardNumber.kt
+++ src/main/kotlin/entity/CardNumber.kt
@@ -14,21 +14,4 @@ enum class CardNumber(val value: Int) {
     KING(10),
     QUEEN(10),
     JACK(10),
-
-    /*ACE({ sumOfCards ->
-        if (sumOfCards + 11 > GameRule.WINNING_NUMBER) 1
-        else 11
-    }),
-    TWO({ _ -> 2 }),
-    THREE({ _ -> 3 }),
-    FOUR({ _ -> 4 }),
-    FIVE({ _ -> 5 }),
-    SIX({ _ -> 6 }),
-    SEVEN({ _ -> 7 }),
-    EIGHT({ _ -> 8 }),
-    NINE({ _ -> 9 }),
-    TEN({ _ -> 10 }),
-    KING({ _ -> 10 }),
-    QUEEN({ _ -> 10 }),
-    JACK({ _ -> 10 }),*/
 }
