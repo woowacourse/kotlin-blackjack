8da7d346db873b386a771b47a793fdd39def1131
pingu244 <69796976+pingu244@users.noreply.github.com>
2023-03-05 23:30:37 +0900

    refactor: Deck에서 back properties 삭제

--- src/main/kotlin/domain/deck/Deck.kt
+++ src/main/kotlin/domain/deck/Deck.kt
@@ -3,7 +3,5 @@ package domain.deck
 import domain.card.Card
 
 class Deck(private val deck: List<Card>) {
-    private val _deck = deck.toMutableList()
-
-    fun giveCard() = _deck.removeLast()
+    fun giveCard() = deck.toMutableList().removeLast()
 }
