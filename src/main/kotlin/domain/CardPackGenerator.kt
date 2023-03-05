0d9b407b7102f46403d0066d162a0761f4cc0478
hyemdooly <hyemdooly@gmail.com>
2023-03-05 15:32:51 +0900

    refactor(CardPackGenerator): createCards 변수, 함수 분리

--- src/main/kotlin/domain/CardPackGenerator.kt
+++ src/main/kotlin/domain/CardPackGenerator.kt
@@ -6,11 +6,16 @@ import model.Rank
 import model.Suit
 
 class CardPackGenerator {
+    private val rankValues = Rank.values()
+    private val suitValues = Suit.values()
+
     fun createCards(): Cards {
         return Cards(
             buildList {
-                Rank.values().forEach { rank -> addAll(Suit.values().map { suit -> Card(rank, suit) }) }
+                rankValues.forEach { rank -> addAll(cardsBySuit(rank)) }
             },
         )
     }
+
+    private fun cardsBySuit(rank: Rank) = suitValues.map { suit -> Card(rank, suit) }
 }
