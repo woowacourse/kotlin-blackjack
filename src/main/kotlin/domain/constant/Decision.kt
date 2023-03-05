6c12568aae7558944785f4170abdce2c24accd11
briandr97 <briandr@naver.com>
2023-03-06 01:25:02 +0900

    style(Decision): 안쓰는 코드 제거 및 정렬

--- src/main/kotlin/domain/constant/Decision.kt
+++ src/main/kotlin/domain/constant/Decision.kt
@@ -1,14 +1,12 @@
 package domain.constant
 
 enum class Decision(val text: String) {
-    YES("y"), NO("n")
-
+    YES("y"),
+    NO("n"),
     ;
 
     companion object {
         fun of(text: String): Decision? =
             values().find { it.text == text }
-
-        private const val ERROR_NOT_DECISION = "y 또는 n 을 입력해야 합니다."
     }
 }
