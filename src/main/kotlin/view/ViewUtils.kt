62d4bf9960450a3c4ab20e18f2d35fb796d41604
hyunji1203 <hj27720@gmail.com>
2023-03-05 14:16:06 +0900

    refactor: if문 when문으로 수정

--- src/main/kotlin/view/ViewUtils.kt
+++ src/main/kotlin/view/ViewUtils.kt
@@ -7,20 +7,22 @@ import entity.Cards
 class ViewUtils {
     companion object {
         fun cardNumberToString(cardNumber: CardNumber): String {
-            if (cardNumber.ordinal in (1..9)) return (cardNumber.ordinal + 1).toString()
-            else if (cardNumber == CardNumber.ACE) return "A"
-            else if (cardNumber == CardNumber.KING) return "K"
-            else if (cardNumber == CardNumber.QUEEN) return "Q"
-            else if (cardNumber == CardNumber.JACK) return "J"
-            return ""
+            return when (cardNumber) {
+                CardNumber.ACE -> "A"
+                CardNumber.KING -> "K"
+                CardNumber.QUEEN -> "Q"
+                CardNumber.JACK -> "J"
+                else -> (cardNumber.ordinal + 1).toString()
+            }
         }
 
         fun cardTypeToString(cardType: CardType): String {
-            if (cardType == CardType.CLUB) return "클로버"
-            else if (cardType == CardType.HEART) return "하트"
-            else if (cardType == CardType.SPADE) return "스페이드"
-            else if (cardType == CardType.DIAMOND) return "다이아몬드"
-            return ""
+            return when (cardType) {
+                CardType.CLUB -> "클로버"
+                CardType.SPADE -> "하트"
+                CardType.HEART -> "스페이드"
+                CardType.DIAMOND -> "다이아몬드"
+            }
         }
 
         fun cardsToString(cards: Cards): String {
