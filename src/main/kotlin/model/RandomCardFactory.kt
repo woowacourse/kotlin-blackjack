8c6bade37a13cbfe665629df83e89d90a398f5e9
hyunji1203 <hj27720@gmail.com>
2023-03-05 18:18:29 +0900

    refactor(RandomCardFactory): 제한 사항 추가

--- src/main/kotlin/model/RandomCardFactory.kt
+++ src/main/kotlin/model/RandomCardFactory.kt
@@ -14,9 +14,16 @@ class RandomCardFactory : CardFactory {
 
     private var index = 0
 
-    private fun generateCard(): Card = cards.value[index++]
+    private fun generateCard(): Card {
+        require(index <= 52) { CARD_CREATE_INDEX_ERROR_MESSAGE }
+        return cards.value[index++]
+    }
 
     override fun generate(count: Int): Cards {
         return Cards((0 until count).map { generateCard() })
+    }
+
+    companion object {
+        private const val CARD_CREATE_INDEX_ERROR_MESSAGE = "카드는 52개 이상 생성할 수 없습니다."
     }
 }
