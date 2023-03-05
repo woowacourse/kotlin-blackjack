fca65efb9a77654677f4adde5337f5421fda3211
rhkrwngud445 <rhkrwngud445@gmail.com>
2023-03-05 17:25:50 +0900

    refactor: makeCards() map을 활용하여 간소화

--- src/main/kotlin/domain/card/CardMaker.kt
+++ src/main/kotlin/domain/card/CardMaker.kt
@@ -1,14 +1,10 @@
 package domain.card
 
-class CardMaker {
-    fun makeCards(): List<Card> {
-        val cards = mutableListOf<Card>()
-
-        for (shape in Shape.values()) {
-            cards.addAll(makeCard(shape))
-        }
-        return cards.toList().shuffled()
-    }
+class CardMaker { // object
+    fun makeCards(): List<Card> = Shape.values()
+        .map { makeCard(it) }
+        .flatten()
+        .shuffled()
 
     private fun makeCard(shape: Shape): List<Card> = CardValue.values().map { Card(shape, it) }
 }
