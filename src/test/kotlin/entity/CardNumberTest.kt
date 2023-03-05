2ead38606c7014cef93acbee15925b17044f40d5
hyunji1203 <hj27720@gmail.com>
2023-03-06 01:01:18 +0900

    fix(CardNumberTest): 오류 수정

--- src/test/kotlin/entity/CardNumberTest.kt
+++ src/test/kotlin/entity/CardNumberTest.kt
@@ -1,5 +1,6 @@
 package entity
 
+import model.NumberStrategy
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
@@ -7,10 +8,10 @@ class CardNumberTest {
     @Test
     fun `플레이어의 카드의 합에 11을 더했을 때 21보다 크면 ACE가 1이다`() {
         // given
-        val cardNumber = CardNumber.ACE
+        val cardSumOfNumber = 13
 
         // when
-        val number = cardNumber.numberStrategy(12)
+        val number = NumberStrategy().judgeAce(cardSumOfNumber)
 
         // then
         assertThat(number).isEqualTo(1)
@@ -19,10 +20,10 @@ class CardNumberTest {
     @Test
     fun `플레이어의 카드의 합에 11을 더했을 때 21보다 작으면 ACE가 11이다`() {
         // given
-        val cardNumber = CardNumber.ACE
+        val cardSumOfNumber = 2
 
         // when
-        val number = cardNumber.numberStrategy(3)
+        val number = NumberStrategy().judgeAce(cardSumOfNumber)
 
         // then
         assertThat(number).isEqualTo(11)
