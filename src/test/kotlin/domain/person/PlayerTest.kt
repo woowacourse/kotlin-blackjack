8381cc8de0ba1253ec733db108f9027421743bf9
vrexpert <vrexpert2302@gmail.com>
2023-03-05 18:45:16 +0900

    test(PlayerTest): 상태가 BUST 또는 HIT 이 되는 경우의 수 테스트

--- src/test/kotlin/domain/person/PlayerTest.kt
+++ src/test/kotlin/domain/person/PlayerTest.kt
@@ -7,7 +7,9 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.MethodSource
 
 class PlayerTest {
 
@@ -44,18 +46,64 @@ class PlayerTest {
     }
 
     @Test
-    fun `카드 패의 합이 21이 넘으면 게임 상태가 BUST 가 된다`() {
-        player.receiveCard(Card(HEART, CardNumber.KING))
-        player.receiveCard(Card(HEART, CardNumber.QUEEN))
-        player.receiveCard(Card(HEART, CardNumber.JACK))
-
-        assertThat(player.gameState).isEqualTo(GameState.BUST)
-    }
-
-    @Test
     fun `카드를 받기를 거부하면 게임 상태가 STAND 가 된다`() {
         player.rejectReceiveCard()
 
         assertThat(player.gameState).isEqualTo(GameState.STAND)
+    }
+
+    @MethodSource("provideCardsBust")
+    @ParameterizedTest
+    fun `ACE 를 1로 간주한 합계가 21을 넘으면 상태가 BUST 이다`(numbers: List<CardNumber>) {
+        numbers.forEach { number -> player.receiveCard(Card(HEART, number)) }
+
+        assertThat(player.gameState).isEqualTo(GameState.BUST)
+    }
+
+    @MethodSource("provideCardsHit")
+    @ParameterizedTest
+    fun `ACE 를 1로 간주한 합계가 21 보다 작으면 상태가 HIT 이다`(numbers: List<CardNumber>) {
+        numbers.forEach { number -> player.receiveCard(Card(HEART, number)) }
+
+        assertThat(player.gameState).isEqualTo(GameState.HIT)
+    }
+
+    companion object {
+        @JvmStatic
+        fun provideCardsBust() = listOf(
+            // 22
+            Arguments.of(
+                listOf(CardNumber.KING, CardNumber.JACK, CardNumber.ACE, CardNumber.ACE),
+            ),
+            // 22
+            Arguments.of(
+                listOf(CardNumber.FOUR, CardNumber.TEN, CardNumber.EIGHT),
+            ),
+            // 30
+            Arguments.of(
+                listOf(CardNumber.TEN, CardNumber.KING, CardNumber.QUEEN),
+            ),
+
+        )
+
+        @JvmStatic
+        fun provideCardsHit() = listOf(
+            // 2
+            Arguments.of(
+                listOf(CardNumber.ACE, CardNumber.ACE),
+            ),
+            // 5
+            Arguments.of(
+                listOf(CardNumber.ACE, CardNumber.ACE, CardNumber.ACE, CardNumber.THREE),
+            ),
+            // 20
+            Arguments.of(
+                listOf(CardNumber.TEN, CardNumber.KING),
+            ),
+            // 21
+            Arguments.of(
+                listOf(CardNumber.TEN, CardNumber.JACK, CardNumber.ACE),
+            ),
+        )
     }
 }
