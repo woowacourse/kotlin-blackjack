d86a96e2453ad4c9401e724b2db4d7a44f3b45cc
pingu244 <69796976+pingu244@users.noreply.github.com>
2023-03-06 15:38:30 +0900

    refactor: Referee의 중첩된 조건문을 풀어서 작성

--- src/main/kotlin/domain/judge/Referee.kt
+++ src/main/kotlin/domain/judge/Referee.kt
@@ -16,14 +16,20 @@ class Referee(private val dealerState: DealerCards, private val players: List<Pl
         val playerSum = player.calculateCardSum()
         val dealerSum = dealerState.calculateCardSum()
 
+        return judgeWithConditions(playerSum, dealerSum)
+    }
+
+    private fun judgeWithConditions(playerSum: Int, dealerSum: Int): Result {
         return when {
-            playerSum > CARD_SUM_MAX_VALUE || dealerSum.checkPlayerLossCondition(playerSum) -> Result.LOSS
-            playerSum > dealerSum || dealerSum > CARD_SUM_MAX_VALUE -> Result.WIN
+            playerSum > CARD_SUM_MAX_VALUE -> Result.LOSS
+            dealerSum.checkPlayerLossCondition(playerSum) -> Result.LOSS
+            playerSum > dealerSum -> Result.WIN
+            dealerSum > CARD_SUM_MAX_VALUE -> Result.WIN
             else -> Result.DRAW
         }
     }
 
-    private fun Int.checkPlayerLossCondition(playerSum: Int) = this > playerSum && this <= CARD_SUM_MAX_VALUE
+    private fun Int.checkPlayerLossCondition(playerSum: Int) = this >= playerSum && this <= CARD_SUM_MAX_VALUE
 
     companion object {
         const val CARD_SUM_MAX_VALUE = 21
