b1a5d65b5f1f043002638c32f7712a16c6686234
tmdgh1592 <tmdgh1592@naver.com>
2023-03-06 17:04:26 +0900

    feat: 플레이어 수를 제한하는 기능 구현

--- src/main/kotlin/blackjack/domain/Players.kt
+++ src/main/kotlin/blackjack/domain/Players.kt
@@ -3,6 +3,12 @@ package blackjack.domain
 import blackjack.dto.HandDTO
 
 class Players(private val players: List<Player>) {
+    init {
+        require(players.size in MINIMUM_PLAYER..MAXIMUM_PLAYER) {
+            "블랙잭은 최소 ${MINIMUM_PLAYER}명에서 최대 ${MAXIMUM_PLAYER}명의 플레이어가 참여 가능합니다. (현재 플레이어수 : ${players.size}명)"
+        }
+    }
+
     fun drawAll(deck: CardDeck) {
         players.forEach { it.addCard(deck.draw()) }
     }
@@ -12,4 +18,9 @@ class Players(private val players: List<Player>) {
     fun toList(): List<Player> = players.toList()
 
     operator fun get(index: Int): Player = players[index]
+
+    companion object {
+        private const val MINIMUM_PLAYER = 1
+        private const val MAXIMUM_PLAYER = 7
+    }
 }
--- src/test/kotlin/blackjack/domain/PlayersTest.kt
+++ src/test/kotlin/blackjack/domain/PlayersTest.kt
@@ -1,9 +1,32 @@
 package blackjack.domain
 
+import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class PlayersTest {
+
+    @Test
+    fun `플레이어는 최소 1명이어야 한다`() {
+        val players = emptyList<Player>()
+
+        assertThatIllegalArgumentException().isThrownBy { Players(players) }
+            .withMessageContaining("블랙잭은 최소 1명에서 최대 7명의 플레이어가 참여 가능합니다. (현재 플레이어수 : 0명)")
+    }
+
+    @Test
+    fun `플레이어는 최대 7명까지 가능하다`() {
+        val players = listOf(
+            Player("a"), Player("b"),
+            Player("c"), Player("d"),
+            Player("e"), Player("f"),
+            Player("g"), Player("h")
+        )
+
+        assertThatIllegalArgumentException().isThrownBy { Players(players) }
+            .withMessageContaining("블랙잭은 최소 1명에서 최대 7명의 플레이어가 참여 가능합니다. (현재 플레이어수 : 8명)")
+    }
+
     @Test
     fun `모든 플레이어의 카드를 2장씩 뽑는다`() {
         val deck = CardDeck(
