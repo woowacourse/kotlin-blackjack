12d088743bed7c11c6587b8e9d60284e57321f4d
Doyeop Kim <dygames34@gmail.com>
2023-03-06 15:17:32 +0900

    refactor: if-else문을 when으로 변경. extension 함수 사용하여 가독성 증가

--- src/main/kotlin/view/ViewUtils.kt
+++ src/main/kotlin/view/ViewUtils.kt
@@ -6,26 +6,28 @@ import entity.Cards
 
 class ViewUtils {
     companion object {
-        fun cardNumberToString(cardNumber: CardNumber): String {
-            if (cardNumber.ordinal in (1..9)) return (cardNumber.ordinal + 1).toString()
-            else if (cardNumber == CardNumber.ACE) return "A"
-            else if (cardNumber == CardNumber.KING) return "K"
-            else if (cardNumber == CardNumber.QUEEN) return "Q"
-            else if (cardNumber == CardNumber.JACK) return "J"
-            return ""
+        private fun CardNumber.toShorthandString(): String {
+            return when (this) {
+                CardNumber.ACE -> "A"
+                CardNumber.KING -> "K"
+                CardNumber.QUEEN -> "Q"
+                CardNumber.JACK -> "J"
+                else -> (ordinal + 1).toString()
+            }
         }
 
-        fun cardTypeToString(cardType: CardType): String {
-            if (cardType == CardType.CLUB) return "클로버"
-            else if (cardType == CardType.HEART) return "하트"
-            else if (cardType == CardType.SPADE) return "스페이드"
-            else if (cardType == CardType.DIAMOND) return "다이아몬드"
-            return ""
+        private fun CardType.toKoreanString(): String {
+            return when (this) {
+                CardType.CLUB -> "클로버"
+                CardType.HEART -> "하트"
+                CardType.SPADE -> "스페이드"
+                CardType.DIAMOND -> "다이아몬드"
+            }
         }
 
         fun cardsToString(cards: Cards): String {
             return cards.value.joinToString(", ") {
-                cardNumberToString(it.cardNumber) + cardTypeToString(it.cardType)
+                it.cardNumber.toShorthandString() + it.cardType.toKoreanString()
             }
         }
     }
