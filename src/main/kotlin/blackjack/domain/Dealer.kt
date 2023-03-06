fbf92ca2f9a8622bafde53a6c8ab61d06ec5e32f
krrong <khk9664@naver.com>
2023-03-06 15:51:47 +0900

    refactor(Dealer): 딜러가 카드를 받을 수 있는 조건을 명시하는 상수값 변경

--- src/main/kotlin/blackjack/domain/Dealer.kt
+++ src/main/kotlin/blackjack/domain/Dealer.kt
@@ -4,6 +4,6 @@ class Dealer(override val cardBunch: CardBunch) : Participant {
     override fun canGetCard(): Boolean = cardBunch.getTotalScore() < ADD_CARD_CONDITION
 
     companion object {
-        private const val ADD_CARD_CONDITION = 16
+        private const val ADD_CARD_CONDITION = 17
     }
 }
