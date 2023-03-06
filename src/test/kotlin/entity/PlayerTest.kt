066f079069548bcba6edf82b287a78978d77fb9c
Doyeop Kim <dygames34@gmail.com>
2023-03-06 14:32:07 +0900

    refactor: 테스트 코드의 본문에선 테스트 하고자 하는 내용인 카드 개수만 보이도록 수정

--- src/test/kotlin/entity/PlayerTest.kt
+++ src/test/kotlin/entity/PlayerTest.kt
@@ -4,9 +4,28 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class PlayerTest {
+    private fun generateCardsByNumber(number: Int): Cards {
+        val cards = mutableListOf<Card>()
+        var remain = number
+        while (remain > 0) {
+            remain -= addCardByNumber(remain, cards)
+        }
+        return Cards(cards)
+    }
+
+    private fun addCardByNumber(remain: Int, cards: MutableList<Card>): Int {
+        return if (remain > 10) {
+            cards.add(Card(CardType.SPADE, CardNumber.TEN))
+            10
+        } else {
+            cards.add(Card(CardType.SPADE, CardNumber.values()[remain - 1]))
+            remain
+        }
+    }
+
     @Test
     fun `플레이어가 가진 카드의 숫자 합이 21 미만이면 한장의 카드를 더 받을 수 있다`() {
-        val dealer = Dealer(Cards(listOf(Card(CardType.SPADE, CardNumber.TEN))))
+        val dealer = Dealer(generateCardsByNumber(10))
         val isDistributable = dealer.isDistributable()
 
         assertThat(isDistributable).isTrue
@@ -14,7 +33,8 @@ class PlayerTest {
 
     @Test
     fun `플레이어가 가진 카드의 숫자 합이 21 이상이면 한장의 카드를 더 받을 수 없다`() {
-        val dealer = Dealer(Cards(listOf(Card(CardType.SPADE, CardNumber.TEN), Card(CardType.SPADE, CardNumber.TEN), Card(CardType.SPADE, CardNumber.TWO))))
+        val dealer = Dealer(generateCardsByNumber(22))
+
         val isDistributable = dealer.isDistributable()
 
         assertThat(isDistributable).isFalse
@@ -24,25 +44,9 @@ class PlayerTest {
     fun `플레이어 카드 숫자의 합이 21이고 딜러 카드 숫자의 합이 21이면 무승부이다`() {
         // given
         val player1 = Player(
-            Name("test"),
-            Cards(
-                listOf(
-                    Card(CardType.HEART, CardNumber.TEN),
-                    Card(CardType.SPADE, CardNumber.TEN),
-                    Card(CardType.CLUB, CardNumber.ACE)
-                )
-            )
+            Name("test"), generateCardsByNumber(21)
         )
-        val dealer = Dealer(
-            Cards(
-                listOf(
-                    Card(CardType.HEART, CardNumber.TEN),
-                    Card(CardType.SPADE, CardNumber.TEN),
-                    Card(CardType.CLUB, CardNumber.ACE)
-                )
-            )
-        )
-        val results = player1.determineGameResult(dealer.cardsNumberSum())
+        val results = player1.determineGameResult(21)
 
         // when
         val except = player1 to GameResultType.DRAW
@@ -55,25 +59,9 @@ class PlayerTest {
     fun `플레이어 카드 숫자의 합이 22이고 딜러 카드 숫자의 합이 22이면 무승부이다`() {
         // given
         val player1 = Player(
-            Name("test"),
-            Cards(
-                listOf(
-                    Card(CardType.HEART, CardNumber.TEN),
-                    Card(CardType.SPADE, CardNumber.TEN),
-                    Card(CardType.CLUB, CardNumber.TWO)
-                )
-            )
+            Name("test"), generateCardsByNumber(22)
         )
-        val dealer = Dealer(
-            Cards(
-                listOf(
-                    Card(CardType.HEART, CardNumber.TEN),
-                    Card(CardType.SPADE, CardNumber.TEN),
-                    Card(CardType.CLUB, CardNumber.TWO)
-                )
-            )
-        )
-        val results = player1.determineGameResult(dealer.cardsNumberSum())
+        val results = player1.determineGameResult(22)
 
         // when
         val except = player1 to GameResultType.DRAW
@@ -86,23 +74,10 @@ class PlayerTest {
     fun `플레이어 카드 숫자의 합이 19이고 딜러 카드 숫자의 합이 22이면 플레이어의 승리이다`() {
         // given
         val player1 = Player(
-            Name("test"),
-            Cards(
-                listOf(
-                    Card(CardType.HEART, CardNumber.TEN), Card(CardType.SPADE, CardNumber.NINE)
-                )
-            )
+            Name("test"), generateCardsByNumber(19)
         )
-        val dealer = Dealer(
-            Cards(
-                listOf(
-                    Card(CardType.HEART, CardNumber.TEN),
-                    Card(CardType.SPADE, CardNumber.TEN),
-                    Card(CardType.CLUB, CardNumber.TWO)
-                )
-            )
-        )
-        val results = player1.determineGameResult(dealer.cardsNumberSum())
+
+        val results = player1.determineGameResult(22)
 
         // when
         val except = player1 to GameResultType.WIN
@@ -115,23 +90,9 @@ class PlayerTest {
     fun `플레이어 카드 숫자의 합이 21이고 딜러 카드 숫자의 합이 20이면 플레이어의 승리이다`() {
         // given
         val player1 = Player(
-            Name("test"),
-            Cards(
-                listOf(
-                    Card(CardType.HEART, CardNumber.SEVEN),
-                    Card(CardType.HEART, CardNumber.SEVEN),
-                    Card(CardType.HEART, CardNumber.SEVEN),
-                )
-            )
+            Name("test"), generateCardsByNumber(21)
         )
-        val dealer = Dealer(
-            Cards(
-                listOf(
-                    Card(CardType.HEART, CardNumber.TEN), Card(CardType.SPADE, CardNumber.TEN)
-                )
-            )
-        )
-        val results = player1.determineGameResult(dealer.cardsNumberSum())
+        val results = player1.determineGameResult(20)
 
         // when
         val except = player1 to GameResultType.WIN
@@ -144,23 +105,9 @@ class PlayerTest {
     fun `플레이어 카드 숫자의 합이 20이고 딜러 카드 숫자의 합이 21이면 플레이어의 패배이다`() {
         // given
         val player1 = Player(
-            Name("test"),
-            Cards(
-                listOf(
-                    Card(CardType.HEART, CardNumber.TEN), Card(CardType.SPADE, CardNumber.TEN)
-                )
-            )
+            Name("test"), generateCardsByNumber(20)
         )
-        val dealer = Dealer(
-            Cards(
-                listOf(
-                    Card(CardType.HEART, CardNumber.TEN),
-                    Card(CardType.SPADE, CardNumber.TEN),
-                    Card(CardType.CLUB, CardNumber.ACE)
-                )
-            )
-        )
-        val results = player1.determineGameResult(dealer.cardsNumberSum())
+        val results = player1.determineGameResult(21)
 
         // when
         val except = player1 to GameResultType.LOSE
@@ -173,25 +120,9 @@ class PlayerTest {
     fun `플레이어 카드 숫자의 합이 22이고 딜러 카드 숫자의 합이 21이면 플레이어의 패배이다`() {
         // given
         val player1 = Player(
-            Name("test"),
-            Cards(
-                listOf(
-                    Card(CardType.HEART, CardNumber.TEN),
-                    Card(CardType.SPADE, CardNumber.TEN),
-                    Card(CardType.CLUB, CardNumber.TWO)
-                )
-            )
+            Name("test"), generateCardsByNumber(22)
         )
-        val dealer = Dealer(
-            Cards(
-                listOf(
-                    Card(CardType.HEART, CardNumber.TEN),
-                    Card(CardType.SPADE, CardNumber.TEN),
-                    Card(CardType.CLUB, CardNumber.ACE)
-                )
-            )
-        )
-        val results = player1.determineGameResult(dealer.cardsNumberSum())
+        val results = player1.determineGameResult(21)
 
         // when
         val except = player1 to GameResultType.LOSE
