bcaacd15cfd104d6733441c358eadb2ea79697ca
re4rk <37167652+re4rk@users.noreply.github.com>
2023-03-06 21:40:17 +0900

    refactor (CardsTest): 테스트 구조 개선

--- src/test/kotlin/blackjack/domain/card/CardsTest.kt
+++ src/test/kotlin/blackjack/domain/card/CardsTest.kt
@@ -1,25 +1,34 @@
 package blackjack.domain.card
 
 import blackjack.domain.card.CardMark.CLOVER
+import blackjack.domain.card.CardMark.SPADE
 import blackjack.domain.card.CardValue.ACE
 import blackjack.domain.card.CardValue.EIGHT
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
 
 class CardsTest {
     @Test
     fun `카드를 추가할 수 있다`() {
         var cards = Cards()
         cards += Card(CLOVER, EIGHT)
-        assertThat(cards.size).isEqualTo(1)
-        assertThat(cards.toList()[0]).isEqualTo(Card(CLOVER, EIGHT))
+        assertAll(
+            { assertThat(cards.size).isEqualTo(1) },
+            { assertThat(cards.toList()[0]).isEqualTo(Card(CLOVER, EIGHT)) },
+        )
     }
 
     @Test
-    fun `ACE 카드가 있는지 확인 할 수 있다`() {
+    fun `ACE 카드가 없으면 false를 반환한다`() {
         var cards = Cards()
         cards += Card(CLOVER, EIGHT)
         assertThat(cards.isContainsACE()).isFalse
+    }
+
+    @Test
+    fun `ACE 카드가 있으면 true를 반환한다`() {
+        var cards = Cards()
         cards += Card(CLOVER, ACE)
         assertThat(cards.isContainsACE()).isTrue
     }
@@ -28,7 +37,7 @@ class CardsTest {
     fun `점수의 합을 반환한다`() {
         var cards = Cards()
         cards += Card(CLOVER, EIGHT)
-        cards += Card(CardMark.SPADE, CardValue.EIGHT)
+        cards += Card(SPADE, EIGHT)
         assertThat(cards.result.score()).isEqualTo(16)
     }
 }
