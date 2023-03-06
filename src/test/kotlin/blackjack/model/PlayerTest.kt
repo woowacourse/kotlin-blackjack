2693fe3bd3baca76972996c4af33da3087d10fdc
hyemdooly <hyemdooly@gmail.com>
2023-03-06 17:31:43 +0900

    refactor(PlayerTest): 가독성 리팩터링

--- src/test/kotlin/blackjack/model/PlayerTest.kt
+++ src/test/kotlin/blackjack/model/PlayerTest.kt
@@ -24,66 +24,49 @@ class PlayerTest {
     }
 
     @Test
-    fun `플레이어는 배팅 금액을 가진다`() {
-        val player = Player(Hand(emptyList()), Name("jason"))
-    }
-
-    @Test
     fun `카드의 합이 21이 넘지 않으면 bust이다`() {
-        val player = Player(
-            Hand(
-                listOf(
-                    Card(Rank.KING, Suit.DIAMOND),
-                    Card(Rank.JACK, Suit.CLOVER),
-                    Card(Rank.ACE, Suit.HEART),
-                ),
-            ),
+        val player = player(
             Name("jason"),
+            Rank.KING,
+            Rank.JACK,
+            Rank.ACE
         )
         assertThat(player.isBust()).isFalse
     }
 
     @Test
     fun `카드의 합이 21이 넘으면 bust이다`() {
-        val player = Player(
-            Hand(
-                listOf(
-                    Card(Rank.KING, Suit.DIAMOND),
-                    Card(Rank.JACK, Suit.CLOVER),
-                    Card(Rank.JACK, Suit.HEART),
-                ),
-            ),
+        val player = player(
             Name("jason"),
+            Rank.KING,
+            Rank.JACK,
+            Rank.DEUCE
         )
         assertThat(player.isBust()).isTrue
     }
 
     @Test
     fun `카드의 합이 21을 초과하지 않으면 hit 한다`() {
-        val player = Player(
-            Hand(
-                listOf(
-                    Card(Rank.ACE, Suit.DIAMOND),
-                    Card(Rank.JACK, Suit.CLOVER),
-                ),
-            ),
+        val player = player(
             Name("jason"),
+            Rank.ACE,
+            Rank.JACK,
         )
         assertThat(player.isHit()).isTrue
     }
 
     @Test
     fun `카드의 합이 21을 초과하면 stay 한다`() {
-        val player = Player(
-            Hand(
-                listOf(
-                    Card(Rank.ACE, Suit.DIAMOND),
-                    Card(Rank.ACE, Suit.CLOVER),
-                    Card(Rank.JACK, Suit.CLOVER),
-                ),
-            ),
+        val player = player(
             Name("jason"),
+            Rank.DEUCE,
+            Rank.JACK,
+            Rank.KING
         )
         assertThat(player.isHit()).isFalse
     }
+
+    private fun player(name: Name, vararg ranks: Rank): Player = Player(
+        Hand(ranks.map { Card(it, Suit.DIAMOND) }), name
+    )
 }
