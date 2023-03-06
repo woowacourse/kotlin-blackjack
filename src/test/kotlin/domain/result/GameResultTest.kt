d194516b73a4d66b4b95c63b8f0cdb1c53750531
briandr97 <briandr@naver.com>
2023-03-06 17:37:57 +0900

    refactor(GameResultTest): 빼먹은 커밋 - PersonGenerator를 이용해 사람을 만드는 방식으로 변경

--- src/test/kotlin/domain/result/GameResultTest.kt
+++ src/test/kotlin/domain/result/GameResultTest.kt
@@ -3,7 +3,9 @@ package domain.result
 import domain.card.Card
 import domain.card.CardNumber
 import domain.card.CardShape
-import domain.person.Dealer
+import domain.person.PersonGenerator.makeDealer
+import domain.person.PersonGenerator.makePlayer
+import domain.person.Persons
 import domain.person.Player
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
@@ -13,21 +15,21 @@ class GameResultTest {
     @Test
     fun `모든 사람의 승패를 결정한다`() {
         val players = listOf<Player>(
-            Player("빅스").apply {
-                receiveCard(Card(CardShape.HEART, CardNumber.TWO))
-                receiveCard(Card(CardShape.DIAMOND, CardNumber.THREE))
+            makePlayer {
+                name("빅스")
+                addTwoCards(Card(CardShape.HEART, CardNumber.TWO), Card(CardShape.DIAMOND, CardNumber.THREE))
+            }.apply {
                 receiveCard(Card(CardShape.CLOVER, CardNumber.FOUR))
             },
-            Player("베르").apply {
-                receiveCard(Card(CardShape.CLOVER, CardNumber.ACE))
-                receiveCard(Card(CardShape.SPADE, CardNumber.KING))
+            makePlayer {
+                name("베르")
+                addTwoCards(Card(CardShape.CLOVER, CardNumber.ACE), Card(CardShape.SPADE, CardNumber.KING))
             },
         )
-        val dealer = Dealer().apply {
-            receiveCard(Card(CardShape.CLOVER, CardNumber.KING))
-            receiveCard(Card(CardShape.SPADE, CardNumber.NINE))
+        val dealer = makeDealer {
+            addTwoCards(Card(CardShape.CLOVER, CardNumber.KING), Card(CardShape.SPADE, CardNumber.NINE))
         }
-        val gameResult = GameResult(dealer, players)
+        val gameResult = GameResult(Persons(dealer, players))
 
         val expectedDealerResult = mapOf(OutCome.WIN to 1, OutCome.LOSE to 1)
         val expectedPlayersResult = mapOf("빅스" to OutCome.LOSE, "베르" to OutCome.WIN)
