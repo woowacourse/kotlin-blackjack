2087f9782cb854af1163b9fefc1f8688af204282
re4rk <37167652+re4rk@users.noreply.github.com>
2023-03-07 02:57:41 +0900

    refactor (ScoreTest): 점수 관련 테스트 코드 추가

---
+++ src/test/kotlin/blackjack/domain/participants/ScoreTest.kt
@@ -0,0 +1,41 @@
+package blackjack.domain.participants
+
+import blackjack.domain.card.Card
+import blackjack.domain.card.CardMark
+import blackjack.domain.card.CardValue
+import blackjack.domain.card.Cards
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class ScoreTest {
+
+    @Test
+    fun `버스트 나지 않는 최댓값을 반환할 수 있다`() {
+        val cards = Cards(
+            setOf(
+                Card(CardMark.CLOVER, CardValue.ACE),
+                Card(CardMark.SPADE, CardValue.ACE),
+            ),
+        )
+
+        val score = Score(cards)
+
+        assertThat(score.score()).isEqualTo(12)
+    }
+
+    @Test
+    fun `버스트 날 때 ACE를 1로 계산한다`() {
+        val cards = Cards(
+            setOf(
+                Card(CardMark.CLOVER, CardValue.ACE),
+                Card(CardMark.CLOVER, CardValue.KING),
+                Card(CardMark.CLOVER, CardValue.QUEEN),
+                Card(CardMark.SPADE, CardValue.QUEEN),
+            ),
+        )
+
+        val score = Score(cards)
+
+        assertThat(score.maxScore).isEqualTo(31)
+    }
+}
