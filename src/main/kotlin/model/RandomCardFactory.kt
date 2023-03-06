5b6ef0d3be0aefade5a457c8d098141cd578aa64
Doyeop Kim <dygames34@gmail.com>
2023-03-06 14:12:38 +0900

    fix: IndexOutOfBoundsException가 나지 않도록 방지

--- src/main/kotlin/model/RandomCardFactory.kt
+++ src/main/kotlin/model/RandomCardFactory.kt
@@ -14,9 +14,18 @@ class RandomCardFactory : CardFactory {
 
     private var index = 0
 
-    private fun generateCard(): Card = cards.value[index++]
+    private fun generateCard(): Card {
+        require(index < cards.value.size) {
+            MESSAGE_TOO_MANY_GENERATE_CARD
+        }
+        return cards.value[index++]
+    }
 
     override fun generate(count: Int): Cards {
         return Cards((0 until count).map { generateCard() })
+    }
+
+    companion object {
+        const val MESSAGE_TOO_MANY_GENERATE_CARD = "생성할 수 있는 카드의 수를 초과했습니다."
     }
 }
--- src/test/kotlin/model/ManualCardFactory.kt
+++ src/test/kotlin/model/ManualCardFactory.kt
@@ -10,10 +10,17 @@ class ManualCardFactory(private val cardTypes: List<CardType>, private val cardN
     private var index = 0
 
     private fun generateCard(): Card {
+        require(index < cardTypes.size && index < cardNumbers.size) {
+            MESSAGE_TOO_MANY_GENERATE_CARD
+        }
         return Card(cardTypes[index], cardNumbers[index++])
     }
 
     override fun generate(count: Int): Cards {
         return Cards((0 until count).map { generateCard() })
+    }
+
+    companion object {
+        const val MESSAGE_TOO_MANY_GENERATE_CARD = "생성할 수 있는 카드의 수를 초과했습니다."
     }
 }
