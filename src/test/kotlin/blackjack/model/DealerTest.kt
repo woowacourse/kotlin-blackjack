17b14996ecd3e35e6764d83ca6985966acc58eb4
hyemdooly <hyemdooly@gmail.com>
2023-03-06 13:57:47 +0900

    refactor(DealerTest): Dealer를 생성하는 공용 함수 추가

--- src/test/kotlin/blackjack/model/DealerTest.kt
+++ src/test/kotlin/blackjack/model/DealerTest.kt
@@ -11,31 +11,17 @@ import org.junit.jupiter.api.Test
 class DealerTest {
     @Test
     fun `카드의 합이 16을 초과하지 않으면 hit 한다`() {
-        val dealer = hitDealer()
+        val dealer = dealer(Rank.JACK, Rank.SIX)
         assertThat(dealer.isHit()).isTrue
     }
 
     @Test
     fun `카드의 합이 16을 초과하면 stay 한다`() {
-        val dealer = stayDealer()
+        val dealer = dealer(Rank.JACK, Rank.SEVEN)
         assertThat(dealer.isHit()).isFalse
     }
 
-    private fun hitDealer(): Dealer = Dealer(
-        Cards(
-            listOf(
-                Card(Rank.JACK, Suit.DIAMOND),
-                Card(Rank.SIX, Suit.CLOVER),
-            )
-        )
-    )
-
-    private fun stayDealer(): Dealer = Dealer(
-        Cards(
-            listOf(
-                Card(Rank.JACK, Suit.DIAMOND),
-                Card(Rank.SEVEN, Suit.CLOVER),
-            )
-        )
+    private fun dealer(vararg ranks: Rank): Dealer = Dealer(
+        Cards(ranks.map { Card(it, Suit.DIAMOND) })
     )
 }
