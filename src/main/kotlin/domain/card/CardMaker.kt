75b61c26696a746e7c9a53a63288353049505cb0
pingu244 <69796976+pingu244@users.noreply.github.com>
2023-03-06 20:04:48 +0900

    refactor: 불필요한 변수 삭제

--- src/main/kotlin/domain/card/CardMaker.kt
+++ src/main/kotlin/domain/card/CardMaker.kt
@@ -2,16 +2,14 @@ package domain.card
 
 class CardMaker {
     fun makeShuffledCards(): List<Card> {
-        val cards = Shape.values().flatMap {
+        return Shape.values().flatMap {
             makeCardWithValues(it)
-        }
-        return cards.shuffled()
+        }.shuffled()
     }
 
     private fun makeCardWithValues(shape: Shape): List<Card> {
-        val result = CardValue.values().map {
+        return CardValue.values().map {
             Card(shape, it)
         }
-        return result
     }
 }
