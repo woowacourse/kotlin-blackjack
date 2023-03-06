564f9556983911e69233eff96bd8c89e88ed1de6
re4rk <37167652+re4rk@users.noreply.github.com>
2023-03-06 13:57:13 +0900

    feat (NameTest): 테스틐 케이스 경계값으로 변경

--- src/test/kotlin/blackjack/domain/NameTest.kt
+++ src/test/kotlin/blackjack/domain/NameTest.kt
@@ -7,7 +7,7 @@ import org.junit.jupiter.params.provider.ValueSource
 
 class NameTest {
     @ParameterizedTest
-    @ValueSource(strings = ["아", "아크,로피,아크,로피,아크,로피,아크"])
+    @ValueSource(strings = ["아", "01234567890123456789"])
     fun `이름은 1~20자 이하다`(name: String) {
         assertDoesNotThrow {
             Name(name)
@@ -15,7 +15,7 @@ class NameTest {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = ["", "가나다라마바사아자차카타파하abcdefjhijklmnopqrstuvwxyz"])
+    @ValueSource(strings = ["", "012345678901234567890"])
     fun `이름은 1~20자 이외면 에러난다`(name: String) {
         assertThrows<IllegalArgumentException> {
             Name(name)
