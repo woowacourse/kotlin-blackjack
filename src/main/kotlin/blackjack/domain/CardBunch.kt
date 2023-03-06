58b2bdd68bfecfc39ff2ca47c3c98e166a1cda05
changhwan <chang5hustle@gmail.com>
2023-03-06 14:20:25 +0900

    refactor(CardBunch): 사용하지 않는 상수 정리

--- src/main/kotlin/blackjack/domain/CardBunch.kt
+++ src/main/kotlin/blackjack/domain/CardBunch.kt
@@ -34,9 +34,7 @@ class CardBunch private constructor(cards: MutableList<Card>) {
     fun isBurst(): Boolean = getTotalScore() > MAX_SCORE_CONDITION
 
     companion object {
-        private const val INITIAL_SIZE_ERROR = "초기 카드는 2장이 배정되어야 합니다."
         private const val DUPLICATE_ERROR = "중복된 카드는 추가할 수 없습니다."
-        private const val INITIAL_CARD_SIZE = 2
         private const val BIG_ACE_SCORE = 11
         private const val SMALL_ACE_SCORE = 1
     }
