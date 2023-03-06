1366fbbecb5c4fb59561229762a94ff58ff82223
ki960213 <ki960213@github.com>
2023-03-06 20:25:52 +0900

    refactor(BlackjackResult): 프로퍼티명 변경
    - player -> playerResult
    - dealer -> dealerResult

--- src/main/kotlin/blackjack/domain/BlackjackResult.kt
+++ src/main/kotlin/blackjack/domain/BlackjackResult.kt
@@ -1,16 +1,16 @@
 package blackjack.domain
 
-class BlackjackResult private constructor(private val player: Map<Player, ResultType>) {
-    private val dealer: Map<ResultType, Int> = ResultType.values().associateWith { type ->
+class BlackjackResult private constructor(private val playerResult: Map<Player, ResultType>) {
+    private val dealerResult: Map<ResultType, Int> = ResultType.values().associateWith { type ->
         when (type) {
-            ResultType.WIN -> player.values.count { it == ResultType.LOSE }
-            ResultType.TIE -> player.values.count { it == ResultType.TIE }
-            ResultType.LOSE -> player.values.count { it == ResultType.WIN }
+            ResultType.WIN -> playerResult.values.count { it == ResultType.LOSE }
+            ResultType.TIE -> playerResult.values.count { it == ResultType.TIE }
+            ResultType.LOSE -> playerResult.values.count { it == ResultType.WIN }
         }
     }
 
-    fun getCountOfDealer(resultType: ResultType) = dealer[resultType]
-    fun getResultOf(player: Player): ResultType? = this.player[player]
+    fun getCountOfDealer(resultType: ResultType) = dealerResult[resultType]
+    fun getResultOf(player: Player): ResultType? = this.playerResult[player]
 
     companion object {
         private const val DEALER_SHOULD_HIT_ERROR = "딜러가 히트해야 한다면 블랙잭 결과를 생성할 수 없습니다."
