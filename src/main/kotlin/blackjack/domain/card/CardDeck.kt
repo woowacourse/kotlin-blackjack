8aa6360fe67ed9156d41cf5d4ca2fa12839c928f
whk06061 <gju06061@naver.com>
2023-03-07 00:46:25 +0900

    feat: 카드를 한 장 발행하고, 해당 카드를 삭제하는 기능 구현

--- src/main/kotlin/blackjack/domain/card/CardDeck.kt
+++ src/main/kotlin/blackjack/domain/card/CardDeck.kt
@@ -2,10 +2,20 @@ package blackjack.domain.card
 
 class CardDeck {
 
-    private val cards = RandomCardsGenerator().generate()
+    private val cards: MutableList<Card> = RandomCardsGenerator().generate().toMutableList()
+
+    init {
+        require(cards.size == 52) { "초기 카드는 52장이어야 합니다." }
+    }
 
     fun checkProvidePossible(): Boolean {
         if (cards.isNotEmpty()) return true
         return false
+    }
+
+    fun provide(): Card {
+        val card = cards[0]
+        cards.removeAt(0)
+        return card
     }
 }
--- src/test/kotlin/blackjack/domain/card/CardDeckTest.kt
+++ src/test/kotlin/blackjack/domain/card/CardDeckTest.kt
@@ -2,8 +2,17 @@ package blackjack.domain.card
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
 
 class CardDeckTest {
+
+    @Test
+    fun `초기에 카드 52장을 가진다`() {
+        assertDoesNotThrow {
+            CardDeck()
+        }
+    }
+
     @Test
     fun `카드 발행이 가능한지 확인한다`() {
         // given
@@ -14,5 +23,20 @@ class CardDeckTest {
 
         // then
         assertThat(actual).isTrue
+    }
+
+    @Test
+    fun `카드를 한 장 발행하고 해당 카드를 삭제한다`() {
+        // given
+        val cardDeck = CardDeck()
+
+        // when
+        repeat(52) {
+            cardDeck.provide()
+        }
+        val actual = cardDeck.checkProvidePossible()
+
+        // then
+        assertThat(actual).isFalse
     }
 }
