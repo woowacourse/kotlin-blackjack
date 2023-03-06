b8d3483d99e632af4d9320484b543ab420d571e4
changhwan <chang5hustle@gmail.com>
2023-03-06 17:03:59 +0900

    refactor(Player,Dealer): Player 상태값 제거로 인하여 함수 시작점 변경

--- src/main/kotlin/blackjack/domain/Dealer.kt
+++ src/main/kotlin/blackjack/domain/Dealer.kt
@@ -1,10 +1,17 @@
 package blackjack.domain
 
+import blackjack.const.MAX_SCORE_CONDITION
+
 class Dealer(override val cardBunch: CardBunch) : Participant {
     fun isOverCondition(): Boolean = cardBunch.getTotalScore() > ADD_CARD_CONDITION
 
-    fun compareScore(player: Player) {
-        player.chooseWinner(cardBunch.getTotalScore())
+    fun compareScore(playerScore: Int): Consequence {
+        val dealerScore = cardBunch.getTotalScore()
+        if (playerScore > MAX_SCORE_CONDITION) return Consequence.LOSE
+        if (playerScore > dealerScore) return Consequence.WIN
+        if (playerScore == dealerScore) return Consequence.DRAW
+        if (dealerScore > MAX_SCORE_CONDITION) return Consequence.WIN
+        return Consequence.LOSE
     }
 
     companion object {
--- src/main/kotlin/blackjack/domain/Player.kt
+++ src/main/kotlin/blackjack/domain/Player.kt
@@ -1,15 +1,6 @@
 package blackjack.domain
 
-import blackjack.const.MAX_SCORE_CONDITION
-
 class Player(val name: String, override val cardBunch: CardBunch) : Participant {
 
-    fun chooseWinner(dealerScore: Int): Consequence {
-        val playerScore = cardBunch.getTotalScore()
-        if (playerScore > MAX_SCORE_CONDITION) return Consequence.LOSE
-        if (playerScore > dealerScore) return Consequence.WIN
-        if (playerScore == dealerScore) return Consequence.DRAW
-        if (dealerScore > MAX_SCORE_CONDITION) return Consequence.WIN
-        return Consequence.LOSE
-    }
+    fun chooseWinner(dealer: Dealer): Consequence = dealer.compareScore(cardBunch.getTotalScore())
 }
