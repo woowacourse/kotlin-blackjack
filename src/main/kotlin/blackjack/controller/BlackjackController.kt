33fbd5ef2874e6c48dbd5290de453fa4bb8dcde1
whk06061 <gju06061@naver.com>
2023-03-07 23:04:42 +0900

    refactor: BlackjackController로 로직 분리 후 불필요한 컨트롤러 함수 제거

--- src/main/kotlin/blackjack/controller/BlackjackController.kt
+++ src/main/kotlin/blackjack/controller/BlackjackController.kt
@@ -1,85 +1,40 @@
 package blackjack.controller
 
-import blackjack.domain.card.CardDeck
+import blackjack.domain.card.CardsGenerator
+import blackjack.domain.card.RandomCardsGenerator
 import blackjack.domain.player.BlackjackManager
-import blackjack.domain.player.Dealer
-import blackjack.domain.player.Participant
-import blackjack.domain.player.Participants
-import blackjack.domain.player.Player
 import blackjack.view.InputView
 import blackjack.view.OutputView
 
 class BlackjackController(
     private val inputView: InputView = InputView(),
     private val outputView: OutputView = OutputView(),
-    private val cardsGenerator: CardsGenerator = RandomCardsGenerator()
+    cardsGenerator: CardsGenerator = RandomCardsGenerator()
 ) {
 
     private var blackjackManager: BlackjackManager = BlackjackManager(cardsGenerator)
-    private var cardDeck: CardDeck = CardDeck(cardsGenerator)
 
     fun run() {
-        val dealer = Dealer()
-        val participants = readParticipants()
-        settingPlayersCards(dealer, participants)
-        readParticipantsMoreCard(participants)
-        giveDealerMoreCard(dealer)
-        participants.values.forEach {
-            it.updateResult(dealer.cards.sumCardsNumber())
-        }
-        dealer.updateResults(participants.values.map { it.cards.sumCardsNumber() })
-        printSumResult(dealer, participants)
-        printFinalResult(dealer, participants)
+        blackjackManager.generateParticipants(inputView::readParticipantsName)
+        blackjackManager.settingPlayersCards(outputView::printSettingCard)
+        provideParticipantsMoreCard()
+        blackjackManager.provideDealerMoreCard(outputView::printDealerHitCardMent)
+        blackjackManager.updatePlayersResult()
+        printSumResult()
+        printFinalResult()
     }
 
-    private fun readParticipants(): Participants =
-        Participants(inputView.readParticipantsName().map { Participant(it) })
-
-    private fun settingPlayersCards(dealer: Dealer, participants: Participants) {
-        blackjackManager.settingPlayersCards(dealer, participants)
-        outputView.printSettingCard(dealer, participants)
-    }
-
-    private fun provideCard(player: Player) {
-        cardDeck.apply {
-            if (checkProvidePossible()) player.addCard(provide())
-            else outputView.printNoCardMessage()
+    private fun provideParticipantsMoreCard() {
+        blackjackManager.participants.values.forEach {
+            blackjackManager.provideParticipantMoreCard(it, inputView::readMoreCard, outputView::printParticipantCards)
         }
     }
 
-    private fun readParticipantsMoreCard(participants: Participants) {
-        participants.values.forEach {
-            readParticipantMoreCard(it)
-        }
+    private fun printSumResult() {
+        outputView.printSumResult(blackjackManager.dealer, blackjackManager.participants)
     }
 
-    private fun readParticipantMoreCard(participant: Participant) {
-        while (true) {
-            val check = participant.checkProvideCardPossible()
-            if (check) {
-                val answer = inputView.readMoreCard(participant.name)
-                if (answer) provideCard(participant)
-                outputView.printParticipantCards(participant)
-                if (!answer) break
-            }
-            if (!check) break
-        }
-    }
-
-    private fun giveDealerMoreCard(dealer: Dealer) {
-        val check = dealer.checkProvideCardPossible()
-        if (check) {
-            provideCard(dealer)
-            outputView.printDealerHitCardMent()
-        }
-        if (!check) outputView.printDealerNotHitCardMent()
-    }
-
-    private fun printSumResult(dealer: Dealer, participants: Participants) {
-        outputView.printSumResult(dealer, participants)
-    }
-
-    private fun printFinalResult(dealer: Dealer, participants: Participants) {
-        outputView.printFinalResult(dealer, participants)
+    private fun printFinalResult() {
+        outputView.printFinalResult(blackjackManager.dealer, blackjackManager.participants)
     }
 }
