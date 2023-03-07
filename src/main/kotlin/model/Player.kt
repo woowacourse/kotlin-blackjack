4c80fb1006c639e7032ca13b572688bebd512943
hyemdooly <hyemdooly@gmail.com>
2023-03-07 16:28:35 +0900

    refactor(Player): 주석 추가

--- src/main/kotlin/model/Player.kt
+++ src/main/kotlin/model/Player.kt
@@ -11,8 +11,8 @@ class Player(hand: Hand, name: Name) : Participant(hand, name) {
         if (dealer.isBust()) return FinalResult.WIN // 딜러 버스트
         if (isBlackJack() && dealer.isBlackJack()) return FinalResult.PUSH // 둘 다 블랙잭
         if (isBlackJack()) return FinalResult.BLACKJACK_WIN // 플레이어만 블랙잭
-        if (playerPoint == dealerPoint) return FinalResult.PUSH // 점수 같음
-        if (playerPoint > dealerPoint) return FinalResult.WIN
+        if (playerPoint == dealerPoint) return FinalResult.PUSH // 딜러, 플레이어 점수 같음
+        if (playerPoint > dealerPoint) return FinalResult.WIN // 플레이어가 더 점수가 큼
         return FinalResult.LOSE
     }
 }
