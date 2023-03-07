5d3a19e968e8c35bc4f830289f85605b6d8d2514
hyunji1203 <hj27720@gmail.com>
2023-03-07 14:25:51 +0900

    refactor(CardDistributeCondition): 상수 관리

--- src/main/kotlin/entity/CardDistributeCondition.kt
+++ src/main/kotlin/entity/CardDistributeCondition.kt
@@ -2,14 +2,16 @@ package entity
 
 class CardDistributeCondition(val value: String) {
     init {
-        require(value == "y" || value == "n") { CONDITION_ERROR_MESSAGE }
+        require(value == CONDITION_YES_MESSAGE || value == CONDITION_NO_MESSAGE) { CONDITION_ERROR_MESSAGE }
     }
 
     fun toBoolean(): Boolean {
-        return value == "y"
+        return value == CONDITION_YES_MESSAGE
     }
 
     companion object {
+        private const val CONDITION_YES_MESSAGE = "y"
+        private const val CONDITION_NO_MESSAGE = "n"
         private const val CONDITION_ERROR_MESSAGE = "y나 n을 입력하여야 합니다."
     }
 }
