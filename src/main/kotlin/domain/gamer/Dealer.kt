def87b66441863f510a50c009ed757c6dc31cc7c
rhkrwngud445 <rhkrwngud445@gmail.com>
2023-03-07 23:08:52 +0900

    refactor: 가짜 생성자의 가변인자 card를  cards로 변경

--- src/main/kotlin/domain/gamer/Dealer.kt
+++ src/main/kotlin/domain/gamer/Dealer.kt
@@ -5,7 +5,7 @@ import domain.gamer.cards.Cards
 
 class Dealer(override val cards: Cards) : Participant(cards) {
 
-    constructor(vararg card: Card) : this(Cards(card.toList()))
+    constructor(vararg cards: Card) : this(Cards(cards.toList()))
 
     fun checkAvailableForPick() = calculateCardSum() <= CARD_PICK_CONDITION
 
--- src/main/kotlin/domain/gamer/Player.kt
+++ src/main/kotlin/domain/gamer/Player.kt
@@ -5,5 +5,5 @@ import domain.gamer.cards.Cards
 
 class Player(val name: String, override val cards: Cards) : Participant(cards) {
 
-    constructor(name: String, vararg card: Card) : this(name, Cards(card.toList()))
+    constructor(name: String, vararg cards: Card) : this(name, Cards(cards.toList()))
 }
