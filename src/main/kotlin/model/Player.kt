7ba8b27e229ff060a30edf43fdd758cb1f75588f
hyemdooly <hyemdooly@gmail.com>
2023-03-07 17:56:09 +0900

    refactor(Player): if문 when으로 수정

--- src/main/kotlin/model/Player.kt
+++ src/main/kotlin/model/Player.kt
@@ -7,13 +7,15 @@ class Player(hand: Hand, name: Name) : Participant(hand, name) {
         val playerPoint = hand.sum()
         val dealerPoint = dealer.hand.sum()
 
-        if (isBust()) return FinalResult.LOSE // 플레이어 버스트
-        if (dealer.isBust()) return FinalResult.WIN // 딜러 버스트
-        if (isBlackJack() && dealer.isBlackJack()) return FinalResult.PUSH // 둘 다 블랙잭
-        if (isBlackJack()) return FinalResult.BLACKJACK_WIN // 플레이어만 블랙잭
-        if (dealer.isBlackJack()) return FinalResult.LOSE // 딜러만 블랙잭
-        if (playerPoint == dealerPoint) return FinalResult.PUSH // 딜러, 플레이어 점수 같음
-        if (playerPoint > dealerPoint) return FinalResult.WIN // 플레이어가 더 점수가 큼
-        return FinalResult.LOSE
+        return when {
+            isBust() -> FinalResult.LOSE // 플레이어 버스트
+            dealer.isBust() -> FinalResult.WIN // 딜러 버스트
+            isBlackJack() && dealer.isBlackJack() -> FinalResult.PUSH // 둘 다 블랙잭
+            isBlackJack() -> FinalResult.BLACKJACK_WIN // 플레이어만 블랙잭
+            dealer.isBlackJack() -> FinalResult.LOSE // 딜러만 블랙잭
+            playerPoint == dealerPoint -> FinalResult.PUSH // 딜러, 플레이어 점수 같음
+            playerPoint > dealerPoint -> FinalResult.WIN // 플레이어가 더 점수가 큼
+            else -> FinalResult.LOSE
+        }
     }
 }
