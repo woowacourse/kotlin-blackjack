c79b5ec8055fb7e92d5f1ed8136dbedc8ac0eedf
otter <a33397397@gmail.com>
2023-03-07 20:08:18 +0900

    refactor: 사용자의 이름을 검사하는 로직 제거
    - 요구사항 외의 불필요한 내용이라 판단해 제거

--- src/main/kotlin/blackjack/domain/player/Player.kt
+++ src/main/kotlin/blackjack/domain/player/Player.kt
@@ -6,16 +6,9 @@ import blackjack.domain.card.Cards
 abstract class Player(val name: String) {
     val cards: Cards = Cards()
 
-    init {
-        require(name.length in 2..10) { ERROR_NAME_LENGTH }
-    }
     abstract fun canHit(): Boolean
 
     fun addCard(card: Card) {
         cards.addCard(card)
-    }
-
-    companion object {
-        private const val ERROR_NAME_LENGTH = "이름은 2글자 이상 10글자 이하여야 합니다."
     }
 }
