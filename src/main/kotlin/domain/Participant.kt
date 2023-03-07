0d2ef558a1e78bd7c6c0e613deb304b29711edd1
chws0508 <chws0508@naver.com>
2023-03-07 14:41:45 +0900

    refactor(Player): Participant에서 하던 승패 반환을 Player가 하도록 수정

--- src/main/kotlin/domain/Participant.kt
+++ src/main/kotlin/domain/Participant.kt
@@ -6,14 +6,4 @@ abstract class Participant(val name: Name, val cards: Cards) {
     fun getScore(): Score {
         return cards.getScore()
     }
-
-    fun getGameResult(opponentScore: Score): GameResultType {
-        val myScore = getScore()
-        if (opponentScore.isBurst() && myScore.isBurst()) return GameResultType.DRAW
-        if (opponentScore.isBurst() && !myScore.isBurst()) return GameResultType.WIN
-        if (!opponentScore.isBurst() && myScore.isBurst()) return GameResultType.LOSE
-        if (opponentScore.getValue() < myScore.getValue()) return GameResultType.WIN
-        if (opponentScore.getValue() == myScore.getValue()) return GameResultType.DRAW
-        return GameResultType.LOSE
-    }
 }
