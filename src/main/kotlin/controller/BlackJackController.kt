d8a0c2ff982beae92297b6d9abecdfb46b1c4bf5
briandr97 <briandr@naver.com>
2023-03-07 15:23:27 +0900

    refactor(BlackJackController): BlackJackGame을 만들면서 BlackJackController가 줄어들게 됨

--- src/main/kotlin/controller/BlackJackController.kt
+++ src/main/kotlin/controller/BlackJackController.kt
@@ -1,57 +1,18 @@
 package controller
 
-import domain.Decision
-import domain.card.Deck
-import domain.person.Dealer
-import domain.person.Persons
-import domain.person.Player
-import domain.result.GameResult
-import view.additional.AdditionalCardView
+import domain.BlackJackGame
+import view.additional.DrawView
 import view.onboarding.OnboardingView
 import view.result.ResultView
 
 class BlackJackController {
     fun runBlackJack() {
-        val deck = Deck()
-        val persons = runOnboarding(deck)
-        runMain(deck, persons)
-        runResult(persons)
-    }
-
-    private fun runOnboarding(deck: Deck): Persons {
-        val persons = Persons.getPersons(OnboardingView.requestInputNames(), deck)
+        val blackJackGame: BlackJackGameBluePrint = BlackJackGame()
+        val persons = blackJackGame.makePersons(OnboardingView.requestInputNames())
         OnboardingView.printInitialSetting(persons)
-        return persons
-    }
-
-    private fun runMain(deck: Deck, persons: Persons) {
-        persons.players.forEach { player -> handOutCardsToPlayer(deck, player) }
-        handOutCardToDealer(deck, persons.dealer)
-    }
-
-    private fun handOutCardToDealer(deck: Deck, dealer: Dealer) {
-        if (dealer.canReceiveMoreCard()) {
-            dealer.receiveCard(deck.getCard())
-            AdditionalCardView.printDealerGetMoreCard()
-            return
-        }
-        AdditionalCardView.printDealerNoMoreCard()
-    }
-
-    private fun handOutCardsToPlayer(deck: Deck, player: Player) {
-        val decision = Decision.of(AdditionalCardView.requestPlayerDecision(player.name))
-            ?: throw IllegalArgumentException("y 또는 n 을 입력해야 합니다.")
-        if (decision == Decision.NO) return
-        player.receiveCard(deck.getCard())
-        AdditionalCardView.printPlayerCards(player)
-        if (player.isBust()) return
-        handOutCardsToPlayer(deck, player)
-    }
-
-    private fun runResult(persons: Persons) {
-        val gameResult = GameResult(persons)
-
-        ResultView.printPersonsCards(persons.dealer, persons.players)
-        ResultView.printFinalResult(gameResult)
+        blackJackGame.handOutCardsToPlayers(persons.players, { DrawView.askPlayerDraw(it) }, { DrawView.printPlayerCards(it) })
+        blackJackGame.handOutCardsToDealer(persons.dealer, { DrawView.printDealerDrew() }, { DrawView.printDealerDidNotDrew() })
+        ResultView.printPersonsCards(persons)
+        ResultView.printFinalResult(blackJackGame.drawResult(persons))
     }
 }
