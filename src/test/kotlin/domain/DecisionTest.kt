eb890a305e4859ccdd236087116b0af49450059b
briandr97 <briandr@naver.com>
2023-03-07 15:21:16 +0900

    refactor(DecisionTest): Decision이 변함에 따라 Test 코드도 변경

--- src/test/kotlin/domain/DecisionTest.kt
+++ src/test/kotlin/domain/DecisionTest.kt
@@ -1,18 +1,16 @@
 package domain
 
+import domain.person.Decision
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertAll
-import org.junit.jupiter.api.assertDoesNotThrow
 
 class DecisionTest {
     @Test
     fun `Decision 은 y 나 n 이어야 한다`() {
         assertAll(
-            { assertDoesNotThrow { Decision.of("y") } },
-            { assertDoesNotThrow { Decision.of("n") } },
-            { assertThat(Decision.of("p")).isNull() },
-            { assertThat(Decision.of("1")).isNull() },
+            { assertThat(Decision.of(true)).isEqualTo(Decision.YES) },
+            { assertThat(Decision.of(false)).isEqualTo(Decision.NO) },
         )
     }
 }
