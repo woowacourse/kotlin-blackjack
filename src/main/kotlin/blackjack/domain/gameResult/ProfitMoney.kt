6884f4a6da08ea558e5cf0c7bfc6ec7dbe1791d7
boogieWoogie <kimjinuk1999@gmail.com>
2023-03-07 20:20:33 +0900

    style: 부생성자와 메소드 순서 변경

--- src/main/kotlin/blackjack/domain/gameResult/ProfitMoney.kt
+++ src/main/kotlin/blackjack/domain/gameResult/ProfitMoney.kt
@@ -2,14 +2,15 @@ package blackjack.domain.gameResult
 
 import blackjack.domain.BattingMoney
 
-data class ProfitMoney(val value: Int) {
-
-    operator fun not(): ProfitMoney {
-        return ProfitMoney(-value)
-    }
+@JvmInline
+value class ProfitMoney(val value: Int) {
 
     constructor(
         battingMoney: BattingMoney,
         gameResult: GameResult,
     ) : this(battingMoney.value * gameResult.profitRate.toInt())
+
+    operator fun not(): ProfitMoney {
+        return ProfitMoney(-value)
+    }
 }
