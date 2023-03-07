04e98982958149d318617008fa298dfc1ae26297
boogieWoogie <kimjinuk1999@gmail.com>
2023-03-07 23:25:01 +0900

    refactor: 플레이어가 카드를 뽑고 확인하는 방식을 메소드로 주입받기

--- src/main/kotlin/blackjack/domain/player/Player.kt
+++ src/main/kotlin/blackjack/domain/player/Player.kt
@@ -1,41 +1,27 @@
 package blackjack.domain.player
 
 import blackjack.domain.BattingMoney
+import blackjack.domain.BlackJackGameParticipant
 import blackjack.domain.card.Cards
 
-//TODO: 인자 갯수를 어떻게 줄여야 할까
 data class Player(
     val name: PlayerName,
     val battingMoney: BattingMoney,
-    val cards: Cards = Cards(),
-) {
+) : BlackJackGameParticipant() {
 
-    constructor(
-        name: String,
-        battingMoney: Int,
-    ) : this(PlayerName(name), BattingMoney(battingMoney))
+//    constructor(
+//        name: String,
+//        battingMoney: Int,
+//    ) : this(PlayerName(name), BattingMoney(battingMoney))
 
-    private fun isPossibleToDrawAdditionalCard(): DrawState {
-        if (cards.getMinimumCardsScore() >= BLACK_JACK_SCORE) {
-            return DrawState.IMPOSSIBLE
-        }
-
-        return DrawState.POSSIBLE
-    }
-
-    fun drawCard(
-        checkCurrentCards: (player: Player) -> Unit = { }
-    ): Boolean {
-
+    fun drawCard(checkCurrentCards: (player: Player) -> Unit = { }): Boolean {
         cards.draw()
         checkCurrentCards(this)
 
-        return isPossibleToDrawAdditionalCard() == DrawState.POSSIBLE
+        return isPossibleToDrawAdditionalCard(BLACK_JACK_SCORE) == DrawState.POSSIBLE
     }
 
-    fun checkIsDrawnNothing(
-        checkCurrentCards: (player: Player) -> Unit = { },
-    ) {
+    fun checkIsDrawnNothing(checkCurrentCards: (player: Player) -> Unit = { }) {
         if (cards.size == Cards.INITIAL_CARDS_SIZE) {
             checkCurrentCards(this)
         }
