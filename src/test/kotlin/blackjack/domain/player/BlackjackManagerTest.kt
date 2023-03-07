cf776d8e8fd94f270972a368dc6b6b6521707319
whk06061 <gju06061@naver.com>
2023-03-08 03:14:11 +0900

    refactor: 테스트 코드 분리 및 참가자 카드 추가 발행 로직 테스트 코드 수정
    - 참가자 카드의 추가 발행 테스트 코드에서 처음 람다식이 실행될 때는 true를, 두번째 람다식이 실행될 때는 false를 반환하도록 수정

--- src/test/kotlin/blackjack/domain/player/BlackjackManagerTest.kt
+++ src/test/kotlin/blackjack/domain/player/BlackjackManagerTest.kt
@@ -1,11 +1,7 @@
 package blackjack.domain.player
 
 import blackjack.domain.Result
-import blackjack.domain.card.Card
-import blackjack.domain.card.CardNumber
-import blackjack.domain.card.CardShape
-import blackjack.domain.card.CardsGenerator
-import blackjack.domain.card.RandomCardsGenerator
+import blackjack.domain.card.TestCardsGenerator
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
@@ -15,7 +11,7 @@ class BlackjackManagerTest {
     fun `입력받은 이름의 참가자를 생성한다`() {
 
         // given
-        val blackjackManager = BlackjackManager(RandomCardsGenerator())
+        val blackjackManager = BlackjackManager(TestCardsGenerator())
         blackjackManager.setup { listOf("aaa", "bbb") }
 
         // when
@@ -28,7 +24,7 @@ class BlackjackManagerTest {
     @Test
     fun `초기에 모든 플레이어에게 카드 두장을 나누어준다`() {
         // given
-        val blackjackManager = BlackjackManager(RandomCardsGenerator())
+        val blackjackManager = BlackjackManager(TestCardsGenerator())
         blackjackManager.setup { listOf("aaa", "bbb") }
 
         // when
@@ -43,28 +39,40 @@ class BlackjackManagerTest {
     @Test
     fun `카드 발행 가능 상태이고 추가 발행을 원한다면 참가자에게 카드를 나눠준다`() {
         // given
-        val blackjackManager = BlackjackManager(RandomCardsGenerator())
+        val blackjackManager = BlackjackManager(TestCardsGenerator())
         blackjackManager.setup { listOf("aaa", "bbb") }
 
         // when
         val participant1 = blackjackManager.participants.values[0]
-        val participant2 = blackjackManager.participants.values[1]
+        var index = 0
+        blackjackManager.provideParticipantMoreCard(participant1, { _: String -> listOf(true, false)[index++] }) {}
 
-        blackjackManager.provideParticipantMoreCard(participant1, { true }, {})
-        blackjackManager.provideParticipantMoreCard(participant2, { false }, {})
-
-        val participant1CardsSize = participant1.cards.values.size
-        val participant2CardsSize = participant2.cards.values.size
+        val actual = participant1.cards.values.size
 
         // then
-        assertThat(participant1CardsSize).isEqualTo(3)
-        assertThat(participant2CardsSize).isEqualTo(2)
+        assertThat(actual).isEqualTo(3)
+    }
+
+    @Test
+    fun `카드 발행 가능 상태이고 추가 발행을 원하지 않는다면 참가자에게 카드를 나눠주지 않는다`() {
+        // given
+        val blackjackManager = BlackjackManager(TestCardsGenerator())
+        blackjackManager.setup { listOf("aaa", "bbb") }
+
+        // when
+        val participant1 = blackjackManager.participants.values[0]
+        blackjackManager.provideParticipantMoreCard(participant1, { false }) {}
+
+        val actual = participant1.cards.values.size
+
+        // then
+        assertThat(actual).isEqualTo(2)
     }
 
     @Test
     fun `카드 발행 가능 상태인 동안 딜러에게 계속 카드를 발행한다`() {
         // given
-        val blackjackManager = BlackjackManager(RandomCardsGenerator())
+        val blackjackManager = BlackjackManager(TestCardsGenerator())
         blackjackManager.setup { listOf("aaa", "bbb") }
 
         // when
@@ -78,7 +86,7 @@ class BlackjackManagerTest {
     @Test
     fun `플레이어들의 게임 결과를 업데이트한다`() {
         // given
-        val blackjackManager = BlackjackManager(RandomCardsGenerator())
+        val blackjackManager = BlackjackManager(TestCardsGenerator())
         blackjackManager.setup { listOf("aaa", "bbb") }
 
         // when
@@ -91,27 +99,5 @@ class BlackjackManagerTest {
         assertThat(dealerResults).isEqualTo(mapOf(Result.WIN to 1, Result.LOSE to 1, Result.DRAW to 0))
         assertThat(participant1Result).isEqualTo(Result.WIN)
         assertThat(participant2Result).isEqualTo(Result.LOSE)
-    }
-
-    class TestCardsGenerator : CardsGenerator {
-        override fun generate(): List<Card> =
-            listOf(
-                // dealer
-                Card(CardNumber.FOUR, CardShape.HEART),
-                Card(CardNumber.FIVE, CardShape.HEART),
-
-                // player1
-                Card(CardNumber.SIX, CardShape.DIAMOND),
-                Card(CardNumber.EIGHT, CardShape.DIAMOND),
-
-                // player2
-                Card(CardNumber.THREE, CardShape.CLOVER),
-                Card(CardNumber.FOUR, CardShape.CLOVER),
-
-                // 추가 발급
-                Card(CardNumber.FOUR, CardShape.SPADE),
-                Card(CardNumber.FIVE, CardShape.SPADE),
-                Card(CardNumber.SIX, CardShape.SPADE)
-            )
     }
 }
