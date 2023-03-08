0837aeb031bdcb9117c731737e5f4f69c0a160f2
mendel <rhthrhrl0@naver.com>
2023-03-08 23:19:04 +0900

    refactor(BetProfitResult): 주생성자에서 프로퍼티 선언을 하지않고 매개변수로 수정

--- src/main/kotlin/domain/result/BetProfitResult.kt
+++ src/main/kotlin/domain/result/BetProfitResult.kt
@@ -3,16 +3,17 @@ package domain.result
 import domain.Dealer
 import domain.Players
 
-class BetProfitResult(private val players: Players, private val dealer: Dealer) {
+class BetProfitResult(players: Players, dealer: Dealer) {
     val playersResult: PlayersResult
-        get() {
-            val nameAndProfits = mutableListOf<NameAndProfit>()
-            players.list.forEach {
-                nameAndProfits.add(NameAndProfit(it.name, it.getGameProfitMoney(dealer)))
-            }
-            return PlayersResult(nameAndProfits)
-        }
 
     val dealerResult: Int
         get() = playersResult.sum * -1
+
+    init {
+        val nameAndProfits = mutableListOf<NameAndProfit>()
+        players.list.forEach {
+            nameAndProfits.add(NameAndProfit(it.name, it.getGameProfitMoney(dealer)))
+        }
+        playersResult = PlayersResult(nameAndProfits)
+    }
 }
