c10cc0c96b5f0e8ff66824ecf151e59815e1e613
hyemdooly <hyemdooly@gmail.com>
2023-03-08 16:13:36 +0900

    refactor(FinalResult, GameResult): FinalResult rate 추가, calculate 삭제

--- src/main/kotlin/model/FinalResult.kt
+++ src/main/kotlin/model/FinalResult.kt
@@ -1,5 +1,5 @@
 package model
 
-enum class FinalResult {
-    BLACKJACK_WIN, WIN, PUSH, LOSE
+enum class FinalResult(val rate: Double) {
+    BLACKJACK_WIN(0.5), WIN(1.0), PUSH(0.0), LOSE(-1.0)
 }
--- src/main/kotlin/model/GameResult.kt
+++ src/main/kotlin/model/GameResult.kt
@@ -4,19 +4,11 @@ class GameResult private constructor(val playersFinalResult: Map<Name, Int>) {
     fun getDealerProfitResult(): Int = playersFinalResult.values.sum() * -1
 
     companion object {
-        private const val ZERO = 0
-        private fun calculate(bet: Bet, result: FinalResult) = when (result) {
-            FinalResult.WIN -> bet.amount
-            FinalResult.BLACKJACK_WIN -> (bet.amount * 0.5).toInt()
-            FinalResult.LOSE -> (bet.amount * -1)
-            FinalResult.PUSH -> (ZERO)
-        }
-
         fun of(dealer: Dealer, betInfos: BetInfos): GameResult =
             GameResult(
                 buildMap {
                     betInfos.infos.forEach {
-                        put(it.key.name, calculate(it.value, it.key.judge(dealer)))
+                        put(it.key.name, (it.value.amount * it.key.judge(dealer).rate).toInt())
                     }
                 }
             )
