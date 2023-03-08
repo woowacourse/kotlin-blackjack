a21974133a227966e22256812c743fe591f6e03e
changhwan <chang5hustle@gmail.com>
2023-03-08 15:04:48 +0900

    refactor(carddeck): carddeck object로 변경

--- src/main/kotlin/blackjack/domain/CardDeck.kt
+++ src/main/kotlin/blackjack/domain/CardDeck.kt
@@ -2,13 +2,11 @@ package blackjack.domain
 
 import blackjack.Shape
 
-class CardDeck() {
+object CardDeck {
 
-    companion object {
-        private val Cards: List<Card> = Shape.values().fold(listOf()) { cards, shape -> cards + makeCard(shape) }
+    private val Cards: List<Card> = Shape.values().fold(listOf()) { cards, shape -> cards + makeCard(shape) }
 
-        private fun makeCard(shape: Shape): List<Card> = CardNumber.values().map { Card(shape, it) }
+    private fun makeCard(shape: Shape): List<Card> = CardNumber.values().map { Card(shape, it) }
 
-        fun drawCard(): Card = Cards.shuffled().first()
-    }
+    fun drawCard(): Card = Cards.shuffled().first()
 }
