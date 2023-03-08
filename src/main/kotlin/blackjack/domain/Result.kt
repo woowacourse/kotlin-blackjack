eba7af04c225bf51815b4f212d7e2f3f609ea7f2
whk06061 <gju06061@naver.com>
2023-03-08 16:07:37 +0900

    refactor: 불필요한 Result 메서드 제거
    - 결과를 받아 반대 결과를 반환하는 메서드 제거

--- src/main/kotlin/blackjack/domain/Result.kt
+++ src/main/kotlin/blackjack/domain/Result.kt
@@ -4,13 +4,4 @@ enum class Result(val word: String) {
     WIN("승"),
     LOSE("패"),
     DRAW("무");
-
-    companion object {
-
-        fun reverse(result: Result) = when (result) {
-            WIN -> LOSE
-            LOSE -> WIN
-            else -> DRAW
-        }
-    }
 }
--- src/test/kotlin/blackjack/domain/ResultTest.kt
+++ src/test/kotlin/blackjack/domain/ResultTest.kt
@@ -1,7 +1,6 @@
 package blackjack.domain
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
@@ -12,10 +11,5 @@ class ResultTest {
     fun `승패무 결과를 가진다`(expected: String) {
         val resultWord: List<String> = Result.values().map { it.word }
         assertThat(resultWord).contains(expected)
-    }
-
-    @Test
-    fun `결과를 받아 반대의 결과를 돌려준다`() {
-        assertThat(Result.reverse(Result.LOSE)).isEqualTo(Result.WIN)
     }
 }
