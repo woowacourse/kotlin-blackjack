38e64b12ab19e054ce1b9c4185d810ddac5400be
mendel <rhthrhrl0@naver.com>
2023-03-08 22:58:47 +0900

    refactor(PlayersResult): 데이터 클래스에서 일반 클래스로 수정

--- src/main/kotlin/domain/PlayersResult.kt
+++ src/main/kotlin/domain/PlayersResult.kt
@@ -1,6 +1,6 @@
 package domain
 
-data class PlayersResult(val list: List<NameAndProfit>) {
+class PlayersResult(val list: List<NameAndProfit>) {
     val sum: Int
         get() = list.sumOf { it.profitMoney }
 }
