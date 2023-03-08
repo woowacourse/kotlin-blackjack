741079197126bbdf641f6be19bf032ea8f67c070
EmilyCh0 <emily9803@ewhain.net>
2023-03-08 16:28:39 +0900

    [TEST(RefereeTest)] Referee 테스트 수정

--- src/test/kotlin/domain/RefereeTest.kt
+++ src/test/kotlin/domain/RefereeTest.kt
@@ -1,5 +1,8 @@
 package domain
 
+import domain.card.Card
+import domain.card.CardShape
+import domain.card.CardValue
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
@@ -8,10 +11,31 @@ class RefereeTest {
     fun `딜러는 18점, 유저1, 유저2의 점수는 19, 15점이다`() {
         // given
         val dealerScore = Score.valueOf(18)
-        val userScore = listOf(Score.valueOf(19), Score.valueOf(15))
+        val users = listOf<User>(
+            User(
+                "유저1",
+                Cards(
+                    listOf(
+                        Card(CardShape.DIAMONDS, CardValue.TEN),
+                        Card(CardShape.DIAMONDS, CardValue.NINE)
+                    )
+                ),
+                1000.0
+            ),
+            User(
+                "유저2",
+                Cards(
+                    listOf(
+                        Card(CardShape.DIAMONDS, CardValue.TEN),
+                        Card(CardShape.DIAMONDS, CardValue.FIVE)
+                    )
+                ),
+                1000.0
+            )
+        )
 
         // when
-        val actual = Referee(dealerScore, userScore).getResult()
+        val actual = Referee(dealerScore).getResult(users)
         val expected = listOf<GameResult>(GameResult.WIN, GameResult.LOSE)
 
         // then
@@ -22,10 +46,31 @@ class RefereeTest {
     fun `딜러는 22점, 유저1, 유저2의 점수는 19, 15점이다`() {
         // given
         val dealerScore = Score.valueOf(22)
-        val userScore = listOf(Score.valueOf(19), Score.valueOf(15))
+        val users = listOf<User>(
+            User(
+                "유저1",
+                Cards(
+                    listOf(
+                        Card(CardShape.DIAMONDS, CardValue.TEN),
+                        Card(CardShape.DIAMONDS, CardValue.NINE)
+                    )
+                ),
+                1000.0
+            ),
+            User(
+                "유저2",
+                Cards(
+                    listOf(
+                        Card(CardShape.DIAMONDS, CardValue.TEN),
+                        Card(CardShape.DIAMONDS, CardValue.FIVE)
+                    )
+                ),
+                1000.0
+            )
+        )
 
         // when
-        val actual = Referee(dealerScore, userScore).getResult()
+        val actual = Referee(dealerScore).getResult(users)
         val expected = listOf<GameResult>(GameResult.WIN, GameResult.WIN)
 
         // then
@@ -33,13 +78,35 @@ class RefereeTest {
     }
 
     @Test
-    fun `딜러는 22점, 유저1, 유저2의 점수는 119, 15점이다`() {
+    fun `딜러는 22점, 유저1, 유저2의 점수는 23, 15점이다`() {
         // given
         val dealerScore = Score.valueOf(22)
-        val userScore = listOf(Score.valueOf(119), Score.valueOf(15))
+        val users = listOf<User>(
+            User(
+                "유저1",
+                Cards(
+                    listOf(
+                        Card(CardShape.DIAMONDS, CardValue.TEN),
+                        Card(CardShape.DIAMONDS, CardValue.TEN),
+                        Card(CardShape.DIAMONDS, CardValue.THREE),
+                    )
+                ),
+                1000.0
+            ),
+            User(
+                "유저2",
+                Cards(
+                    listOf(
+                        Card(CardShape.DIAMONDS, CardValue.TEN),
+                        Card(CardShape.DIAMONDS, CardValue.FIVE)
+                    )
+                ),
+                1000.0
+            )
+        )
 
         // when
-        val actual = Referee(dealerScore, userScore).getResult()
+        val actual = Referee(dealerScore).getResult(users)
         val expected = listOf<GameResult>(GameResult.DRAW, GameResult.WIN)
 
         // then
