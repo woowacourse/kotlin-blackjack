5d875e9285003dec39549d6824b213fd003a3796
pingu244 <69796976+pingu244@users.noreply.github.com>
2023-03-08 17:04:55 +0900

    refactor: 블랙잭으로 승리하는 경우 추가

--- src/main/kotlin/domain/judge/Result.kt
+++ src/main/kotlin/domain/judge/Result.kt
@@ -1,13 +1,15 @@
 package domain.judge
 
-enum class Result(val result: String) {
-    WIN("승"),
-    DRAW("무"),
-    LOSS("패");
+enum class Result(val result: String, val revenue: Double) {
+    WIN("승", 1.0),
+    DRAW("무", 0.0),
+    LOSS("패", -1.0),
+    BLACKJACK_WIN("승", 1.5);
 
     fun reverseResult(): Result = when (this) {
         WIN -> LOSS
         LOSS -> WIN
+        BLACKJACK_WIN -> LOSS
         else -> DRAW
     }
 }
