cb3f13cc480a220b4394531950f2f29cfdd86985
Doyeop Kim <dygames34@gmail.com>
2023-03-08 22:23:28 +0900

    refactor: Money 연산자 오버로딩 구현

--- src/main/kotlin/entity/Money.kt
+++ src/main/kotlin/entity/Money.kt
@@ -1,4 +1,7 @@
 package entity
 
 @JvmInline
-value class Money(val value: Int)
+value class Money(val value: Int) {
+    operator fun times(other: Double): Money = Money((value * other).toInt())
+    operator fun times(other: Int): Money = Money(value * other)
+}
