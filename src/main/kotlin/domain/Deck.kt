a943f4df7d078ac64c8aa3ac8305d5dc9f5b4725
s9hn <tnghqn@naver.com>
2023-03-08 19:02:20 +0900

    [REFACTOR(Deck)] 방어적복사구현, isNotEmpty메소드 활용

--- src/main/kotlin/domain/Deck.kt
+++ src/main/kotlin/domain/Deck.kt
@@ -2,11 +2,13 @@ package domain
 
 import domain.card.Card
 
-class Deck(private val _cards: MutableList<Card>) {
-    val cards: List<Card> get() = _cards.toList()
+class Deck(_cards: List<Card>) {
+    private val _cards: MutableList<Card> = _cards.toMutableList()
+    val cards: List<Card>
+        get() = _cards.toList()
 
     init {
-        require(_cards.size != EMPTY) { ERROR_EMPTY_DECK }
+        require(_cards.isNotEmpty()) { ERROR_EMPTY_DECK }
     }
 
     fun addCard(card: List<Card>) {
@@ -15,6 +17,5 @@ class Deck(private val _cards: MutableList<Card>) {
 
     companion object {
         private const val ERROR_EMPTY_DECK = "[ERROR] 아무런 카드가 없습니다."
-        private const val EMPTY = 0
     }
 }
