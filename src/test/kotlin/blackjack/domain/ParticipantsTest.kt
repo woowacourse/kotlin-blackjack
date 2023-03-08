c5a21eef67f2027eed3caf74377f19e33d727e32
changhwan <chang5hustle@gmail.com>
2023-03-08 22:49:18 +0900

    test(ParticipantsTest): 딜러 최초 분배카드 합 16에 따른 카드 분배 시나리오 테스트

--- src/test/kotlin/blackjack/domain/ParticipantsTest.kt
+++ src/test/kotlin/blackjack/domain/ParticipantsTest.kt
@@ -27,6 +27,31 @@ class ParticipantsTest {
             ),
         )
     }
+
+    @Test
+    fun `딜러의 첫카드 2장의 합이 16 이하라면 카드한장을 더뽑는다`() {
+        val participants = Participants(listOf("krong", "dogpig"), StubAddDealerCardDeck())
+        participants.judgmentDealerAddCard()
+        assertThat(participants.dealer.cardBunch.cards).isEqualTo(
+            listOf(
+                Card(HEART, CardNumber.TWO),
+                Card(HEART, CardNumber.THREE),
+                Card(HEART, CardNumber.FIVE),
+            ),
+        )
+    }
+
+    @Test
+    fun `딜러의 첫카드 2장의 합이 16 이상이라면 카드한장를 뽑지 않는다`() {
+        val participants = Participants(listOf("krong", "dogpig"), StubAddDealerCardDeck2())
+        participants.judgmentDealerAddCard()
+        assertThat(participants.dealer.cardBunch.cards).isEqualTo(
+            listOf(
+                Card(HEART, CardNumber.JACK),
+                Card(HEART, CardNumber.SEVEN)
+            ),
+        )
+    }
 }
 
 class StubAddPlayerCardDeck : CardDeck {
@@ -35,13 +60,53 @@ class StubAddPlayerCardDeck : CardDeck {
         Card(HEART, CardNumber.TWO),
         Card(HEART, CardNumber.THREE),
         // 플레이어 두명 초기화
+        // 첫번째 유저
         Card(HEART, CardNumber.TWO),
         Card(HEART, CardNumber.FOUR),
+        // 두번째 유저
         Card(HEART, CardNumber.FIVE),
         Card(HEART, CardNumber.SIX),
         // 플레이어 별로 한장씩 추가
         Card(HEART, CardNumber.FIVE),
         Card(HEART, CardNumber.SIX),
+    )
+
+    override fun drawCard(): Card = cardDeck.removeFirst()
+}
+
+class StubAddDealerCardDeck : CardDeck {
+    private val cardDeck = mutableListOf(
+        // 딜러 초기화
+        Card(HEART, CardNumber.TWO),
+        Card(HEART, CardNumber.THREE),
+        // 플레이어 두명 초기화
+        // 첫번째 유저
+        Card(HEART, CardNumber.TWO),
+        Card(HEART, CardNumber.FOUR),
+        // 두번째 유저
+        Card(HEART, CardNumber.FIVE),
+        Card(HEART, CardNumber.SIX),
+        // 추가 딜러가 받는카드
+        Card(HEART, CardNumber.FIVE),
+    )
+
+    override fun drawCard(): Card = cardDeck.removeFirst()
+}
+
+class StubAddDealerCardDeck2 : CardDeck {
+    private val cardDeck = mutableListOf(
+        // 딜러 초기화
+        Card(HEART, CardNumber.JACK),
+        Card(HEART, CardNumber.SEVEN),
+        // 플레이어 두명 초기화
+        // 첫번째 유저
+        Card(HEART, CardNumber.TWO),
+        Card(HEART, CardNumber.FOUR),
+        // 두번째 유저
+        Card(HEART, CardNumber.FIVE),
+        Card(HEART, CardNumber.SIX),
+        // 추가 딜러가 받는카드
+        Card(HEART, CardNumber.FIVE),
     )
 
     override fun drawCard(): Card = cardDeck.removeFirst()
