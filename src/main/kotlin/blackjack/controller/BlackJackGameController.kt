8289531dddf354e679853f446d1d109179ff489a
boogieWoogie <kimjinuk1999@gmail.com>
2023-03-08 17:00:32 +0900

    refactor: 컨트롤러 코드 정리하기

--- src/main/kotlin/blackjack/controller/BlackJackGameController.kt
+++ src/main/kotlin/blackjack/controller/BlackJackGameController.kt
@@ -7,36 +7,31 @@ import blackjack.view.OutputView
 class BlackJackGameController(
     private val blackJackGame: BlackJackGame = BlackJackGame(),
 ) {
-    fun start(){
+
+    fun run(){
+        initPlayers()
+        drawAdditionalCards()
+        checkResult()
+    }
+
+    private fun initPlayers() {
         val playersName = InputView.requestPlayersName()
         val battingMoneys = InputView.requestPlayersBattingMoney(playersName)
 
-        blackJackGame.initPlayers(
-           playerNames = playersName,
-            battingMoneys = battingMoneys
-        )
+        blackJackGame.initPlayers(playersName, battingMoneys)
+        OutputView.printCardDividingMessage(blackJackGame.dealer, blackJackGame.players)
     }
 
-    fun run() {
-        val playerNames = InputView.requestPlayersName()
-        val battingMoneys = InputView.requestPlayersBattingMoney(playerNames)
-
-        blackJackGame.initPlayers(
-            playerNames,
-            battingMoneys,
-        )
-
-        OutputView.printCardDividingMessage(blackJackGame.dealer, blackJackGame.players)
-
+    private fun drawAdditionalCards() {
         blackJackGame.drawAdditionalCardsForPlayers(
             isPlayerWantedAdditionalCards = InputView::requestAdditionalDraw,
             checkCurrentCards = OutputView::printPlayerCurrentCards
         )
-
         OutputView.printIsDealerReceivedCard(blackJackGame.drawAdditionalCardsForDealer())
+    }
 
+    private fun checkResult() {
         OutputView.printFinalCards(blackJackGame.dealer, blackJackGame.players)
-
         OutputView.printGameResults(blackJackGame.judgeGameResults())
     }
 }
