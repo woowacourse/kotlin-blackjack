d419bc1980efd79c0a66aecf4409864419fb3e44
hyemdooly <hyemdooly@gmail.com>
2023-03-09 15:26:46 +0900

    refactor(Cards): Cards 구현

--- src/main/kotlin/model/CardPack.kt
+++ src/main/kotlin/model/CardPack.kt
@@ -1,26 +1,16 @@
 package model
 
-import java.util.LinkedList
-
-class CardPack(private val cards: LinkedList<Card>) {
+class CardPack(private val cards: Cards) {
     val size
         get() = cards.size
 
-    constructor(cards: List<Card>) : this(LinkedList(cards))
+    constructor(cards: List<Card>) : this(Cards(cards))
 
-    init {
-        require(cards.distinct().size == cards.size) { CARD_DUPLICATE_ERROR }
-    }
+    fun pop(): Card = cards.pop()
 
-    fun pop(): Card {
-        require(cards.isNotEmpty()) { OUT_OF_INDEX_CARDS_CURSOR }
-        return cards.pop()
-    }
-
-    fun shuffled() = CardPack(cards.shuffled())
+    fun shuffled() = CardPack(cards.toList().shuffled())
 
     companion object {
         private const val CARD_DUPLICATE_ERROR = "카드는 중복될 수 없습니다."
-        private const val OUT_OF_INDEX_CARDS_CURSOR = "카드를 모두 사용했습니다."
     }
 }
--- src/main/kotlin/model/Hand.kt
+++ src/main/kotlin/model/Hand.kt
@@ -1,37 +1,18 @@
 package model
 
-import java.util.LinkedList
-
-data class Hand(private val cards: LinkedList<Card>) {
+data class Hand(private val cards: Cards) {
     val size
         get() = cards.size
 
-    constructor(cards: List<Card>) : this(LinkedList(cards))
-
-    init {
-        require(cards.distinct().size == cards.size) { CARD_DUPLICATE_ERROR }
-    }
+    constructor(cards: List<Card>) : this(Cards(cards))
 
     fun toList() = cards.toList()
 
     fun add(card: Card) {
-        require(!cards.contains(card)) { CARD_DUPLICATE_ERROR }
         cards.add(card)
     }
 
-    fun sum(): Int {
-        var sum = filterSum { it != Rank.ACE }
-        sum = filterSum(sum) { it == Rank.ACE }
-        return sum
-    }
-
-    private fun filterSum(score: Int = 0, condition: (Rank) -> Boolean): Int {
-        var sum = score
-        cards.filter { condition(it.rank) }.forEach {
-            sum += it.rank.getScore(sum)
-        }
-        return sum
-    }
+    fun sum(): Int = cards.sum()
 
     companion object {
         private const val CARD_DUPLICATE_ERROR = "카드는 중복될 수 없습니다."
