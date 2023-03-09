2ca1467397daab486a0f7d8036e1c2bc6a6252f2
tmdgh1592 <tmdgh1592@naver.com>
2023-03-09 16:07:57 +0900

    test(카드 모양): 카드 모양 테스트 코드에서 인자 변경

--- src/test/kotlin/blackjack/domain/SuitTest.kt
+++ src/test/kotlin/blackjack/domain/SuitTest.kt
@@ -7,8 +7,8 @@ import org.junit.jupiter.params.provider.CsvSource
 class SuitTest {
 
     @ParameterizedTest
-    @CsvSource("스페이드,스페이드", "다이아몬드,다이아몬드", "하트,하트", "클로버,클로버")
-    fun `카드 모양에는 스페이드, 다이아몬드, 하트, 클로버가 있다`(suit: Suit, expected: String) {
+    @CsvSource("SPADE, SPADE", "HEART, HEART", "DIAMOND, DIAMOND", "CLOVER, CLOVER")
+    fun `카드 모양에는 스페이드, 하트, 다이아몬드, 클로버가 있다`(suit: Suit, expected: String) {
         assertThat(suit.name).isEqualTo(expected)
     }
 }
