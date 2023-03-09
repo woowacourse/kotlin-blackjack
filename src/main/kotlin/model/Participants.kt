c5f4359612cc07ee08a6ace722149b1b1f04992e
hyemdooly <hyemdooly@gmail.com>
2023-03-09 14:28:00 +0900

    refactor(Participants): 프로퍼티 변경

--- src/main/kotlin/model/Participants.kt
+++ src/main/kotlin/model/Participants.kt
@@ -1,7 +1,5 @@
 package model
-data class Participants(private val participants: List<Participant>) {
-    fun toList(): List<Participant> = participants.toList()
-    companion object {
-        fun of(dealer: Dealer, players: Players) = Participants(listOf(dealer as Participant) + players.toList() as List<Participant>)
-    }
+data class Participants(val dealer: Dealer, val players: Players) {
+
+    fun toList(): List<Participant> = listOf(dealer as Participant) + players.toList() as List<Participant>
 }
