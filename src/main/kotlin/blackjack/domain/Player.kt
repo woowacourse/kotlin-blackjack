800dee5a0539ec247a8301d797dfc6517f7a81c4
tmdgh1592 <tmdgh1592@naver.com>
2023-03-09 16:09:06 +0900

    refactor: 플레이어가 카드를 뽑을 것인지 판단하는 콜백의 기본값 설정

--- src/main/kotlin/blackjack/domain/Player.kt
+++ src/main/kotlin/blackjack/domain/Player.kt
@@ -1,6 +1,6 @@
 package blackjack.domain
 
-class Player(name: String, private val needToDraw: () -> Boolean) : Participant(name) {
+class Player(name: String, private val needToDraw: () -> Boolean = { true }) : Participant(name) {
     override fun getFirstOpenCards(): List<Card> = getCards()
 
     override fun canDraw(): Boolean = !isBust() && needToDraw()
