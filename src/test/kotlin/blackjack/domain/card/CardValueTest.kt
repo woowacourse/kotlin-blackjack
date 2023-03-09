7902e0a0fecd6b43d3ae1dde6be9f9acab5a376b
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-03-09 13:46:59 +0900

    test: 카드 리스트에 ACE가 포함되어있는지 확인하는 테스트 추가

--- src/test/kotlin/blackjack/domain/card/CardValueTest.kt
+++ src/test/kotlin/blackjack/domain/card/CardValueTest.kt
@@ -1,6 +1,7 @@
 package blackjack.domain.card
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
@@ -9,5 +10,17 @@ class CardValueTest {
     @CsvSource(value = ["ACE,1", "KING,10", "QUEEN,10", "JACK,10", "TEN,10", "NINE,9", "EIGHT,8", "SEVEN,7", "SIX,6", "FIVE,5", "FOUR,4", "THREE,3", "TWO,2"])
     fun `카드의 값들이 나온다`(cardValue: CardValue, value: Int) {
         assertThat(cardValue.value).isEqualTo(value)
+    }
+
+    @Test
+    fun `카드리스트들에 ACE 카드가 있으면 true가 반환된다`() {
+        val cardValues = listOf(CardValue.TEN, CardValue.EIGHT, CardValue.ACE)
+        assertThat(CardValue.containsAce(cardValues)).isTrue()
+    }
+
+    @Test
+    fun `카드리스트들에 ACE 카드가 있으면 false가 반환된다`() {
+        val cardValues = listOf(CardValue.TEN, CardValue.EIGHT, CardValue.ACE)
+        assertThat(CardValue.containsAce(cardValues)).isFalse()
     }
 }
