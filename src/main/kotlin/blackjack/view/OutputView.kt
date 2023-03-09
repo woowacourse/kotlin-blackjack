1bb65e1c5495eddbbfed9d8b364c27cd89738e2f
whk06061 <gju06061@naver.com>
2023-03-09 15:54:32 +0900

    refactor: 불필요한 코드 제거 및 함수 분리

--- src/main/kotlin/blackjack/view/OutputView.kt
+++ src/main/kotlin/blackjack/view/OutputView.kt
@@ -2,9 +2,11 @@ package blackjack.view
 
 import blackjack.domain.Result
 import blackjack.domain.player.Dealer
+import blackjack.domain.player.DealerResult
 import blackjack.domain.player.Participant
 import blackjack.domain.player.Participants
 import blackjack.domain.player.Player
+import blackjack.domain.player.PlayersResults
 
 class OutputView {
 
@@ -22,11 +24,8 @@ class OutputView {
         println(DEALER_HIT_CARD_MENT)
     }
 
-    fun printDealerNotHitCardMent() {
-        println(DEALER_NOT_HIT_CARD_MENT)
-    }
-
     fun printSumResult(dealer: Dealer, participants: Participants) {
+        println()
         printPlayerCardsSumResult(dealer)
         println(SUM_RESULT_MENT.format(dealer.cards.sumCardsNumber()))
         participants.values.forEach {
@@ -36,16 +35,25 @@ class OutputView {
         }
     }
 
-    fun printFinalResult(dealer: Dealer, participants: Participants) {
+    fun printFinalResult(playersResults: PlayersResults, dealerResult: DealerResult) {
+        println()
         println(FINAL_RESULT_MENT)
-        var dealerResultMessage = FINAL_DEALER_RESULT_MENT
-        if (dealer.results[Result.WIN] != 0) dealerResultMessage += "${dealer.results[Result.WIN]}${Result.WIN.word} "
-        if (dealer.results[Result.LOSE] != 0) dealerResultMessage += "${dealer.results[Result.LOSE]}${Result.LOSE.word} "
-        if (dealer.results[Result.DRAW] != 0) dealerResultMessage += "${dealer.results[Result.DRAW]}${Result.DRAW.word} "
+        var dealerResultMessage = FINAL_DEALER_RESULT_MENT + printDealerFinalResut(dealerResult)
         println(dealerResultMessage)
-        participants.values.forEach {
-            println(FINAL_PARTICIPANT_RESULT_MENT.format(it.name, it.result.word))
+        playersResults.results.forEach {
+            println(FINAL_PARTICIPANT_RESULT_MENT.format(it.result.first, it.result.second.word))
         }
+    }
+
+    private fun printDealerFinalResut(dealerResult: DealerResult): String {
+        var dealerResultMessage = ""
+        val dealerWinCount = dealerResult.results.count { it == Result.WIN }
+        val dealerLoseCount = dealerResult.results.count { it == Result.LOSE }
+        val dealerDrawCount = dealerResult.results.count { it == Result.DRAW }
+        if (dealerWinCount != 0) dealerResultMessage += "${dealerWinCount}${Result.WIN.word} "
+        if (dealerLoseCount != 0) dealerResultMessage += "${dealerLoseCount}${Result.LOSE.word} "
+        if (dealerDrawCount != 0) dealerResultMessage += "${dealerDrawCount}${Result.DRAW.word} "
+        return dealerResultMessage
     }
 
     fun printParticipantCards(participant: Participant) {
@@ -63,6 +71,7 @@ class OutputView {
         val dealerCard = dealer.cards.values[0]
         print(DEALER_CARD_MENT.format(dealerCard.number.word + dealerCard.shape.word))
     }
+
     private fun printPlayerCardsSumResult(player: Player) {
         val cardsWord: String = player.cards.values.map {
             it.number.word + it.shape.word
@@ -74,7 +83,6 @@ class OutputView {
         private const val SETTING_CARD_MENT = "딜러와 %s에게 2장의 카드를 나누었습니다."
         private const val DEALER_CARD_MENT = "딜러 카드: %s"
         private const val DEALER_HIT_CARD_MENT = "딜러는 16 이하라 한장의 카드를 더 받았습니다."
-        private const val DEALER_NOT_HIT_CARD_MENT = "딜러는 16 초과라 한장의 카드를 더 받지않았습니다."
         private const val SUM_RESULT_MENT = " - 결과: %d"
         private const val FINAL_RESULT_MENT = "## 최종 승패"
         private const val FINAL_DEALER_RESULT_MENT = "딜러: "
