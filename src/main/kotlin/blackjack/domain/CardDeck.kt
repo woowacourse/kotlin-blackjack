b856fda0bcf5f24f7f103a1892ed170f75e541b0
tmdgh1592 <tmdgh1592@naver.com>
2023-03-09 16:03:52 +0900

    refactor: 카드덱에 가변인자를 전달받는 부생성자 추가

--- src/main/kotlin/blackjack/domain/CardDeck.kt
+++ src/main/kotlin/blackjack/domain/CardDeck.kt
@@ -1,6 +1,8 @@
 package blackjack.domain
 
 class CardDeck(deck: List<Card> = Card.all().shuffled()) {
+    constructor(vararg cards: Card) : this(cards.toList())
+
     private val deck: MutableList<Card> = deck.toMutableList()
 
     fun draw(): Card = deck.removeFirst()
