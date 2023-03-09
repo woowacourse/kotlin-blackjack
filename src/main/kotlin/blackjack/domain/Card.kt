ed332f209139a0d5e9201113eea82757f39ab3be
changhwan <chang5hustle@gmail.com>
2023-03-09 15:36:42 +0900

    refactor(card,cardbunch): 카드가 Ace인지 판단 로직 card로 이동

--- src/main/kotlin/blackjack/domain/Card.kt
+++ src/main/kotlin/blackjack/domain/Card.kt
@@ -2,4 +2,6 @@ package blackjack.domain
 
 import blackjack.Shape
 
-data class Card(val shape: Shape, val cardNumber: CardNumber)
+data class Card(val shape: Shape, val cardNumber: CardNumber) {
+    fun isAce(): Boolean = cardNumber == CardNumber.ACE
+}
--- src/main/kotlin/blackjack/domain/CardBunch.kt
+++ src/main/kotlin/blackjack/domain/CardBunch.kt
@@ -31,11 +31,7 @@ class CardBunch private constructor(cards: MutableList<Card>) {
 
     fun isBlackJack(): Boolean = cards.size == 2 && getTotalScore() == 21
 
-    private fun containsAce(): Boolean {
-        var result = false
-        cards.forEach { if (it.cardNumber == CardNumber.ACE) result = true }
-        return result
-    }
+    private fun containsAce(): Boolean = cards.map { card -> card.isAce() }.reduce { acc, isAce -> acc && isAce }
 
     companion object {
         private const val ACE_SCORE_GAP = 10
