e16294abf9a53e319e33d579d3a7614e099c9d46
hyunji1203 <hj27720@gmail.com>
2023-03-10 00:45:45 +0900

    feat: 최종 수익 출력 기능 추가

--- src/main/kotlin/view/ResultView.kt
+++ src/main/kotlin/view/ResultView.kt
@@ -2,6 +2,7 @@ package view
 
 import entity.Dealer
 import entity.DealerGameResult
+import entity.Player
 import entity.Players
 import entity.PlayersGameResult
 import view.ViewUtils.Companion.isString
@@ -28,6 +29,14 @@ class ResultView {
         }
     }
 
+    fun printUserBettingResult(dealerBattingResult: Double, playersBattingResult: MutableMap<Player, Double>) {
+        println(MESSAGE_TOTAL_PROFIT_MONEY)
+        println(MESSAGE_DEALER_BETTING_RESULT)
+        playersBattingResult.forEach { (name, result) ->
+            println(MESSAGE_PLAYERS_BETTING_RESULT.format(name, result))
+        }
+    }
+
     companion object {
         private const val MESSAGE_DEALER_GAME_STATUS = "딜러 카드: %s - 결과: %d"
         private const val MESSAGE_PLAYERS_GAME_STATUS = "%s카드: %s - 결과: %d"
@@ -35,5 +44,8 @@ class ResultView {
         private const val MESSAGE_DEALER_GAME_RESULT = "딜러: %s"
         private const val MESSAGE_PLAYERS_GAME_RESULT = "%s: %s"
         private const val MESSAGE_GAME_RESULT_TYPE = "%d%s"
+        private const val MESSAGE_TOTAL_PROFIT_MONEY = "##최종 수익"
+        private const val MESSAGE_DEALER_BETTING_RESULT = "딜러: %d"
+        private const val MESSAGE_PLAYERS_BETTING_RESULT = "%s: %d"
     }
 }
