81b425b3caf21f0f4c9e564e3f38cce605276cfe
krrong <khk9664@naver.com>
2023-03-10 01:10:03 +0900

    feat(InputView): 베팅 금액을 입력받는 기능 구현

--- src/main/kotlin/blackjack/view/InputView.kt
+++ src/main/kotlin/blackjack/view/InputView.kt
@@ -1,10 +1,12 @@
 package blackjack.view
 
+import blackjack.domain.BettingMoney
 import blackjack.domain.Player
 
 object InputView {
     private const val GET_PLAYER_NAME_SCRIPT = "게임에 참가할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)"
     private const val GET_DECISION_SCRIPT = "%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)"
+    private const val BETTING_SCRIPT = "%s의 배팅 금액은?"
     private const val INCORRECT_DECISION_ERROR = "올바른 입력이 아닙니다(y,n)으로 입력해 주세요"
     fun getPlayerNames(): List<String> {
         return runCatching {
@@ -27,6 +29,18 @@ object InputView {
         }.getOrElse {
             println(it.message)
             getHitOrNot(player)
+        }
+    }
+
+    fun getBettingMoney(names: List<String>): List<BettingMoney> {
+        return runCatching {
+            names.map { name ->
+                println(BETTING_SCRIPT.format(name))
+                BettingMoney(readln().toInt())
+            }
+        }.getOrElse {
+            println(it.message)
+            getBettingMoney(names)
         }
     }
 }
