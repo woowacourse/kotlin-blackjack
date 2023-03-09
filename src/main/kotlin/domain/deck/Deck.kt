ba073aaf952d7f8138204697146de152c0ae9125
rhkrwngud445 <rhkrwngud445@gmail.com>
2023-03-09 12:45:16 +0900

    refactor: Card 리스트를 미리 만들어두고 재활용 할 수 있게 수정

--- src/main/kotlin/domain/deck/Deck.kt
+++ src/main/kotlin/domain/deck/Deck.kt
@@ -1,11 +1,10 @@
 package domain.deck
 
 import domain.card.Card
-import domain.card.CardMaker
 
 object Deck {
     private const val ERROR_EMPTY_DECK = "[ERROR] 카드가 존재하지 않습니다."
-    private var deck = CardMaker().makeCards()
+    private var deck = Card.getAllCard()
     private var cardPosition = 0
 
     fun makeDeck() {
