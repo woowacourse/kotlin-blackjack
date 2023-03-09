8d2cc63cd018c565de3571c191ade731e24b2c20
hyunji1203 <hj27720@gmail.com>
2023-03-10 01:33:01 +0900

    refactor: 상수 처리

--- src/main/kotlin/entity/UserBettingResult.kt
+++ src/main/kotlin/entity/UserBettingResult.kt
@@ -1,5 +1,7 @@
 package entity
 
+import misc.GameRule
+
 class UserBettingResult {
     val playersBettingResults = mutableMapOf<Player, Double>()
 
@@ -12,20 +14,24 @@ class UserBettingResult {
 
     private fun getPlayerBettingResult(player: Player, dealer: Dealer, playersGameResult: PlayersGameResult): Double {
         val playerBettingMoney = player.userInformation.bettingMoney.value.toDouble()
-        if (player.cards.sumOfNumbers() == 21 && player.cards.value.size == 2) return playerBettingMoney + playerBettingMoney * 1.5
-        if (playersGameResult.value[player] == GameResultType.WIN || dealer.cards.sumOfNumbers() > 21) return 2 * playerBettingMoney
-        if (player.cards.sumOfNumbers() == 21 && dealer.cards.sumOfNumbers() == 21) return playerBettingMoney
-        if (player.cards.sumOfNumbers() > 21) return -playerBettingMoney
+        if (player.cards.sumOfNumbers() == GameRule.WINNING_NUMBER && player.cards.value.size == 2) return playerBettingMoney + playerBettingMoney * 1.5
+        if (playersGameResult.value[player] == GameResultType.WIN || dealer.cards.sumOfNumbers() > GameRule.WINNING_NUMBER) return 2 * playerBettingMoney
+        if (player.cards.sumOfNumbers() == GameRule.WINNING_NUMBER && dealer.cards.sumOfNumbers() == GameRule.WINNING_NUMBER) return playerBettingMoney
+        if (player.cards.sumOfNumbers() > GameRule.WINNING_NUMBER) return -playerBettingMoney
         if (playersGameResult.value[player] == GameResultType.LOSE) return -playerBettingMoney
-        return 0.0
+        return INIT_PROFIT_MONEY
     }
 
     fun getDealerBettingResult(): Double {
-        var dealerProfitMoney = 0.0
+        var dealerProfitMoney = INIT_PROFIT_MONEY
         playersBettingResults.forEach { (_, profitMoney) ->
             dealerProfitMoney += profitMoney
         }
         dealerProfitMoney = -dealerProfitMoney
         return dealerProfitMoney
+    }
+
+    companion object {
+        private const val INIT_PROFIT_MONEY = 0.0
     }
 }
