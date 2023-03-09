4d911c1de36aca5fa894e4781f7ff7c530d36b9a
inseonyun <yuninseon@naver.com>
2023-03-09 16:29:12 +0900

    refactor(GameResult): 불필욯한 변수 생성 제거

--- src/main/kotlin/model/GameResult.kt
+++ src/main/kotlin/model/GameResult.kt
@@ -6,7 +6,6 @@ class GameResult(private val participants: Participants) {
     val loseCount: Int
         get() = getResult(Result.LOSE)
     private fun getResult(winOrLose: Result): Int {
-        val res = participants.players.count { participants.dealer.getGameResult(it) == winOrLose }
-        return res
+        return participants.players.count { participants.dealer.getGameResult(it) == winOrLose }
     }
 }
