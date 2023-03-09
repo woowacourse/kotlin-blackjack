71d66352262251ecbd1981bce08d5c122ce4f1be
vrexpert <vrexpert2302@gmail.com>
2023-03-09 16:16:22 +0900

    refactor(view): view 를 오브젝트에서 클래스로 변경

--- src/main/kotlin/view/ResultView.kt
+++ src/main/kotlin/view/ResultView.kt
@@ -2,26 +2,19 @@ package view
 
 import domain.card.Card
 import domain.constant.BlackJackConstants.DEALER_STAND_CONDITION
-import domain.person.Dealer
+import domain.person.Participants
 import domain.person.Person
-import domain.person.Player
 import domain.result.CardsScore
 import domain.result.GameResult
 import domain.result.OutCome
 
-object ResultView {
-    private const val FINAL_OUTCOME_SCRIPT = "## 최종 승패"
-    private const val DEALER_SCRIPT = "딜러: "
-    private const val SHARE_TWO_CARDS_SCRIPT = "%s와 %s에게 2장의 카드를 나누었습니다."
-    private const val INITIAL_CARDS_SCRIPT = "%s 카드: %s"
-    private const val RESULT_CARDS_SCRIPT = "%s 카드: %s - 결과: %s"
-    private const val DEALER_ONE_MORE_CARD_SCRIPT = "딜러는 ${DEALER_STAND_CONDITION}이하라 한장의 카드를 더 받았습니다."
-    private const val DEALER_NO_MORE_CARD_SCRIPT = "딜러는 ${DEALER_STAND_CONDITION}초과라 한장의 카드를 받지 않습니다."
-
+class ResultView {
     private fun cardToString(card: Card) =
         ViewUtils.cardNumberToText(card.number) + ViewUtils.cardShapeToText(card.shape)
 
-    fun printInitialSetting(players: List<Player>, dealer: Dealer) {
+    fun printInitialSetting(participants: Participants) {
+        val players = participants.players
+        val dealer = participants.dealer
         println()
         println(SHARE_TWO_CARDS_SCRIPT.format(dealer.name, players.joinToString(", ") { it.name }))
         println(INITIAL_CARDS_SCRIPT.format(dealer.name, dealer.showOneCard().joinToString { cardToString(it) }))
@@ -33,13 +26,17 @@ object ResultView {
         println(INITIAL_CARDS_SCRIPT.format(person.name, person.cards.value.joinToString(", ") { cardToString(it) }))
     }
 
-    fun printDealerGetMoreCard() = println(DEALER_ONE_MORE_CARD_SCRIPT + "\n")
+    fun printDealerGetCardOrNot(isGetCard: Boolean) {
+        if (isGetCard) {
+            println(DEALER_ONE_MORE_CARD_SCRIPT + "\n")
+            return
+        }
+        println(DEALER_NO_MORE_CARD_SCRIPT)
+    }
 
-    fun printDealerNoMoreCard() = println(DEALER_NO_MORE_CARD_SCRIPT + "\n")
-
-    fun printPersonsCardsResult(dealer: Dealer, players: List<Player>) {
-        printPersonCardsResult(dealer)
-        players.forEach { printPersonCardsResult(it) }
+    fun printPersonsCardsResult(participants: Participants) {
+        printPersonCardsResult(participants.dealer)
+        participants.players.forEach { printPersonCardsResult(it) }
         println()
     }
 
@@ -67,5 +64,15 @@ object ResultView {
 
     private fun printPlayerResult(playerResult: Map<String, OutCome>) {
         playerResult.entries.forEach { println("${it.key}: ${it.value.text}") }
+    }
+
+    companion object {
+        private const val FINAL_OUTCOME_SCRIPT = "## 최종 승패"
+        private const val DEALER_SCRIPT = "딜러: "
+        private const val SHARE_TWO_CARDS_SCRIPT = "%s와 %s에게 2장의 카드를 나누었습니다."
+        private const val INITIAL_CARDS_SCRIPT = "%s 카드: %s"
+        private const val RESULT_CARDS_SCRIPT = "%s 카드: %s - 결과: %s"
+        private const val DEALER_ONE_MORE_CARD_SCRIPT = "딜러는 ${DEALER_STAND_CONDITION}이하라 한장의 카드를 더 받았습니다."
+        private const val DEALER_NO_MORE_CARD_SCRIPT = "딜러는 ${DEALER_STAND_CONDITION}초과라 한장의 카드를 받지 않습니다."
     }
 }
