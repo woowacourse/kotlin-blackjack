fea1fe535dafcb08d4982d301fc09b174079e811
tmdgh1592 <tmdgh1592@naver.com>
2023-03-09 16:04:59 +0900

    test(카드 목록): 카드 목록 테스트 코드 추가

--- src/test/kotlin/blackjack/domain/CardsTest.kt
+++ src/test/kotlin/blackjack/domain/CardsTest.kt
@@ -5,14 +5,22 @@ import org.junit.jupiter.api.Test
 
 class CardsTest {
     @Test
-    fun `카드 목록에 카드를 추가한다`() {
+    fun `카드 목록에 카드를 한장 추가한다`() {
+        val cards = Cards()
+        cards.add(Card(CardNumber.ACE, Suit.SPADE))
+
+        assertThat(cards.items.size).isEqualTo(1)
+    }
+
+    @Test
+    fun `카드 목록에서 첫번째 카드를 반환한다`() {
         val cards = Cards(
             Card(CardNumber.ACE, Suit.SPADE),
             Card(CardNumber.THREE, Suit.SPADE),
             Card(CardNumber.FIVE, Suit.SPADE)
         )
 
-        assertThat(cards.items.size).isEqualTo(3)
+        assertThat(cards.getFirstCard()).isEqualTo(Card(CardNumber.ACE, Suit.SPADE))
     }
 
     @Test
@@ -32,5 +40,46 @@ class CardsTest {
         )
 
         assertThat(cards.calculateTotalScore()).isEqualTo(12)
+    }
+
+    @Test
+    fun `점수가 17점 이상이면 스테이다`() {
+        val cards = Cards(
+            Card(CardNumber.ACE, Suit.SPADE),
+            Card(CardNumber.SIX, Suit.SPADE)
+        )
+
+        assertThat(cards.isStay()).isTrue
+    }
+
+    @Test
+    fun `점수가 17점 미만이면 스테이가 아니다`() {
+        val cards = Cards(
+            Card(CardNumber.ACE, Suit.SPADE),
+            Card(CardNumber.FIVE, Suit.SPADE)
+        )
+
+        assertThat(cards.isStay()).isFalse
+    }
+
+    @Test
+    fun `점수가 21점을 초과하면 버스트다`() {
+        val cards = Cards(
+            Card(CardNumber.JACK, Suit.SPADE),
+            Card(CardNumber.QUEEN, Suit.SPADE),
+            Card(CardNumber.TWO, Suit.SPADE)
+        )
+
+        assertThat(cards.isOverBlackjack()).isTrue
+    }
+
+    @Test
+    fun `점수가 21점을 초과하지 않으면 버스트가 아니다`() {
+        val cards = Cards(
+            Card(CardNumber.JACK, Suit.SPADE),
+            Card(CardNumber.QUEEN, Suit.SPADE)
+        )
+
+        assertThat(cards.isOverBlackjack()).isFalse
     }
 }
