80c173b0bfb4cd51c0686051d1883a43b209bc9c
inseonyun <yuninseon@naver.com>
2023-03-10 15:44:30 +0900

    refactor(Participant): isBust 함수 수정

--- src/main/kotlin/model/Participant.kt
+++ src/main/kotlin/model/Participant.kt
@@ -8,15 +8,12 @@ abstract class Participant(val name: Name) {
     abstract fun getFirstOpenCards(): Cards
     abstract fun isPossibleDrawCard(): Boolean
     abstract fun isHit(needToDraw: (String) -> Boolean): Boolean
+    fun isBust(): Boolean = cards.sum() > PARTICIPANT_STANDARD_BUST_POINT
     fun isBlackJack(): Boolean = cards.size == 2 && cards.sum() == 21
     fun isDealer(): Boolean = name.value == DEALER
     fun drawFirst(cardDeck: CardDeck) {
         cards.add(cardDeck.drawCard())
         cards.add(cardDeck.drawCard())
-    }
-    fun isBust(): Boolean {
-        if (cards.sum() > PARTICIPANT_STANDARD_BUST_POINT) return true
-        return false
     }
     fun getGameResult(other: Participant): Result {
         if (isBlackJack() && other.isBlackJack()) return Result.DRAW
