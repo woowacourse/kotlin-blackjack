667a4756c402ef772e170d9333a10b491b6c9221
briandr97 <briandr@naver.com>
2023-03-10 16:17:13 +0900

    feat(Dummy): 테스트를 위해 더미로 카드를 만들어놓음

---
+++ src/test/kotlin/domain/Dummy.kt
@@ -0,0 +1,25 @@
+package domain
+
+import domain.card.Card
+import domain.card.CardNumber.ACE
+import domain.card.CardNumber.FIVE
+import domain.card.CardNumber.FOUR
+import domain.card.CardNumber.KING
+import domain.card.CardNumber.SIX
+import domain.card.CardNumber.TEN
+import domain.card.CardNumber.THREE
+import domain.card.CardNumber.TWO
+import domain.card.CardShape.CLOVER
+
+object Dummy {
+    val CLOVER_ACE = Card(CLOVER, ACE)
+    val HEART_ACE = Card(CLOVER, ACE)
+    val CLOVER_TWO = Card(CLOVER, TWO)
+    val HEART_TWO = Card(CLOVER, TWO)
+    val CLOVER_THREE = Card(CLOVER, THREE)
+    val CLOVER_FOUR = Card(CLOVER, FOUR)
+    val CLOVER_FIVE = Card(CLOVER, FIVE)
+    val CLOVER_SIX = Card(CLOVER, SIX)
+    val CLOVER_TEN = Card(CLOVER, TEN)
+    val CLOVER_KING = Card(CLOVER, KING)
+}
