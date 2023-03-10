58f589d73c68ab16c13ad1dfdc5408368c85659c
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-03-10 14:49:09 +0900

    test[Money]: 돈이 1000원으로 나누어떨어지는지 확인하는 테스트

--- src/test/kotlin/blackjack/domain/participants/MoneyTest.kt
+++ src/test/kotlin/blackjack/domain/participants/MoneyTest.kt
@@ -9,7 +9,7 @@ class MoneyTest {
 
     @ParameterizedTest
     @ValueSource(ints = [1000, 1000000])
-    fun `돈은 1000원 이상이다`(money: Int) {
+    fun `돈은 1000원으로 나누어떨어져야 한다`(money: Int) {
         assertDoesNotThrow {
             Money(money)
         }
