8c0da029ff283c535cbcf898876bc9052979a17a
EmilyCh0 <emily9803@ewhain.net>
2023-03-10 17:48:15 +0900

    [FEAT(UserNameContainer)] 참가자 8명 제한

--- src/main/kotlin/domain/UserNameContainer.kt
+++ src/main/kotlin/domain/UserNameContainer.kt
@@ -5,6 +5,7 @@ class UserNameContainer(val names: List<String>) {
     init {
         exceptDuplicatedName()
         exceptEmptyName()
+        require(names.size <= MAXIMUM_PLAYERS) { ERROR_TOO_MANY_PLAYERS }
     }
 
     private fun exceptDuplicatedName() {
@@ -22,6 +23,8 @@ class UserNameContainer(val names: List<String>) {
     companion object {
         private const val ERROR_DUPLICATE_USER_NAME = "[ERROR] 유저의 이름이 중복되었습니다."
         private const val ERROR_EMPTY_USER_NAME = "[ERROR] 유저의 이름이 비어있습니다."
+        private const val ERROR_TOO_MANY_PLAYERS = "[ERROR] 참가자는 최대 8명입니다."
         private const val BLANK = ""
+        private const val MAXIMUM_PLAYERS = 8
     }
 }
