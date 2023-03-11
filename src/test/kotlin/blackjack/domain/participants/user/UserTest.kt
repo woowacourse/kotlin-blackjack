8cd2df4479f6d16a620f68327df902f24e65f7a8
re4rk <37167652+re4rk@users.noreply.github.com>
2023-03-11 15:39:15 +0900

    refactor(UserTest) : 유저에만 해당하는 테스트 코드로 변경

--- src/test/kotlin/blackjack/domain/participants/user/UserTest.kt
+++ src/test/kotlin/blackjack/domain/participants/user/UserTest.kt
@@ -1,43 +1,56 @@
 package blackjack.domain.participants.user
 
-import blackjack.domain.card.Card
-import blackjack.domain.card.CardMark
-import blackjack.domain.card.CardValue
+import blackjack.domain.card.Cards
+import blackjack.domain.state.Fixtures.CLOVER_ACE
+import blackjack.domain.state.Fixtures.CLOVER_EIGHT
+import blackjack.domain.state.Fixtures.CLOVER_JACK
+import blackjack.domain.state.Fixtures.CLOVER_KING
+import blackjack.domain.state.Fixtures.CLOVER_QUEEN
+import blackjack.domain.state.Fixtures.CLOVER_SEVEN
 import blackjack.domain.state.endTurn.BlackJack
+import blackjack.domain.state.endTurn.Bust
+import blackjack.domain.state.endTurn.Stay
 import blackjack.domain.state.inTurn.Hit
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
 
 class UserTest {
     @Test
     fun `카드를 뽑을 수 있다`() {
         val user = Guest()
-        user.draw(Card(CardMark.CLOVER, CardValue.EIGHT))
+        user.draw(CLOVER_ACE)
         assertThat(user.state.size).isEqualTo(1)
     }
 
     @Test
     fun `점수의 합을 반환한다`() {
-        val user = Guest()
-        user.draw(Card(CardMark.CLOVER, CardValue.EIGHT))
-        user.draw(Card(CardMark.SPADE, CardValue.EIGHT))
-        assertThat(user.score).isEqualTo(16)
+        val user = Guest(state = Hit(Cards(CLOVER_EIGHT, CLOVER_SEVEN)))
+        assertThat(user.score).isEqualTo(15)
     }
 
     @Test
-    fun `ACE를 2장 뽑아도 히트다`() {
-        val user = Guest()
-        user.draw(Card(CardMark.CLOVER, CardValue.ACE))
-        user.draw(Card(CardMark.SPADE, CardValue.ACE))
-        println(user.state)
-        assertThat(user.state is Hit).isTrue
+    fun `유저가 히트인지 알 수 있다`() {
+        val user = Guest(state = Hit(Cards(CLOVER_ACE, CLOVER_ACE)))
+        assertThat(user.isHit).isTrue
     }
 
     @Test
-    fun `ACE한장과 10이 나오면 블랙잭이다`() {
+    fun `유저가 히트가 아닌지 알 수 있다`() {
         val user = Guest()
-        user.draw(Card(CardMark.CLOVER, CardValue.ACE))
-        user.draw(Card(CardMark.SPADE, CardValue.KING))
-        assertThat(user.state is BlackJack).isTrue
+        assertAll(
+            {
+                user.state = BlackJack(Cards(CLOVER_ACE, CLOVER_KING))
+                assertThat(user.isHit).isFalse
+            },
+            {
+                user.state = Stay(Cards(CLOVER_ACE, CLOVER_ACE))
+                assertThat(user.isHit).isFalse
+            },
+            {
+                user.state = Bust(Cards(CLOVER_KING, CLOVER_QUEEN, CLOVER_JACK))
+                assertThat(user.isHit).isFalse
+            },
+        )
     }
 }
