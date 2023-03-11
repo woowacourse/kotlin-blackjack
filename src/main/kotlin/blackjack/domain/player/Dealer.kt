1410d3ae9b7336efbdff80fb32df33285119b6a1
whk06061 <gju06061@naver.com>
2023-03-12 02:42:18 +0900

    refactor: Dealer의 승패 결과 판단 기능 삭제
    - 2단계에서는 Dealer의 승패 판단 기능이 불필요하여 삭제

--- src/main/kotlin/blackjack/domain/player/Dealer.kt
+++ src/main/kotlin/blackjack/domain/player/Dealer.kt
@@ -1,19 +1,10 @@
 package blackjack.domain.player
 
-import blackjack.domain.Result
 import blackjack.domain.card.Cards
 
 class Dealer(name: String = "딜러", cards: Cards = Cards()) : Player(name, cards) {
 
     override fun checkProvideCardPossible(): Boolean = (cards.calculateScore() <= PARTICIPANT_MORE_CARD_CRITERIA)
-
-    fun calculateResults(participantsResults: ParticipantsResults): DealerResult {
-        val results: MutableList<Result> = mutableListOf()
-        participantsResults.results.map { participantResult ->
-            results.add(participantResult.result.reverse())
-        }
-        return DealerResult(results)
-    }
 
     companion object {
         const val PARTICIPANT_MORE_CARD_CRITERIA = 16
--- src/test/kotlin/blackjack/domain/player/DealerTest.kt
+++ src/test/kotlin/blackjack/domain/player/DealerTest.kt
@@ -1,6 +1,5 @@
 package blackjack.domain.player
 
-import blackjack.domain.Result
 import blackjack.domain.card.Card
 import blackjack.domain.card.CardNumber
 import blackjack.domain.card.CardShape
@@ -20,45 +19,5 @@ class DealerTest {
 
         // then
         assertThat(actual).isTrue
-    }
-
-    @Test
-    fun `플레이어들의 승패가 1패 1승일때, 딜러의 승패를 계산하면, 1승 1패이다`() {
-
-        // given
-        val participantsResults =
-            ParticipantsResults(listOf(ParticipantResult("aaa", Result.LOSE), ParticipantResult("bbb", Result.WIN)))
-
-        // when
-        val actual: DealerResult = Dealer().calculateResults(participantsResults)
-
-        // then
-        assertThat(actual).isEqualTo(DealerResult(listOf(Result.WIN, Result.LOSE)))
-    }
-
-    @Test
-    fun `플레이어들의 승패가 2승일 때, 딜러의 승패를 계산하면, 2패이다`() {
-        // given
-        val participantsResults =
-            ParticipantsResults(listOf(ParticipantResult("aaa", Result.WIN), ParticipantResult("bbb", Result.WIN)))
-
-        // when
-        val actual: DealerResult = Dealer().calculateResults(participantsResults)
-
-        // then
-        assertThat(actual).isEqualTo(DealerResult(listOf(Result.LOSE, Result.LOSE)))
-    }
-
-    @Test
-    fun `플레이어들의 승패가 1무 1승일때, 딜러의 승패를 계산하면, 1무 1패이다`() {
-        // given
-        val participantsResults =
-            ParticipantsResults(listOf(ParticipantResult("aaa", Result.DRAW), ParticipantResult("bbb", Result.WIN)))
-
-        // when
-        val actual: DealerResult = Dealer().calculateResults(participantsResults)
-
-        // then
-        assertThat(actual).isEqualTo(DealerResult(listOf(Result.DRAW, Result.LOSE)))
     }
 }
