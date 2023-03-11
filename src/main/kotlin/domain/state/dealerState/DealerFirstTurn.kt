67de9a5cf48f39862e24c21263747f42d1a72e98
vrexpert <vrexpert2302@gmail.com>
2023-03-11 21:03:38 +0900

    feat(dealerState): DealerFirstTurn 상태 구현 및 테스트

---
+++ src/main/kotlin/domain/state/dealerState/DealerFirstTurn.kt
@@ -0,0 +1,23 @@
+package domain.state.dealerState
+
+import domain.Score
+import domain.card.Card
+import domain.card.Hand
+import domain.constant.BlackJackConstants.BLACK_JACK_NUMBER
+import domain.constant.BlackJackConstants.DEALER_STAND_CONDITION
+import domain.state.BlackJack
+import domain.state.Started
+import domain.state.State
+import domain.state.Stay
+
+class DealerFirstTurn(value: Hand) : Started(value) {
+    override fun draw(card: Card): State {
+        val score = Score.of(hand.apply { add(card) })
+        return when {
+            hand.value.size == 1 -> DealerFirstTurn(hand)
+            score.isSame(BLACK_JACK_NUMBER) -> BlackJack(hand)
+            score.isBiggerThan(DEALER_STAND_CONDITION) -> Stay(hand)
+            else -> DealerHit(hand)
+        }
+    }
+}
