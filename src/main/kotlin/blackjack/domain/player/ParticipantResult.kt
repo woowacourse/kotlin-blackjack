c621d1628b5a2f78da89effe73631d52a6d16407
whk06061 <gju06061@naver.com>
2023-03-12 02:13:44 +0900

    refactor: 불필요한 파일 삭제

--- src/main/kotlin/blackjack/domain/player/ParticipantResult.kt
+++
@@ -1,5 +0,0 @@
-package blackjack.domain.player
-
-import blackjack.domain.Result
-
-data class ParticipantResult(val name: String, val result: Result)
--- src/main/kotlin/blackjack/domain/player/PlayersResult.kt
+++
@@ -1,3 +0,0 @@
-package blackjack.domain.player
-
-data class PlayersResult(val dealerResult: DealerResult, val participantsResults: ParticipantsResults)
