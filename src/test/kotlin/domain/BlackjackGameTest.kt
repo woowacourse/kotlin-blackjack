bf40c2c802ecef2478a7de7836b46027b39db40c
pingu244 <69796976+pingu244@users.noreply.github.com>
2023-03-11 22:21:45 +0900

    refactor: 직접적인 값을 비교하도록 테스트 수정

--- src/test/kotlin/domain/BlackjackGameTest.kt
+++ src/test/kotlin/domain/BlackjackGameTest.kt
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.Test
 
 class BlackjackGameTest {
 
-    val deck = Deck(
+    private val deck = Deck(
         listOf(
             Card(Shape.SPADE, CardValue.QUEEN),
             Card(Shape.SPADE, CardValue.KING),
@@ -21,7 +21,7 @@ class BlackjackGameTest {
             Card(Shape.SPADE, CardValue.TWO)
         )
     )
-    val blackjackGame = BlackjackGame(Names(listOf("pingu")), listOf(0), deck)
+    private val blackjackGame = BlackjackGame(Names(listOf("pingu")), listOf(0), deck)
 
     @Test
     fun `입력값이 false라면 카드를 뽑지않는다`() {
@@ -50,11 +50,12 @@ class BlackjackGameTest {
     fun `딜러가 16이하의 값을 가졌다면 16을 넘을 때까지 계속 카드를 뽑는다`() {
         blackjackGame.play({ false }, {}, {})
         val expect = mutableListOf(
-            Card(Shape.DIAMOND, CardValue.TEN),
-            Card(Shape.CLOVER, CardValue.JACK),
-            Card(Shape.DIAMOND, CardValue.QUEEN)
+            Card(Shape.SPADE, CardValue.TWO),
+            Card(Shape.HEART, CardValue.THREE),
+            Card(Shape.DIAMOND, CardValue.QUEEN),
+            Card(Shape.SPADE, CardValue.KING)
         )
 
-        assertThat(blackjackGame.dealer.ownCards.calculateCardSum()).isEqualTo(25)
+        assertThat(blackjackGame.dealer.ownCards.cards).isEqualTo(expect)
     }
 }
