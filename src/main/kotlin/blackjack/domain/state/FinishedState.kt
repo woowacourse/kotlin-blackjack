83aba923865dfb702e5051019e05ec5e18b5ab1c
tmdgh1592 <tmdgh1592@naver.com>
2023-03-12 21:04:26 +0900

    refactor: 더 이상 카드를 뽑을 수 없는 상태에서 카드를 뽑거나 스테이시 자기 자신을 반환하도록 변경

--- src/main/kotlin/blackjack/domain/state/FinishedState.kt
+++ src/main/kotlin/blackjack/domain/state/FinishedState.kt
@@ -11,13 +11,9 @@ abstract class FinishedState(protected val cards: Cards) : CardState {
         check(cards.isGreaterOrEqualsCardSize(MINIMUM_CARDS_SIZE)) { "카드를 최소 2장 보유해야 합니다." }
     }
 
-    override fun draw(card: Card): CardState {
-        throw IllegalStateException("더 이상 카드를 뽑을 수 없습니다.")
-    }
+    override fun draw(card: Card): CardState = this
 
-    override fun stay(): CardState {
-        throw IllegalStateException("게임이 종료되어 스테이할 수 없습니다.")
-    }
+    override fun stay(): CardState = this
 
     override fun getAllCards(): List<Card> = cards.items
 
