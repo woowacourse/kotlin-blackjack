979f9001b5c8ddfd721097e5d7f1065668001276
Sujin Kim <sujinkim919@gmail.com>
2023-03-13 04:30:19 +0900

    refactor: 수익금 관련 비율 상수화

--- src/main/kotlin/blackjack/domain/betting/BettingResult.kt
+++ src/main/kotlin/blackjack/domain/betting/BettingResult.kt
@@ -5,11 +5,15 @@ import blackjack.domain.result.BlackjackResult
 import blackjack.domain.result.ResultType
 
 class BettingResult
-    private constructor(private val bettingResult: Map<Player, Int>) {
+private constructor(private val bettingResult: Map<Player, Int>) {
     fun getPlayerEarningMoney(player: Player) = bettingResult[player]
     fun getDealerEarningMoney(): Int = -bettingResult.values.sum()
 
     companion object {
+        private const val BLACKJACK_EARNING_RATE = 1.5
+        private const val TIE_EARNING_RATE = 1.0
+        private const val LOSE_EARNING_RATE = -1.0
+
         fun of(players: List<Player>, gameResult: BlackjackResult): BettingResult {
             val results = mutableMapOf<Player, Int>()
 
@@ -23,9 +27,9 @@ class BettingResult
 
         private fun getEarningMoney(money: BettingMoney, resultType: ResultType?): Int {
             return when (resultType) {
-                ResultType.WIN -> money * 1.5
-                ResultType.TIE -> money * 1.0
-                ResultType.LOSE -> money * -1.0
+                ResultType.WIN -> money * BLACKJACK_EARNING_RATE
+                ResultType.TIE -> money * TIE_EARNING_RATE
+                ResultType.LOSE -> money * LOSE_EARNING_RATE
                 null -> 0
             }
         }
