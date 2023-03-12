cf5c16f51d39061262f18dd7671718842416fafe
tmdgh1592 <tmdgh1592@naver.com>
2023-03-13 02:01:20 +0900

    refactor: 콜백 대신 블랙잭 이벤트 리스너를 통해 전달하도록 변경

--- src/main/kotlin/blackjack/domain/Blackjack.kt
+++ src/main/kotlin/blackjack/domain/Blackjack.kt
@@ -4,23 +4,19 @@ import blackjack.domain.card.CardDeck
 import blackjack.domain.participant.Dealer
 import blackjack.domain.participant.Participant
 import blackjack.domain.participant.Participants
-import blackjack.domain.result.GameResult
 
 class Blackjack(private val deck: CardDeck, private val players: List<Participant>) {
-    fun start(
-        onStartDrawn: (Participants) -> Unit,
-        onFirstDrawn: (Participant) -> Unit,
-        onDrawnMore: (Participant) -> Unit,
-        onEndGame: (List<GameResult>) -> Unit,
-    ) {
+    fun start(blackjackEventListener: BlackjackEventListener) {
         val participants = Participants(players + Dealer())
-        onStartDrawn(participants)
 
-        onEndGame(
-            participants
-                .drawFirst(deck, onFirstDrawn)
-                .takeTurns(deck, onDrawnMore)
-                .getGameResult()
-        )
+        with(blackjackEventListener) {
+            onStartDrawn(participants)
+            onEndGame(
+                participants
+                    .drawFirst(deck, ::onFirstDrawn)
+                    .takeTurns(deck, ::onDrawnMore)
+                    .getGameResult()
+            )
+        }
     }
 }
---
+++ src/main/kotlin/blackjack/domain/BlackjackEventListener.kt
@@ -0,0 +1,12 @@
+package blackjack.domain
+
+import blackjack.domain.participant.Participant
+import blackjack.domain.participant.Participants
+import blackjack.domain.result.GameResult
+
+interface BlackjackEventListener {
+    fun onStartDrawn(participants: Participants)
+    fun onFirstDrawn(participant: Participant)
+    fun onDrawnMore(participant: Participant)
+    fun onEndGame(gameResults: List<GameResult>)
+}
