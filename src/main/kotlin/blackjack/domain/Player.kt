dda1cd03dec730a4e2821fea3e234cce213e1682
changhwan <chang5hustle@gmail.com>
2023-03-13 06:12:40 +0900

    feat(Player): 딜러와 승패여부결정 함수 작성

--- src/main/kotlin/blackjack/domain/Player.kt
+++ src/main/kotlin/blackjack/domain/Player.kt
@@ -6,4 +6,17 @@ import blackjack.domain.state.State
 class Player(val name: String, override var state: State, val bettingAmount: Int) : Participant {
 
     override fun isOverCondition(): Boolean = state !is Hit
+
+    fun getConsequence(dealer: Dealer): Consequence {
+        return when (compareValuesBy(this, dealer, { it.state }, { it.state.hand.getTotalScore() })) {
+            1 -> Consequence.WIN
+            0 -> Consequence.DRAW
+            -1 -> Consequence.LOSE
+            else -> throw IllegalStateException(INVALID_COMPARE_ERROR)
+        }
+    }
+
+    companion object {
+        private const val INVALID_COMPARE_ERROR = "결과비교의 문제가 있습니다"
+    }
 }
