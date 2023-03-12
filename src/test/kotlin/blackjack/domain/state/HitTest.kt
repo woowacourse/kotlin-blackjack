c48764cb60d5876822d015572a63fa3221aaa75b
changhwan <chang5hustle@gmail.com>
2023-03-12 17:19:33 +0900

    test(HitTest):카드를 뽑았을때 21이 넘지 않았다면 hit을 반환하는지 테스트

--- src/test/kotlin/blackjack/domain/state/HitTest.kt
+++ src/test/kotlin/blackjack/domain/state/HitTest.kt
@@ -10,4 +10,10 @@ class HitTest {
         val hit = Hit(CardBunchForState(Cards.nine, Cards.eight))
         assertThat(hit.draw(Cards.jack)).isInstanceOf(Burst::class.java)
     }
+
+    @Test
+    fun `카드를 뽑았을때 21이 넘지 않는다면 Hit객체를 반환한다`() {
+        val hit = Hit(CardBunchForState(Cards.two, Cards.eight))
+        assertThat(hit.draw(Cards.three)).isInstanceOf(Hit::class.java)
+    }
 }
