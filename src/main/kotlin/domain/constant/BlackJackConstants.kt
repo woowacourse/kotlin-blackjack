7b55eb5a3dc0f749b74a117072005b294fca5b83
vrexpert <vrexpert2302@gmail.com>
2023-03-12 13:57:50 +0900

    refactor(constant): 사용하지 않는 상수 삭제

--- src/main/kotlin/domain/constant/BlackJackConstants.kt
+++ src/main/kotlin/domain/constant/BlackJackConstants.kt
@@ -2,8 +2,5 @@ package domain.constant
 
 object BlackJackConstants {
     const val BLACK_JACK_NUMBER = 21
-    const val NOTHING = 0
-    const val BIG_ACE = 11
-    const val SMALL_ACE = 1
     const val DEALER_STAND_CONDITION = 16
 }
