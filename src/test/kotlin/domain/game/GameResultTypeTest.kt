04efee8b948537ffce04d4a7e7c6285f58fedc36
chws0508 <chws0508@naver.com>
2023-03-12 13:28:33 +0900

    refactor(): 조건문 수정 및 private 추가

--- src/test/kotlin/domain/game/GameResultTypeTest.kt
+++ src/test/kotlin/domain/game/GameResultTypeTest.kt
@@ -8,50 +8,6 @@ import org.junit.jupiter.params.provider.MethodSource
 import java.util.stream.Stream
 
 class GameResultTypeTest {
-    // @Test
-    // fun `승리 일 경우에는 베팅머니의 1배를 수익으로 반환한다`() {
-    //     // given
-    //     val bettingMoney = 1000
-    //     // when
-    //     val actual = GameResultType.getProfit(bettingMoney, GameResultType.WIN)
-    //     val expected = 1000
-    //     // then
-    //     assertThat(actual).isEqualTo(expected)
-    // }
-    //
-    // @Test
-    // fun `패배일 경우에는 베팅머니의 -1배를 수익으로 반환한다`() {
-    //     // given
-    //     val bettingMoney = 1000
-    //     // when
-    //     val actual = GameResultType.getProfit(bettingMoney, GameResultType.LOSE)
-    //     val expected = -1000
-    //     // then
-    //     assertThat(actual).isEqualTo(expected)
-    // }
-    //
-    // @Test
-    // fun `무승부인 경우에는 베팅머니의 0배를 수익으로 반환한다`() {
-    //     // given
-    //     val bettingMoney = 1000
-    //     // when
-    //     val actual = GameResultType.getProfit(bettingMoney, GameResultType.DRAW)
-    //     val expected = 0
-    //     // then
-    //     assertThat(actual).isEqualTo(expected)
-    // }
-    //
-    // @Test
-    // fun `블랙잭인 경우에는 베팅머니의 2분의3배를 수익으로 반환한다`() {
-    //     // given
-    //     val bettingMoney = 1000
-    //     // when
-    //     val actual = GameResultType.getProfit(bettingMoney, GameResultType.BLACKJACK)
-    //     val expected = 1500
-    //     // then
-    //     assertThat(actual).isEqualTo(expected)
-    // }
-
     @Test
     fun `플레이어의 점수가 Burst 이면 딜러의 상태와 상관없이 패배이다`() {
         // given
