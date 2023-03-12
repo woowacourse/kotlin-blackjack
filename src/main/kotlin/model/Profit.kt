537585eed089db4e10d2b35cbdcfdc357f5a5e4d
inseonyun <yuninseon@naver.com>
2023-03-12 22:12:48 +0900

    refactor(Profit): operator 함수 재정의

--- src/main/kotlin/model/Profit.kt
+++ src/main/kotlin/model/Profit.kt
@@ -2,6 +2,7 @@ package model
 
 @JvmInline
 value class Profit(val value: Long) {
+    operator fun unaryMinus() = Profit(-value)
     companion object {
         fun of(money: Money, cardGameResult: CardGameResult): Profit = Profit((money.value * cardGameResult.multiple).toLong())
     }
