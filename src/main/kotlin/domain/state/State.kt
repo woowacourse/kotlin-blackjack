c48c3e491ccd49d9a1bcd4cb36fcbb35dd3495b6
vrexpert <vrexpert2302@gmail.com>
2023-03-12 13:45:06 +0900

    refactor(state): State 인터페이스에 점수 계산 및 수익 계산 함수 추가

--- src/main/kotlin/domain/state/State.kt
+++ src/main/kotlin/domain/state/State.kt
@@ -2,10 +2,13 @@ package domain.state
 
 import domain.card.Card
 import domain.money.Money
+import domain.money.Profit
+import domain.result.Score
 
 interface State {
     fun draw(card: Card): State
     fun stay(): State
     fun getHandCards(): List<Card>
-    fun profit(money: Money): Double
+    fun getScore(): Score
+    fun profit(other: State, money: Money): Profit
 }
