ee7b7ed12becbbc8a86af89da692c6c87fa240fa
tmdgh1592 <tmdgh1592@naver.com>
2023-03-12 20:45:12 +0900

    refactor: 버스트 상태의 수익률 프로퍼티를 -1.0으로 변경

--- src/main/kotlin/blackjack/domain/state/BustState.kt
+++ src/main/kotlin/blackjack/domain/state/BustState.kt
@@ -3,10 +3,10 @@ package blackjack.domain.state
 import blackjack.domain.card.Cards
 import blackjack.domain.money.Money
 
-class BustState(cards: Cards, override val earningRate: Double = 1.0) : FinishedState(cards) {
+class BustState(cards: Cards, override val earningRate: Double = -1.0) : FinishedState(cards) {
     init {
         check(cards.isBust) { "버스트 상태는 21점을 초과해야 합니다." }
     }
 
-    override fun profit(other: CardState, money: Money): Money = -(money * earningRate)
+    override fun profit(other: CardState, money: Money): Money = money * earningRate
 }
