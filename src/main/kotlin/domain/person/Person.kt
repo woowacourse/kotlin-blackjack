798796b1d06cd820f4a2cc5fe5dfbec775fa1478
vrexpert <vrexpert2302@gmail.com>
2023-03-12 19:53:47 +0900

    refactor(Person): 상태 패턴으로 변경에 따른 Person 메서드 추가

--- src/main/kotlin/domain/person/Person.kt
+++ src/main/kotlin/domain/person/Person.kt
@@ -1,17 +1,33 @@
 package domain.person
 
 import domain.card.Card
-import domain.card.Cards
-import domain.constant.GameState
+import domain.card.Hand
+import domain.money.Money
+import domain.result.Score
+import domain.state.Finished
+import domain.state.Started
+import domain.state.State
 
-abstract class Person(value: List<Card>, open val name: String) {
-    val cards: Cards = Cards(value)
+abstract class Person(val name: String) {
 
-    fun receiveCard(values: List<Card>) {
-        values.forEach { cards.add(it) }
+    abstract var state: State
+
+    val score: Score
+        get() = Score.of(Hand(state.getHandCards()))
+
+    fun isFinished() = state is Finished
+
+    fun isStarted() = state is Started
+
+    fun receiveCard(cards: List<Card>) {
+        cards.forEach { state = state.draw(it) }
     }
 
-    protected abstract fun checkState(): GameState
+    fun stay() {
+        state = state.stay()
+    }
 
-    fun isState(state: GameState) = checkState() == state
+    fun getHandCards() = state.getHandCards()
+
+    fun getPlayerProfit(other: State, money: Money) = state.profit(other, money)
 }
---
+++ src/test/kotlin/domain/person/PersonTest.kt
@@ -0,0 +1,54 @@
+package domain.person
+
+import domain.card.Card
+import domain.card.CardNumber
+import domain.card.CardShape
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+class PersonTest {
+    @Test
+    fun `Person 은 이름을 가진다`() {
+        val player = Player("베르")
+
+        Assertions.assertThat(player.name).isEqualTo("베르")
+    }
+
+    @Test
+    fun `Person 은 카드를 받아서 추가할 수 있다`() {
+        val player = Player("베르")
+
+        player.receiveCard(
+            listOf(
+                Card(CardShape.HEART, CardNumber.TEN),
+                Card(CardShape.HEART, CardNumber.KING),
+            ),
+        )
+
+        Assertions.assertThat(player.getHandCards()).isEqualTo(
+            listOf(
+                Card(CardShape.HEART, CardNumber.TEN),
+                Card(CardShape.HEART, CardNumber.KING),
+            ),
+        )
+    }
+
+    @Test
+    fun `Person 의 카드를 확인할 수 있다`() {
+        val player = Player("베르")
+
+        player.receiveCard(
+            listOf(
+                Card(CardShape.HEART, CardNumber.KING),
+                Card(CardShape.HEART, CardNumber.TEN),
+            ),
+        )
+
+        Assertions.assertThat(player.getHandCards()).isEqualTo(
+            listOf(
+                Card(CardShape.HEART, CardNumber.KING),
+                Card(CardShape.HEART, CardNumber.TEN),
+            ),
+        )
+    }
+}
