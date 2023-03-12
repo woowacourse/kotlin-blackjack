2c0d95a69f32def1ed44ed6ba9a055ff6b5d3b54
inseonyun <yuninseon@naver.com>
2023-03-12 21:47:10 +0900

    refactor(InputView): 잘못된 입력에 대한 예외 처리 구현

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -1,27 +1,36 @@
 package view
 
 class InputView {
-    fun readName(): List<String> {
+    fun readName(): List<String> = runCatching {
         println(MESSAGE_INPUT_NAME)
         val input = readln().replace(" ", "")
         require(input.isNotBlank()) { NULL_ERROR }
         return input.split(",")
+    }.getOrElse {
+        println(it.message!!)
+        readName()
     }
 
-    fun readBettingMoney(name: String): Long {
+    fun readBettingMoney(name: String): Long = runCatching {
         println(MESSAGE_INPUT_BETTING_MONEY.format(name))
         val input = readln()
         require(input.toLongOrNull() != null) { IS_NOT_INTEGER_ERROR }
         return input.toLong()
+    }.getOrElse {
+        println(it.message!!)
+        readBettingMoney(name)
     }
 
-    fun readYesOrNo(name: String): Boolean {
+    fun readYesOrNo(name: String): Boolean = runCatching {
         println(MESSAGE_INPUT_YES_OR_NO.format(name))
         val input = readln()
         require(input.isNotBlank()) { NULL_ERROR }
         require(Regex("[$Y$y$N$n]").matches(input)) { IS_NOT_YES_OR_NO_ERROR }
         if (input == Y || input == y) return true
         return false
+    }.getOrElse {
+        println(it.message!!)
+        readYesOrNo(name)
     }
 
     companion object {
