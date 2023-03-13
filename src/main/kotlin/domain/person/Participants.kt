8dc172fde9b6d777d0229e70dc97fb355ac5f8f7
vrexpert <vrexpert2302@gmail.com>
2023-03-13 14:02:20 +0900

    refactor(Participants): 이름 중복 검증 로직 제거

--- src/main/kotlin/domain/person/Participants.kt
+++ src/main/kotlin/domain/person/Participants.kt
@@ -2,15 +2,6 @@ package domain.person
 
 class Participants(val dealer: Dealer, val players: List<Player>) {
 
-    init {
-        checkPlayerNamesDuplicated()
-    }
-
-    private fun checkPlayerNamesDuplicated() {
-        val playerNames = players.map { it.name }
-        require(playerNames.size == playerNames.toSet().size)
-    }
-
     companion object {
         fun from(getPlayerNames: () -> List<String>): Participants {
             val dealer = Dealer()
