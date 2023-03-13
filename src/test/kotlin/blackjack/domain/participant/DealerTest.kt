f56c2b168fc7a872ea5361b2d24281bed8ce883e
tmdgh1592 <tmdgh1592@naver.com>
2023-03-13 10:11:57 +0900

    test: 참여자 테스트 코드 수정

--- src/test/kotlin/blackjack/domain/participant/DealerTest.kt
+++ src/test/kotlin/blackjack/domain/participant/DealerTest.kt
@@ -1,78 +1,70 @@
 package blackjack.domain.participant
 
+import blackjack.domain.SPADE_ACE
+import blackjack.domain.SPADE_FIVE
+import blackjack.domain.SPADE_JACK
+import blackjack.domain.SPADE_KING
+import blackjack.domain.SPADE_SIX
+import blackjack.domain.SPADE_TWO
 import blackjack.domain.card.Card
 import blackjack.domain.card.CardNumber
 import blackjack.domain.card.Suit
+import blackjack.domain.state.HitState
+import blackjack.domain.state.StartState
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
 class DealerTest {
-    lateinit var dealer: Dealer
-
-    @BeforeEach
-    fun setUp() {
-        dealer = Dealer()
-    }
-
     @Test
     fun `딜러는 이름을 갖는다`() {
+        val dealer = Dealer()
         assertThat(dealer.name).isEqualTo("딜러")
     }
 
     @Test
     fun `딜러가 처음 공개할 카드는 1장이다`() {
-        with(dealer) {
-            addCard(Card(CardNumber.JACK, Suit.SPADE))
-            addCard(Card(CardNumber.QUEEN, Suit.SPADE))
-        }
+        val dealer = Dealer(cardState = StartState(SPADE_JACK, SPADE_KING))
 
         assertThat(dealer.getFirstOpenCards().size).isEqualTo(1)
     }
 
     @Test
     fun `딜러는 자신이 처음 공개할 카드를 반환한다`() {
-        dealer.addCard(Card(CardNumber.ACE, Suit.SPADE))
-        dealer.addCard(Card(CardNumber.FIVE, Suit.SPADE))
+        val dealer = Dealer(cardState = StartState(SPADE_JACK, SPADE_KING))
 
-        assertThat(dealer.getFirstOpenCards()).isEqualTo(listOf(Card(CardNumber.ACE, Suit.SPADE)))
+        assertThat(dealer.getFirstOpenCards()).isEqualTo(listOf(SPADE_ACE))
     }
 
     @Test
     fun `딜러는 카드의 합이 16점 이하면 카드를 뽑을 수 있다`() {
-        dealer.addCard(Card(CardNumber.ACE, Suit.SPADE))
-        dealer.addCard(Card(CardNumber.FIVE, Suit.SPADE))
+        val dealer = Dealer(cardState = StartState(SPADE_ACE, SPADE_FIVE))
 
         assertThat(dealer.canDraw()).isTrue
     }
 
     @Test
     fun `딜러는 카드의 합이 17점 이상이면 더 이상 카드를 뽑을 수 없다`() {
-        dealer.addCard(Card(CardNumber.ACE, Suit.SPADE))
-        dealer.addCard(Card(CardNumber.SIX, Suit.SPADE))
+        val dealer = Dealer(cardState = StartState(SPADE_ACE, SPADE_SIX))
 
         assertThat(dealer.canDraw()).isFalse
     }
 
     @Test
     fun `딜러는 카드 목록에 카드를 추가한다`() {
-        dealer.addCard(Card(CardNumber.TWO, Suit.SPADE))
+        val dealer = Dealer(cardState = HitState(SPADE_ACE))
+        dealer.draw(SPADE_TWO)
 
-        assertThat(dealer.getCards()).containsExactly(Card(CardNumber.TWO, Suit.SPADE))
+        assertThat(dealer.getCards()).containsExactly(SPADE_ACE, SPADE_TWO)
     }
 
     @Test
     fun `딜러가 보유한 카드를 반환한다`() {
-        dealer.addCard(Card(CardNumber.ACE, Suit.SPADE))
-        dealer.addCard(Card(CardNumber.JACK, Suit.SPADE))
+        val dealer = Dealer(cardState = HitState(SPADE_ACE, SPADE_JACK))
 
         assertThat(dealer.getCards()).isEqualTo(
-            listOf(
-                Card(CardNumber.ACE, Suit.SPADE),
-                Card(CardNumber.JACK, Suit.SPADE)
-            )
+            listOf(SPADE_ACE, SPADE_JACK)
         )
     }
 
@@ -89,8 +81,9 @@ class DealerTest {
         secondCardNumber: CardNumber,
         expected: Int
     ) {
-        dealer.addCard(Card(firstCardNumber, firstCardSuit))
-        dealer.addCard(Card(secondCardNumber, secondCardSuit))
+        val dealer = Dealer()
+            .draw(Card(firstCardNumber, firstCardSuit))
+            .draw(Card(secondCardNumber, secondCardSuit))
 
         assertThat(dealer.getTotalScore()).isEqualTo(expected)
     }
--- src/test/kotlin/blackjack/domain/participant/PlayerTest.kt
+++ src/test/kotlin/blackjack/domain/participant/PlayerTest.kt
@@ -1,8 +1,17 @@
 package blackjack.domain.participant
 
+import blackjack.domain.SPADE_ACE
+import blackjack.domain.SPADE_EIGHT
+import blackjack.domain.SPADE_FOUR
+import blackjack.domain.SPADE_JACK
+import blackjack.domain.SPADE_KING
+import blackjack.domain.SPADE_THREE
+import blackjack.domain.SPADE_TWO
 import blackjack.domain.card.Card
 import blackjack.domain.card.CardNumber
 import blackjack.domain.card.Suit
+import blackjack.domain.money.BetMoney
+import blackjack.domain.state.HitState
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -10,11 +19,11 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
 class PlayerTest {
-    lateinit var player: Player
+    private lateinit var player: Player
 
     @BeforeEach
     fun setUp() {
-        // player = Player("pobi")
+        val player = Player("pobi", money = BetMoney(1000))
     }
 
     @Test
@@ -24,62 +33,71 @@ class PlayerTest {
 
     @Test
     fun `플레이어가 처음 공개할 카드는 2장이다`() {
-        with(player) {
-            addCard(Card(CardNumber.JACK, Suit.SPADE))
-            addCard(Card(CardNumber.QUEEN, Suit.SPADE))
-        }
+        val player = Player(
+            name = "pobi",
+            money = BetMoney(1000),
+            cardState = HitState(SPADE_ACE, SPADE_TWO, SPADE_THREE)
+        )
 
         assertThat(player.getFirstOpenCards().size).isEqualTo(2)
     }
 
     @Test
     fun `플레이어는 자신이 처음 공개할 카드를 반환한다`() {
-        player.addCard(Card(CardNumber.ACE, Suit.SPADE))
-        player.addCard(Card(CardNumber.FIVE, Suit.SPADE))
+        val player = Player(
+            name = "pobi",
+            money = BetMoney(1000),
+            cardState = HitState(SPADE_ACE, SPADE_TWO, SPADE_THREE)
+        )
 
         assertThat(player.getFirstOpenCards()).isEqualTo(
-            listOf(
-                Card(CardNumber.ACE, Suit.SPADE),
-                Card(CardNumber.FIVE, Suit.SPADE)
-            )
+            listOf(SPADE_ACE, SPADE_TWO)
         )
     }
 
     @Test
     fun `카드의 합이 21을 초과하지 않으면 카드를 뽑을 수 있다`() {
-        player.addCard(Card(CardNumber.ACE, Suit.SPADE))
-        player.addCard(Card(CardNumber.KING, Suit.SPADE))
+        val player = Player(
+            name = "pobi",
+            money = BetMoney(1000),
+            cardState = HitState(SPADE_ACE, SPADE_KING)
+        )
 
         assertThat(player.canDraw()).isTrue
     }
 
     @Test
     fun `카드의 합이 21을 초과하면 더 이상 카드를 뽑을 수 없다`() {
-        player.addCard(Card(CardNumber.FOUR, Suit.SPADE))
-        player.addCard(Card(CardNumber.EIGHT, Suit.SPADE))
-        player.addCard(Card(CardNumber.KING, Suit.SPADE))
+        val player = Player(
+            name = "pobi",
+            money = BetMoney(1000),
+            cardState = HitState(SPADE_FOUR, SPADE_EIGHT, SPADE_KING)
+        )
 
         assertThat(player.canDraw()).isFalse
     }
 
     @Test
     fun `플레이어는 카드 목록에 카드를 추가한다`() {
-        player.addCard(Card(CardNumber.TWO, Suit.SPADE))
+        val player = Player(
+            name = "pobi",
+            money = BetMoney(1000),
+            cardState = HitState(SPADE_FOUR, SPADE_EIGHT)
+        )
+        player.draw(SPADE_KING)
 
-        assertThat(player.getCards()).containsExactly(Card(CardNumber.TWO, Suit.SPADE))
+        assertThat(player.getCards()).containsExactly(SPADE_FOUR, SPADE_EIGHT, SPADE_KING)
     }
 
     @Test
     fun `플레이어가 보유한 카드를 반환한다`() {
-        player.addCard(Card(CardNumber.ACE, Suit.SPADE))
-        player.addCard(Card(CardNumber.JACK, Suit.SPADE))
-
-        assertThat(player.getCards()).isEqualTo(
-            listOf(
-                Card(CardNumber.ACE, Suit.SPADE),
-                Card(CardNumber.JACK, Suit.SPADE)
-            )
+        val player = Player(
+            name = "pobi",
+            money = BetMoney(1000),
+            cardState = HitState(SPADE_ACE, SPADE_JACK)
         )
+
+        assertThat(player.getCards()).containsExactly(SPADE_ACE, SPADE_JACK)
     }
 
     @ParameterizedTest
@@ -95,97 +113,10 @@ class PlayerTest {
         secondCardNumber: CardNumber,
         expected: Int
     ) {
-        player.addCard(Card(firstCardNumber, firstCardSuit))
-        player.addCard(Card(secondCardNumber, secondCardSuit))
+        val player = Player(name = "pobi", money = BetMoney(1000))
+            .draw(Card(firstCardNumber, firstCardSuit))
+            .draw(Card(secondCardNumber, secondCardSuit))
 
         assertThat(player.getTotalScore()).isEqualTo(expected)
     }
-
-    // @Test
-    // fun `본인과 상대방이 모두 21점을 초과하면 무승부이다`() {
-    //     val me = Player("부나").apply {
-    //         addCard(Card(CardNumber.JACK, Suit.SPADE))
-    //         addCard(Card(CardNumber.QUEEN, Suit.SPADE))
-    //         addCard(Card(CardNumber.TWO, Suit.SPADE))
-    //     }
-    //     val other = Player("반달").apply {
-    //         addCard(Card(CardNumber.JACK, Suit.DIAMOND))
-    //         addCard(Card(CardNumber.QUEEN, Suit.DIAMOND))
-    //         addCard(Card(CardNumber.THREE, Suit.DIAMOND))
-    //     }
-    //
-    //     assertThat(me judge other).isEqualTo(GameResult.DRAW)
-    // }
-
-    // @Test
-    // fun `본인만 21점을 초과하면 패배한다`() {
-    //     val me = Player("부나").apply {
-    //         addCard(Card(CardNumber.JACK, Suit.SPADE))
-    //         addCard(Card(CardNumber.QUEEN, Suit.SPADE))
-    //         addCard(Card(CardNumber.TWO, Suit.SPADE))
-    //     }
-    //     val other = Player("반달").apply {
-    //         addCard(Card(CardNumber.JACK, Suit.DIAMOND))
-    //         addCard(Card(CardNumber.QUEEN, Suit.DIAMOND))
-    //     }
-    //
-    //     assertThat(me judge other).isEqualTo(GameResult.LOSE)
-    // }
-    //
-    // @Test
-    // fun `상대방만 21점을 초과하면 승리한다`() {
-    //     val me = Player("부나").apply {
-    //         addCard(Card(CardNumber.JACK, Suit.SPADE))
-    //         addCard(Card(CardNumber.QUEEN, Suit.SPADE))
-    //     }
-    //     val other = Player("반달").apply {
-    //         addCard(Card(CardNumber.JACK, Suit.DIAMOND))
-    //         addCard(Card(CardNumber.QUEEN, Suit.DIAMOND))
-    //         addCard(Card(CardNumber.TWO, Suit.SPADE))
-    //     }
-    //
-    //     assertThat(me judge other).isEqualTo(GameResult.WIN)
-    // }
-    //
-    // @Test
-    // fun `본인과 상대방이 21점을 초과하지 않고 점수가 동일하면 무승부이다`() {
-    //     val me = Player("부나").apply {
-    //         addCard(Card(CardNumber.JACK, Suit.SPADE))
-    //         addCard(Card(CardNumber.QUEEN, Suit.SPADE))
-    //     }
-    //     val other = Player("반달").apply {
-    //         addCard(Card(CardNumber.JACK, Suit.DIAMOND))
-    //         addCard(Card(CardNumber.QUEEN, Suit.DIAMOND))
-    //     }
-    //
-    //     assertThat(me judge other).isEqualTo(GameResult.DRAW)
-    // }
-    //
-    // @Test
-    // fun `본인과 상대방이 21점을 초과하지 않고 본인의 점수가 높으면 승리한다`() {
-    //     val me = Player("부나").apply {
-    //         addCard(Card(CardNumber.JACK, Suit.SPADE))
-    //         addCard(Card(CardNumber.QUEEN, Suit.SPADE))
-    //     }
-    //     val other = Player("반달").apply {
-    //         addCard(Card(CardNumber.JACK, Suit.DIAMOND))
-    //         addCard(Card(CardNumber.TWO, Suit.DIAMOND))
-    //     }
-    //
-    //     assertThat(me judge other).isEqualTo(GameResult.WIN)
-    // }
-    //
-    // @Test
-    // fun `본인과 상대방이 21점을 초과하지 않고 상대방의 점수가 높으면 패배한다`() {
-    //     val me = Player("부나").apply {
-    //         addCard(Card(CardNumber.JACK, Suit.SPADE))
-    //         addCard(Card(CardNumber.TWO, Suit.SPADE))
-    //     }
-    //     val other = Player("반달").apply {
-    //         addCard(Card(CardNumber.JACK, Suit.DIAMOND))
-    //         addCard(Card(CardNumber.QUEEN, Suit.DIAMOND))
-    //     }
-    //
-    //     assertThat(me judge other).isEqualTo(GameResult.LOSE)
-    // }
 }
