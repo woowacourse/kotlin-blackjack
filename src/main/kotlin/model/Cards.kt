eb58336d6fe71cc5569de6dd7e6960e1c2329fc3
inseonyun <yuninseon@naver.com>
2023-03-13 14:52:09 +0900

    refactor(Cards): 매직넘버 상수화

--- src/main/kotlin/model/Cards.kt
+++ src/main/kotlin/model/Cards.kt
@@ -13,7 +13,7 @@ class Cards(cards: Set<Card>) {
 
     fun sum(): Int {
         var sum = _cards.sumOf { it.rank.score }
-        if (_cards.find { it.rank == Rank.ACE } != null && sum + ACE_BONUS_TEN <= PARTICIPANT_STANDARD_BUST_POINT) {
+        if (_cards.find { it.rank == Rank.ACE } != null && sum + ACE_BONUS_TEN < PARTICIPANT_STANDARD_BUST_POINT) {
             sum += ACE_BONUS_TEN
         }
         return sum
@@ -21,12 +21,14 @@ class Cards(cards: Set<Card>) {
 
     fun firstCard(): Card = cards.first()
 
-    fun isBust(): Boolean = sum() > PARTICIPANT_STANDARD_BUST_POINT
-    fun isBlackJack(): Boolean = size == 2 && sum() == 21
+    fun isBust(): Boolean = sum() >= PARTICIPANT_STANDARD_BUST_POINT
+    fun isBlackJack(): Boolean = size == BLACKJACK_CARD_SIZE && sum() == BLACKJACK_CARD_SUM
 
     companion object {
         const val DEALER_STANDARD_HIT_POINT = 16
-        const val PARTICIPANT_STANDARD_BUST_POINT = 21
+        private const val PARTICIPANT_STANDARD_BUST_POINT = 22
         private const val ACE_BONUS_TEN = 10
+        private const val BLACKJACK_CARD_SIZE = 2
+        private const val BLACKJACK_CARD_SUM = 21
     }
 }
