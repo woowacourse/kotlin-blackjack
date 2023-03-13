00a755ed595fed2e8296937bf9b4ec7852f03f96
ki960213 <ki960213@github.com>
2023-03-13 19:06:06 +0900

    test(DealTest): Deal 클래스의 생성자의 기본 인자를 삭제함에 따라 테스트 코드도 변경

--- src/test/kotlin/blackjack/domain/state/DealTest.kt
+++ src/test/kotlin/blackjack/domain/state/DealTest.kt
@@ -9,7 +9,7 @@ class DealTest {
 
     @Test
     fun `딜 상태일 때 스테이하면 에러가 발생한다`() {
-        val state = Deal(Hand(listOf()))
+        val state = Deal(Hand(listOf()), null)
 
         Assertions.assertThatIllegalStateException().isThrownBy { state.stay() }
             .withMessage("게임이 진행중일 때만 스테이 상태가 될 수 있습니다.")
@@ -17,7 +17,7 @@ class DealTest {
 
     @Test
     fun `딜 상태일 때 수익을 달라고 하면 에러가 발생한다`() {
-        val state = Deal(Hand(listOf()))
+        val state = Deal(Hand(listOf()), null)
 
         Assertions.assertThatIllegalStateException().isThrownBy { state.getProfit() }
             .withMessage("게임이 끝나기 전까지는 수익을 계산할 수 없습니다.")
@@ -25,7 +25,7 @@ class DealTest {
 
     @Test
     fun `딜 상태일 때 베팅하면 에러가 발생한다`() {
-        val state = Deal(Hand(listOf()))
+        val state = Deal(Hand(listOf()), null)
 
         Assertions.assertThatIllegalStateException().isThrownBy { state.betting(getAnyMoney()) }
             .withMessage("이미 베팅을 했습니다.")
@@ -33,7 +33,7 @@ class DealTest {
 
     @Test
     fun `딜 상태이고 드로우 했을 때 핸드의 카드 개수가 2장 미만이면 딜 상태가 된다`() {
-        var state: State = Deal(Hand(listOf()))
+        var state: State = Deal(Hand(listOf()), null)
 
         state = state.draw(getAnyCard())
 
@@ -42,7 +42,7 @@ class DealTest {
 
     @Test
     fun `딜 상태이고 드로우 했을 때 핸드의 카드 개수가 2장 이상이면 히트 상태가 된다`() {
-        var state: State = Deal(Hand(listOf()))
+        var state: State = Deal(Hand(listOf()), null)
         state = state.draw(getAnyCard())
 
         state = state.draw(getAnyCard())
