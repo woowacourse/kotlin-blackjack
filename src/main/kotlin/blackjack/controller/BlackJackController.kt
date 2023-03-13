4d501eb3c649d1e8f89e26580f13dead70e11012
Yerin <myheaven_b@naver.com>
2023-03-13 14:17:07 +0900

    refactor: 게임진행에 따라 controller를 3분할로 진행

--- src/main/kotlin/blackjack/controller/BlackJackController.kt
+++ src/main/kotlin/blackjack/controller/BlackJackController.kt
@@ -2,6 +2,8 @@ package blackjack.controller
 
 import blackjack.domain.BlackJackGame
 import blackjack.domain.CardPack
+import blackjack.domain.GameResult
+import blackjack.domain.PlayerGameResult
 import blackjack.view.InputView
 import blackjack.view.OutputView
 
@@ -10,16 +12,28 @@ class BlackJackController(
 ) {
     fun run() {
         runCatching {
-            blackJackGame.entryPlayers(InputView.requestPlayersName())
-            OutputView.printCardDividingMessage(blackJackGame.showParticipants())
-            blackJackGame.drawAdditionalCards()
-            OutputView.printIsDealerReceivedCard(blackJackGame.drawAdditionalCardForDealer())
-            OutputView.printFinalCards(blackJackGame.showParticipants())
-            val gameResults = blackJackGame.judgeGameResults()
-            OutputView.printGameResults(playerGameResults = gameResults.first, dealerGameResult = gameResults.second)
-            OutputView.printBetResults(blackJackGame.judgeBetResults(playersGameResult = gameResults.first))
+            startBlackJackGame()
+            onBlackJackGame()
+            endBlackJackGame(blackJackGame.judgeGameResults())
         }.onFailure { exception ->
             OutputView.printErrorMessage(exception)
         }
+    }
+
+    private fun startBlackJackGame() {
+        blackJackGame.entryPlayers(InputView.requestPlayersName())
+        OutputView.printCardDividingMessage(blackJackGame.showParticipants())
+    }
+
+    private fun onBlackJackGame() {
+        // TODO: 이거만 고치면 댐
+        blackJackGame.drawAdditionalCards()
+        OutputView.printIsDealerReceivedCard(blackJackGame.drawAdditionalCardForDealer())
+    }
+
+    private fun endBlackJackGame(gameResults: Pair<List<PlayerGameResult>, List<GameResult>>) {
+        OutputView.printFinalCards(blackJackGame.showParticipants())
+        OutputView.printGameResults(playerGameResults = gameResults.first, dealerGameResult = gameResults.second)
+        OutputView.printBetResults(blackJackGame.judgeBetResults(playersGameResult = gameResults.first))
     }
 }
