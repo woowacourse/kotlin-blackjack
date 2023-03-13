ced7b32038890b28df8447520f2d9a68e8687d88
tmdgh1592 <tmdgh1592@naver.com>
2023-03-13 23:11:09 +0900

    refactor: 블랙잭 시작 메서드의 로직 분리

--- src/main/kotlin/blackjack/domain/Blackjack.kt
+++ src/main/kotlin/blackjack/domain/Blackjack.kt
@@ -5,23 +5,23 @@ import blackjack.domain.listener.BlackjackEventListener
 import blackjack.domain.participant.Dealer
 import blackjack.domain.participant.Participant
 import blackjack.domain.participant.Participants
+import blackjack.domain.result.GameResult
 
 class Blackjack(
     private val deck: CardDeck,
     private val players: List<Participant>,
-    private val blackjackEventListener: BlackjackEventListener,
+    private val eventListener: BlackjackEventListener,
 ) {
     fun start() {
         val participants = Participants(players + Dealer())
+        eventListener.onStartDrawn(participants)
 
-        with(blackjackEventListener) {
-            onStartDrawn(participants)
-            onEndGame(
-                participants
-                    .drawFirst(deck, ::onFirstDrawn)
-                    .takeTurns(deck, ::onDrawnMore)
-                    .getGameResult()
-            )
-        }
+        val result = startGame(participants)
+        eventListener.onEndGame(result)
     }
+
+    private fun startGame(participants: Participants): List<GameResult> = participants
+        .drawFirst(deck, eventListener::onFirstDrawn)
+        .takeTurns(deck, eventListener::onDrawnMore)
+        .getGameResult()
 }
