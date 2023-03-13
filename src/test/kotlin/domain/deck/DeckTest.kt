98e2a26fbd728092da55dd3fedf31cc388e7a9b2
rhkrwngud445 <rhkrwngud445@gmail.com>
2023-03-13 20:13:53 +0900

    refactor: assertAll로 반복 테스트를 변경

--- src/test/kotlin/domain/deck/DeckTest.kt
+++ src/test/kotlin/domain/deck/DeckTest.kt
@@ -9,6 +9,7 @@ import domain.card.CardValue.SIX
 import domain.card.Shape
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
 import org.junit.jupiter.api.assertThrows
 
 class DeckTest {
@@ -29,8 +30,16 @@ class DeckTest {
     @Test
     fun `새로운 카드들을 덱에 저장한다`() {
         val deck = Deck((SpadeCardsOf(FIVE, SIX, SEVEN).getCards()))
-        assertThat(deck.giveCard()).isEqualTo(Card(Shape.SPADE, FIVE))
-        assertThat(deck.giveCard()).isEqualTo(Card(Shape.SPADE, SIX))
-        assertThat(deck.giveCard()).isEqualTo(Card(Shape.SPADE, SEVEN))
+        assertAll(
+            {
+                assertThat(deck.giveCard()).isEqualTo(Card(Shape.SPADE, FIVE))
+            },
+            {
+                assertThat(deck.giveCard()).isEqualTo(Card(Shape.SPADE, SIX))
+            },
+            {
+                assertThat(deck.giveCard()).isEqualTo(Card(Shape.SPADE, SEVEN))
+            }
+        )
     }
 }
