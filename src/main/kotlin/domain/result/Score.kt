9d01b5b84fb7c99d61ebb59f43847d93a9bb4a84
vrexpert <vrexpert2302@gmail.com>
2023-03-13 14:27:02 +0900

    refactor(Score): 점수 계산과 객체 생성 메서드 분리

--- src/main/kotlin/domain/result/Score.kt
+++ src/main/kotlin/domain/result/Score.kt
@@ -10,11 +10,16 @@ class Score private constructor(val value: Int) {
     fun isBiggerThan(other: Score) = value > other.value
 
     companion object {
+        private const val ACE_PLUS_POINT = 10
         fun of(hand: Hand): Score {
             val total = hand.getTotalCards()
-            if (!hand.hasAce()) return Score(total)
-            if (total + 10 > BlackJackConstants.BLACK_JACK_NUMBER) return Score(total)
-            return Score(total + 10)
+            return Score(getScoreWithAce(hand, total))
+        }
+
+        private fun getScoreWithAce(hand: Hand, total: Int): Int {
+            if (!hand.hasAce()) return total
+            if (total + ACE_PLUS_POINT > BlackJackConstants.BLACK_JACK_NUMBER) return total
+            return total + ACE_PLUS_POINT
         }
     }
 }
