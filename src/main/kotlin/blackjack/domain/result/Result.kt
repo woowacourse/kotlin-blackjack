ba574a65e93316af1227684db9e06ca5b7fa52a8
whk06061 <gju06061@naver.com>
2023-03-15 02:58:52 +0900

    refactor: Result의 reverse 메소드 제거

--- src/main/kotlin/blackjack/domain/result/Result.kt
+++ src/main/kotlin/blackjack/domain/result/Result.kt
@@ -4,9 +4,4 @@ enum class Result(val rate: Double) {
     DRAW(0.0),
     LOSE(-1.0),
     WIN(1.0);
-
-    fun reverse(): Result {
-        val index = this.ordinal
-        return Result.values()[Result.values().size - 1 - index]
-    }
 }
