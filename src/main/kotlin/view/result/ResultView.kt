00823a5335658193e1caf46d9733525bc0e299c5
briandr97 <briandr@naver.com>
2023-03-14 21:28:36 +0900

    refactor(ResultView): ResultOutput을 따로 두지 않고 ResultView로 합침

--- src/main/kotlin/view/result/ResultView.kt
+++ src/main/kotlin/view/result/ResultView.kt
@@ -1,13 +1,14 @@
 package view.result
 
+import domain.person.Person
 import domain.person.Persons
 import domain.result.Casino
-import view.result.ResultOutput.printDealerProfit
-import view.result.ResultOutput.printFinalOutcome
-import view.result.ResultOutput.printPersonCards
-import view.result.ResultOutput.printPlayerProfit
+import view.ViewCommon.toText
 
 object ResultView {
+    private const val INITIAL_CARDS_SCRIPT = "%s 카드: %s - 결과: %s"
+    private const val FINAL_OUTCOME_SCRIPT = "## 최종 수익"
+    private const val DEALER_SCRIPT = "딜러: "
 
     fun printResult(casino: Casino) {
         printPersonsCards(casino.persons)
@@ -20,9 +21,20 @@ object ResultView {
         println()
     }
 
+    private fun printPersonCards(person: Person) {
+        println(
+            INITIAL_CARDS_SCRIPT.format(
+                person.name,
+                person.showHandOfCards().joinToString(",") { it.toText() },
+                person.getTotal(),
+            ),
+        )
+    }
+
     private fun printFinalResult(casino: Casino) {
-        printFinalOutcome()
-        printDealerProfit(casino.getDealerProfit())
-        printPlayerProfit(casino.getPlayersProfit())
+        println(FINAL_OUTCOME_SCRIPT)
+        println("${DEALER_SCRIPT}${casino.getDealerProfit()}")
+        casino.getPlayersProfit().entries
+            .forEach { println("${it.key}: ${it.value}") }
     }
 }
