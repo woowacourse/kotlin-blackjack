b82ed83d1c8edc6388c2008c525ff7187af374b8
s9hn <tnghqn@naver.com>
2023-03-25 01:37:24 +0900

    [REFACTOR(Deck)] 어디서든 덱 생성 가능하도록 수정, 테스트코드 추가

--- src/main/kotlin/model/domain/card/Card.kt
+++ src/main/kotlin/model/domain/card/Card.kt
@@ -3,13 +3,20 @@ package model.domain.card
 import model.tools.CardNumber
 import model.tools.Shape
 
-class Card private constructor(
+data class Card constructor(
     val shape: Shape,
     val cardNumber: CardNumber,
 ) {
 
     companion object {
         private val cardCache: MutableMap<Pair<Shape, CardNumber>, Card> = mutableMapOf()
+        val deck = Shape.values().flatMap { shape -> matchCardValueAndShape(shape) }
+
+        private fun matchCardValueAndShape(shape: Shape): List<Card> =
+            CardNumber.values().map { cardNumber ->
+                of(shape, cardNumber)
+            }
+
         fun of(shape: Shape, cardNumber: CardNumber): Card =
             cardCache.getOrPut(shape to cardNumber) { Card(shape, cardNumber) }
     }
