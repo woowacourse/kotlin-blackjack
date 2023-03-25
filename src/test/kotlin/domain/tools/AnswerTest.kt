23ea8d1953bab359d9bc32d5094eb02ab5a3155a
s9hn <tnghqn@naver.com>
2023-03-25 16:05:19 +0900

    [FEAT(Answer)] Answer 객체 생성 및 테스트

--- src/test/kotlin/domain/tools/AnswerTest.kt
+++ src/test/kotlin/domain/tools/AnswerTest.kt
@@ -1,16 +1,16 @@
-package domain
+package domain.tools
 
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
-import view.Answer
+import view.tools.Answer
 
 class AnswerTest {
     @ParameterizedTest
     @ValueSource(strings = ["q", "1", "!"])
     fun `답변인 y, n가 아닌 경우 예외가 발생한다`(answer: String) {
         // when, then
-        assertThrows<IllegalArgumentException>("[ERROR] 올바른 입력이 아닙니다.") {
+        assertThrows<IllegalArgumentException>("[ERROR] 올바른 문자를 입력해주세요") {
             Answer(answer)
         }
     }
