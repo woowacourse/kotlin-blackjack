339a5c82e9aad3a7ad68c7e345d3018c6040f100
murjune <sis9221302@gmail.com>
2024-03-05 15:59:25 +0900

    study : sealed class

---
+++ src/test/kotlin/study/SealedClass.kt
@@ -0,0 +1,20 @@
+package study
+
+import org.junit.jupiter.api.Test
+
+
+sealed class Dog(val name: String) {
+    class Baby : Dog("아기강아지")
+    class Old : Dog("늙은 강아지")
+    class Puppy3 : Dog("")
+
+    fun bulk() = println("왈오라")
+}
+
+class SealedClass {
+    @Test
+    fun `abstract class`() {
+        Dog.Baby().bulk()
+        Dog.Old().bulk()
+    }
+}
