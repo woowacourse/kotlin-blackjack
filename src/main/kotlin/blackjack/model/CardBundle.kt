3cd2d771195e464dda70d67f72ec2d18e7e717ae
Namyunsuk <yunsuk0328@naver.com>
2024-03-05 19:26:02 +0900

    fix: 카드 번호 중 1 제거

--- src/main/kotlin/blackjack/model/CardBundle.kt
+++ src/main/kotlin/blackjack/model/CardBundle.kt
@@ -1,7 +1,7 @@
 package blackjack.model
 
 object CardBundle {
-    private val denominations = listOf("A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "K", "Q", "J")
+    private val denominations = listOf("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "K", "Q", "J")
     private val suites = listOf("하트", "다이아몬드", "스페이드", "클로버")
     private val bundle =
         List(denominations.size * suites.size) {
