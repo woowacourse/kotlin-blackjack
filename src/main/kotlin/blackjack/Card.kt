e3ba981d73f6e7fe58a75273862a80fc9d06ef7c
hxeyexn <kck85075@kumoh.ac.kr>
2024-03-05 16:39:40 +0900

    feat(Card): 카드는 카드 모양과 카드 숫자를 저장
    - CardShape 는 카드 모양을 저장하는 enum
    - CardNumber 는 카드 숫자를 저장하는 enum

---
+++ src/main/kotlin/blackjack/Card.kt
@@ -0,0 +1,3 @@
+package blackjack
+
+data class Card(private val shape: CardShape, private val number: CardNumber)
---
+++ src/main/kotlin/blackjack/CardShape.kt
@@ -0,0 +1,10 @@
+package blackjack
+
+enum class CardShape(
+    name: String,
+) {
+    HEART("하트"),
+    CLOVER("클로버"),
+    SPADE("스페이드"),
+    DIAMOND("다이아몬드"),
+}
