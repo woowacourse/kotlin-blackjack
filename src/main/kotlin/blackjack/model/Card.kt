b8652b6a745f4035c8c0a88ca1fd4b8a7f2455e6
Gimun Kim <kmkim7575@gmail.com>
2024-03-05 17:53:08 +0900

    feat: 카드 한 장에 대한 객체 및 에이스일 때 값을 판단하는 로직 추가
    Co-authored-by: haeum808 <haeumueah@gmail.com>

---
+++ src/main/kotlin/blackjack/model/Card.kt
@@ -0,0 +1,17 @@
+package blackjack.model
+
+class Card private constructor(val shape: String, val title: String, val value: Int) {
+    companion object {
+        fun of(
+            shape: Shape,
+            cardValue: CardValue,
+            total: Int,
+        ): Card {
+            return if (cardValue.value == 1 && total <= 10) {
+                Card(shape.title, cardValue.title, 11)
+            } else {
+                Card(shape.title, cardValue.title, cardValue.value)
+            }
+        }
+    }
+}
---
+++ src/main/kotlin/blackjack/model/Shape.kt
@@ -0,0 +1,8 @@
+package blackjack.model
+
+enum class Shape(val title: String) {
+    HEART("하트"),
+    DIAMOND("다이아몬드"),
+    SPADE("스페이드"),
+    CLOVER("클로버"),
+}
