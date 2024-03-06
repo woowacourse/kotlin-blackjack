e2067c8f775d82960bdc69313ac3e09d809fee9f
murjune <sis9221302@gmail.com>
2024-03-06 12:39:26 +0900

    feat : Card Fixture함수

---
+++ src/test/kotlin/blackjack/fixture/CardFixtures.kt
@@ -0,0 +1,10 @@
+package blackjack.fixture
+
+import blackjack.model.Card
+import blackjack.model.Rank
+import blackjack.model.Suit
+
+fun createCard(
+    type: Suit = Suit.SPADE,
+    rank: Rank = Rank.Six,
+) = Card(type, rank)
