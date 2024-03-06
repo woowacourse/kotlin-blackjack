3cd890b085b9c690d93109f22738f5ede816a62e
JunJangE <wnswkd486@gmail.com>
2024-03-06 21:03:38 +0900

    feat: Result toString() 커스텀
    Co-Authored-By: Junyoung-WON <92203597+junyoung-won@users.noreply.github.com>

--- src/main/kotlin/blackjack/model/Result.kt
+++ src/main/kotlin/blackjack/model/Result.kt
@@ -4,4 +4,12 @@ data class Result(
     val win: Int = 0,
     val defeat: Int = 0,
     val push: Int = 0,
-)
+) {
+    override fun toString(): String {
+        var answer = ""
+        if (win != 0) answer += "${win}승 "
+        if (push != 0) answer += "${push}무 "
+        if (defeat != 0) answer += "${defeat}패 "
+        return answer
+    }
+}
