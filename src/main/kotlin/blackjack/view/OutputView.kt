c93b394b90e94b761c2bbbefa27eb48ac931dbe6
JunJangE <wnswkd486@gmail.com>
2024-03-06 21:17:38 +0900

    feat: 플레이어와 딜러의 초기 카드 정보를 출력하는 기능 구현
    Co-Authored-By: Junyoung-WON <92203597+junyoung-won@users.noreply.github.com>

--- src/main/kotlin/blackjack/view/OutputView.kt
+++ src/main/kotlin/blackjack/view/OutputView.kt
@@ -1,4 +1,31 @@
 package blackjack.view
 
+import blackjack.model.Dealer
+import blackjack.model.Participants
+import blackjack.model.Player
+
 object OutputView {
+    fun printGameSetting(
+        dealer: Dealer,
+        participants: Participants,
+    ) {
+        println("${dealer.name}와 ${participants.players.joinToString(", ") { it.name }}에게 2장의 카드를 나누었습니다.")
+        showDealerInitCard(dealer) // 딜러 패 출력
+        showPlayersInitCards(participants)
+    }
+
+    private fun showDealerInitCard(dealer: Dealer) {
+        val showCard = dealer.deck.cards.first()
+        println("${dealer.name}: $showCard")
+    }
+
+    private fun showPlayersInitCards(participants: Participants) {
+        participants.players.forEach { player ->
+            showPlayerCards(player)
+        }
+    }
+
+    fun showPlayerCards(player: Player) {
+        println("${player.name}: ${player.deck.cards.joinToString(", ")}")
+    }
 }
