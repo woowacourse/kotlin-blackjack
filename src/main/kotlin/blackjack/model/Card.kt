6fa8e4302ffdfcc53a87363c8e1a2459497b27ba
JunJangE <wnswkd486@gmail.com>
2024-03-06 21:01:24 +0900

    feat: Card toString() 커스텀
    Co-Authored-By: Junyoung-WON <92203597+junyoung-won@users.noreply.github.com>

--- src/main/kotlin/blackjack/model/Card.kt
+++ src/main/kotlin/blackjack/model/Card.kt
@@ -1,3 +1,7 @@
 package blackjack.model
 
-class Card(val pattern: Pattern, val number: CardNumber)
+class Card(val pattern: Pattern, val number: CardNumber) {
+    override fun toString(): String {
+        return "${number.displayName}${pattern.displayName}"
+    }
+}
