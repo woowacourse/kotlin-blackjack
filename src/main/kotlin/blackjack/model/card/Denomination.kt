c5d4550445812410a28fbecc881ed7e955454166
김예찬 <yechan@gim-yechan-ui-noteubug.local>
2024-03-06 15:00:00 +0900

    feat: Denomination class 작성

--- src/main/kotlin/blackjack/model/card/Denomination.kt
+++ src/main/kotlin/blackjack/model/card/Denomination.kt
@@ -1,4 +1,16 @@
 package blackjack.model.card
 
-class Denomination {
+enum class Denomination(private val score: Int) {
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
+    TEN(10),
+    JACK(10),
+    QUEEN(10),
+    KING(10),
 }
