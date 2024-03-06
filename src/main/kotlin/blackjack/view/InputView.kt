a097788075ec63e26efa4f721daaf3fd617f3e26
hxeyexn <kck85075@kumoh.ac.kr>
2024-03-06 22:13:26 +0900

    feat(InputView): 플레이어가 카드를 추가로 받을지 입력

--- src/main/kotlin/blackjack/view/InputView.kt
+++ src/main/kotlin/blackjack/view/InputView.kt
@@ -1,5 +1,7 @@
 package blackjack.view
 
+import blackjack.model.Player
+
 class InputView {
     fun readPlayersName(): List<String> {
         println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)")
@@ -7,5 +9,18 @@ class InputView {
         require(input.isNotBlank()) { "이름은 공백일 수 없습니다." }
 
         return input.split(",").map { it.trim() }
+    }
+
+    fun readIsHit(player: Player): Boolean {
+        println(
+            player.name + "는 한장의 카드를 더 받겠습니까?(${Decision.YES.output}는 ${Decision.YES.input}, ${Decision.NO.output}는 ${Decision.NO.input})",
+        )
+        val input = readln()
+
+        return when (input) {
+            Decision.YES.input -> true
+            Decision.NO.input -> false
+            else -> throw IllegalArgumentException("잘못된 형식을 입력했습니다.")
+        }
     }
 }
