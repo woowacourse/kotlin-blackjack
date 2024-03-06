43f4f5cb88fade7e52e0fb3f6aaa07384c294dc2
김예찬 <yechan@gim-yechan-ui-noteubug.local>
2024-03-06 16:15:30 +0900

    feat: Hand Class 생성

--- src/main/kotlin/blackjack/model/card/Hand.kt
+++ src/main/kotlin/blackjack/model/card/Hand.kt
@@ -1,3 +1,9 @@
 package blackjack.model.card
 
-class Hand
+const val MINIMUM_CARDS_COUNT = 2
+
+class Hand(val cards: List<Card>) {
+    init {
+        require(cards.size >= MINIMUM_CARDS_COUNT)
+    }
+}
--- src/test/kotlin/blackjack/HandTest.kt
+++ src/test/kotlin/blackjack/HandTest.kt
@@ -1,3 +1,24 @@
 package blackjack
 
-class HandTest
+import blackjack.model.card.Card
+import blackjack.model.card.Denomination
+import blackjack.model.card.Hand
+import blackjack.model.card.Suit
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class HandTest {
+    @Test
+    fun `Hand 객체 생성`() {
+        val cards = listOf(Card(Denomination.ACE, Suit.HEARTS), Card(Denomination.SIX, Suit.SPADES))
+        val hand = Hand(cards)
+        assertThat(hand.cards).isEqualTo(cards)
+    }
+
+    @Test
+    fun `2장 이상 존재해야함`() {
+        val cards = listOf(Card(Denomination.ACE, Suit.HEARTS))
+        assertThrows<IllegalArgumentException> { Hand(cards) }
+    }
+}
