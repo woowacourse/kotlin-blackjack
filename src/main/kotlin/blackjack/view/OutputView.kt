245236cf1f530877cce501759cf7400bf83a1d5c
Gimun Kim <kmkim7575@gmail.com>
2024-03-06 21:49:11 +0900

    feat: 딜러 카드 기준 충족 여부에 따른 분배 메시지 및 최종 결과 출력 기능 추가
    Co-authored-by: haeum808 <haeumueah@gmail.com>

--- src/main/kotlin/blackjack/view/OutputView.kt
+++ src/main/kotlin/blackjack/view/OutputView.kt
@@ -1,5 +1,6 @@
 package blackjack.view
 
+import blackjack.model.Scoreboard
 import blackjack.model.Stat
 
 class OutputView {
@@ -8,8 +9,55 @@ class OutputView {
         playerStats: List<Stat>,
     ) {
         printDistributionMessage(playerStats, dealerStat)
-        printDealerCards(dealerStat)
+        println(getDealerCardResult(dealerStat))
         printPlayerCards(playerStats)
+    }
+
+    fun printDealerHit(dealerStat: Stat) {
+        println("${dealerStat.name}는 16이하라 한장의 카드를 더 받았습니다.")
+    }
+
+    fun printFinalCards(
+        dealerStat: Stat,
+        playerStats: List<Stat>,
+    ) {
+        println(
+            MESSAGE_RESULT.format(
+                MESSAGE_CARD_INFO.format(
+                    dealerStat.name,
+                    dealerStat.cards.joinToString(SEPARATOR_CARDS) { "${it.value}${it.title}" },
+                ),
+            ),
+        )
+
+        playerStats.forEach { playerStat ->
+            MESSAGE_RESULT.format(
+                MESSAGE_CARD_INFO.format(
+                    playerStat.name,
+                    playerStat.cards.joinToString(SEPARATOR_CARDS) { "${it.value}${it.title}" },
+                ),
+            )
+        }
+    }
+
+    fun printResult(
+        dealerResult: Scoreboard,
+        playerResult: List<String>,
+        playerStats: List<Stat>,
+        dealerStat: Stat,
+    ) {
+        println("## 최종 승패")
+        println("${dealerStat.name}: ${getDealerResult(dealerResult)}")
+        playerResult.zip(playerStats) { result, stat ->
+            println("${stat.name}: $result")
+        }
+    }
+
+    private fun getDealerResult(dealerResult: Scoreboard): String {
+        val winningResult = if (dealerResult.win > 0) "${dealerResult.win}승 " else ""
+        val drawResult = if (dealerResult.draw > 0) "${dealerResult.draw}무 " else ""
+        val losingResult = if (dealerResult.lose > 0) "${dealerResult.lose}패" else ""
+        return winningResult + drawResult + losingResult
     }
 
     private fun printPlayerCards(playerStats: List<Stat>) {
@@ -23,12 +71,10 @@ class OutputView {
         }
     }
 
-    private fun printDealerCards(dealerStat: Stat) {
-        println(
-            MESSAGE_CARD_INFO.format(
-                dealerStat.name,
-                dealerStat.cards.joinToString(SEPARATOR_CARDS) { "${it.value}${it.title}" },
-            ),
+    private fun getDealerCardResult(dealerStat: Stat): String {
+        return MESSAGE_CARD_INFO.format(
+            dealerStat.name,
+            dealerStat.cards.joinToString(SEPARATOR_CARDS) { "${it.value}${it.title}" },
         )
     }
 
@@ -43,6 +89,7 @@ class OutputView {
     companion object {
         private const val MESSAGE_DISTRIBUTION = "%s와 %s에게 2장의 카드를 나누었습니다."
         private const val MESSAGE_CARD_INFO = "%s카드: %s"
+        private const val MESSAGE_RESULT = "%s - 결과: %d"
         private const val SEPARATOR_CARDS = ", "
     }
 }
