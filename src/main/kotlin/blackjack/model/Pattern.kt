022892b13451b4e00bca709a6342a0eb0d5269bd
JunJangE <wnswkd486@gmail.com>
2024-03-06 21:01:13 +0900

    feat: Pattern displayName 프로퍼티 추가
    Co-Authored-By: Junyoung-WON <92203597+junyoung-won@users.noreply.github.com>

--- src/main/kotlin/blackjack/model/Pattern.kt
+++ src/main/kotlin/blackjack/model/Pattern.kt
@@ -1,5 +1,8 @@
 package blackjack.model
 
-enum class Pattern {
-    HEART, SPACE, DIAMOND, CLOVER;
+enum class Pattern(val displayName: String) {
+    HEART("하트"),
+    SPACE("스페이드"),
+    DIAMOND("다이아몬드"),
+    CLOVER("클로버"),
 }
