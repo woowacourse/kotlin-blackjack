9de50995429fbbc399cce04d05679a77933df053
JunJangE <wnswkd486@gmail.com>
2024-03-06 21:12:45 +0900

    feat: 게임 시작 시 2장씩을 뽑는 drawTwoCards
    Co-Authored-By: Junyoung-WON <92203597+junyoung-won@users.noreply.github.com>

--- src/main/kotlin/blackjack/model/BlackJackGame.kt
+++ src/main/kotlin/blackjack/model/BlackJackGame.kt
@@ -4,4 +4,12 @@ class BlackJackGame(
     val dealer: Dealer,
     val participants: Participants,
 ) {
+    fun drawTwoCards(gameDeck: GameDeck) {
+        repeat(2) {
+            dealer.takeCard(gameDeck.drawCard())
+            participants.players.forEach { player ->
+                player.takeCard(gameDeck.drawCard())
+            }
+        }
+    }
 }
