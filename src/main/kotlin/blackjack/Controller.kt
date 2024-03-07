579723ef418230ec4d3cb14f267df5892c95c8f2
somin <salth0601@gmail.com>
2024-03-07 11:06:39 +0900

    feat: 플레이어의 이름 입력받는 기능 구현

---
+++ src/main/kotlin/blackjack/Controller.kt
@@ -0,0 +1,14 @@
+package blackjack
+
+import blackjack.model.Dealer
+import blackjack.model.DeckManager
+import blackjack.model.Player
+import blackjack.view.InputView
+import blackjack.view.OutputView
+
+class Controller(
+) {
+    fun run() {
+        val names: List<String> = InputView.getNames()
+    }
+}
