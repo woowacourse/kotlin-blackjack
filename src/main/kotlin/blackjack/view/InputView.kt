a0b7e2db73e36c4505501536e9c6b7b9e9266eef
somin <salth0601@gmail.com>
2024-03-07 15:24:54 +0900

    feat: 입력받은 플레이어 이름이 중복되면 재입력 받기

--- src/main/kotlin/blackjack/view/InputView.kt
+++ src/main/kotlin/blackjack/view/InputView.kt
@@ -5,7 +5,9 @@ object InputView {
     tailrec fun getNames(): List<String> {
         println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)")
         val input = readln().split(",").map { it.trim() }
-        if (input.all { it.isNotEmpty() }) return input
+        val isNotDuplicated = input.toSet().size == input.size
+        val isNotEmpty = input.all { it.isNotEmpty() }
+        if (isNotDuplicated && isNotEmpty) return input
         return getNames()
     }
 
