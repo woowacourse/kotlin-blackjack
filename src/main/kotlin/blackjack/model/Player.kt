85b515851f133b63e194189a5d5b5a55fed5d3fd
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2024-03-07 16:14:20 +0900

    refactor: addCard 로직 수정 및 출력 관련 함수 변경

--- src/main/kotlin/blackjack/model/Player.kt
+++ src/main/kotlin/blackjack/model/Player.kt
@@ -1,13 +1,19 @@
 package blackjack.model
 
 class Player(val name: String, val handCards: HandCards) {
-    fun addCard(isAdd: Boolean) {
-        if (isAdd) {
+    fun addCard(playerInput: Boolean): Boolean =
+        if (isCanAddCard(playerInput)) {
             handCards.add()
+            true
+        } else {
+            false
         }
-    }
+
+    private fun isCanAddCard(isAdd: Boolean) = isAdd && !isBust()
 
     fun isBust(): Boolean = handCards.calculateCardScore() > 21
 
-    fun getCards() = handCards.cards.joinToString(", ") { "${it.cardNumber}${it.pattern}" }
+    fun getCards() = handCards.cards.joinToString(", ") { "${it.cardNumber.value}${it.pattern.shape}" }
+
+    fun getScore() = handCards.calculateCardScore()
 }
