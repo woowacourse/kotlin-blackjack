eb217dbf14931eb85f8c68f2d0e6c77966e0d7ed
somin <salth0601@gmail.com>
2024-03-07 15:20:25 +0900

    feat: 입력받은 플레이어 이름에서 공백 삭제

--- src/main/kotlin/blackjack/view/InputView.kt
+++ src/main/kotlin/blackjack/view/InputView.kt
@@ -4,7 +4,7 @@ object InputView {
 
     tailrec fun getNames(): List<String> {
         println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)")
-        val input = readln().split(",")
+        val input = readln().split(",").map { it.trim() }
         if (input.all { it.isNotEmpty() }) return input
         return getNames()
     }
