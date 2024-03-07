e71395e4f9275dae1022106a9ddcc3682d7bc404
JunJangE <wnswkd486@gmail.com>
2024-03-07 20:17:52 +0900

    feat: 딜러는 카드의 총합이 16이하일 때, 카드를 뽑는 기능 구현
    Co-Authored-By: Junyoung-WON <92203597+junyoung-won@users.noreply.github.com>

--- src/main/kotlin/blackjack/model/Dealer.kt
+++ src/main/kotlin/blackjack/model/Dealer.kt
@@ -3,3 +3,22 @@ package blackjack.model
 import blackjack.base.BaseHolder
 
 class Dealer(override val humanName: HumanName = HumanName(DEFAULT_DEALER_NAME)) : BaseHolder() {
+    fun drawDealerCard(
+        gameDeck: GameDeck,
+        printDealerDrawCard: () -> Unit,
+    ) {
+        while (hand.state == UserState.RUNNING) {
+            if (hand.calculate() <= THRESHOLD) {
+                printDealerDrawCard()
+                takeCard(gameDeck.drawCard())
+            } else {
+                hand.changeState(UserState.STAY)
+            }
+        }
+    }
+
+    companion object {
+        private const val DEFAULT_DEALER_NAME = "딜러"
+        const val THRESHOLD = 16
+    }
+}
--- src/test/kotlin/blackjack/model/DealerTest.kt
+++ src/test/kotlin/blackjack/model/DealerTest.kt
@@ -10,6 +10,15 @@ class DealerTest {
         val card = Card(Pattern.HEART, CardNumber.ACE)
         dealer.takeCard(card)
 
-        assertThat(dealer.deck.cards.last()).isEqualTo(card)
+        assertThat(dealer.hand.cards.last()).isEqualTo(card)
+    }
+
+    @Test
+    fun `딜러가 뽑은 카드의 총 합은 16을 넘어야한다`() {
+        val dealer = Dealer()
+        val gameDeck = GameDeck()
+        dealer.drawDealerCard(gameDeck) {}
+
+        assertThat(dealer.hand.calculate() > Dealer.THRESHOLD).isTrue()
     }
 }
