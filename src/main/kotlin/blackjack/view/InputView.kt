df45a146a52d1414cf7192a434cbd0f18e4dc1e3
jaeyeongjo <nos05075@naver.com>
2024-03-07 20:55:50 +0900

    feat: 카드 추가 뽑기에 대한 입력값 유효성 검사
    Co-authored-by: JoYehyun99 <jo990714@naver.com>

--- src/main/kotlin/blackjack/view/InputView.kt
+++ src/main/kotlin/blackjack/view/InputView.kt
@@ -5,14 +5,15 @@ import blackjack.model.ParticipantName
 object InputView {
     fun readNames(): List<String> {
         println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)")
-        return readln().split(",")
+        return (readlnOrNull() ?: "").split(",")
     }
 
     fun askMoreCard(name: ParticipantName): Boolean {
         println("${name}는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)")
-        return when (readln()) {
+        return when (readlnOrNull() ?: "") {
             "y" -> true
-            else -> false
+            "n" -> false
+            else -> askMoreCard(name)
         }
     }
 }
