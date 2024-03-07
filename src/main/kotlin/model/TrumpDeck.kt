3562e8a8481de5c3c853da35de77d326ef7352ba
Hogu59 <dbstjdguswkd@naver.com>
2024-03-07 19:25:18 +0900

    refactor(TrumpDeck): shuffle 기능 추가

--- src/main/kotlin/model/TrumpDeck.kt
+++ src/main/kotlin/model/TrumpDeck.kt
@@ -2,9 +2,12 @@ package model
 
 object TrumpDeck : Deck {
     private val cards: MutableList<Card> =
-        (0..51).map {
-            Card.from(it)
-        }.toMutableList()
+        (0..51)
+            .shuffled()
+            .map {
+                Card.from(it)
+            }
+            .toMutableList()
 
     override fun pop(): Card {
         return cards.removeAt(0)
