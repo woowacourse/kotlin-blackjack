ac52c817e4f523bb09dc543d8d9aaa438757ddd4
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2024-03-07 11:06:34 +0900

    feat: 카드 추가 여부 입력 구현

---
+++ src/main/kotlin/blackjack/view/IsAddCardInputView.kt
@@ -0,0 +1,21 @@
+package blackjack.view
+
+class IsAddCardInputView {
+    fun readIsAddCard(playerName: String): Boolean {
+        println("${playerName}는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)")
+        val input = readln()
+        validateChoice(input)
+        return input == YES
+    }
+
+    private fun validateChoice(input: String) {
+        require(input == YES || input == NO) {
+            "$YES 또는 ${NO}만 입력 가능합니다."
+        }
+    }
+
+    companion object {
+        private const val YES = "y"
+        private const val NO = "n"
+    }
+}
