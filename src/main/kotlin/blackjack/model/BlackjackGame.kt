7c896f634a375a39029423dea36239da034e4d01
Namyunsuk <yunsuk0328@naver.com>
2024-03-07 21:00:41 +0900

    feat: 딜러와 플레이어가 시작 시 카드를 받는 기능 구현

--- src/main/kotlin/blackjack/model/BlackjackGame.kt
+++ src/main/kotlin/blackjack/model/BlackjackGame.kt
@@ -1,6 +1,25 @@
 package blackjack.model
 
 object BlackjackGame {
+    private const val INIT_RECEIVE_CARD_COUNT = 2
+
+    fun initCard(
+        dealer: Dealer,
+        players: Players,
+        cardProvider: CardProvider,
+    ) {
+        dealer.initReceiveCard(cardProvider)
+        players.playerGroup.forEach {
+            it.initReceiveCard(cardProvider)
+        }
+    }
+
+    private fun Role.initReceiveCard(cardProvider: CardProvider) {
+        repeat(INIT_RECEIVE_CARD_COUNT) {
+            receiveCard(Card.from(cardProvider))
+        }
+    }
+
     fun updateGameResult(
         players: Players,
         dealer: Dealer,
