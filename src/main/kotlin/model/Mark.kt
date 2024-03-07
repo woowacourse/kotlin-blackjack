77a096087a79f2f8d20cdf10e302443b12c1c609
Hogu59 <dbstjdguswkd@naver.com>
2024-03-07 19:19:25 +0900

    refactor(Mark): enum class 네이밍 충돌 방지

--- src/main/kotlin/model/Mark.kt
+++ src/main/kotlin/model/Mark.kt
@@ -1,6 +1,6 @@
 package model
 
-enum class Mark(name: String, val order: Int) {
+enum class Mark(val mark: String, val order: Int) {
     SPADE("스페이드", 0),
     CLOVER("클로버", 1),
     HEART("하트", 2),
