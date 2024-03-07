56de405a81db699e9f3cbbde400fb5412123699f
somin <salth0601@gmail.com>
2024-03-07 11:02:08 +0900

    refactor: GameTable 클래스를 Deck을 관리하는 DeckManager 클래스로 수정

--- src/main/kotlin/blackjack/model/DeckManager.kt
+++ src/main/kotlin/blackjack/model/DeckManager.kt
@@ -1,16 +1,19 @@
 package blackjack.model
 
-class GameTable {
-    private val dealer: Dealer = Dealer()
+class DeckManager {
     private val deck: Deck = Deck()
 
-    fun initGame(players: List<Player>) {
+    fun initGame(dealer: Dealer, players: List<Player>) {
         players.forEach {
             it.pickCard()
             it.pickCard()
         }
         dealer.pickCard()
         dealer.pickCard()
+    }
+
+    infix fun giveCardTo(participant: Participant) {
+        participant.addCard(deck.pick())
     }
 
     private fun Participant.pickCard() {
