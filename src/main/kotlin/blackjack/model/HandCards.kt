b55ba50d1ccfe3dcb3cffa6544395154181d3124
songpink <jejeje04@naver.com>
2024-03-07 13:57:35 +0900

    refactor: HandCards를 data클래스에서 일반클래스로 변경

--- src/main/kotlin/blackjack/model/HandCards.kt
+++ src/main/kotlin/blackjack/model/HandCards.kt
@@ -1,6 +1,6 @@
 package blackjack.model
 
-data class HandCards(
+class HandCards(
     val cards: List<Card>,
     private val pointCalculator: PointCalculator = DefaultPointCalculator(),
 ) {
@@ -15,4 +15,17 @@ data class HandCards(
     fun isBust(): Boolean = sumOrNull() == null
 
     fun isBlackjack(): Boolean = (sumOrNull() == 21) && (cards.size == 2)
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as HandCards
+
+        return cards == other.cards
+    }
+
+    override fun hashCode(): Int {
+        return cards.hashCode()
+    }
 }
