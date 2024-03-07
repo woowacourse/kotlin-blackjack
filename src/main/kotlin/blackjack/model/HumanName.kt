7d5676035484fb486492eb9a65624f77f9b8c568
JunJangE <wnswkd486@gmail.com>
2024-03-07 20:41:45 +0900

    feat: 이름 유효성 검사
    Co-Authored-By: Junyoung-WON <92203597+junyoung-won@users.noreply.github.com>

--- src/main/kotlin/blackjack/model/HumanName.kt
+++ src/main/kotlin/blackjack/model/HumanName.kt
@@ -2,5 +2,13 @@ package blackjack.model
 
 @JvmInline
 value class HumanName(val name: String) {
+    init {
+        require(name.length in NAME_RANGE) { "이름의 길이는 1에서 20 사이여야 합니다" }
+    }
+
     override fun toString(): String = name
+
+    companion object {
+        val NAME_RANGE = 1..20
+    }
 }
