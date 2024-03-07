9cc75a18f1521d96dff77342931c5db3b1dcb4cf
JunJangE <wnswkd486@gmail.com>
2024-03-07 20:03:45 +0900

    refactor: Result에서 GameResult로 클래스명 변경
    Co-Authored-By: Junyoung-WON <92203597+junyoung-won@users.noreply.github.com>

---
+++ src/main/kotlin/blackjack/model/GameResult.kt
@@ -0,0 +1,27 @@
+package blackjack.model
+
+data class GameResult(
+    val win: Int = DEFAULT_RESULT_VALUE,
+    val defeat: Int = DEFAULT_RESULT_VALUE,
+    val push: Int = DEFAULT_RESULT_VALUE,
+) {
+    override fun toString(): String {
+        var answer = ""
+        if (win != DEFAULT_RESULT_VALUE) answer += "${win}승 "
+        if (push != DEFAULT_RESULT_VALUE) answer += "${push}무 "
+        if (defeat != DEFAULT_RESULT_VALUE) answer += "${defeat}패 "
+        return answer
+    }
+
+    fun deepCopy(
+        newWin: Int = DEFAULT_RESULT_VALUE,
+        newDefeat: Int = DEFAULT_RESULT_VALUE,
+        newPush: Int = DEFAULT_RESULT_VALUE,
+    ): GameResult {
+        return copy(win = win + newWin, defeat = defeat + newDefeat, push = push + newPush)
+    }
+
+    companion object {
+        const val DEFAULT_RESULT_VALUE = 0
+    }
+}
