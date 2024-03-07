aee759ac5942a024229b6186273dd770c4171272
김예찬 <yechan@gim-yechan-ui-noteubug.local>
2024-03-07 17:26:14 +0900

    feat: 플레이어 Hit 여부 확인하기

--- src/main/kotlin/blackjack/controller/BlackJackController.kt
+++ src/main/kotlin/blackjack/controller/BlackJackController.kt
@@ -1,10 +1,17 @@
 package blackjack.controller
 
+import blackjack.view.showPlayerDrawMessage
 import blackjack.view.showPlayersNameReadMessage
 
 object BlackJackController {
     fun run() {
         val playersName = readPlayersName()
+        drawOrNot(askPlayerDraw())
+    }
+
+    private fun drawOrNot(drawOrNot: String?): Boolean {
+        if (drawOrNot == "y") return true
+        return false
     }
 
     private fun readPlayersName(): List<String>? {
@@ -14,6 +21,18 @@ object BlackJackController {
         } catch (e: IllegalArgumentException) {
             println("플레이어는 최소 1명 이상이어야 합니다.")
             readPlayersName()
+        }
+    }
+
+    private fun askPlayerDraw(): String? {
+        showPlayerDrawMessage()
+        return try {
+            val drawMessage = readlnOrNull()?.trim()?.lowercase()
+            require(drawMessage == "y" || drawMessage == "n")
+            drawMessage
+        } catch (e: IllegalArgumentException) {
+            println("플레이어의 이름을 입력해주세요.")
+            askPlayerDraw()
         }
     }
 }
