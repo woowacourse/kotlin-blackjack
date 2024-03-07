5b1041e85fce69589981016d9818a382f64507ca
JunJangE <wnswkd486@gmail.com>
2024-03-07 20:24:53 +0900

    feat: 플레이어의 이름을 받는 메서드명 변경 및 출력문 추가
    Co-Authored-By: Junyoung-WON <92203597+junyoung-won@users.noreply.github.com>

--- src/main/kotlin/blackjack/view/InputView.kt
+++ src/main/kotlin/blackjack/view/InputView.kt
@@ -3,7 +3,8 @@ package blackjack.view
 import blackjack.model.HumanName
 
 object InputView {
-    fun inputParticipantsNames(): List<String> {
+    fun inputPlayersNames(): List<String> {
+        println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)")
         return readln().split(",").map { it.trim() }
     }
 
