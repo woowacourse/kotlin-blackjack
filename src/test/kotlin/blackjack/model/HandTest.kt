c816986a672816856503cecbdb6814f648067d01
JunJangE <wnswkd486@gmail.com>
2024-03-07 20:15:38 +0900

    refactor: Hand 클래스명 변경으로 인한 테스트 코드 파일명 변경
    Co-Authored-By: Junyoung-WON <92203597+junyoung-won@users.noreply.github.com>

--- src/test/kotlin/blackjack/model/HandTest.kt
+++ src/test/kotlin/blackjack/model/HandTest.kt
@@ -3,50 +3,50 @@ package blackjack.model
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 
-class DeckTest {
+class HandTest {
     @Test
     fun `카드 덱에 Ace, Queen이 있을 때, 카드 점수는 21이다`() {
-        val deck = Deck(listOf(Card(Pattern.HEART, CardNumber.ACE), Card(Pattern.HEART, CardNumber.QUEEN)))
-        Assertions.assertThat(deck.calculate()).isEqualTo(21)
+        val hand = Hand(listOf(Card(Pattern.HEART, CardNumber.ACE), Card(Pattern.HEART, CardNumber.QUEEN)))
+        Assertions.assertThat(hand.calculate()).isEqualTo(Hand.BLACKJACK_NUMBER)
     }
 
     @Test
     fun `카드 덱에 Ace, Queen, King이 있을 때, 카드 점수는 21이다`() {
-        val deck =
-            Deck(
+        val hand =
+            Hand(
                 listOf(
                     Card(Pattern.HEART, CardNumber.ACE),
                     Card(Pattern.HEART, CardNumber.QUEEN),
                     Card(Pattern.HEART, CardNumber.KING),
                 ),
             )
-        Assertions.assertThat(deck.calculate()).isEqualTo(21)
+        Assertions.assertThat(hand.calculate()).isEqualTo(Hand.BLACKJACK_NUMBER)
     }
 
     @Test
     fun `카드 덱에 Ace 두 장과 9 한 장이 있을 때, 카드 점수는 21이다`() {
-        val deck =
-            Deck(
+        val hand =
+            Hand(
                 listOf(
                     Card(Pattern.HEART, CardNumber.ACE),
-                    Card(Pattern.SPACE, CardNumber.ACE),
+                    Card(Pattern.SPADE, CardNumber.ACE),
                     Card(Pattern.HEART, CardNumber.NINE),
                 ),
             )
-        Assertions.assertThat(deck.calculate()).isEqualTo(21)
+        Assertions.assertThat(hand.calculate()).isEqualTo(Hand.BLACKJACK_NUMBER)
     }
 
     @Test
     fun `카드 덱에 Ace 두 장, King 한 장, 9 한 장이 있을 때, 카드 점수는 21이다`() {
-        val deck =
-            Deck(
+        val hand =
+            Hand(
                 listOf(
                     Card(Pattern.HEART, CardNumber.ACE),
-                    Card(Pattern.SPACE, CardNumber.ACE),
-                    Card(Pattern.SPACE, CardNumber.KING),
+                    Card(Pattern.SPADE, CardNumber.ACE),
+                    Card(Pattern.SPADE, CardNumber.KING),
                     Card(Pattern.HEART, CardNumber.NINE),
                 ),
             )
-        Assertions.assertThat(deck.calculate()).isEqualTo(21)
+        Assertions.assertThat(hand.calculate()).isEqualTo(Hand.BLACKJACK_NUMBER)
     }
 }
