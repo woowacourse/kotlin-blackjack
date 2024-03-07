250184a628a1118018a317d604358c9325baca72
JunJangE <wnswkd486@gmail.com>
2024-03-07 20:07:45 +0900

    feat: 게임의 승패 여부를 BaseHolder가 갖도록 구현
    Co-Authored-By: Junyoung-WON <92203597+junyoung-won@users.noreply.github.com>

--- src/main/kotlin/blackjack/base/BaseHolder.kt
+++ src/main/kotlin/blackjack/base/BaseHolder.kt
@@ -1,17 +1,31 @@
 package blackjack.base
 
 import blackjack.model.Card
+import blackjack.model.GameResult
+import blackjack.model.GameResult.Companion.DEFAULT_RESULT_VALUE
 import blackjack.model.Hand
 
 abstract class BaseHolder(
     hand: Hand = Hand(),
+    gameResult: GameResult = GameResult(),
 ) : BaseHuman() {
     private var _hand: Hand = hand
     val hand: Hand
         get() = _hand
 
+    private var _gameResult: GameResult = gameResult
+    val gameResult: GameResult
+        get() = _gameResult
 
     fun takeCard(card: Card) {
         _hand += card
+    }
+
+    fun changeResult(
+        newWin: Int = DEFAULT_RESULT_VALUE,
+        newDefeat: Int = DEFAULT_RESULT_VALUE,
+        newPush: Int = DEFAULT_RESULT_VALUE,
+    ) {
+        _gameResult = gameResult.deepCopy(newWin, newDefeat, newPush)
     }
 }
