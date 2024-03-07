7d3fcab1703ac72308121d3034700d54955e32ed
hxeyexn <kck85075@kumoh.ac.kr>
2024-03-07 17:06:17 +0900

    feat(Referee): 플레이어의 승패 결정 로직 수정
    - 딜러 카드 패의 합이 21 초과 이고, 플레이어 카드 패의 합이 21 이하이면 플레이어가 승리

--- src/main/kotlin/blackjack/model/Referee.kt
+++ src/main/kotlin/blackjack/model/Referee.kt
@@ -4,23 +4,22 @@ class Referee {
     fun judgeWinningResult(
         dealerSum: Int,
         playerResult: Map<String, Int>,
-    ): PlayerWinning {
-        return PlayerWinning(
-            playerResult.mapValues { (_, value) ->
-                determineGameResult(dealerSum, value)
+    ): PlayerWinning =
+        PlayerWinning(
+            playerResult.mapValues { (_, playerSum) ->
+                determineGameResult(dealerSum, playerSum)
             },
         )
-    }
 
     private fun determineGameResult(
         dealerSum: Int,
         playerSum: Int,
-    ): WinningResultStatus {
-        return when {
+    ): WinningResultStatus =
+        when {
+            playerSum > 21 -> WinningResultStatus.DEFEAT
+            dealerSum > 21 -> WinningResultStatus.VICTORY
             dealerSum > playerSum -> WinningResultStatus.DEFEAT
             dealerSum == playerSum -> WinningResultStatus.DRAW
-            playerSum > 21 -> WinningResultStatus.DEFEAT
             else -> WinningResultStatus.VICTORY
         }
-    }
 }
--- src/test/kotlin/blackjack/model/RefereeTest.kt
+++ src/test/kotlin/blackjack/model/RefereeTest.kt
@@ -21,4 +21,22 @@ class RefereeTest {
         val winningResult = Referee().judgeWinningResult(dealerSum, playerResult)
         assertThat(winningResult).isEqualTo(result)
     }
+
+    @Test
+    fun `딜러 카드 패의 합이 21 초과 이고, 플레이어 카드 패의 합이 21 이하이면 플레이어가 승리한다`() {
+        val dealerSum = 24
+        val playerResult = mapOf("해나" to 17, "심지" to 20, "악어" to 18, "팡태" to 25)
+
+        val result =
+            PlayerWinning(
+                mapOf(
+                    "해나" to WinningResultStatus.VICTORY,
+                    "심지" to WinningResultStatus.VICTORY,
+                    "악어" to WinningResultStatus.VICTORY,
+                    "팡태" to WinningResultStatus.DEFEAT,
+                ),
+            )
+        val winningResult = Referee().judgeWinningResult(dealerSum, playerResult)
+        assertThat(winningResult).isEqualTo(result)
+    }
 }
