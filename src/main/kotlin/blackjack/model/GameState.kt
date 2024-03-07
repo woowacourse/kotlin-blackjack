c8ea9b1aa17c5eea3c98a28efd0318d96a2d8b5d
JunJangE <wnswkd486@gmail.com>
2024-03-07 20:25:18 +0900

    refactor: 리팩터링으로 인한 불필요한 클래스 파일 삭제
    Co-Authored-By: Junyoung-WON <92203597+junyoung-won@users.noreply.github.com>

--- src/main/kotlin/blackjack/model/GameState.kt
+++
@@ -1,7 +0,0 @@
-package blackjack.model
-
-sealed class GameState {
-    data object Running : GameState()
-
-    class Finished(val state: UserState, val result: Result = Result()) : GameState()
-}
