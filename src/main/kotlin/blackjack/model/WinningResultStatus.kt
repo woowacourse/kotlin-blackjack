b54b4646b93a201191751e3c7c9606386c4a28ba
hxeyexn <kck85075@kumoh.ac.kr>
2024-03-07 16:07:38 +0900

    feat(WinningResultStatus): 플레이어의 승리, 패배 무승부를 뒤바꿈
    - 플레이어의 승리는 딜러의 패배로 플레이어의 패배는 딜러의 승리로 기록된다.

--- src/main/kotlin/blackjack/model/WinningResultStatus.kt
+++ src/main/kotlin/blackjack/model/WinningResultStatus.kt
@@ -4,4 +4,12 @@ enum class WinningResultStatus(val output: String) {
     VICTORY("승"),
     DEFEAT("패"),
     DRAW("무"),
+    ;
+
+    fun reverse(): WinningResultStatus =
+        when (this) {
+            VICTORY -> DEFEAT
+            DEFEAT -> VICTORY
+            DRAW -> DRAW
+        }
 }
