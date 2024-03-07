fccae18fb8afa3da6825d2808bc40d110ae8b5c8
Gimun Kim <kmkim7575@gmail.com>
2024-03-07 11:30:00 +0900

    feat: 딜러와 플레이어들에게 카드 나눠주는 절차 구현
    Co-authored-by: haeum808 <haeumueah@gmail.com>

--- src/main/kotlin/blackjack/controller/BlackJackController.kt
+++ src/main/kotlin/blackjack/controller/BlackJackController.kt
@@ -1,14 +1,33 @@
 package blackjack.controller
 
+import blackjack.model.CardDeck
+import blackjack.model.Dealer
+import blackjack.model.Player
 import blackjack.view.InputView
 import blackjack.view.OutputView
 
 class BlackJackController(
-    val inputView: InputView,
-    val outputView: OutputView,
+    private val inputView: InputView,
+    private val outputView: OutputView,
+    private val cardDeck: CardDeck,
 ) {
     fun startGame() {
         val playerNames = getPlayerNames()
+
+        val dealer = Dealer(playerNames.toSet())
+        val players = List(playerNames.size) { Player(playerNames[it]) { inputView.readContinueInput(playerNames[it]) ?: "" } }
+
+        dealer.drawCard { cardDeck.pick() }
+        players.forEach { player ->
+            player.initializeCards {
+                cardDeck.pick()
+            }
+        }
+
+        val dealerStat = dealer.getStat()
+        val playersStats = players.map { it.getStat() }
+
+        outputView.printInitialStats(dealerStat, playersStats)
     }
 
     private fun getPlayerNames(): List<String> {
