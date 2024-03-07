88a163b4656effa64b823a0ddca490e22d434e37
김진우 <jinwoo3661@naver.com>
2024-03-07 15:43:46 +0900

    test: Dealer 모델 검증 테스트

---
+++ src/test/kotlin/blackjack/model/DealerTest.kt
@@ -0,0 +1,37 @@
+package blackjack.model
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class DealerTest {
+
+    class Dealer(name: String = DEFAULT_DEALER_NAME) : Participant(name) {
+
+        fun openFirstCard(): Card {
+            return getCards().firstOrNull() ?: throw IllegalArgumentException(ERROR_CARD_INDEX)
+        }
+
+        companion object {
+            private const val DEFAULT_DEALER_NAME = "딜러"
+            private const val ERROR_CARD_INDEX = "가지고 있는 카드가 없습니다."
+        }
+    }
+
+    @Test
+    fun `딜러는 첫번째 카드를 공개할 수 있다`() {
+        val dealer = Dealer()
+        val card = Card(Denomination.NINE, Suit.DIAMOND)
+
+        dealer.draw(card)
+        val firstDealerCard = dealer.openFirstCard()
+        assertThat(firstDealerCard.getCardSuit()).isEqualTo(card.getCardSuit())
+        assertThat(firstDealerCard.getCardDenomination()).isEqualTo(card.getCardDenomination())
+    }
+
+    @Test
+    fun `딜러는 첫번째 카드를 공개할 수 없으면, 에러를 던진다`() {
+        val dealer = Dealer()
+        assertThrows<IllegalArgumentException> { dealer.openFirstCard() }
+    }
+}
