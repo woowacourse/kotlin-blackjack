903fac9f165f93cdc103359c9bf99650e854101b
Hogu59 <dbstjdguswkd@naver.com>
2024-03-07 19:28:50 +0900

    refactor(InputView): 이름을 다루는 Wrapper class인 Name으로 변경

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -1,5 +1,7 @@
 package view
 
+import model.Name
+
 object InputView {
     private const val HEADER_READ_PLAYER_NAMES = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)"
     private const val HEADER_READ_ANSWER = "%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)"
@@ -9,8 +11,8 @@ object InputView {
         return readln().split(",").map { it.trim() }.toList()
     }
 
-    fun readAnswer(name: String): String {
-        println(HEADER_READ_ANSWER.format(name))
+    fun readAnswer(name: Name): String {
+        println(HEADER_READ_ANSWER.format(name.name))
         return readln()
     }
 }
