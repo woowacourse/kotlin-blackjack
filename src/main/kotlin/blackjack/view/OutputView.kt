f3ffbe0982929ea35f2073614dbe6301c00a09f4
Junyoung-WON <wjyo60@gmail.com>
2024-03-08 15:05:49 +0900

    refactor: 중복되는 출력을 문자열 생성 메서드를 활용하여 리팩터링 진행

--- src/main/kotlin/blackjack/view/OutputView.kt
+++ src/main/kotlin/blackjack/view/OutputView.kt
@@ -1,5 +1,6 @@
 package blackjack.view
 
+import blackjack.base.BaseHolder
 import blackjack.model.Dealer
 import blackjack.model.Participants.Companion.INITIAL_CARD_COUNTS
 import blackjack.model.Player
@@ -30,8 +31,11 @@ object OutputView {
     }
 
     fun showPlayerCards(player: Player) {
-        println("${player.humanName}: ${player.hand.cards.joinToString(", ")}")
+        println(convertHolderCardsToString(player))
     }
+
+    private fun convertHolderCardsToString(holder: BaseHolder) =
+        "${holder.humanName}: ${holder.hand.cards.joinToString(", ")}"
 
     fun printDealerDrawCard() {
         println("\n딜러의 카드가 ${Dealer.THRESHOLD}이하 이므로, 1장의 카드를 더 받습니다.")
@@ -47,12 +51,12 @@ object OutputView {
     }
 
     private fun showDealerCardsResult(dealer: Dealer) {
-        println("${dealer.humanName}: ${dealer.hand.cards.joinToString(", ")} - 결과: ${dealer.hand.calculate()}")
+        println("${convertHolderCardsToString(dealer)} - 결과: ${dealer.hand.calculate()}")
     }
 
     private fun showPlayersCardsResult(playerGroup: PlayerGroup) {
         playerGroup.players.forEach { player ->
-            println("${player.humanName}: ${player.hand.cards.joinToString(", ")} - 결과: ${player.hand.calculate()}")
+            println("${convertHolderCardsToString(player)} - 결과: ${player.hand.calculate()}")
         }
     }
 
