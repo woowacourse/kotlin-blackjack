3a9320506c296ebe84a556f4f20323fedcad2636
songpink <jejeje04@naver.com>
2024-03-08 14:52:53 +0900

    feat: Deck 생성

---
+++ src/main/kotlin/blackjack/model/Deck.kt
@@ -0,0 +1,43 @@
+package blackjack.model
+
+@JvmInline
+value class Deck(
+    val cards: List<Card>,
+) : List<Card> by cards {
+    constructor(vararg cards: Card) : this(cards.toList())
+
+    fun pull(): Card {
+        return cards.shuffled().first()
+    }
+
+    fun spread(playerSize: Int): List<Card> {
+        return cards.take(DEFAULT_CARDS_COUNT * (DEALER_COUNT + playerSize))
+    }
+
+    companion object {
+        private val DECK: Deck = create()
+        private const val DEFAULT_DECK_SIZE = 1
+        private const val DEFAULT_CARDS_COUNT = 2
+        private const val DEALER_COUNT = 1
+
+        @JvmStatic
+        fun create(size: Int = DEFAULT_DECK_SIZE): Deck {
+            require(size >= DEFAULT_DECK_SIZE)
+            if (size == DEFAULT_DECK_SIZE) return DECK
+            return Deck(
+                List(size) { create().cards }.flatten(),
+            )
+        }
+
+        @JvmStatic
+        private fun create(): Deck {
+            val suits = Suit.entries
+            val ranks = Rank.entries
+            val deck: List<Card> =
+                suits.flatMap { suit ->
+                    ranks.map { rank -> Card(suit, rank) }
+                }.shuffled()
+            return Deck(deck)
+        }
+    }
+}
