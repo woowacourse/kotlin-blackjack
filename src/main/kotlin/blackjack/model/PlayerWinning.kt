c9a7b8b20c6378f3a47cb220de2860bbbd76ceea
sh1mj1 <7wlgns@gmail.com>
2024-03-08 14:23:47 +0900

    refactor(PlayerWinning): 딜러 승패 결과를 판단하는 로직 간소화

--- src/main/kotlin/blackjack/model/PlayerWinning.kt
+++ src/main/kotlin/blackjack/model/PlayerWinning.kt
@@ -4,6 +4,6 @@ data class PlayerWinning(val result: Map<PlayerName, WinningResultStatus>) {
     fun judgeDealerWinningResult(): DealerWinning =
         DealerWinning(
             result.values.groupingBy { it.reverse() }
-                .fold(initialValue = 0) { accumulator, _ -> accumulator + 1 }.toSortedMap(),
+                .eachCount(),
         )
 }
