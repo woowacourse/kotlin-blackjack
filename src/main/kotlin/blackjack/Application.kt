922e8a482efbdae16b3cc483a8774faa0cb2d531
김진우 <jinwoo3661@naver.com>
2024-03-08 11:13:06 +0900

    feat: Application main 구현

---
+++ src/main/kotlin/blackjack/Application.kt
@@ -0,0 +1,13 @@
+package blackjack
+
+import blackjack.controller.BlackJackController
+
+fun main() {
+    val blackJackController = BlackJackController()
+    blackJackController.apply {
+        startGameFlow()
+        playGame()
+        calculateResult()
+        showResult()
+    }
+}
