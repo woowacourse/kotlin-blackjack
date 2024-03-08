8781ae34bd1d4700686a35184a1f9cc1c5661716
sh1mj1 <7wlgns@gmail.com>
2024-03-08 15:00:08 +0900

    feat(Participants2): 딜러와 플레이어들을 가짐
    - 참가자들은 게임 시작 시 카드를 2장 받는다.
    - 딜러 카드 패의 합을 가져온다.
    - 플레이어들의 카드 패의 합을 이름과 짝 지어서 가져온다.

--- src/main/kotlin/blackjack/model/Participants.kt
+++ src/main/kotlin/blackjack/model/Participants.kt
@@ -26,3 +26,16 @@ data class Participants(val participants: List<Role>) {
         private val PARTICIPANTS_COUNT_RANGE = MIN_PARTICIPANTS_COUNT..MAX_PARTICIPANTS_COUNT
     }
 }
+
+data class Participants2(val dealer: Dealer, val players: Players) {
+    fun addInitialCards() {
+        dealer.addInitialCards()
+        players.players.forEach {
+            it.addInitialCards()
+        }
+    }
+
+    fun getDealerSum(): Int = dealer.cardHand.sum()
+
+    fun getPlayerResult(): Map<PlayerName, Int> = players.players.associate { it.name to it.cardHand.sum() }
+}
--- src/test/kotlin/blackjack/model/ParticipantsTest.kt
+++ src/test/kotlin/blackjack/model/ParticipantsTest.kt
@@ -4,7 +4,6 @@ import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
-import kotlin.IllegalArgumentException
 
 class ParticipantsTest {
     private val defaultParticipants =
@@ -111,6 +110,95 @@ class ParticipantsTest {
         val cardHandSize1 = participants.participants[0].cardHand.hand.size
         val cardHandSize2 = participants.participants[1].cardHand.hand.size
         val cardHandSize3 = participants.participants[2].cardHand.hand.size
+
+        assertThat(cardHandSize1).isEqualTo(2)
+        assertThat(cardHandSize2).isEqualTo(2)
+        assertThat(cardHandSize3).isEqualTo(2)
+    }
+
+    @Test
+    fun `딜러 카드 패의 합을 가져온다`() {
+        val actual = defaultParticipants.getDealerSum()
+
+        assertThat(actual).isEqualTo(17)
+    }
+
+    @Test
+    fun `플레이어들의 카드 패의 합을 이름과 짝 지어서 가져온다`() {
+        val actual = defaultParticipants.getPlayerResult()
+
+        assertThat(actual).isEqualTo(
+            mapOf(
+                PlayerName("심지") to 15,
+                PlayerName("해나") to 16,
+                PlayerName("악어") to 10,
+                PlayerName("팡태") to 17,
+            ),
+        )
+    }
+}
+
+class ParticipantsTest2 {
+    private val defaultParticipants =
+        Participants2(
+            Dealer(
+                CardHand(
+                    Card(CardShape.CLOVER, CardNumber.ACE),
+                    Card(CardShape.SPADE, CardNumber.SIX),
+                ),
+            ),
+            Players(
+                listOf(
+                    Player(
+                        PlayerName("심지"),
+                        CardHand(
+                            Card(CardShape.CLOVER, CardNumber.FIVE),
+                            Card(CardShape.SPADE, CardNumber.KING),
+                        ),
+                    ),
+                    Player(
+                        PlayerName("해나"),
+                        CardHand(
+                            Card(CardShape.HEART, CardNumber.SIX),
+                            Card(CardShape.DIAMOND, CardNumber.QUEEN),
+                        ),
+                    ),
+                    Player(
+                        PlayerName("악어"),
+                        CardHand(
+                            Card(CardShape.HEART, CardNumber.SEVEN),
+                            Card(CardShape.CLOVER, CardNumber.THREE),
+                        ),
+                    ),
+                    Player(
+                        PlayerName("팡태"),
+                        CardHand(
+                            Card(CardShape.DIAMOND, CardNumber.ACE),
+                            Card(CardShape.SPADE, CardNumber.SIX),
+                        ),
+                    ),
+                ),
+            ),
+        )
+
+    @Test
+    fun `참가자들에게 카드를 2장씩 준다`() {
+        val participants =
+            Participants2(
+                Dealer(CardHand(emptyList())),
+                Players(
+                    listOf(
+                        Player(PlayerName("심지"), CardHand(emptyList())),
+                        Player(PlayerName("해나"), CardHand(emptyList())),
+                    ),
+                ),
+            )
+
+        participants.addInitialCards()
+
+        val cardHandSize1 = participants.dealer.cardHand.hand.size
+        val cardHandSize2 = participants.players.players[0].cardHand.hand.size
+        val cardHandSize3 = participants.players.players[1].cardHand.hand.size
 
         assertThat(cardHandSize1).isEqualTo(2)
         assertThat(cardHandSize2).isEqualTo(2)
