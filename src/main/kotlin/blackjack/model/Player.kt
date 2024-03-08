9649315ce6fc44ce55ac29da14056d562f321f95
songpink <jejeje04@naver.com>
2024-03-08 14:21:15 +0900

    feat: Players 부생성자 추가

--- src/main/kotlin/blackjack/model/Player.kt
+++ src/main/kotlin/blackjack/model/Player.kt
@@ -4,6 +4,8 @@ class Player(val name: String, handCards: HandCards) {
     private val _handCards: MutableList<Card> = handCards.cards.toMutableList()
     val handCards: HandCards get() = HandCards(_handCards.toList())
 
+    constructor(pair: Pair<String, HandCards>) : this(pair.first, pair.second)
+
     fun hit(card: Card) {
         _handCards.add(card)
     }
