1cabf96397699e8202fb2918bb475c5333981928
Junyoung-WON <wjyo60@gmail.com>
2024-03-08 14:41:58 +0900

    test: 게임 덱 테스트의 import 수정 및 테스트 케이스 추가

--- src/test/kotlin/blackjack/model/GameDeckTest.kt
+++ src/test/kotlin/blackjack/model/GameDeckTest.kt
@@ -1,12 +1,22 @@
 package blackjack.model
 
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class GameDeckTest {
     @Test
     fun `카드 덱의 크기는 52개이다`() {
         val gameDeck = GameDeck()
-        Assertions.assertThat(gameDeck.cards.size).isEqualTo(GameDeck.DECK_SIZE)
+        assertThat(gameDeck.cards.size).isEqualTo(GameDeck.DECK_SIZE)
+    }
+
+    @Test
+    fun `카드 덱에서 카드 1장을 뽑아올 수 있다`() {
+        val gameDeck = GameDeck()
+        val initSize = gameDeck.cards.size
+        val anotherCard = Card(Pattern.CLOVER, CardNumber.EIGHT)
+        val cardFromDeck = gameDeck.drawCard()
+        assertThat(gameDeck.cards.size).isEqualTo(initSize - 1)
+        assertThat(cardFromDeck.javaClass).isEqualTo(anotherCard.javaClass)
     }
 }
