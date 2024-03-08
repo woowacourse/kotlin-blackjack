e3ff5e88737992d3ce159eb426f79b198d783c60
Namyunsuk <yunsuk0328@naver.com>
2024-03-08 13:54:46 +0900

    refactor: 플레이어명에 대한 유효성 검증 분리

--- src/test/kotlin/blackjack/model/PlayerNameTest.kt
+++ src/test/kotlin/blackjack/model/PlayerNameTest.kt
@@ -6,15 +6,21 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
 class PlayerNameTest {
-    @ValueSource(strings = ["OLIVE", "olives", "seogii", "chaechae", "123", "올리브", "서기!"])
+    @ValueSource(strings = ["", "olives", "byeori"])
     @ParameterizedTest
-    fun `이름이 다섯 글자 이하 소문자가 아닐 경우 예외가 발생한다`(name: String) {
+    fun `이름이 1~5글자 사이가 아닐 경우 예외가 발생한다`(name: String) {
+        assertThrows<IllegalArgumentException> { PlayerName(name) }
+    }
+
+    @ValueSource(strings = ["OLIVE", "oLive", "ㅇlive", "올리브", "서기!"])
+    @ParameterizedTest
+    fun `이름이 소문자가 아닐 경우 예외가 발생한다`(name: String) {
         assertThrows<IllegalArgumentException> { PlayerName(name) }
     }
 
     @ValueSource(strings = ["chae", "olive", "seogi"])
     @ParameterizedTest
-    fun `이름이 다섯 글자 이하 소문자일 경우 예외가 발생하지 않는다`(name: String) {
+    fun `이름이 조건에 맞는 경우 예외가 발생하지 않는다`(name: String) {
         assertDoesNotThrow { PlayerName(name) }
     }
 }
