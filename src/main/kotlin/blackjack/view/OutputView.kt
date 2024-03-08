e760de52cd59f75c1733a4a2dfcc5745282f088b
Namyunsuk <yunsuk0328@naver.com>
2024-03-08 14:20:50 +0900

    refactor: 중복 코드 제거

--- src/main/kotlin/blackjack/view/OutputView.kt
+++ src/main/kotlin/blackjack/view/OutputView.kt
@@ -18,20 +18,18 @@ class OutputView {
         println("딜러: ${dealer.getDealerInitCardsMessage()}")
 
         players.playerGroup.forEach {
-            println(it.getPlayerCardsMessage())
+            println(it.getCardsMessage(it.name.toString()))
         }
         lineBreak()
     }
 
-    fun printPlayerCardsMessage(player: Player) {
-        println("${player.name}카드: ${player.getCardsMessage()}")
-    }
-
-    private fun Player.getPlayerCardsMessage() = "${name}카드: ${getCardsMessage()}"
-
     fun printDealerAdditionalCardMessage() {
         lineBreak()
         println(DEALER_ADDITIONAL_CARD_MESSAGE)
+    }
+
+    fun printPlayerCardsMessage(player: Player) {
+        println(player.getCardsMessage(player.name.toString()))
     }
 
     fun printPlayersCardResult(
@@ -39,9 +37,9 @@ class OutputView {
         players: Players,
     ) {
         lineBreak()
-        println(dealer.getDealerCardsMessage() + dealer.getPlayerCardResult())
+        println(dealer.getCardsMessage(DEALER_NAME_MESSAGE) + dealer.getPlayerCardResult())
         players.playerGroup.forEach {
-            println(it.getPlayerCardsMessage() + it.getPlayerCardResult())
+            println(it.getCardsMessage(it.name.toString()) + it.getPlayerCardResult())
         }
         lineBreak()
     }
@@ -75,10 +73,8 @@ class OutputView {
         return getCards()[0].toCardMessage()
     }
 
-    private fun Dealer.getDealerCardsMessage() = "딜러 카드: ${getCardsMessage()}"
-
-    private fun Role.getCardsMessage(): String {
-        return getCards().joinToString(separator = CARDS_SEPARATOR, transform = { it.toCardMessage() })
+    private fun Role.getCardsMessage(name: String): String {
+        return "${name}카드: ${getCards().joinToString(separator = CARDS_SEPARATOR, transform = { it.toCardMessage() })}"
     }
 
     private fun Card.toCardMessage() = "${denomination.value}${suite.value}"
@@ -93,5 +89,6 @@ class OutputView {
         private const val DIVIDE_CARD_FINISH_MESSAGE = "딜러와 %s에게 2장의 나누었습니다."
         private const val DEALER_ADDITIONAL_CARD_MESSAGE = "딜러는 16이하라 한장의 카드를 더 받았습니다."
         private const val FINAL_GAME_RESULT_MESSAGE = "## 최종 승패"
+        private const val DEALER_NAME_MESSAGE = "딜러 "
     }
 }
