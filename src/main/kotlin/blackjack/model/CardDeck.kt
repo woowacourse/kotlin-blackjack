2282f95c37ef76d59ee2cb51d61a10feb2a43e3e
haeum808 <haeum808@gmail.com>
2024-03-10 00:11:02 +0900

    chore: ktlint에 맞추어 줄 바꿈 수정

--- src/main/kotlin/blackjack/model/CardDeck.kt
+++ src/main/kotlin/blackjack/model/CardDeck.kt
@@ -6,16 +6,19 @@ import java.util.Queue
 object CardDeck {
     private var cards: Queue<Card> = shuffledCards()
 
-    fun pick(): Card = cards.poll() ?: run {
-        cards = shuffledCards()
-        pick()
-    }
+    fun pick(): Card =
+        cards.poll() ?: run {
+            cards = shuffledCards()
+            pick()
+        }
 
     private fun shuffledCards(): LinkedList<Card> {
-        return LinkedList(Shape.entries.flatMap { shape ->
-            CardValue.entries.map { cardValue ->
-                Card(shape.title, cardValue.title, cardValue.value)
-            }
-        }.shuffled())
+        return LinkedList(
+            Shape.entries.flatMap { shape ->
+                CardValue.entries.map { cardValue ->
+                    Card(shape.title, cardValue.title, cardValue.value)
+                }
+            }.shuffled(),
+        )
     }
 }
