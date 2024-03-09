b095701e75afd9cacb5509894d4f1134d326ab1e
dpcks0509 <dpcks0509@gmail.com>
2024-03-10 03:10:40 +0900

    refactor: 카드 출력 변환 로직 view로 이동

--- src/main/kotlin/blackjack/model/Card.kt
+++ src/main/kotlin/blackjack/model/Card.kt
@@ -1,32 +1,3 @@
 package blackjack.model
 
-class Card(val number: CardNumber, val symbol: CardSymbol) {
-    override fun toString(): String {
-        val cardNumber = convertCardNumber()
-        val cardSymbol = convertCardSymbol()
-        return cardNumber + cardSymbol
-    }
-
-    private fun convertCardSymbol(): String {
-        val cardSymbol =
-            when (symbol) {
-                CardSymbol.DIAMOND -> "다이아몬드"
-                CardSymbol.HEART -> "하트"
-                CardSymbol.SPADE -> "스페이드"
-                CardSymbol.CLOVER -> "클로버"
-            }
-        return cardSymbol
-    }
-
-    private fun convertCardNumber(): String {
-        val cardNumber =
-            when (number) {
-                CardNumber.JACK -> "J"
-                CardNumber.QUEEN -> "Q"
-                CardNumber.KING -> "K"
-                CardNumber.ACE -> "A"
-                else -> number.value.toString()
-            }
-        return cardNumber
-    }
-}
+data class Card(val number: CardNumber, val symbol: CardSymbol)
