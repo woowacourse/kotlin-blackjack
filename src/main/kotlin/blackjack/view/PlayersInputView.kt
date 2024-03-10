2ed20322f8fcd0fb6ca06b1a529f2c45186f998b
gaeun <geun5744@gmail.com>
2024-03-10 17:27:38 +0900

    refactor: 함수 세분화

--- src/main/kotlin/blackjack/view/PlayersInputView.kt
+++ src/main/kotlin/blackjack/view/PlayersInputView.kt
@@ -6,17 +6,21 @@ import blackjack.model.participant.Players
 class PlayersInputView {
     fun readPlayerNames(deck: Deck): Players {
         println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)")
-        return runCatching {
-            val names =
-                readln().split(SPLIT_DELIMITER)
-                    .map { it.trim() }
-                    .filterNot { it.isBlank() }
-            Players.playerNamesOf(names, deck)
-        }.onFailure {
-            println(it.message)
-            return readPlayerNames(deck)
-        }.getOrThrow()
+        return retryWhileNoException(deck)
     }
+
+    private fun retryWhileNoException(deck: Deck) = runCatching {
+        val names =
+            readln().splitNames()
+        Players.playerNamesOf(names, deck)
+    }.onFailure {
+        println(it.message)
+        return readPlayerNames(deck)
+    }.getOrThrow()
+
+    private fun String.splitNames() = split(SPLIT_DELIMITER)
+        .map { it.trim() }
+        .filterNot { it.isBlank() }
 
     companion object {
         private const val SPLIT_DELIMITER = ","
