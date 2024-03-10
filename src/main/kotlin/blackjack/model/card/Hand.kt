5b2915c880ad755a71c87888db178bf018bfd171
murjune <sis9221302@gmail.com>
2024-03-11 01:44:20 +0900

    refactor : Hand 가 List<Card> 를 상속 받도록 변경

--- src/main/kotlin/blackjack/model/card/Hand.kt
+++ src/main/kotlin/blackjack/model/card/Hand.kt
@@ -6,33 +6,19 @@ import blackjack.model.PointCalculator
 class Hand(
     val cards: List<Card>,
     private val pointCalculator: PointCalculator = DefaultPointCalculator(BLACKJACK_NUMBER),
-) {
+) : List<Card> by cards {
     init {
         require(cards.size >= MIN_HAND_CARDS_SIZE) { "손패는 $MIN_HAND_CARDS_SIZE 장 이상임" }
     }
 
     constructor(vararg cards: Card) : this(cards.toList())
 
-    fun first(): Card = cards.first()
 
     fun sumOptimized(): Int = pointCalculator.sumOf(cards)
 
     fun isBust(): Boolean = sumOptimized() > BLACKJACK_NUMBER
 
     fun isBlackjack(): Boolean = (sumOptimized() == BLACKJACK_NUMBER) && (cards.size == MIN_HAND_CARDS_SIZE)
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Hand
-
-        return cards == other.cards
-    }
-
-    override fun hashCode(): Int {
-        return cards.hashCode()
-    }
 
     companion object {
         private const val BLACKJACK_NUMBER = 21
