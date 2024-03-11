0b51b438bf47260687b53329c3dd7bef80b64dd5
Junyoung-WON <wjyo60@gmail.com>
2024-03-11 18:34:34 +0900

    refactor: Dealer의 name 속성 제거 및 HumanName의 클래스명 변경으로 인한 리팩터링

--- src/main/kotlin/blackjack/view/OutputView.kt
+++ src/main/kotlin/blackjack/view/OutputView.kt
@@ -1,6 +1,5 @@
 package blackjack.view
 
-import blackjack.base.BaseHolder
 import blackjack.model.Dealer
 import blackjack.model.Hand
 import blackjack.model.Participants.Companion.INITIAL_CARD_COUNTS
@@ -14,7 +13,7 @@ object OutputView {
         playerGroup: PlayerGroup,
     ) {
         println(
-            "\n${dealer.humanName}와 ${playerGroup.players.joinToString(", ") { it.humanName.name }}에게 ${INITIAL_CARD_COUNTS}장의 카드를 나누었습니다.",
+            "\n딜러와 ${playerGroup.players.joinToString(", ") { it.name.name }}에게 ${INITIAL_CARD_COUNTS}장의 카드를 나누었습니다.",
         )
         showDealerInitCard(dealer)
         showPlayersInitCards(playerGroup)
@@ -23,7 +22,7 @@ object OutputView {
 
     private fun showDealerInitCard(dealer: Dealer) {
         val showCard = dealer.hand.cards.first()
-        println("${dealer.humanName}: $showCard")
+        println("딜러: $showCard")
     }
 
     private fun showPlayersInitCards(playerGroup: PlayerGroup) {
@@ -33,15 +32,15 @@ object OutputView {
     }
 
     fun showPlayerCards(player: Player) {
-        println(convertHolderCardsToString(player))
+        println(convertPlayerCardsToString(player))
         showStateOfPlayer(player)
     }
 
     private fun showStateOfPlayer(player: Player) {
         when (player.hand.state) {
-            UserState.BUST -> println("${player.humanName}의 카드가 ${Hand.BLACKJACK_NUMBER}을 초과하여 Bust되었습니다.")
-            UserState.STAY -> println("${player.humanName}이(가) Stay에 성공하였습니다.")
-            UserState.BLACKJACK -> println("${player.humanName}이(가) BlackJack을 달성했습니다.")
+            UserState.BUST -> println("${player.name}의 카드가 ${Hand.BLACKJACK_NUMBER}을 초과하여 Bust되었습니다.")
+            UserState.STAY -> println("${player.name}이(가) Stay에 성공하였습니다.")
+            UserState.BLACKJACK -> println("${player.name}이(가) BlackJack을 달성했습니다.")
             UserState.RUNNING -> return
         }
     }
@@ -63,12 +62,12 @@ object OutputView {
     }
 
     private fun showDealerCardsResult(dealer: Dealer) {
-        println("${convertHolderCardsToString(dealer)} - 결과: ${dealer.hand.calculate()}")
+        println("${convertDealerCardsToString(dealer)} - 결과: ${dealer.hand.calculate()}")
     }
 
     private fun showPlayersCardsResult(playerGroup: PlayerGroup) {
         playerGroup.players.forEach { player ->
-            println("${convertHolderCardsToString(player)} - 결과: ${player.hand.calculate()}")
+            println("${convertPlayerCardsToString(player)} - 결과: ${player.hand.calculate()}")
         }
     }
 
@@ -77,9 +76,9 @@ object OutputView {
         playerGroup: PlayerGroup,
     ) {
         println("\n[ 최종 승패 ]")
-        println("${dealer.humanName}: ${dealer.gameResult}")
+        println("딜러: ${dealer.gameResult}")
         playerGroup.players.forEach { player ->
-            println("${player.humanName}: ${player.gameResult}")
+            println("${player.name}: ${player.gameResult}")
         }
     }
 }
