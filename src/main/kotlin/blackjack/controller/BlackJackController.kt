ad7e7bede7ff5637a73fd827879d4df918b18d25
songpink <jejeje04@naver.com>
2024-03-11 15:51:37 +0900

    refactor: 함수 호출 순서에 맞춰 배치

--- src/main/kotlin/blackjack/controller/BlackJackController.kt
+++ src/main/kotlin/blackjack/controller/BlackJackController.kt
@@ -14,10 +14,22 @@ class BlackJackController(
 ) {
     fun start() {
         val playersNames: List<String> = inputView.inputPlayerNames()
-        val deck: Deck = Deck.create()
+        val deck: Deck = Deck.create(1)
         val (dealer, players) = spreadCards(deck, playersNames)
         hitParticipant(players, deck, dealer)
         showScoreBoard(players, dealer)
+    }
+
+    private fun spreadCards(
+        deck: Deck,
+        playersNames: List<String>,
+    ): Participant {
+        val (playerHand, dealerHand) =
+            ParticipantsHand.from(deck.spread(playersNames.size))
+        val dealer = Dealer(dealerHand)
+        val players: List<Player> = Player.createPlayers(playersNames, playerHand)
+        outputView.showDivided(dealerHand.first(), players)
+        return Participant(dealer, players)
     }
 
     private fun hitParticipant(
@@ -28,19 +40,6 @@ class BlackJackController(
         hitPlayers(players, deck)
         dealer.hitUntilBust(deck) { outputView.showDealerHitCard() }
         outputView.showDealerScore(dealer.hand.cards, dealer.hand.sumOptimized())
-    }
-
-    private fun showScoreBoard(
-        players: List<Player>,
-        dealer: Dealer,
-    ) {
-        players.forEach {
-            val name = it.name
-            val handCards = it.hand
-            outputView.showPlayerScore(name, handCards.cards, handCards.sumOptimized())
-        }
-        val scoreBoard = dealer.createScoreBoard(players)
-        outputView.showScoreBoard(scoreBoard)
     }
 
     private fun hitPlayers(
@@ -55,15 +54,16 @@ class BlackJackController(
         }
     }
 
-    private fun spreadCards(
-        deck: Deck,
-        playersNames: List<String>,
-    ): Participant {
-        val (playerHand, dealerHand) =
-            ParticipantsHand.from(deck.spread(playersNames.size))
-        val dealer = Dealer(dealerHand)
-        val players: List<Player> = Player.createPlayers(playersNames, playerHand)
-        outputView.showDivided(dealerHand.first(), players)
-        return Participant(dealer, players)
+    private fun showScoreBoard(
+        players: List<Player>,
+        dealer: Dealer,
+    ) {
+        players.forEach {
+            val name = it.name
+            val handCards = it.hand
+            outputView.showPlayerScore(name, handCards.cards, handCards.sumOptimized())
+        }
+        val scoreBoard = dealer.createScoreBoard(players)
+        outputView.showScoreBoard(scoreBoard)
     }
 }
