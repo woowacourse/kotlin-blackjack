abc439f1242846e678b2cf272014bf017441fb05
kkosang <eiwoqp77@gmail.com>
2024-03-12 01:42:37 +0900

    refactor: Test Fixture 적용

--- src/test/kotlin/blackjack/model/BlackJackTest.kt
+++ src/test/kotlin/blackjack/model/BlackJackTest.kt
@@ -2,20 +2,50 @@ package blackjack.model
 
 import blackjack.state.State
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class BlackJackTest {
+    private lateinit var defaultBlackJack: BlackJack
+    private lateinit var bustBlackJack: BlackJack
+    private lateinit var aceCountOneBlackJack: BlackJack
+    private lateinit var aceCountFourBlackJack: BlackJack
+
+    @BeforeEach
+    fun setUp() {
+        defaultBlackJack = BlackJack()
+        bustBlackJack =
+            BlackJack().apply {
+                addCard(Card(Denomination.KING, Suit.SPADE))
+                addCard(Card(Denomination.KING, Suit.CLOVER))
+                addCard(Card(Denomination.KING, Suit.DIAMOND))
+            }
+        aceCountOneBlackJack =
+            BlackJack().apply {
+                addCard(Card(Denomination.ACE, Suit.SPADE))
+                addCard(Card(Denomination.KING, Suit.CLOVER))
+                addCard(Card(Denomination.NINE, Suit.DIAMOND))
+            }
+        aceCountFourBlackJack =
+            BlackJack().apply {
+                addCard(Card(Denomination.ACE, Suit.SPADE))
+                addCard(Card(Denomination.ACE, Suit.CLOVER))
+                addCard(Card(Denomination.ACE, Suit.HEART))
+                addCard(Card(Denomination.ACE, Suit.DIAMOND))
+                addCard(Card(Denomination.SEVEN, Suit.DIAMOND))
+                addCard(Card(Denomination.NINE, Suit.SPADE))
+            }
+    }
+
     @Test
     fun `유저가 더 이상 카드를 드로우 하지 않을 경우 테스트`() {
-        val blackJack = BlackJack()
-        blackJack.switchToStayState()
-        assertThat(blackJack.state).isEqualTo(State.Finish.Stay)
+        defaultBlackJack.switchToStayState()
+        assertThat(defaultBlackJack.state).isEqualTo(State.Finish.Stay)
     }
 
     @Test
     fun `BlackJack 생성시 상태 확인 테스트`() {
-        val blackjack = BlackJack()
-        assertThat(blackjack.checkDrawState()).isEqualTo(true)
+        assertThat(defaultBlackJack.checkDrawState()).isEqualTo(true)
     }
 
     @Test
@@ -42,40 +72,19 @@ class BlackJackTest {
 
     @Test
     fun `카드를 드로우 했을 때, 정상적인 Bust 상태로 전환 테스트`() {
-        val bustBlackJack = BlackJack()
-        bustBlackJack.apply {
-            addCard(Card(Denomination.KING, Suit.SPADE))
-            addCard(Card(Denomination.KING, Suit.CLOVER))
-            addCard(Card(Denomination.KING, Suit.DIAMOND))
-        }
         bustBlackJack.changeState()
         assertThat(bustBlackJack.state).isEqualTo(State.Finish.Bust)
     }
 
     @Test
     fun `카드를 드로우 했을 때, ACE가 1개 있는 경우 버스트 상태에서 정상적으로 Hit 상태 전환 테스트`() {
-        val aceBlackJack = BlackJack()
-        aceBlackJack.apply {
-            addCard(Card(Denomination.ACE, Suit.SPADE))
-            addCard(Card(Denomination.KING, Suit.CLOVER))
-            addCard(Card(Denomination.NINE, Suit.DIAMOND))
-        }
-        aceBlackJack.changeState()
-        assertThat(aceBlackJack.state).isEqualTo(State.Action.Hit)
+        aceCountOneBlackJack.changeState()
+        assertThat(aceCountOneBlackJack.state).isEqualTo(State.Action.Hit)
     }
 
     @Test
     fun `카드를 드로우 했을 때, ACE가 4개 있는 경우 버스트 상태에서 정상적으로 Hit 상태 전환 테스트`() {
-        val aceBlackJack = BlackJack()
-        aceBlackJack.apply {
-            addCard(Card(Denomination.ACE, Suit.SPADE))
-            addCard(Card(Denomination.ACE, Suit.CLOVER))
-            addCard(Card(Denomination.ACE, Suit.HEART))
-            addCard(Card(Denomination.ACE, Suit.DIAMOND))
-            addCard(Card(Denomination.SEVEN, Suit.DIAMOND))
-            addCard(Card(Denomination.NINE, Suit.SPADE))
-        }
-        aceBlackJack.changeState()
-        assertThat(aceBlackJack.state).isEqualTo(State.Action.Hit)
+        aceCountFourBlackJack.changeState()
+        assertThat(aceCountFourBlackJack.state).isEqualTo(State.Action.Hit)
     }
 }
--- src/test/kotlin/blackjack/model/DealerTest.kt
+++ src/test/kotlin/blackjack/model/DealerTest.kt
@@ -1,10 +1,31 @@
 package blackjack.model
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 class DealerTest {
+    private lateinit var drawableDealer: Dealer
+    private lateinit var unDrawableDealer: Dealer
+
+    @BeforeEach
+    fun setUp() {
+        drawableDealer = Dealer()
+        drawableDealer.apply {
+            draw(Card(Denomination.FOUR, Suit.SPADE))
+            draw(Card(Denomination.FOUR, Suit.DIAMOND))
+            draw(Card(Denomination.FOUR, Suit.CLOVER))
+            draw(Card(Denomination.FOUR, Suit.HEART))
+        }
+
+        unDrawableDealer = Dealer()
+        unDrawableDealer.apply {
+            draw(Card(Denomination.QUEEN, Suit.SPADE))
+            draw(Card(Denomination.SEVEN, Suit.SPADE))
+        }
+    }
+
     @Test
     fun `딜러는 첫번째 카드를 공개할 수 있다`() {
         val dealer = Dealer()
@@ -24,19 +45,11 @@ class DealerTest {
 
     @Test
     fun `딜러는 손패의 합이 16을 초과하지 않으면 카드를 드로우 해야 한다`() {
-        val drawableDealer = Dealer()
-        drawableDealer.draw(Card(Denomination.FOUR, Suit.SPADE))
-        drawableDealer.draw(Card(Denomination.FOUR, Suit.DIAMOND))
-        drawableDealer.draw(Card(Denomination.FOUR, Suit.CLOVER))
-        drawableDealer.draw(Card(Denomination.FOUR, Suit.HEART))
         assertThat(drawableDealer.checkDealerScoreCondition()).isTrue()
     }
 
     @Test
     fun `딜러는 손패의 합이 16을 초과할 때 카드를 더 이상 드로우할 수 없다`() {
-        val unDrawableDealer = Dealer()
-        unDrawableDealer.draw(Card(Denomination.QUEEN, Suit.SPADE))
-        unDrawableDealer.draw(Card(Denomination.SEVEN, Suit.SPADE))
         assertThat(unDrawableDealer.checkDealerScoreCondition()).isFalse()
     }
 }
