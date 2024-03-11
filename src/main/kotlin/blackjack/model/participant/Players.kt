2ae8206ad75b60cc6c1a5a765ab6048a1a078096
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2024-03-11 22:40:14 +0900

    fix: 중복된 검증 로직 삭제

--- src/main/kotlin/blackjack/model/participant/Players.kt
+++ src/main/kotlin/blackjack/model/participant/Players.kt
@@ -7,7 +7,6 @@ class Players(private val names: Set<String>, deck: Deck) {
 
     init {
         require(names.size in MIN_PLAYER_COUNT..MAX_PLAYER_COUNT) { "${MIN_PLAYER_COUNT}명 이상 ${MAX_PLAYER_COUNT}명 이하의 플레이어만 가능합니다." }
-        require(names.size == names.toSet().size) { "중복된 이름은 플레이어로 등록할 수 없습니다." }
     }
 
     companion object {
