c484ca28f39136d35a2695d1710b3bd8a2fc74c0
김진우 <jinwoo3661@naver.com>
2024-03-12 01:38:19 +0900

    feat: 오류에 대한 출력 기능 추가

--- src/main/kotlin/blackjack/model/Dealer.kt
+++ src/main/kotlin/blackjack/model/Dealer.kt
@@ -2,16 +2,18 @@ package blackjack.model
 
 class Dealer(name: String = DEFAULT_DEALER_NAME) : Participant(name) {
     fun openFirstCard(): Card? {
-        return getCards().firstOrNull()
+        return getCards().firstOrNull().also { card ->
+            card ?: println(ERROR_CARD_INDEX)
+        }
     }
 
-    fun checkDealerScoreCondition(): Boolean {
+    fun checkShouldDealerDrawCard(): Boolean {
         return getBlackJackScore() <= MIN_HAND_CARD_SCORE
     }
 
     companion object {
         private const val DEFAULT_DEALER_NAME = "딜러"
-        private const val ERROR_CARD_INDEX = "가지고 있는 카드가 없습니다."
+        const val ERROR_CARD_INDEX = "딜러가 가지고 있는 카드가 없습니다."
         const val MIN_HAND_CARD_SCORE: Int = 16
     }
 }
