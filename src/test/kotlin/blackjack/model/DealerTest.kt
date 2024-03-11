5582b8781f2aba9dd997e9789ac6c7a6d548423d
aprilgom <aprilgom@gmail.com>
2024-03-11 21:31:19 +0900

    test(DealerTest): 테스트 명 변경

--- src/test/kotlin/blackjack/model/DealerTest.kt
+++ src/test/kotlin/blackjack/model/DealerTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test
 class DealerTest {
 
     @Test
-    fun `카드 총 합이 16 이하면 true를 반환한다`() {
+    fun `딜러는 카드 총 합이 16 이하면 카드를 더 뽑아야 한다`() {
         val dealer = Dealer()
         dealer.addCard(Card(CardNumber.`3`, Suit.`하트`))
         val actual = dealer.isHitable()
@@ -14,7 +14,7 @@ class DealerTest {
     }
 
     @Test
-    fun `카드 총 합이 17 이상이면 false를 반환한다`() {
+    fun `딜러는 카드 총 합이 17 이상이면 카드 뽑는것을 멈춘다`() {
         val dealer = Dealer()
         dealer.addCard(Card(CardNumber.`10`, Suit.`하트`))
         dealer.addCard(Card(CardNumber.`7`, Suit.`하트`))
