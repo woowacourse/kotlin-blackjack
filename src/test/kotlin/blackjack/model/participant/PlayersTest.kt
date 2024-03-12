eda53eebee10c90aafb2555a5b7c1dab084743bd
kimhm0728 <kimhm0728@naver.com>
2024-03-12 20:24:19 +0900

    test: Players 예외가 발생하지 않는 테스트 추가

--- src/test/kotlin/blackjack/model/participant/PlayersTest.kt
+++ src/test/kotlin/blackjack/model/participant/PlayersTest.kt
@@ -1,6 +1,7 @@
 package blackjack.model.participant
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
@@ -19,6 +20,13 @@ class PlayersTest {
     fun `플레이어 수가 제한 범위를 넘기는 경우 예외가 발생한다`(playersName: List<String>) {
         assertThrows<IllegalArgumentException> {
             Players.from(playersName)
+        }
+    }
+
+    @Test
+    fun `플레이어명, 플레이어 수가 올바른 경우 예외가 발생하지 않는다`() {
+        assertDoesNotThrow {
+            Players.from(listOf("olive", "abc", "defgh"))
         }
     }
 
