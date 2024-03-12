6f8e8291881e395f290ba31bf64cd0fbf678fcd9
Gimun Kim <kmkim7575@gmail.com>
2024-03-12 14:21:54 +0900

    refactor: CardDeck의 카드들을 지연 초기화하도록 변경

--- src/main/kotlin/blackjack/model/CardDeck.kt
+++ src/main/kotlin/blackjack/model/CardDeck.kt
@@ -1,10 +1,11 @@
 package blackjack.model
 
 object CardDeck {
-    private val leftCards: MutableList<Card> =
+    private val leftCards: MutableList<Card> by lazy {
         Shape.entries.flatMap { shape ->
             CardValue.entries.map { cardValue -> Card(shape, cardValue.title, cardValue.value) }
         }.shuffled().toMutableList()
+    }
 
     fun pick(): Card? = leftCards.removeLastOrNull()
 }
