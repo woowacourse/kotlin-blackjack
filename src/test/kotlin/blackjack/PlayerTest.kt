579829a3ffeaf3ee498390a2c956fa3899bee10d
김예찬 <yechan@gim-yechan-ui-noteubug.local>
2024-03-12 11:00:33 +0900

    refect: playerTest draw state확인 테스트

--- src/test/kotlin/blackjack/PlayerTest.kt
+++ src/test/kotlin/blackjack/PlayerTest.kt
@@ -5,16 +5,25 @@ import blackjack.model.card.Card
 import blackjack.model.card.Denomination
 import blackjack.model.card.Hand
 import blackjack.model.card.Suit
+import blackjack.model.game.State
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class PlayerTest {
     @Test
     fun `Player 생성`() {
-        val cards = Hand(mutableListOf(Card(Denomination.ACE, Suit.HEARTS), Card(Denomination.SIX, Suit.SPADES)))
-        val player = Player("하디", cards)
+        val hand = Hand(mutableListOf(Card(Denomination.ACE, Suit.HEARTS), Card(Denomination.SIX, Suit.SPADES)))
+        val player = Player("하디", hand)
 
         assertThat(player.name).isEqualTo("하디")
-        assertThat(player.hand).isEqualTo(cards)
+        assertThat(player.hand).isEqualTo(hand)
+    }
+
+    @Test
+    fun `Player의 Hit 상태여부`() {
+        val hand = Hand(mutableListOf(Card(Denomination.ACE, Suit.HEARTS), Card(Denomination.SIX, Suit.SPADES)))
+        val player = Player("하디", hand)
+
+        assertThat(player.state).isEqualTo(State.Running.Hit)
     }
 }
