5508676f2197adfb7c7253489a3977ff093b9039
murjune <sis9221302@gmail.com>
2024-03-13 02:49:20 +0900

    test : RunningTest 이름 수정

--- src/test/kotlin/blackjack/state/RunningTest.kt
+++ src/test/kotlin/blackjack/state/RunningTest.kt
@@ -6,9 +6,9 @@ import blackjack.model.card.Rank
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
-class StateTest {
+class RunningTest {
     @Test
-    fun `Running 상태에서 hit 하면, hand 가 bust 면 Bust 상태 반환`() {
+    fun `hand 가 bust 일 때 hit 하면, Bust 상태 반환`() {
         // given
         val hand = Hand(createCard(rank = Rank.TEN), createCard(rank = Rank.TEN))
         val card = createCard(rank = Rank.TWO)
@@ -20,7 +20,7 @@ class StateTest {
     }
 
     @Test
-    fun `Running 상태에서 hit 하면, hand 가 bust 가 아니면 Running 상태가 된다`() {
+    fun `hand 가 bust 가 아니고 hit 하면, Running 상태가 반환`() {
         // given
         val hand = Hand(createCard(rank = Rank.TEN), createCard(rank = Rank.TEN))
         val card = createCard(rank = Rank.ACE)
@@ -32,7 +32,7 @@ class StateTest {
     }
 
     @Test
-    fun `Running 상태이고 stay 했을 때, hand 가 blackJack 이면 BlackJack 상태가 된다`() {
+    fun `stay 했을 때, hand 가 blackJack 이면 BlackJack 상태가 된다`() {
         // given
         val hand = Hand(createCard(rank = Rank.TEN), createCard(rank = Rank.ACE))
         val runningState = State.Running(hand)
@@ -44,7 +44,7 @@ class StateTest {
     }
 
     @Test
-    fun `Running 상태이고 stay 했을 때, hand 가 Bust 면 Bust 상태가 된다`() {
+    fun `stay 했을 때, hand 가 Bust 면 Bust 상태가 된다`() {
         // given
         val hand = Hand(createCard(rank = Rank.TEN), createCard(rank = Rank.TEN), createCard(rank = Rank.TWO))
         val runningState = State.Running(hand)
@@ -55,7 +55,7 @@ class StateTest {
     }
 
     @Test
-    fun `Running 상태이고 stay 했을 때, hand 가 Bust 도 BlackJack 도 아니면 Stop 상태가 된다`() {
+    fun `stay 했을 때, hand 가 Bust 도 BlackJack 도 아니면 Stop 상태가 된다`() {
         // given
         val hand = Hand(createCard(rank = Rank.TEN), createCard(rank = Rank.TEN))
         val runningState = State.Running(hand)
