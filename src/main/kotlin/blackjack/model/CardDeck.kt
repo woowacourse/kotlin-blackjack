3bc0b90643b270f9c0facbc0cc98b8f1d63cd321
Gimun Kim <kmkim7575@gmail.com>
2024-03-12 14:26:45 +0900

    refactor: 현재 뽑을 수 있는 카드 변수명 변경

--- src/main/kotlin/blackjack/model/CardDeck.kt
+++ src/main/kotlin/blackjack/model/CardDeck.kt
@@ -1,11 +1,11 @@
 package blackjack.model
 
 object CardDeck {
-    private val leftCards: MutableList<Card> by lazy {
+    private val availableCards: MutableList<Card> by lazy {
         Shape.entries.flatMap { shape ->
             CardValue.entries.map { cardValue -> Card(shape, cardValue.title, cardValue.value) }
         }.shuffled().toMutableList()
     }
 
-    fun pick(): Card? = leftCards.removeLastOrNull()
+    fun pick(): Card? = availableCards.removeLastOrNull()
 }
