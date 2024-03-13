8b1fc219a6d517ea87e88179743e342d3bec1ac6
sh1mj1 <7wlgns@gmail.com>
2024-03-14 08:40:26 +0900

    feat(OutputView2): 블랙잭 게임을 위해 출력을 해준다

---
+++ src/main/kotlin/blackjack/view/OutputView2.kt
@@ -0,0 +1,99 @@
+package blackjack.view
+
+import blackjack.model.DealerWinning
+import blackjack.model.PlayerWinning
+import blackjack.model.card.CardHand
+import blackjack.model.role.Dealer
+import blackjack.model.role.Players
+import blackjack.model.role.Role
+
+class OutputView2 {
+    fun printInitialDealing(
+        dealer: Dealer,
+        players: Players,
+    ) {
+        printInitialDealingPrompt(dealer, players)
+        printInitialCardHands(dealer, players)
+    }
+
+    private fun printInitialDealingPrompt(
+        dealer: Dealer,
+        players: Players,
+    ) {
+        println(dealer.name.name + "와 " + players.players.joinToString { it.name.name } + "에게 2장씩 카드를 나눴습니다")
+    }
+
+    private fun printInitialCardHands(
+        dealer: Dealer,
+        players: Players,
+    ) {
+        print(NAME_CARD_HAND_FORMAT.format(dealer.name.name))
+        printFirstCardHand(dealer.state.getCardHands())
+
+        players.players.forEach {
+            print(NAME_CARD_HAND_FORMAT.format(it.name.name))
+            printAllCardHand(it.state.getCardHands())
+            println()
+        }
+    }
+
+    private fun printFirstCardHand(cardHand: CardHand) {
+        val (shape, number) = cardHand.hand.first()
+        println(number.number.toString() + shape.shape)
+    }
+
+    fun printPlayerCardHand(role: Role) {
+        print(NAME_CARD_HAND_FORMAT.format(role.name.name))
+        printAllCardHand(role.state.getCardHands())
+    }
+
+    fun printDealerHit() = println("\n딜러는 16이하라 한장의 카드를 더 받았습니다.")
+
+    fun printFinalCardHands(
+        dealer: Dealer,
+        players: Players,
+    ) {
+        println("\n")
+
+        printPlayerCardHand(dealer)
+        println(CARD_HAND_SUM_FORMAT.format(dealer.state.getCardHands().calculateScore()))
+
+        players.players.forEach {
+            printPlayerCardHand(it)
+            println(CARD_HAND_SUM_FORMAT.format(it.state.getCardHands().calculateScore()))
+        }
+    }
+
+    fun printWinningResult(
+        dealerWinning: DealerWinning,
+        playerWinning: PlayerWinning,
+    ) {
+        println("\n## 최종 승패")
+        printDealerWinningResult(dealerWinning)
+        printPlayerWinningResult(playerWinning)
+    }
+
+    private fun printPlayerWinningResult(playerWinning: PlayerWinning) {
+        playerWinning.result.forEach { (name, status) ->
+            println("${name.name}: ${status.output}")
+        }
+        println()
+    }
+
+    private fun printDealerWinningResult(dealerWinning: DealerWinning) {
+        print("딜러: ")
+        dealerWinning.result.forEach {
+            print(it.value.toString() + it.key.output + " ")
+        }
+        println()
+    }
+
+    private fun printAllCardHand(cardHand: CardHand) {
+        print(cardHand.hand.joinToString { it.number.output + it.shape.shape })
+    }
+
+    companion object {
+        private const val NAME_CARD_HAND_FORMAT = "%s 카드: "
+        private const val CARD_HAND_SUM_FORMAT = " - 결과: %d"
+    }
+}
