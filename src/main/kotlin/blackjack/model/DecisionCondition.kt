626a13029b201b7c0c71e62440f3cd67c7fdd2ba
sh1mj1 <7wlgns@gmail.com>
2024-03-14 08:54:59 +0900

    refactor(DecisionCondition): 사용하지 않는 클래스 삭제

--- src/main/kotlin/blackjack/model/DecisionCondition.kt
+++
@@ -1,9 +0,0 @@
-package blackjack.model
-
-fun interface DecisionCondition {
-    fun isHit(cardHandSum: Int): Boolean
-}
-
-class DealerDecisionCondition : DecisionCondition {
-    override fun isHit(cardHandSum: Int): Boolean = cardHandSum <= 16
-}
