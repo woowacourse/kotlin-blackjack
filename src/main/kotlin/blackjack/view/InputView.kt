bf16d820e2e90a24799b7f9aab7b20f49256c9eb
Namyunsuk <yunsuk0328@naver.com>
2024-03-14 13:35:50 +0900

    feat: 배팅금액을 입력 받는 기능 구현

--- src/main/kotlin/blackjack/view/InputView.kt
+++ src/main/kotlin/blackjack/view/InputView.kt
@@ -17,6 +17,15 @@ class InputView {
         }
     }
 
+    tailrec fun readPlayerBattingAmount(player: Player): Int {
+        println(READ_PLAYER_BATTING_AMOUNT_MESSAGE.format(player.name))
+        val battingAmount = readln()
+        return battingAmount.toIntOrNull() ?: run {
+            println(INVALID_BATTING_AMOUNT_MESSAGE)
+            readPlayerBattingAmount(player)
+        }
+    }
+
     private fun getMoreCardDecision(moreCardDecision: String): Boolean? {
         if (moreCardDecision == MORE_CARD_MESSAGE) return true
         if (moreCardDecision == NO_MORE_CARD_MESSAGE) return false
@@ -26,6 +35,8 @@ class InputView {
     companion object {
         private const val PLAYERS_NAME_DELIMITER = ","
         private const val READ_PLAYERS_NAME_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)"
+        private const val READ_PLAYER_BATTING_AMOUNT_MESSAGE = "%s의 배팅 금액은?"
+        private const val INVALID_BATTING_AMOUNT_MESSAGE = "배팅금액은 숫자로 입력해 주세요."
         private const val READ_MORE_CARD_DECISION_MESSAGE = "%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)"
         private const val INVALID_MORE_CARD_DECISION_MESSAGE = "소문자 y 또는 n으로 입력해 주세요."
         private const val MORE_CARD_MESSAGE = "y"
