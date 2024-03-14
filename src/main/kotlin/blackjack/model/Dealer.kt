25b32771abdc24c93a16a854259e6a11ed766fd6
김진우 <jinwoo3661@naver.com>
2024-03-15 00:58:04 +0900

    feat: 딜러의 베팅 계산 기능 구현

--- src/main/kotlin/blackjack/model/Dealer.kt
+++ src/main/kotlin/blackjack/model/Dealer.kt
@@ -5,6 +5,47 @@ class Dealer(wallet: Wallet = Wallet(DEFAULT_DEALER_NAME)) : Participant(wallet)
         return getCards().firstOrNull()
     }
 
+    fun settleBettingMoneys(
+        players: List<Player>,
+        results: List<Result>,
+    ) {
+        players.zip(results).forEach { (player, result) ->
+            val payout = settleBettingPayout(
+                result = result,
+                isBlackJackState = player.checkBlackJackState()
+            )
+            val playerBettingResultMoney = player.getBettingMoney() * payout.toInt()
+            val dealerBettingMoney = getBettingMoney()
+            settleBettingMoney(dealerBettingMoney - playerBettingResultMoney)
+            player.settleBettingMoney(playerBettingResultMoney)
+        }
+    }
+
+    private fun settleBettingPayout(
+        result: Result,
+        isBlackJackState: Boolean,
+    ): Float {
+        return when (result) {
+            Result.WIN -> {
+                if (isBlackJackState) {
+                    1.5f
+                } else {
+                    1f
+                }
+            }
+
+            Result.DRAW -> {
+                if (isBlackJackState) {
+                    1.5f
+                } else {
+                    1f
+                }
+            }
+
+            Result.LOSE -> -1f
+        }
+    }
+
     override fun openInitCards(): List<Card> {
         return getCards().firstOrNull()?.let { listOf(it) } ?: listOf()
     }
