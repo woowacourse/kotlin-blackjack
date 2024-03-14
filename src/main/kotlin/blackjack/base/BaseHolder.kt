89fc32d468895160cbae60fbc83ba3edabd805fb
Junyoung-WON <wjyo60@gmail.com>
2024-03-14 18:07:51 +0900

    refactor: BaseHolder가 게임 진행 상태 State를 가지며 상태 변경에 필요한 메서드들을 구현

--- src/main/kotlin/blackjack/base/BaseHolder.kt
+++ src/main/kotlin/blackjack/base/BaseHolder.kt
@@ -1,27 +1,34 @@
 package blackjack.base
 
-import blackjack.model.Card
+import blackjack.model.GameDeck
 import blackjack.model.GameResult
-import blackjack.model.GameResult.Companion.DEFAULT_RESULT_VALUE
-import blackjack.model.GameStatus
+import blackjack.model.Hit
+import blackjack.model.Running
+import blackjack.model.State
 
-abstract class BaseHolder(
-    val status: GameStatus = GameStatus(),
-    gameResult: GameResult = GameResult(),
-) {
+abstract class BaseHolder(gameResult: GameResult = GameResult()) {
+    private var _state: State = Running()
+    val state: State
+        get() = _state
+
     private var _gameResult: GameResult = gameResult
     val gameResult: GameResult
         get() = _gameResult
 
-    fun takeCard(card: Card) {
-        status.getCard(card)
+    fun initHands(gameDeck: GameDeck) {
+        _state = Running().drawInitCards(gameDeck)
     }
 
-    fun changeResult(
-        newWin: Int = DEFAULT_RESULT_VALUE,
-        newDefeat: Int = DEFAULT_RESULT_VALUE,
-        newPush: Int = DEFAULT_RESULT_VALUE,
-    ) {
-        _gameResult += GameResult(newWin, newDefeat, newPush)
+    protected fun changeState(state: State) {
+        _state = state
+    }
+
+    fun getCard(gameDeck: GameDeck) {
+        if (state is Hit)
+            changeState((state as Hit).getCard(gameDeck.drawCard()))
+    }
+
+    fun changeResult(newGameResult: GameResult) {
+        _gameResult += newGameResult
     }
 }
