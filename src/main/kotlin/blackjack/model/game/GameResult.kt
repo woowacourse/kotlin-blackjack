27107a4d801b2e0c0ff154b77dc477e13a3150aa
김예찬 <yechan@gim-yechan-ui-noteubug.local>
2024-03-14 16:11:39 +0900

    refect: GameResult내부 calculateFinalProfits 함수 세분화

--- src/main/kotlin/blackjack/model/game/GameResult.kt
+++ src/main/kotlin/blackjack/model/game/GameResult.kt
@@ -14,22 +14,35 @@ class GameResult(
     fun getDrawCount(): Int = playerResults.count { it.result == Result.DRAW }
 
     fun calculateFinalProfits() {
+        playerResults.forEach { calculatePlayerProfit(it) }
+        calculateDealerProfit()
+    }
+
+    private fun calculatePlayerProfit(playerResult: PlayerResult) {
+        val player = playerResult.player
+        val playerBlackjack = player.hand.isBlackjack()
         val dealerBlackjack = dealer.hand.isBlackjack()
-        playerResults.forEach { playerResult ->
-            val player = playerResult.player
-            val playerBlackjack = player.hand.isBlackjack()
-            if (playerBlackjack && !dealerBlackjack) {
+        when {
+            playerBlackjack && !dealerBlackjack -> {
                 val winnings = (player.bettingMoney.bettingMoney * 1.5).toInt()
                 player.profit += winnings
-            } else if (playerBlackjack && dealerBlackjack) {
-            } else {
-                when (playerResult.result) {
-                    Result.PLAYER_WIN -> player.profit += player.bettingMoney.bettingMoney
-                    Result.DEALER_WIN -> player.profit -= player.bettingMoney.bettingMoney
-                    Result.DRAW -> {} // 무승부 (profit 변화 없음)
-                }
             }
+
+            playerBlackjack && dealerBlackjack -> {}
+            else -> adjustProfitBasedOnResult(playerResult)
         }
+    }
+
+    private fun adjustProfitBasedOnResult(playerResult: PlayerResult) {
+        val player = playerResult.player
+        when (playerResult.result) {
+            Result.PLAYER_WIN -> player.profit += player.bettingMoney.bettingMoney
+            Result.DEALER_WIN -> player.profit -= player.bettingMoney.bettingMoney
+            Result.DRAW -> {} // 무승부 (profit 변화 없음)
+        }
+    }
+
+    private fun calculateDealerProfit() {
         dealer.profit = playerResults.sumOf { -it.player.profit }
     }
 }
