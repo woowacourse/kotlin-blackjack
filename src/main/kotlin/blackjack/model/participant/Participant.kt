54e62eccb7e7a7cf28386854380ae5ef1632a4a5
murjune <sis9221302@gmail.com>
2024-03-15 00:02:14 +0900

    feat : Participant play, judge 구현, finishState 변수 추가

--- src/main/kotlin/blackjack/model/participant/Participant.kt
+++ src/main/kotlin/blackjack/model/participant/Participant.kt
@@ -1,19 +1,59 @@
 package blackjack.model.participant
 
+import blackjack.model.Betting
+import blackjack.model.Profit
 import blackjack.model.card.Card
 import blackjack.model.card.Hand
 import blackjack.state.State
 
-sealed class Participant(val name: String, protected var state: State) {
+sealed class Participant(
+    val name: String,
+    val betting: Betting = Betting(0),
+    initState: State,
+) {
+    private var _state = initState
+    val state get() = _state
+
+    private var _finishState: State.Finish? = null
+    val finishState get() = _finishState
     val hand: Hand get() = state.hand
-    val ratePoint: Int get() = state.ratePoint
+
+    fun sumScore(): Int = state.sumScore()
 
     abstract fun play(
         onDraw: () -> Card,
         onDone: (Participant) -> Unit,
     )
 
-    abstract fun judge(other: Participant): GameResult
+    protected fun play(
+        onHitCondition: () -> Boolean,
+        onDraw: () -> Card,
+        onDone: (Participant) -> Unit,
+    ) {
+        if (onHitCondition()) {
+            _state = State.Running(hand).hit(onDraw())
+            onDone(this)
+            play(onDraw, onDone)
+        } else {
+            State.Running(hand).stay().also {
+                _state = it
+                _finishState = it
+            }
+        }
+    }
 
-    fun sumScore() = state.sumScore()
+    fun isBust() = state is State.Finish.Bust
+
+    open fun judge(
+        betting: Betting,
+        other: Participant,
+    ): Profit {
+        val finish = finishState ?: throw IllegalStateException(ERROR_JUDGE_WHEN_RUNNING)
+        val otherFinish = other.finishState ?: throw IllegalStateException(ERROR_JUDGE_WHEN_RUNNING)
+        return finish.calculateProfit(betting, otherFinish.ratePoint)
+    }
+
+    protected companion object {
+        const val ERROR_JUDGE_WHEN_RUNNING = "Participant 가 Running 일 때는 judge 를 할 수 없습니다"
+    }
 }
