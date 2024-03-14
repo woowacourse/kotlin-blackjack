ff6df1dc75ddbed854d9327d5c692ef145c52f17
JunJangE <wnswkd486@gmail.com>
2024-03-15 02:03:23 +0900

    feat: 카드 캐싱

--- src/main/kotlin/blackjack/model/Card.kt
+++ src/main/kotlin/blackjack/model/Card.kt
@@ -2,14 +2,23 @@ package blackjack.model
 
 class Card private constructor(val pattern: Pattern, val number: CardNumber) {
     companion object {
-        fun createDeck(): List<Card> {
-            val newCards = mutableListOf<Card>()
+        private val newCards = mutableSetOf<Card>()
+
+        init {
             Pattern.entries.forEach { pattern ->
                 CardNumber.entries.forEach { number ->
                     newCards.add(Card(pattern = pattern, number = number))
                 }
             }
-            return newCards
+        }
+
+        fun createDeck(): List<Card> = newCards.toList()
+
+        fun from(
+            pattern: Pattern,
+            number: CardNumber,
+        ): Card {
+            return newCards.first { it.pattern == pattern && it.number == number }
         }
 
         fun of(value: List<Pair<Pattern, CardNumber>>): List<Card> {
