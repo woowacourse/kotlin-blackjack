4aabeb67a0189a35a00840d671ddb0f7eb90eb5e
haeum808 <haeum808@gmail.com>
2024-03-14 20:47:46 +0900

    feat: 최종 수익 출력하는 기능 구현

--- src/main/kotlin/blackjack/view/OutputView.kt
+++ src/main/kotlin/blackjack/view/OutputView.kt
@@ -1,8 +1,6 @@
 package blackjack.view
 
-import blackjack.model.domain.Judge
 import blackjack.model.domain.ParticipantInfo
-import blackjack.model.entitiy.GameResult
 import blackjack.model.store.Store
 
 object OutputView {
@@ -10,9 +8,8 @@ object OutputView {
     private const val MESSAGE_CARD_INFO = "%s카드: %s"
     private const val MESSAGE_DEALER_HIT = "%s는 16이하라 한장의 카드를 더 받았습니다."
     private const val MESSAGE_PARTICIPANT_CARD_RESULT = "%s - 결과: %d"
-    private const val MESSAGE_TITLE_RESULT = "\n## 최종 승패"
-    private const val MESSAGE_CARD_STATUS = "%s: %s"
-    private const val EMPTY_STRING = ""
+    private const val MESSAGE_TITLE_RESULT = "\n## 최종 수익"
+    private const val MESSAGE_CARD_STATUS2 = "%s: %d"
     private const val NEW_LINE = "\n"
 
     fun printInitialStats(
@@ -55,12 +52,12 @@ object OutputView {
         }
     }
 
-    fun printResult(store: Store, judge: Judge) {
+    fun printResult(store: Store) {
         println(MESSAGE_TITLE_RESULT)
-        println(MESSAGE_CARD_STATUS.format(store.dealerInfo.name, getDealerResult(judge.getGameResults())))
+        println(MESSAGE_CARD_STATUS2.format(store.dealerInfo.name, getDealerResult(store)))
 
-        judge.getGameResults().zip(store.playersInfo) { result, stat ->
-            println(MESSAGE_CARD_STATUS.format(stat.name, result.value))
+        store.playersInfo.forEach {
+            println(MESSAGE_CARD_STATUS2.format(it.name, it.batingAmount))
         }
     }
 
@@ -81,16 +78,8 @@ object OutputView {
         }
     }
 
-    private fun getDealerResult(result: List<GameResult>): String {
-        val winCount = result.count { it == GameResult.WIN }
-        val drawCount = result.count { it == GameResult.DRAW }
-        val loseCount = result.count { it == GameResult.LOSE }
-
-        val winningResult = if (loseCount > 0) "${loseCount}승" else EMPTY_STRING
-        val drawResult = if (drawCount > 0) "${drawCount}무 " else EMPTY_STRING
-        val losingResult = if (winCount > 0) "${winCount}패 " else EMPTY_STRING
-
-        return losingResult + drawResult + winningResult
+    private fun getDealerResult(store: Store): Int {
+        return store.playersInfo.sumOf { it.batingAmount.unaryMinus() }
     }
 
     private fun getDealerCardResult(store: Store): String {
@@ -100,6 +89,6 @@ object OutputView {
 
     private fun printDistributionMessage(store: Store) {
         val names = store.playersInfo.joinToString { it.name }
-        println(MESSAGE_DISTRIBUTION.format(store.dealerInfo.name, names))
+        println("\n${MESSAGE_DISTRIBUTION.format(store.dealerInfo.name, names)})")
     }
 }
