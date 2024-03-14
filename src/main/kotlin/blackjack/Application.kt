48bc5c244538d44a765412fea60bb42da36f3bff
haeum808 <haeum808@gmail.com>
2024-03-14 18:53:02 +0900

    refactor: BlackJackController에 Dispatcher, Store 주입

--- src/main/kotlin/blackjack/Application.kt
+++ src/main/kotlin/blackjack/Application.kt
@@ -1,7 +1,12 @@
 package blackjack
 
 import blackjack.controller.BlackJackController
+import blackjack.model.dispatcher.Dispatcher
+import blackjack.model.store.Store
 
 fun main() {
-    BlackJackController.startGame()
+    val dispatcher = Dispatcher()
+    val store = Store(dispatcher)
+    val blackJackController = BlackJackController(dispatcher, store)
+    blackJackController.startGame()
 }
