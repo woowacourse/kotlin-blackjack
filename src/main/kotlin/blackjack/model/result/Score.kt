ef6e70af33bcb51fc08678b01c7ed19a62766eb6
sh1mj1 <7wlgns@gmail.com>
2024-03-14 09:56:28 +0900

    feat(Score): 카드 손 패 점수를 통해 딜러와의 승패를 결정한다

---
+++ src/main/kotlin/blackjack/model/result/Score.kt
@@ -0,0 +1,19 @@
+package blackjack.model.result
+
+@JvmInline
+value class Score(val point: Int) : Comparable<Score> {
+    override fun compareTo(other: Score): Int = point - other.point
+
+    fun determineWinning(dealerScore: Score): WinningResultStatus = when {
+        this > BLACK_JACK_SCORE -> WinningResultStatus.DEFEAT
+        dealerScore > BLACK_JACK_SCORE -> WinningResultStatus.VICTORY
+        dealerScore > this -> WinningResultStatus.DEFEAT
+        this > dealerScore -> WinningResultStatus.VICTORY
+        dealerScore == this -> WinningResultStatus.DRAW
+        else -> WinningResultStatus.VICTORY
+    }
+
+    companion object {
+        private val BLACK_JACK_SCORE = Score(21)
+    }
+}
