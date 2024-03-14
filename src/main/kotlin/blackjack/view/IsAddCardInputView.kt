68104987cdfc9cbc917b83d3d32ca779c4fbdf88
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2024-03-14 09:43:40 +0900

    refactor: Input 메시지 상수화

--- src/main/kotlin/blackjack/view/IsAddCardInputView.kt
+++ src/main/kotlin/blackjack/view/IsAddCardInputView.kt
@@ -2,7 +2,7 @@ package blackjack.view
 
 class IsAddCardInputView {
     fun readIsAddCard(playerName: String): Boolean {
-        println("${playerName}는 한 장의 카드를 더 받겠습니까?(예는 $YES, 아니오는 $NO)")
+        println(PLAYER_ADD_CARDS_MESSAGE.format(playerName))
         val input = readln()
         runCatching {
             validateChoice(input)
@@ -15,12 +15,14 @@ class IsAddCardInputView {
 
     private fun validateChoice(input: String) {
         require(input == YES || input == NO) {
-            "${YES}또는 ${NO}만 입력 가능합니다."
+            INVALID_INPUT_MESSAGE
         }
     }
 
     companion object {
         private const val YES = "y"
         private const val NO = "n"
+        private const val PLAYER_ADD_CARDS_MESSAGE = "%s는 한 장의 카드를 더 받겠습니까?(예는 $YES, 아니오는 $NO)"
+        private const val INVALID_INPUT_MESSAGE = "${YES}또는 ${NO}만 입력 가능합니다.."
     }
 }
--- src/main/kotlin/blackjack/view/PlayersInputView.kt
+++ src/main/kotlin/blackjack/view/PlayersInputView.kt
@@ -5,7 +5,7 @@ import blackjack.model.participant.Players
 
 class PlayersInputView {
     fun readPlayerNames(deck: Deck): Players {
-        println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)")
+        println(PLAYERS_INPUT_MESSAGE)
         return runCatching {
             val names =
                 readln().split(SPLIT_DELIMITER)
@@ -20,5 +20,6 @@ class PlayersInputView {
 
     companion object {
         private const val SPLIT_DELIMITER = ","
+        private const val PLAYERS_INPUT_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)"
     }
 }
