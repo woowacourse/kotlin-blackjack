df51bba45bb13647b6237765f2328b8c7c4eae4f
JunJangE <wnswkd486@gmail.com>
2024-03-15 01:40:14 +0900

    refactor: Controller와 domain 로직 분리

--- src/main/kotlin/blackjack/controller/GameController.kt
+++ src/main/kotlin/blackjack/controller/GameController.kt
@@ -6,15 +6,14 @@ import blackjack.model.GameState
 import blackjack.model.GameState.End
 import blackjack.model.GameState.Play
 import blackjack.model.Participants
-import blackjack.model.Participants.Companion.INITIAL_CARD_COUNTS
 import blackjack.model.PlayerGroup
-import blackjack.model.ProfitResults
 import blackjack.view.InputView
 import blackjack.view.InputView.askHitOrStay
 import blackjack.view.InputView.inputPlayerBettingAmount
 import blackjack.view.OutputView
 import blackjack.view.OutputView.printDealerDrawCard
 import blackjack.view.OutputView.printEveryCards
+import blackjack.view.OutputView.printGameResult
 import blackjack.view.OutputView.printGameSetting
 import blackjack.view.OutputView.showPlayerCards
 
@@ -31,8 +30,25 @@ class GameController(private var gameState: GameState = Play) {
     private fun play() {
         createParticipants().onSuccess { participants ->
             GameDeck.resetCurrentDeck()
-            startGame(participants = participants)
+            playGame(participants)
         }.onFailure { e ->
+            OutputView.printError(e)
+        }
+    }
+
+    private fun playGame(participants: Participants) {
+        participants.startGame(
+            inputPlayerBettingAmount = ::inputPlayerBettingAmount,
+            printGameSetting = ::printGameSetting,
+            askHitOrStay = ::askHitOrStay,
+            showPlayerCards = ::showPlayerCards,
+            printDealerDrawCard = ::printDealerDrawCard,
+            printEveryCards = ::printEveryCards,
+        ).onSuccess { profitResults ->
+            printGameResult(profitResults)
+            gameState = End
+        }.onFailure { e ->
+            participants.resetHand()
             OutputView.printError(e)
         }
     }
@@ -50,59 +66,5 @@ class GameController(private var gameState: GameState = Play) {
         val playerGroup = PlayerGroup()
         playerGroup.addPlayer(playerNames = playersNames)
         return playerGroup
-    }
-
-    private fun startGame(participants: Participants) {
-        playGame(participants = participants).onSuccess { profitResults ->
-            OutputView.printGameResult(profitResults)
-            gameState = End
-        }.onFailure { e ->
-            participants.resetHand()
-            OutputView.printError(e)
-        }
-    }
-
-    private fun playGame(participants: Participants): Result<ProfitResults> {
-        return runCatching {
-            start(participants = participants)
-            finish(participants = participants)
-        }
-    }
-
-    private fun start(participants: Participants) {
-        initBetting(playerGroup = participants.playerGroup)
-        initSetting(participants = participants)
-        runPlayersTurn(playerGroup = participants.playerGroup)
-        runDealerTurn(dealer = participants.dealer)
-    }
-
-    private fun initBetting(playerGroup: PlayerGroup) {
-        playerGroup.startBetting { player ->
-            inputPlayerBettingAmount(nickname = player.userInfo.nickname)
-        }
-    }
-
-    private fun initSetting(participants: Participants) {
-        repeat(INITIAL_CARD_COUNTS) { participants.initParticipantsDeck() }
-        printGameSetting(participants = participants)
-    }
-
-    private fun runPlayersTurn(playerGroup: PlayerGroup) {
-        playerGroup.drawPlayerCard(
-            shouldDrawCard = { player -> askHitOrStay(player.userInfo.nickname) },
-            newPlayer = { showPlayerCards(it) },
-        )
-    }
-
-    private fun runDealerTurn(dealer: Dealer) {
-        dealer.drawCard(
-            shouldDrawCard = { dealer.shouldDrawCard() },
-            newCardHolder = { printDealerDrawCard(dealer = it as Dealer) },
-        )
-    }
-
-    private fun finish(participants: Participants): ProfitResults {
-        printEveryCards(participants = participants)
-        return participants.calculateResult()
     }
 }
--- src/main/kotlin/blackjack/model/Participants.kt
+++ src/main/kotlin/blackjack/model/Participants.kt
@@ -4,6 +4,56 @@ class Participants(
     val dealer: Dealer,
     val playerGroup: PlayerGroup,
 ) {
+    fun startGame(
+        inputPlayerBettingAmount: (nickname: Nickname) -> String,
+        printGameSetting: (participants: Participants) -> Unit,
+        askHitOrStay: (nickname: Nickname) -> Boolean,
+        showPlayerCards: (player: Player) -> Unit,
+        printDealerDrawCard: (dealer: Dealer) -> Unit,
+        printEveryCards: (participants: Participants) -> Unit,
+    ): Result<ProfitResults> {
+        return runCatching {
+            initBetting(inputPlayerBettingAmount)
+            initSetting(printGameSetting)
+            runPlayersTurn(askHitOrStay, showPlayerCards)
+            runDealerTurn(printDealerDrawCard)
+            finish(printEveryCards)
+        }
+    }
+
+    private fun initBetting(inputPlayerBettingAmount: (nickname: Nickname) -> String) {
+        playerGroup.startBetting { player ->
+            inputPlayerBettingAmount(player.userInfo.nickname)
+        }
+    }
+
+    private fun initSetting(printGameSetting: (participants: Participants) -> Unit) {
+        repeat(INITIAL_CARD_COUNTS) { initParticipantsDeck() }
+        printGameSetting(this)
+    }
+
+    private fun runPlayersTurn(
+        askHitOrStay: (nickname: Nickname) -> Boolean,
+        showPlayerCards: (player: Player) -> Unit,
+    ) {
+        playerGroup.drawPlayerCard(
+            shouldDrawCard = { player -> askHitOrStay(player.userInfo.nickname) },
+            newPlayer = { player -> showPlayerCards(player) },
+        )
+    }
+
+    private fun runDealerTurn(printDealerDrawCard: (dealer: Dealer) -> Unit) {
+        dealer.drawCard(
+            shouldDrawCard = { dealer.shouldDrawCard() },
+            newCardHolder = { printDealerDrawCard(it as Dealer) },
+        )
+    }
+
+    private fun finish(printEveryCards: (participants: Participants) -> Unit): ProfitResults {
+        printEveryCards(this)
+        return calculateResult()
+    }
+
     fun initParticipantsDeck() {
         dealer.addCard(card = GameDeck.drawCard())
         playerGroup.players.forEach { player ->
@@ -16,7 +66,7 @@ class Participants(
         dealer.getState().hand().reset()
     }
 
-    fun calculateResult(): ProfitResults {
+    private fun calculateResult(): ProfitResults {
         val result = mutableListOf<ProfitResult>()
         var totalProfit = 0.0
 
