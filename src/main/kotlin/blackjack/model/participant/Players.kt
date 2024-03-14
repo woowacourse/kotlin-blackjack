fabb76fd737a84d9d0a47d9034214558d1dfb11f
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2024-03-14 09:58:52 +0900

    refactor: Players 에러 메시지 상수화

--- src/main/kotlin/blackjack/model/participant/Players.kt
+++ src/main/kotlin/blackjack/model/participant/Players.kt
@@ -6,12 +6,14 @@ class Players(private val names: Set<String>, deck: Deck) {
     val gamePlayers: List<Player> = names.map { Player(it, deck) }
 
     init {
-        require(names.size in MIN_PLAYER_COUNT..MAX_PLAYER_COUNT) { "${MIN_PLAYER_COUNT}명 이상 ${MAX_PLAYER_COUNT}명 이하의 플레이어만 가능합니다." }
+        require(names.size in MIN_PLAYER_COUNT..MAX_PLAYER_COUNT) { ERROR_PLAYER_COUNT }
     }
 
     companion object {
         private const val MIN_PLAYER_COUNT = 1
         private const val MAX_PLAYER_COUNT = 6
+        private const val ERROR_PLAYER_COUNT = "플레이어는 ${MIN_PLAYER_COUNT}명 이상 ${MAX_PLAYER_COUNT}명 이하만 가능합니다."
+        private const val ERROR_DUPLICATE_PLAYER = "중복된 이름은 플레이어로 등록할 수 없습니다."
 
         fun playerNamesOf(
             names: List<String>,
@@ -23,7 +25,7 @@ class Players(private val names: Set<String>, deck: Deck) {
 
         private fun validateDuplicateNames(numbers: List<String>) {
             require(numbers.distinct().size == numbers.size) {
-                "중복된 이름은 플레이어로 등록할 수 없습니다."
+                ERROR_DUPLICATE_PLAYER
             }
         }
     }
