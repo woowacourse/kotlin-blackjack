57002d313761d275eb24777c6096db32306343d0
haeum808 <haeum808@gmail.com>
2024-03-14 18:48:36 +0900

    refactor: data class Scoreboard -> enum class GameResult로 변경

--- src/main/kotlin/blackjack/model/Scoreboard.kt
+++
@@ -1,7 +0,0 @@
-package blackjack.model
-
-data class Scoreboard(
-    val win: Int,
-    val draw: Int,
-    val lose: Int,
-)
---
+++ src/main/kotlin/blackjack/model/entitiy/GameResult.kt
@@ -0,0 +1,7 @@
+package blackjack.model.entitiy
+
+enum class GameResult(val value: String) {
+    WIN("승"),
+    DRAW("무"),
+    LOSE("패"),
+}
