e173cccabab0f3f9b6c2cba8ee65c7393ad2ccd2
hevton <hevton22@gmail.com>
2024-03-15 20:20:29 +0900

    refactor(BetMoney): Money로 이름 수정

--- src/main/kotlin/model/participants/Money.kt
+++ src/main/kotlin/model/participants/Money.kt
@@ -1,6 +1,7 @@
 package model.participants
 
-class BetMoney (val amount: Int = 0) {
+@JvmInline
+value class Money (val amount: Int = 0) {
 
     init {
         require(amount >= 0) { ERROR_INVALID_AMOUNT }
@@ -9,7 +10,7 @@ class BetMoney (val amount: Int = 0) {
         const val ERROR_INVALID_TYPE = "배티 금액은 숫자여야 합니다"
         const val ERROR_INVALID_AMOUNT = "배팅 금액은 0보다 커야 합니다"
 
-        fun fromInput(input: String): BetMoney = input.toInt().run(::BetMoney)
+        fun fromInput(input: String): Money = input.toInt().run(::Money)
 
         private fun String.toInt(): Int {
             return this.toIntOrNull() ?: throw IllegalArgumentException(ERROR_INVALID_TYPE)
--- src/test/kotlin/model/participants/MoneyTest.kt
+++ src/test/kotlin/model/participants/MoneyTest.kt
@@ -4,22 +4,22 @@ import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
-class BetMoneyTest {
+class MoneyTest {
     @ParameterizedTest
     @ValueSource(strings = ["야호", "팡태", "제이든"])
     fun `배팅 금액은 숫자여야 합니다`(input: String) {
         assertThatThrownBy {
-            BetMoney.fromInput(input)
+            Money.fromInput(input)
         }.isExactlyInstanceOf(IllegalArgumentException::class.java)
-            .hasMessage(BetMoney.ERROR_INVALID_TYPE)
+            .hasMessage(Money.ERROR_INVALID_TYPE)
     }
 
     @ParameterizedTest
     @ValueSource(strings = ["-1", "-1500"])
     fun `배팅 금액은 0 이상의 정수여야 합니다`(input: String) {
         assertThatThrownBy {
-            BetMoney.fromInput(input)
+            Money.fromInput(input)
         }.isExactlyInstanceOf(IllegalArgumentException::class.java)
-            .hasMessage(BetMoney.ERROR_INVALID_AMOUNT)
+            .hasMessage(Money.ERROR_INVALID_AMOUNT)
     }
 }
