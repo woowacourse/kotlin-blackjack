120d37f98c2b1510ce2335f5ab478fb1c0a9a451
Hogu59 <dbstjdguswkd@naver.com>
2024-03-16 04:24:28 +0900

    refactor(Players): Deck에 대한 의존성 제거

--- src/main/kotlin/model/human/Players.kt
+++ src/main/kotlin/model/human/Players.kt
@@ -1,18 +1,14 @@
 package model.human
 
 import model.Hand
-import model.card.Deck
 
 class Players private constructor(val players: List<Player>) {
     companion object {
         const val ERROR_EXCEED_PLAYERS = "플레이어의 수는 1 ~ 10 사이여야 합니다."
 
-        fun ofList(
-            names: List<String>,
-            deck: Deck,
-        ): Players {
-            return names.validateLength().map {
-                Player(Hand(deck), HumanName.fromInput(it))
+        fun ofList(names: List<String>): Players {
+            return names.validateLength().map { name ->
+                Player(Hand(), HumanInfo(name))
             }.run {
                 Players(this)
             }
