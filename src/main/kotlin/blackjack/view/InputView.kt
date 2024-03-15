e06d08e91ed3ee20078c0cd952aa0d7b807edd82
songpink <jejeje04@naver.com>
2024-03-15 16:24:12 +0900

    feat: 베팅금액을 입력받는 기능 구현

--- src/main/kotlin/blackjack/view/InputView.kt
+++ src/main/kotlin/blackjack/view/InputView.kt
@@ -8,6 +8,15 @@ class InputView {
         return readln().split(DELIMITER).map { it.trim() }
     }
 
+    fun inputPlayerBetAmount(playerNames: List<String>): MutableList<Int> {
+        val playersBudgets: MutableList<Int> = mutableListOf()
+        playerNames.forEach { playerName ->
+            println(MESSAGE_INPUT_PLAYERS_BUDGET.format(playerName))
+            playersBudgets.add(readln().toInt())
+        }
+        return playersBudgets
+    }
+
     tailrec fun inputWhetherHit(player: Player): Boolean {
         println(MESSAGE_INPUT_WHETHER_HIT.format(player.name))
         val input = readln()
@@ -22,6 +31,7 @@ class InputView {
 
     companion object {
         private const val MESSAGE_INPUT_PLAYERS_NAME = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)"
+        private const val MESSAGE_INPUT_PLAYERS_BUDGET = "%s의 베팅 금액은?"
         private const val HIT = "y"
         private const val STAY = "n"
         private const val MESSAGE_INPUT_WHETHER_HIT = "\n%s는 한장의 카드를 더 받겠습니까?(예는 $HIT, 아니오는 $STAY)"
