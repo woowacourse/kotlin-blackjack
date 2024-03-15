f8e28c376ceb028669ba9ce852f84fbc9a0826d8
Hogu59 <dbstjdguswkd@naver.com>
2024-03-16 03:48:10 +0900

    feat(Card): 생성자 추가 구현, 에이스 판단 구현
    - 주생성자 사용, 팩토리 메서드 추가 구현
    - 에이스 판단 구현

--- src/main/kotlin/model/card/Card.kt
+++ src/main/kotlin/model/card/Card.kt
@@ -1,11 +1,12 @@
 package model.card
 
-class Card private constructor(val valueType: ValueType, val markType: MarkType) {
+class Card(val valueType: ValueType, val markType: MarkType) {
+    fun isAce(): Boolean = (valueType == ValueType.ACE)
+
     companion object {
         const val INVALID_RANGE = "0 ~ 51 사이의 숫자 범위여야 합니다"
         private const val MIN_NUMBER = 0
         private const val MAX_NUMBER = 51
-        private const val DIVIDER = 13
 
         private fun Int.validateRange(): Int {
             require((MIN_NUMBER..MAX_NUMBER).contains(this)) { INVALID_RANGE }
@@ -14,21 +15,15 @@ class Card private constructor(val valueType: ValueType, val markType: MarkType)
 
         fun from(number: Int): Card {
             return number.validateRange().run {
-                Card(getValue(number), getMark(number))
+                Card(intToValueType(number), intToMarkType(number))
             }
         }
 
-        private fun getMark(number: Int): MarkType {
-            return when (number / DIVIDER) {
-                MarkType.SPADE.order -> MarkType.SPADE
-                MarkType.CLOVER.order -> MarkType.CLOVER
-                MarkType.HEART.order -> MarkType.HEART
-                else -> MarkType.DIAMOND
-            }
-        }
-
-        private fun getValue(number: Int): ValueType {
-            return ValueType.values()[number % DIVIDER]
+        fun ofStringType(
+            valueType: String,
+            markType: String,
+        ): Card {
+            return Card(stringToValueType(valueType), stringToMarkType(markType))
         }
     }
 }
