123fd3bc018bc8334fbf2dc4164a3a65475e7c64
Hogu59 <dbstjdguswkd@naver.com>
2024-03-16 04:19:31 +0900

    feat(Point): 점수를 담는 클래스 구현
    - 카드, 혹은 핸드의 점수를 담는 클래스 구현

--- src/main/kotlin/model/Point.kt
+++ src/main/kotlin/model/Point.kt
@@ -1,3 +1,15 @@
 package model
 
-data class Point(val amount: Int)
+data class Point(val amount: Int) : Comparable<Point> {
+    fun isLessOrEqualTo(number: Int) = amount <= number
+
+    fun plus(number: Int): Point {
+        return Point(amount + number)
+    }
+
+    override fun compareTo(other: Point): Int = this.amount.compareTo(other.amount)
+
+    fun isLessThan(number: Int) = amount < number
+
+    fun isBlackjackPoint(): Boolean = this.amount == 21
+}
