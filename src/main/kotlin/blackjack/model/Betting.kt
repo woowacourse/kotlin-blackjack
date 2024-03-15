f1193434d1d89148960a019a4af85d9709c8d4cb
murjune <sis9221302@gmail.com>
2024-03-15 10:51:26 +0900

    feat : Betting 최소 금액 0원 이상

--- src/main/kotlin/blackjack/model/Betting.kt
+++ src/main/kotlin/blackjack/model/Betting.kt
@@ -3,10 +3,12 @@ package blackjack.model
 @JvmInline
 value class Betting(val amount: Int) {
     init {
+        require(amount >= MIN_BETTING_AMOUNT) {"Betting 의 최소 금액은 $MIN_BETTING_AMOUNT 다"}
         require(amount % BETTING_UNIT == 0) { "$amount - Betting 금액의 단위는 $BETTING_UNIT 다" }
     }
 
     companion object {
+        private const val MIN_BETTING_AMOUNT = 0
         private const val BETTING_UNIT = 10_000
     }
 }
