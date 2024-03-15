f88f8923f168ce1022e5d82a9628ae173b2bdb5e
Hogu59 <dbstjdguswkd@naver.com>
2024-03-16 04:33:28 +0900

    feat(InputView): 베팅 금액을 입력 받는 로직 구현
    - 입력된 내용이 1 이상의 int 범위 내 숫자가 아닐경우 null 반환

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -2,9 +2,11 @@ package view
 
 import model.Answer
 import model.human.HumanName
+import model.human.Player
 
 object InputView {
     private const val HEADER_READ_PLAYER_NAMES = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)"
+    private const val HEADER_READ_BETTING_AMOUNT = "\n%s의 배팅 금액은?"
     private const val HEADER_READ_ANSWER = "%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)"
 
     private val POSITIVE = listOf("y", "yes", "o", "예")
@@ -15,6 +17,12 @@ object InputView {
         return readln().split(",").map { it.trim() }.toList()
     }
 
+    fun readBettingAmount(player: Player): Int? {
+        println(HEADER_READ_BETTING_AMOUNT.format(player.getName()))
+        val amount = readln().toIntOrNull()
+        if (amount == null || amount <= 0) return null
+        return amount
+    }
 
     fun readAnswer(humanName: HumanName): Answer? {
         println(HEADER_READ_ANSWER.format(humanName.name))
