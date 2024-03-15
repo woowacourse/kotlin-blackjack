08ac1d01b63135050634e3818fb7fdbd9eee186c
Hogu59 <dbstjdguswkd@naver.com>
2024-03-16 04:31:53 +0900

    refactor(Human): HumanInfo 조회를 위한 함수 구현, 이외 함수 삭제

--- src/main/kotlin/model/human/Human.kt
+++ src/main/kotlin/model/human/Human.kt
@@ -1,46 +1,19 @@
 package model.human
 
 import model.Hand
-import model.Point
 
-abstract class Human(open val hand: Hand, open val humanName: HumanName) {
-    fun getPointIncludingAce(): Point {
-        return if (hand.hasAce()) {
-            decideAceValue()
-        } else {
-            hand.getPoint()
-        }
+abstract class Human(val hand: Hand, open val humanInfo: HumanInfo) {
+    fun getName(): String {
+        return humanInfo.humanName.name
     }
 
-    private fun decideAceValue(): Point {
-        val point = hand.getPoint().amount
-        return if (point <= ACE_POINT) {
-            Point(point + ACE_POINT)
-        } else {
-            Point(point)
-        }
+    fun getMoneyAmount(): Int {
+        return this.humanInfo.money.amount
     }
 
-    fun hits(count: Int) {
-        repeat(count) {
-            hit()
-        }
-    }
-
-    fun hit(): Boolean {
-        if (isPossible()) {
-            hand.draw()
-            return isPossible()
-        }
-        return false
-    }
-    fun isBlackJack(): Boolean  =
-        (this.hand.cards.size == 2 && this.hand.isBlackjackPoint())
+    fun isBlackJack(): Boolean = (this.hand.cards.size == 2 && this.hand.isBlackjackPoint())
 
     fun isBusted(): Boolean = !this.hand.isNotBusted()
-    abstract fun isPossible(): Boolean
 
-    companion object {
-        private const val ACE_POINT = 10
-    }
+    abstract fun isPossible(): Boolean
 }
