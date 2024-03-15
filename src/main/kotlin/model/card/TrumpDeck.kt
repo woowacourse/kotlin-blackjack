20d1f30f00c7cc94882fa211fdd08106295d0d42
Hogu59 <dbstjdguswkd@naver.com>
2024-03-16 04:01:15 +0900

    feat(TrumpDeck): 덱이 비어있을 때 다시 채우는 기능 구현
    - 덱에 카드가 0장이 될 경우, 덱을 셔플하여 다시 카드를 채움.

--- src/main/kotlin/model/card/TrumpDeck.kt
+++ src/main/kotlin/model/card/TrumpDeck.kt
@@ -4,15 +4,21 @@ object TrumpDeck : Deck {
     private const val MIN_VALUE = 0
     private const val MAX_VALUE = 51
 
-    private val cards: MutableList<Card> =
-        (MIN_VALUE..MAX_VALUE)
-            .shuffled()
-            .map {
-                Card.from(it)
-            }
-            .toMutableList()
+    private val cards = mutableListOf<Card>()
 
     override fun pop(): Card {
+        if (cards.isEmpty()) shuffle()
         return cards.removeAt(0)
+    }
+
+    private fun shuffle() {
+        cards.addAll(
+            (MIN_VALUE..MAX_VALUE)
+                .shuffled()
+                .map {
+                    Card.from(it)
+                }
+                .toMutableList(),
+        )
     }
 }
