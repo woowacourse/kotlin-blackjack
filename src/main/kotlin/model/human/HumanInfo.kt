b41ba4055869258af5c11db9dbf6987628b4ed92
Hogu59 <dbstjdguswkd@naver.com>
2024-03-16 04:16:38 +0900

    feat(HumanInfo): Money, HumanName을 담는 클래스
    - 배팅금액인 Money, 이름인 HumanName을 담는 WrapperClass

---
+++ src/main/kotlin/model/human/HumanInfo.kt
@@ -0,0 +1,25 @@
+package model.human
+
+import model.Money
+
+class HumanInfo(val humanName: HumanName, var money: Money = Money(0)) {
+    constructor(humanName: String) : this(HumanName(humanName), Money(0))
+
+    fun changeMoney(amount: Int) {
+        money.changeAmount(amount)
+    }
+
+    fun exchangeMoney(
+        other: HumanInfo,
+        rate: Double,
+    ) {
+        this.money.exchange(other.money, rate)
+    }
+
+    companion object {
+        fun ofPrimitive(
+            name: String,
+            amount: Int,
+        ): HumanInfo = HumanInfo(HumanName(name), Money(amount))
+    }
+}
