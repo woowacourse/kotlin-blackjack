011081a6f8e2e68449fdea4679a4302b4fbe4284
kimhm0728 <kimhm0728@naver.com>
2024-03-17 01:52:28 +0900

    refactor: Amount 클래스를 value class로 수정

--- src/main/kotlin/blackjack/model/participant/Amount.kt
+++ src/main/kotlin/blackjack/model/participant/Amount.kt
@@ -1,6 +1,7 @@
 package blackjack.model.participant
 
-class Amount(val price: Int) {
+@JvmInline
+value class Amount(val price: Int) {
     constructor(amount: Double) : this(amount.toInt())
 
     operator fun times(other: Double) = Amount(price * other)
