a694b843bca82e2f94e2818d620ee0d39e0f81c5
dpcks0509 <dpcks0509@gmail.com>
2024-03-17 02:42:54 +0900

    refactor: PlayerInformation, DealerInformation 클래스 data class로 선언

--- src/main/kotlin/blackjack/model/ParticipantInformation.kt
+++ src/main/kotlin/blackjack/model/ParticipantInformation.kt
@@ -1,9 +1,9 @@
 package blackjack.model
 
-sealed class ParticipantInformation(val name: ParticipantName) {
-    class PlayerInformation(name: ParticipantName, val bettingAmount: BettingAmount) : ParticipantInformation(name)
+sealed class ParticipantInformation(open val name: ParticipantName) {
+    data class PlayerInformation(override val name: ParticipantName, val bettingAmount: BettingAmount) : ParticipantInformation(name)
 
-    class DealerInformation(name: ParticipantName = ParticipantName(DEFAULT_DEALER_NAME)) :
+    data class DealerInformation(override val name: ParticipantName = ParticipantName(DEFAULT_DEALER_NAME)) :
         ParticipantInformation(name) {
         companion object {
             private const val DEFAULT_DEALER_NAME = "딜러"
