5eed86ef8b5c9f67d15708a2e2e4b4b5286d27d2
hevton <hevton22@gmail.com>
2024-03-16 23:01:06 +0900

    refactor(Money): 오타 수정

--- src/main/kotlin/model/participants/Money.kt
+++ src/main/kotlin/model/participants/Money.kt
@@ -7,7 +7,7 @@ value class Money(val amount: Int = 0) {
     }
 
     companion object {
-        const val ERROR_INVALID_TYPE = "배티 금액은 숫자여야 합니다"
+        const val ERROR_INVALID_TYPE = "배팅 금액은 숫자여야 합니다"
         const val ERROR_INVALID_AMOUNT = "배팅 금액은 0보다 커야 합니다"
 
         fun fromInput(input: String): Money = input.toInt().run(::Money)
