b6e14ce742fb998fea4c4c0ed59837dba3e284b2
hevton <hevton22@gmail.com>
2024-03-16 23:02:47 +0900

    refactor(ParticipantName): value class로 정의

--- src/main/kotlin/model/participants/ParticipantName.kt
+++ src/main/kotlin/model/participants/ParticipantName.kt
@@ -1,6 +1,7 @@
 package model.participants
 
-class ParticipantName private constructor(val name: String) {
+@JvmInline
+value class ParticipantName private constructor(val name: String) {
     companion object {
         const val ERROR_INVALID_LENGTH = "이름의 길이는 1~10 이어야 합니다."
         private const val LENGTH_LOWER_BOUND = 1
