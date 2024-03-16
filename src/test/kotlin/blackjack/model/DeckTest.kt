6cc0c4de6bd8b9e052732eedf97e0f85759fbefa
aprilgom <aprilgom@gmail.com>
2024-03-16 15:56:03 +0900

    refactor(DeckTest): Deck이 모자랄때의 에러 검사를 assertThrow로 체크

--- src/test/kotlin/blackjack/model/DeckTest.kt
+++ src/test/kotlin/blackjack/model/DeckTest.kt
@@ -2,6 +2,7 @@ package blackjack.model
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class DeckTest {
     @Test
@@ -18,15 +19,10 @@ class DeckTest {
     fun `카드 뭉치에 충분한 카드가 없을 경우 Deck이 모자라다는 예외를 내보낸다`() {
         val deck = Deck(listOf(Card(CardNumber.Ace, Suit.Spade)))
         val player = Player("cheolsoo")
-        val expected = "덱을 모두 사용했습니다."
-        lateinit var actual: String
-        runCatching {
+        assertThrows<NoSuchElementException> {
             player.pickCard(deck)
             player.pickCard(deck)
-        }.onFailure {
-            actual = it.message!!
         }
-        assertThat(actual).isEqualTo(expected)
     }
     private fun Participant.pickCard(deck: Deck) {
         this.addCard(deck.pop())
