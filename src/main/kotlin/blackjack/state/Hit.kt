0e84527ce8c06da53c37a913870f6067da37f371
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2024-03-17 00:34:50 +0900

    feat: Hit 상태 클래스 구현

---
+++ src/main/kotlin/blackjack/state/Hit.kt
@@ -0,0 +1,10 @@
+package blackjack.state
+
+import blackjack.model.participant.Player
+
+class Hit(private val player: Player) : State {
+    override fun drawCard(): State {
+        player.addCard()
+        return if (player.isBust()) Bust(player) else this
+    }
+}
