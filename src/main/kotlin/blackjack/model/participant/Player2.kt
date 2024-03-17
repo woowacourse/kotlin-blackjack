77353c150d9e7f71fe7f3b81d4e9e5e7b2147ae5
gaeun <geun5744@gmail.com>
2024-03-17 23:28:00 +0900

    fix: Player2 로직 수정

--- src/main/kotlin/blackjack/model/participant/Player2.kt
+++ src/main/kotlin/blackjack/model/participant/Player2.kt
@@ -1,48 +1,33 @@
 package blackjack.model.participant
 
 import blackjack.model.deck.Card
-import blackjack.model.deck.Deck
-import blackjack.model.deck.HandCards
+import blackjack.model.deck.HandCards2
 import blackjack.model.participant.testState.Finish
 import blackjack.model.participant.testState.Gaming
-import blackjack.model.participant.testState.HandCardState
-import blackjack.model.participant.testState.InitState
 
 class Player2(val name: String) {
-    private val handCards = HandCards()
+    private val handCards = HandCards2()
 
     fun initCards(cards: List<Card>) {
         require(cards.size == 2) {
             "초기 카드는 2장으로 제한됩니다."
         }
-        handCards.add(cards)
+        handCards.initCard(cards)
     }
 
-    private fun playTurn(
+    fun playTurn(
+        cards: (Int) -> List<Card>,
         isHit: (String) -> Boolean,
-        deck: Deck,
-    ) {
-        var currentState: Gaming = InitState()
-
-        do {
-            val isHit = isHit(name)
-            val currentState = currentState.nextTurn(handCards, isHit)
-        } while (currentState is Finish)
-
-        do {
-            addCard(deck.draw(1), currentState)
-            val nextState = currentState.nextTurn(handCards, isHit(name))
-        } while (nextState is Finish)
-    }
-
-    private fun addCard(
-        cards: List<Card>,
-        currentState: HandCardState,
-    ) {
-        if (currentState is Finish) {
-            throw IllegalStateException("더 이상 카드를 뽑을 수 없습니다.")
-        } else {
-            handCards.add(cards)
+        showResult: (Player2) -> Unit,
+    ): Finish {
+        while (handCards.state is Gaming) {
+            handCards.playTurn(isHit(name), cards)
+            showResult(this)
         }
+        return handCards.state as Finish
     }
+
+    fun getAllCards() = handCards.cards
+
+    fun getScore() = handCards.calculateScore()
 }
