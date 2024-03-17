bb134ad1301a6ddabf50bd718df514423499f5cc
Junyoung-WON <wjyo60@gmail.com>
2024-03-17 20:25:03 +0900

    refactor: 사용자 입력(문자열)으로부터 베팅금을 설정하는 기능 추가에 따른 테스트코드 리팩터링

--- src/test/kotlin/blackjack/model/ProfitTest.kt
+++ src/test/kotlin/blackjack/model/ProfitTest.kt
@@ -10,7 +10,8 @@ class ProfitTest {
     @ParameterizedTest
     @ValueSource(strings = ["2000", "10000", "30000"])
     fun `입력한 베팅금을 갖는다`(input: String) {
-        val profit = Profit(input)
+        val profit = Profit()
+        profit.initBettingMoney(input)
 
         assertThat(profit.amount).isEqualTo(input.toDouble())
     }
@@ -18,19 +19,22 @@ class ProfitTest {
     @ParameterizedTest
     @ValueSource(strings = ["한글", "abcd", "10000-5000"])
     fun `베팅금은 숫자로만 입력해야 한다`(notNumber: String) {
-        assertThrows<IllegalArgumentException> { Profit(notNumber) }
+        val profit = Profit()
+        assertThrows<IllegalArgumentException> { profit.initBettingMoney(notNumber) }
     }
 
     @ParameterizedTest
     @ValueSource(strings = ["-2900", "0"])
     fun `베팅금은 0보다 큰 정수여야한다`(lessThanZero: String) {
-        assertThrows<IllegalArgumentException> { Profit(lessThanZero) }
+        val profit = Profit()
+        assertThrows<IllegalArgumentException> { profit.initBettingMoney(lessThanZero) }
     }
 
     @ParameterizedTest
     @ValueSource(strings = ["5000", "10000", "150000"])
     fun `무승부인 경우 수익을 0원으로 만든다`(bettingMoney: String) {
-        val profit = Profit(bettingMoney)
+        val profit = Profit()
+        profit.initBettingMoney(bettingMoney)
         profit.giveBackBettingMoney()
         assertThat(profit.amount).isEqualTo(Profit.INITIAL_AMOUNT)
     }
@@ -38,7 +42,8 @@ class ProfitTest {
     @ParameterizedTest
     @ValueSource(strings = ["5000", "10000", "150000"])
     fun `패배한 경우 베팅한 만큼의 금액을 모두 잃는다`(bettingMoney: String) {
-        val profit = Profit(bettingMoney)
+        val profit = Profit()
+        profit.initBettingMoney(bettingMoney)
         profit.lostAllBettingMoney()
         assertThat(profit.amount).isEqualTo(-bettingMoney.toDouble())
     }
@@ -46,7 +51,8 @@ class ProfitTest {
     @ParameterizedTest
     @ValueSource(strings = ["5000", "10000", "150000"])
     fun `블랙잭으로 승리한 경우 베팅금의 150%를 얻는다`(bettingMoney: String) {
-        val profit = Profit(bettingMoney)
+        val profit = Profit()
+        profit.initBettingMoney(bettingMoney)
         profit.earnProfitForBlackJack()
         assertThat(profit.amount).isEqualTo(bettingMoney.toDouble() * Profit.BLACKJACK_ODDS)
     }
