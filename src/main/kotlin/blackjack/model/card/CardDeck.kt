61230de2924c471b6e2c3dbe872c09705eff5025
sh1mj1 <7wlgns@gmail.com>
2024-03-18 06:52:15 +0900

    refactor(RandomCardDrawStrategy): 프로덕션 코드에 적용

--- src/main/kotlin/blackjack/model/card/CardDeck.kt
+++ src/main/kotlin/blackjack/model/card/CardDeck.kt
@@ -6,25 +6,5 @@ object CardDeck {
             CardNumber.entries.map { number ->
                 Card(shape, number)
             }
-        }.shuffled()
-
-    private var cardIndex = 0
-
-    private fun shuffleDeck(): List<Card> = cardDeck.shuffled().toList()
-
-    fun getRandomCard(): Card {
-        val result =
-            runCatching {
-                cardDeck[cardIndex++]
-            }
-        return result.getOrElse {
-            resetCardDeck()
-            getRandomCard()
         }
-    }
-
-    private fun resetCardDeck() {
-        cardIndex = 0
-        shuffleDeck()
-    }
 }
--- src/test/kotlin/blackjack/model/card/state/InitialStateTest.kt
+++ src/test/kotlin/blackjack/model/card/state/InitialStateTest.kt
@@ -1,15 +1,17 @@
 package blackjack.model.card.state
 
-import blackjack.model.card.CardDeck
 import blackjack.model.card.CardHand
+import blackjack.model.card.RandomCardDrawStrategy
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class InitialStateTest {
+    private val cardDrawStrategy = RandomCardDrawStrategy
+
     @Test
     fun `첫 턴에서 카드를 드로우하면 랜덤으로 카드 한 장을 뽑아서 InitialState 가 된다`() {
         val initialState = InitialState(CardHand())
-        val actualCardsState = initialState.draw(CardDeck.getRandomCard())
+        val actualCardsState = initialState.draw(cardDrawStrategy.drawCard())
 
         assertThat(actualCardsState is InitialState).isTrue
         assertThat(actualCardsState.countCards()).isEqualTo(1)
@@ -17,8 +19,8 @@ class InitialStateTest {
 
     @Test
     fun `첫 턴에서 카드를 한장 뽑은 상태에서 카드를 드로우하면 랜덤으로 카드 한 장을 더 뽑아서 Hit 혹은 BlackJack 가 된다`() {
-        val initialState = InitialState(CardHand()).draw(CardDeck.getRandomCard())
-        val actualCardsState = initialState.draw(CardDeck.getRandomCard())
+        val initialState = InitialState(CardHand()).draw(cardDrawStrategy.drawCard())
+        val actualCardsState = initialState.draw(cardDrawStrategy.drawCard())
 
         assertThat(actualCardsState is Hit || actualCardsState is BlackJack).isTrue
         assertThat(actualCardsState.countCards()).isEqualTo(2)
