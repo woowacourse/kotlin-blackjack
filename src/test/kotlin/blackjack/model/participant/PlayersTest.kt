9f25920ed1010ceb75716aa68497e79d1715e7a7
kimhm0728 <kimhm0728@naver.com>
2024-03-18 03:23:16 +0900

    refactor: 테스트를 위한 Players 가짜 생성자 구현

--- src/test/kotlin/blackjack/model/participant/PlayersTest.kt
+++ src/test/kotlin/blackjack/model/participant/PlayersTest.kt
@@ -5,41 +5,46 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 
-private fun battingAmounts(size: Int) = List(size) { DEFAULT_BATTING_AMOUNT }
+private fun Players(
+    names: List<String>,
+    amounts: List<Int> = List(names.size) { DEFAULT_BATTING_AMOUNT },
+): Players {
+    return Players.from(names, amounts)
+}
 
 class PlayersTest {
     @Test
     fun `플레이어명이 중복된 경우 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
-            Players.from(listOf("olive", "olive"), battingAmounts(2))
+            Players(listOf("olive", "olive"))
         }
     }
 
     @Test
     fun `플레이어 수가 2명 미만인 경우 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
-            Players.from(listOf("a"), battingAmounts(1))
+            Players(listOf("a"))
         }
     }
 
     @Test
     fun `플레이어 수가 8명 초과인 경우 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
-            Players.from(listOf("a", "b", "c", "d", "e", "f", "g", "h", "i"), battingAmounts(9))
+            Players(listOf("a", "b", "c", "d", "e", "f", "g", "h", "i"))
         }
     }
 
     @Test
     fun `플레이어명 크기와 배팅 금액 크기가 다른 경우 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
-            Players.from(listOf("olive", "abc", "defgh"), battingAmounts(2))
+            Players(listOf("olive", "abc", "defgh"), List(2) { DEFAULT_BATTING_AMOUNT })
         }
     }
 
     @Test
     fun `플레이어명, 플레이어 수, 배팅 금액이 모두 올바른 경우 예외가 발생하지 않는다`() {
         assertDoesNotThrow {
-            Players.from(listOf("olive", "abc", "defgh"), battingAmounts(3))
+            Players(listOf("olive", "abc", "defgh"))
         }
     }
 }
