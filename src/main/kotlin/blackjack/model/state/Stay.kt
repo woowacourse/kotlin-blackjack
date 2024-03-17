db45a4146b356470ccf836f44629913ad0c78cf0
Junyoung-WON <wjyo60@gmail.com>
2024-03-17 20:17:34 +0900

    refactor: Stay가 될 때 승패 여부에 따라 베팅금을 받도록 구현 및 승패 연산 수정

--- src/main/kotlin/blackjack/model/state/Stay.kt
+++ src/main/kotlin/blackjack/model/state/Stay.kt
@@ -3,23 +3,45 @@ package blackjack.model.state
 import blackjack.base.BaseHolder
 import blackjack.model.GameResult
 import blackjack.model.Hand
+import blackjack.model.Profit
 
-class Stay(hand: Hand) : Finished(hand) {
+class Stay(
+    hand: Hand,
+    profit: Profit
+) : Finished(hand, profit) {
     // 자신이 스테이일 때 상대와 비교
     override fun decideWinner(opponent: BaseHolder): GameResult {
         return when (opponent.state) {
-            is Bust -> GameResult(win = 1)
-            is BlackJack -> GameResult(defeat = 1)
-            is Stay -> compareWhenBothStay(opponent.state.hand.calculate())
+            is Bust -> {
+                opponent.addResult(GameResult(defeat = 1))
+                GameResult(win = 1)
+            }
+            is BlackJack -> {
+                profit.lostAllBettingMoney()
+                opponent.addResult(GameResult(win = 1))
+                GameResult(defeat = 1)
+            }
+            is Stay -> compareWhenBothStay(opponent)
             is Hit -> GameResult()
             is Running -> GameResult()
         }
     }
 
-    private fun compareWhenBothStay(opponentPoint: Int): GameResult {
+    private fun compareWhenBothStay(opponent: BaseHolder): GameResult {
         val myPoint = hand.calculate()
-        return if (myPoint > opponentPoint) GameResult(win = 1)
-        else if (myPoint < opponentPoint) GameResult(defeat = 1)
-        else GameResult(push = 1)
+        val opponentPoint = opponent.state.hand.calculate()
+        return if (myPoint > opponentPoint) {
+            opponent.addResult(GameResult(defeat = 1))
+            GameResult(win = 1)
+        }
+        else if (myPoint < opponentPoint) {
+            profit.lostAllBettingMoney()
+            opponent.addResult(GameResult(win = 1))
+            GameResult(defeat = 1)
+        } else {
+            profit.giveBackBettingMoney()
+            opponent.addResult(GameResult(push = 1))
+            GameResult(push = 1)
+        }
     }
 }
