fb8f41f0c078a85d13b524b4492690efc03f5812
Hogu59 <dbstjdguswkd@naver.com>
2024-03-17 15:58:49 +0900

    remove(Denomination, Suit): 불필요한 팩토리 메소드 삭제

--- src/main/kotlin/model/card/Denomination.kt
+++ src/main/kotlin/model/card/Denomination.kt
@@ -1,43 +1,23 @@
 package model.card
 
-enum class Denomination(val rank: String, val amount: Int) {
-    ACE("A", 1),
-    TWO("2", 2),
-    THREE("3", 3),
-    FOUR("4", 4),
-    FIVE("5", 5),
-    SIX("6", 6),
-    SEVEN("7", 7),
-    EIGHT("8", 8),
-    NINE("9", 9),
-    TEN("10", 10),
-    JACK("J", 10),
-    QUEEN("Q", 10),
-    KING("K", 10),
+enum class Denomination(val point: Int) {
+    ACE(1),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
+    TEN(10),
+    JACK(10),
+    QUEEN(10),
+    KING(10),
 }
 
 fun intToValueType(number: Int): Denomination {
-    return Denomination.values()[number % DIVIDER]
-}
-
-fun stringToValueType(input: String): Denomination {
-    return when (input.lowercase()) {
-        "1" -> Denomination.ACE
-        "a" -> Denomination.ACE
-        "2" -> Denomination.TWO
-        "3" -> Denomination.THREE
-        "4" -> Denomination.FOUR
-        "5" -> Denomination.FIVE
-        "6" -> Denomination.SIX
-        "7" -> Denomination.SEVEN
-        "8" -> Denomination.EIGHT
-        "9" -> Denomination.NINE
-        "10" -> Denomination.TEN
-        "j" -> Denomination.JACK
-        "q" -> Denomination.QUEEN
-        "k" -> Denomination.KING
-        else -> throw IllegalArgumentException("잘못된 ValueType 을 입력하셨습니다.")
-    }
+    return Denomination.entries[number % DIVIDER]
 }
 
 private const val DIVIDER = 13
--- src/main/kotlin/model/card/Suit.kt
+++ src/main/kotlin/model/card/Suit.kt
@@ -1,29 +1,19 @@
 package model.card
 
-enum class Suit(val mark: String, val order: Int) {
-    SPADE("스페이드", 0),
-    CLOVER("클로버", 1),
-    HEART("하트", 2),
-    DIAMOND("다이아몬드", 3),
+enum class Suit(val order: Int) {
+    SPADE(0),
+    CLOVER(1),
+    HEART(2),
+    DIAMOND(3),
 }
 
-fun intToMarkType(number: Int): Suit {
+fun intToSuitType(number: Int): Suit {
     return when (number / DIVIDER) {
         Suit.SPADE.order -> Suit.SPADE
         Suit.CLOVER.order -> Suit.CLOVER
         Suit.HEART.order -> Suit.HEART
         Suit.DIAMOND.order -> Suit.DIAMOND
         else -> throw IllegalArgumentException("잘못된 숫자를 입력하셨습니다. (0~51 사이의 숫자만 가능합니다)")
-    }
-}
-
-fun stringToMarkType(input: String): Suit {
-    return when (input.lowercase()) {
-        "d" -> Suit.DIAMOND
-        "s" -> Suit.SPADE
-        "c" -> Suit.CLOVER
-        "h" -> Suit.HEART
-        else -> throw IllegalArgumentException("잘못된 MarkType 을 입력하셨습니다.")
     }
 }
 
