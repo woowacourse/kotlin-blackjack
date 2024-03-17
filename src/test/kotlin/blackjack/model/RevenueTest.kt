6e2bf56631cf71350d6cb49001710f335c5be258
김진우 <jinwoo3661@naver.com>
2024-03-18 03:08:57 +0900

    refactor: 테스트코드명 명확하게 하여 의도 드러내도록 리팩토링

--- src/test/kotlin/blackjack/model/RevenueTest.kt
+++ src/test/kotlin/blackjack/model/RevenueTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test
 class RevenueTest {
 
     @Test
-    fun `(딜러 lose , 플레이어 블랙잭) 정상적인 Payout과 수익금 테스트`() {
+    fun `딜러 lose , 플레이어 블랙잭인 상황에서 수익금은 150원이어야 한다`() {
         val dealerResult = mapOf(Result.LOSE to 1)
         val player =
             Player(Wallet(Identification("누누"), 100)).apply {
@@ -23,22 +23,22 @@ class RevenueTest {
     }
 
     @Test
-    fun `(딜러 lose , 플레이어 win) 정상적인 Payout과 수익금 테스트`() {
+    fun `딜러 lose , 플레이어 win인 상황에서 수익금은 100원이어야 한다`() {
         val dealerResult = mapOf(Result.LOSE to 1)
-        val playerResults = mapOf(Player(Wallet(Identification("누누"), 50)) to Result.WIN)
+        val playerResults = mapOf(Player(Wallet(Identification("누누"), 100)) to Result.WIN)
         val gameResult = GameResult(dealerResult, playerResults)
         val actualRevenues = gameResult.calculateRevenuePercentages()
 
         assertEquals(1, actualRevenues.size)
         assertEquals("누누", actualRevenues.first().name)
-        assertEquals(50, actualRevenues.first().revenue)
+        assertEquals(100, actualRevenues.first().revenue)
     }
 
     @Test
-    fun `(딜러 블랙잭 , 플레이어 블랙잭 ) 정상적인 Payout과 수익금 테스트`() {
+    fun `딜러 블랙잭, 플레이어 블랙잭인 상황에서 수익금은 0원이어야 한다`() {
         val dealerResult = mapOf(Result.DRAW to 1)
         val player =
-            Player(Wallet(Identification("누누"), 50)).apply {
+            Player(Wallet(Identification("누누"), 100)).apply {
                 draw(Card(Denomination.ACE, Suit.CLOVER))
                 draw(Card(Denomination.KING, Suit.DIAMOND))
             }
@@ -48,13 +48,13 @@ class RevenueTest {
 
         assertEquals(1, actualRevenues.size)
         assertEquals("누누", actualRevenues.first().name)
-        assertEquals(50, actualRevenues.first().revenue)
+        assertEquals(0, actualRevenues.first().revenue)
     }
 
     @Test
-    fun `(딜러 draw , 플레이어 draw) 정상적인 Payout과 수익금 테스트`() {
+    fun `딜러 draw , 플레이어 draw인 상황에서 수익금은 0원이어야 한다`() {
         val dealerResult = mapOf(Result.DRAW to 1)
-        val playerResults = mapOf(Player(Wallet(Identification("누누"), 50)) to Result.DRAW)
+        val playerResults = mapOf(Player(Wallet(Identification("누누"), 100)) to Result.DRAW)
         val gameResult = GameResult(dealerResult, playerResults)
         val actualRevenues = gameResult.calculateRevenuePercentages()
 
@@ -64,14 +64,14 @@ class RevenueTest {
     }
 
     @Test
-    fun `(딜러 win , 플레이어 lose) 정상적인 Payout과 수익금 테스트`() {
+    fun `딜러 win , 플레이어 lose인 상황에서 수익금은 -100원이어야 한다`() {
         val dealerResult = mapOf(Result.WIN to 1)
-        val playerResults = mapOf(Player(Wallet(Identification("누누"), 50)) to Result.LOSE)
+        val playerResults = mapOf(Player(Wallet(Identification("누누"), 100)) to Result.LOSE)
         val gameResult = GameResult(dealerResult, playerResults)
         val actualRevenues = gameResult.calculateRevenuePercentages()
 
         assertEquals(1, actualRevenues.size)
         assertEquals("누누", actualRevenues.first().name)
-        assertEquals(-50, actualRevenues.first().revenue)
+        assertEquals(-100, actualRevenues.first().revenue)
     }
 }
