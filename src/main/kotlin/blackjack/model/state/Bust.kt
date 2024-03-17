575d2de1562ce0f9bfad5d36f7c862925a38bdc5
Junyoung-WON <wjyo60@gmail.com>
2024-03-17 20:14:58 +0900

    refactor: Bust가 될 때 갖고 있던 수익금을 모두 잃도록 구현 및 승패 연산 수정

--- src/main/kotlin/blackjack/model/state/Bust.kt
+++ src/main/kotlin/blackjack/model/state/Bust.kt
@@ -4,13 +4,24 @@ import blackjack.base.BaseHolder
 import blackjack.model.GameResult
 import blackjack.model.Hand
 import blackjack.model.Player
+import blackjack.model.Profit
 
-class Bust(hand: Hand) : Finished(hand) {
+class Bust(
+    hand: Hand,
+    profit: Profit
+) : Finished(hand, profit) {
+    init {
+        this.profit.lostAllBettingMoney()
+    }
+
     // 자신이 Bust일 때 상대와 비교
     override fun decideWinner(opponent: BaseHolder): GameResult {
         // 플레이어(상대)가 버스트라면 딜러 승리
-        if (opponent is Player && opponent.state is Bust)
+        if (opponent is Player && opponent.state is Bust) {
+            opponent.addResult(GameResult(defeat = 1))
             return GameResult(win = 1)
+        }
+        opponent.addResult(GameResult(win = 1))
         return GameResult(defeat = 1)
     }
 }
