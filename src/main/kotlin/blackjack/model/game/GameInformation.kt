c9c090c106d7c8021af6d35f9c63f23b676ce020
dpcks0509 <dpcks0509@gmail.com>
2024-03-18 04:29:08 +0900

    refactor: State 판별 로직 위치 변경

--- src/main/kotlin/blackjack/model/game/GameInformation.kt
+++ src/main/kotlin/blackjack/model/game/GameInformation.kt
@@ -3,34 +3,17 @@ package blackjack.model.game
 import blackjack.model.card.Card
 import blackjack.model.user.Hand
 
-class GameInformation(val hand: Hand = Hand(), state: GameState = GameState.Running.HIT) {
-    private var _state: GameState = state
+class GameInformation(val hand: Hand = Hand()) {
+    private var _state: GameState = GameState.Running.HIT
     val state: GameState
         get() = _state
 
     fun drawCard(card: Card) {
         hand.drawCard(card)
-        judgeState()
+        _state = state.judgeState(hand)
     }
 
     fun changeStateToStay() {
         _state = GameState.Finished.STAY
-    }
-
-    private fun judgeState() {
-        val point = hand.score.point
-        _state =
-            when {
-                point > BLACKJACK_POINT -> GameState.Finished.BUST
-                point == BLACKJACK_POINT && hand.cards.size == BLACKJACK_CARD_SIZE ->
-                    GameState.Finished.BLACKJACK
-
-                else -> GameState.Running.HIT
-            }
-    }
-
-    companion object {
-        private const val BLACKJACK_CARD_SIZE = 2
-        private const val BLACKJACK_POINT = 21
     }
 }
--- src/main/kotlin/blackjack/model/game/GameState.kt
+++ src/main/kotlin/blackjack/model/game/GameState.kt
@@ -1,6 +1,24 @@
 package blackjack.model.game
 
+import blackjack.model.user.Hand
+
 sealed interface GameState {
+    fun judgeState(hand: Hand): GameState {
+        val point = hand.score.point
+        return when {
+            point > BLACKJACK_POINT -> Finished.BUST
+            point == BLACKJACK_POINT && hand.cards.size == BLACKJACK_CARD_SIZE ->
+                Finished.BLACKJACK
+
+            else -> Running.HIT
+        }
+    }
+
+    companion object {
+        private const val BLACKJACK_CARD_SIZE = 2
+        private const val BLACKJACK_POINT = 21
+    }
+
     enum class Running : GameState {
         HIT,
     }
