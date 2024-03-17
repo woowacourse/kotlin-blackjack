91a20ca59c1521393050fc36726b8ab69844399c
Junyoung-WON <wjyo60@gmail.com>
2024-03-17 11:08:49 +0900

    feat: 베팅금을 가지며 베팅금에 대한 검증을 수행하는 원시값 포장 클래스 생성

---
+++ src/main/kotlin/blackjack/model/BettingMoney.kt
@@ -0,0 +1,11 @@
+package blackjack.model
+
+class BettingMoney(betting: String) {
+    val amount: Int
+
+    init {
+        requireNotNull(betting.toIntOrNull()) { "베팅금을 숫자로만 입력해주세요" }
+        require(betting.toInt() > 0) { "베팅금은 0보다 큰 정수로 입력해주세요" }
+        amount = betting.toInt()
+    }
+}
