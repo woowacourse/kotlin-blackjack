cd234c38307d0d94128ffa5b0bbe565525257e2c
Junyoung-WON <wjyo60@gmail.com>
2024-03-17 12:02:17 +0900

    refactor: 베팅금 변수 이름을 더 명확하게 변경

--- src/main/kotlin/blackjack/model/Profit.kt
+++ src/main/kotlin/blackjack/model/Profit.kt
@@ -1,11 +1,11 @@
 package blackjack.model
 
-class Profit(betting: String) {
+class Profit(bettingMoney: String) {
     val amount: Int
 
     init {
-        requireNotNull(betting.toIntOrNull()) { "베팅금을 숫자로만 입력해주세요" }
-        require(betting.toInt() > 0) { "베팅금은 0보다 큰 정수로 입력해주세요" }
-        amount = betting.toInt()
+        requireNotNull(bettingMoney.toIntOrNull()) { "베팅금을 숫자로만 입력해주세요" }
+        require(bettingMoney.toInt() > 0) { "베팅금은 0보다 큰 정수로 입력해주세요" }
+        amount = bettingMoney.toInt()
     }
 }
