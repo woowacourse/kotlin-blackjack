286713a7c88e06f4a34549306056268ce5158dd5
Junyoung-WON <wjyo60@gmail.com>
2024-03-17 20:16:43 +0900

    refactor: BlackJack이 될 때 갖고 있던 수익금의 1.5배를 받도록 구현 및 승패 연산 수정
    단, 상대(딜러)가 블랙잭이라면 무승부이므로 베팅금을 다시 그대로 돌려받는다.(수익금 0원)

--- src/main/kotlin/blackjack/model/state/BlackJack.kt
+++ src/main/kotlin/blackjack/model/state/BlackJack.kt
@@ -3,13 +3,25 @@ package blackjack.model.state
 import blackjack.base.BaseHolder
 import blackjack.model.GameResult
 import blackjack.model.Hand
+import blackjack.model.Profit
 
-class BlackJack(hand: Hand) : Finished(hand) {
+class BlackJack(
+    hand: Hand,
+    profit: Profit,
+) : Finished(hand, profit) {
+    init {
+        this.profit.earnProfitForBlackJack()
+    }
+
     // 자신이 블랙잭일 때 상대와 비교
     override fun decideWinner(opponent: BaseHolder): GameResult {
         // 상대가 블랙잭일 때 무승부
-        if (opponent.state is BlackJack)
+        if (opponent.state is BlackJack) {
+            profit.giveBackBettingMoney()
+            opponent.addResult(GameResult(push = 1))
             return GameResult(push = 1)
+        }
+        opponent.addResult(GameResult(defeat = 1))
         return GameResult(win = 1)
     }
 }
