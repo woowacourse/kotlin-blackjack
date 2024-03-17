f2822311c01c0ab27436124803a776a383036c03
Junyoung-WON <wjyo60@gmail.com>
2024-03-17 12:10:15 +0900

    feat: 베팅금을 0원으로 만드는 메서드 작성 및 백킹 프로퍼티 활용

--- src/main/kotlin/blackjack/model/Profit.kt
+++ src/main/kotlin/blackjack/model/Profit.kt
@@ -1,11 +1,17 @@
 package blackjack.model
 
 class Profit(bettingMoney: String) {
+    private var _amount = 0
     val amount: Int
+        get() = _amount
 
     init {
         requireNotNull(bettingMoney.toIntOrNull()) { "베팅금을 숫자로만 입력해주세요" }
         require(bettingMoney.toInt() > 0) { "베팅금은 0보다 큰 정수로 입력해주세요" }
-        amount = bettingMoney.toInt()
+        _amount = bettingMoney.toInt()
+    }
+
+    fun giveBackBettingMoney() {
+        _amount = 0
     }
 }
