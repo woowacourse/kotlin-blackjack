18394f4a576c0de1db1307b50fb6c54206070f94
gaeun <geun5744@gmail.com>
2024-03-17 16:59:40 +0900

    feat: Blackjack2 객체 구현

--- src/main/kotlin/blackjack/model/participant/testState/Blackjack2.kt
+++ src/main/kotlin/blackjack/model/participant/testState/Blackjack2.kt
@@ -1,13 +1,21 @@
 package blackjack.model.participant.testState
 
+import blackjack.model.BattingMoney
 import blackjack.model.participant.CompetitionResult
 
 class Blackjack2() : Finish() {
-    fun getResult(opponentScore: Int): CompetitionResult {
+    fun getProfit(
+        opponentScore: Int,
+        battingMoney: BattingMoney,
+    ): BattingMoney {
+        return battingMoney.times(getResult(opponentScore).profit)
+    }
+
+    private fun getResult(opponentScore: Int): CompetitionResult {
         return if (opponentScore == 21) {
             CompetitionResult.SAME
         } else {
-            CompetitionResult.WIN
+            CompetitionResult.BLACKJACK
         }
     }
 }
