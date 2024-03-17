aaa98cd46975adb195781bfb83b54211fe2fc397
sh1mj1 <7wlgns@gmail.com>
2024-03-17 15:14:44 +0900

    feat(Bust): 수익률을 구현한다
    - 플레이어가 버스트일 대 딜러가 무엇이든 상관없이 항상 -1.0 배의 손해를 입는다

--- src/main/kotlin/blackjack/model/card/state/Bust.kt
+++ src/main/kotlin/blackjack/model/card/state/Bust.kt
@@ -2,4 +2,6 @@ package blackjack.model.card.state
 
 import blackjack.model.card.CardHand
 
-class Bust(cardHand: CardHand) : Done(cardHand)
+class Bust(cardHand: CardHand) : Done(cardHand) {
+    override fun earningRate(other: CardHandState): Double = -1.0
+}
---
+++ src/test/kotlin/blackjack/model/card/state/BustTest.kt
@@ -0,0 +1,64 @@
+package blackjack.model.card.state
+
+import blackjack.model.card.Card
+import blackjack.model.card.CardHand
+import blackjack.model.card.CardNumber
+import blackjack.model.card.CardShape
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class BustTest {
+    private val bust =
+        Bust(
+            CardHand(
+                Card(CardShape.SPADE, CardNumber.FIVE),
+                Card(CardShape.HEART, CardNumber.JACK),
+                Card(CardShape.CLOVER, CardNumber.EIGHT),
+            ),
+        )
+
+    @DisplayName("플레이어: 버스트,  딜러: 버스트 이면 플레이어는 -1.0 배의 손해를 입는다")
+    @Test
+    fun `플레이어와 딜러가 둘 다 버스트`() {
+        val other: CardHandState =
+            Bust(
+                CardHand(
+                    Card(CardShape.SPADE, CardNumber.ACE),
+                    Card(CardShape.CLOVER, CardNumber.JACK),
+                    Card(CardShape.DIAMOND, CardNumber.THREE),
+                    Card(CardShape.HEART, CardNumber.NINE),
+                ),
+            )
+        val actualProfit = bust.calculateProfit(1_000, other)
+        Assertions.assertThat(actualProfit).isEqualTo(-1_000.0)
+    }
+
+    @DisplayName("플레이어: 버스트,  딜러: 스테이 이면 플레이어는 -1.0 배의 손해를 입는다")
+    @Test
+    fun `플레이어는 버스트이고 딜러가 스테이`() {
+        val other: CardHandState =
+            Stay(
+                CardHand(
+                    Card(CardShape.DIAMOND, CardNumber.THREE),
+                    Card(CardShape.HEART, CardNumber.NINE),
+                ),
+            )
+        val actualProfit = bust.calculateProfit(1_000, other)
+        Assertions.assertThat(actualProfit).isEqualTo(-1_000.0)
+    }
+
+    @DisplayName("플레이어: 버스트,  딜러: 블랙잭 이면 플레이어는 -1.0 배의 손해를 입는다")
+    @Test
+    fun `플레이어는 버스트이고 딜러가 블랙잭`() {
+        val other: CardHandState =
+            BlackJack(
+                CardHand(
+                    Card(CardShape.DIAMOND, CardNumber.ACE),
+                    Card(CardShape.HEART, CardNumber.QUEEN),
+                ),
+            )
+        val actualProfit = bust.calculateProfit(1_000, other)
+        Assertions.assertThat(actualProfit).isEqualTo(-1_000.0)
+    }
+}
