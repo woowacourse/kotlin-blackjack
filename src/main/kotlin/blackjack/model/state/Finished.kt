62f8fb079216bab4eed8c358688d94daffbf2597
Junyoung-WON <wjyo60@gmail.com>
2024-03-17 20:09:50 +0900

    refactor: Profit 멤버 변수가 추가됨에 따른 override 리팩터링

--- src/main/kotlin/blackjack/model/state/Finished.kt
+++ src/main/kotlin/blackjack/model/state/Finished.kt
@@ -2,8 +2,12 @@ package blackjack.model.state
 
 import blackjack.model.Card
 import blackjack.model.Hand
+import blackjack.model.Profit
 
-sealed class Finished(override val hand: Hand) : State, CompareImpl {
+sealed class Finished(
+    override val hand: Hand,
+    override val profit: Profit,
+) : State, CompareImpl {
     override fun getCard(card: Card): State = this
 
     override fun updateState(totalPoint: Int): State = this
--- src/main/kotlin/blackjack/model/state/Progressing.kt
+++ src/main/kotlin/blackjack/model/state/Progressing.kt
@@ -3,12 +3,16 @@ package blackjack.model.state
 import blackjack.base.BaseHolder
 import blackjack.model.GameResult
 import blackjack.model.Hand
+import blackjack.model.Profit
 
-sealed class Progressing(override val hand: Hand) : State {
+sealed class Progressing(
+    override val hand: Hand,
+    override val profit: Profit,
+) : State {
     override fun updateState(totalPoint: Int): State {
-        return if (totalPoint > Hand.BLACKJACK_NUMBER) Bust(hand)
-        else if (totalPoint == Hand.BLACKJACK_NUMBER) Stay(hand)
-        else Running(hand)
+        return if (totalPoint > Hand.BLACKJACK_NUMBER) Bust(hand, profit)
+        else if (totalPoint == Hand.BLACKJACK_NUMBER) Stay(hand, profit)
+        else Running(hand, profit)
     }
 
     override fun decideWinner(opponent: BaseHolder): GameResult = GameResult()
