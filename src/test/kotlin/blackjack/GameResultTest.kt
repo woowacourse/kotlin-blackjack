30b9968cccf2910d330472f3859c28b26a611d5a
ii2001 <iivks2001@gmail.com>
2024-03-18 14:02:54 +0900

    refect: 수익률 계산부분 변경에따른 테스트 코드 변경

--- src/test/kotlin/blackjack/GameResultTest.kt
+++ src/test/kotlin/blackjack/GameResultTest.kt
@@ -3,23 +3,18 @@ import blackjack.model.card.Denomination
 import blackjack.model.card.Hand
 import blackjack.model.card.Suit
 import blackjack.model.game.BettingMoney
-import blackjack.model.game.GameResult
 import blackjack.model.game.Result
-import blackjack.model.player.Dealer
 import blackjack.model.player.PlayerResult
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class GameResultTest {
-    private val dealer = Dealer(Hand(listOf(Card(Denomination.TWO, Suit.CLUBS)).toMutableList()))
-
     @Test
     fun `플레이어의 상태가 무승부라면 수익률은 0이다`() {
         val player =
             Player("Test", Hand(listOf(Card(Denomination.ACE, Suit.HEARTS)).toMutableList()), BettingMoney(1000))
         val playerResult = PlayerResult(player, Result.DRAW)
-        val gameResult = GameResult(dealer, listOf(playerResult))
-        assertEquals(0, gameResult.playerProfits[player])
+        assertEquals(0, playerResult.profit)
     }
 
     @Test
@@ -31,9 +26,7 @@ class GameResultTest {
                 BettingMoney(1000),
             )
         val playerResult = PlayerResult(player, Result.PLAYER_WIN)
-        val gameResult = GameResult(dealer, listOf(playerResult))
-        val expectedProfit = (1500).toInt()
-        assertEquals(expectedProfit, gameResult.playerProfits[player])
+        assertEquals(1500, playerResult.profit)
     }
 
     @Test
@@ -41,7 +34,6 @@ class GameResultTest {
         val player =
             Player("Test", Hand(listOf(Card(Denomination.ACE, Suit.HEARTS)).toMutableList()), BettingMoney(1000))
         val playerResult = PlayerResult(player, Result.DEALER_WIN)
-        val gameResult = GameResult(dealer, listOf(playerResult))
-        assertEquals(-1000, gameResult.playerProfits[player])
+        assertEquals(-1000, playerResult.profit)
     }
 }
