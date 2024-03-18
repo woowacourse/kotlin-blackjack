67c30479cb4c644aeff96efc1cc7654c7be75433
Hogu59 <dbstjdguswkd@naver.com>
2024-03-18 21:33:47 +0900

    style(HumanName): 문자열 상수화

--- src/main/kotlin/model/human/HumanName.kt
+++ src/main/kotlin/model/human/HumanName.kt
@@ -6,7 +6,7 @@ data class HumanName(val name: String) {
     }
 
     companion object {
-        private const val ERROR_INVALID_LENGTH = "이름의 길이는 1 이상 10 이하 이어야 합니다."
+        const val ERROR_INVALID_LENGTH = "이름의 길이는 1 이상 10 이하 이어야 합니다."
         private const val MIN_NAME_LENGTH: Int = 1
         private const val MAX_NAME_LENGTH: Int = 10
         private val NAME_LENGTH_RANGE: IntRange = MIN_NAME_LENGTH..MAX_NAME_LENGTH
