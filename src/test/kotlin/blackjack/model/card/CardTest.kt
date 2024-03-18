2d371423e6f141d81e5435d1a954a8c53982881d
Namyunsuk <yunsuk0328@naver.com>
2024-03-18 15:23:08 +0900

    test: 참조값을 비교하도록 수정

--- src/test/kotlin/blackjack/model/card/CardTest.kt
+++ src/test/kotlin/blackjack/model/card/CardTest.kt
@@ -8,6 +8,6 @@ class CardTest {
     fun `카드 생성 시 캐싱된 카드뭉치에서 카드를 가져오는지 확인한다`() {
         val actual = Card.of(Denomination.ACE, Suite.HEART)
         val expected = Card.of(Denomination.ACE, Suite.HEART)
-        assertThat(actual).isEqualTo(expected)
+        assertThat(actual).isSameAs(expected)
     }
 }
