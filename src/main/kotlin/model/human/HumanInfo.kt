c2502c0eb27378df8901cf8cbde624e14d3bafe5
Hogu59 <dbstjdguswkd@naver.com>
2024-03-19 08:46:02 +0900

    feat(PlayersResult): 각 플레이어의 게임 결과를 담는 클래스 구현

--- src/main/kotlin/model/human/HumanInfo.kt
+++ src/main/kotlin/model/human/HumanInfo.kt
@@ -5,15 +5,20 @@ import model.Money
 class HumanInfo(val humanName: HumanName, val money: Money = Money(DEFAULT_MONEY_AMOUNT)) {
     constructor(humanName: String) : this(HumanName(humanName), Money(DEFAULT_MONEY_AMOUNT))
 
-    fun changeMoney(amount: Int) {
+    fun setMoney(amount: Int) {
         money.changeAmount(amount)
     }
 
-    fun applyResultToMoney(
-        other: HumanInfo,
-        rate: Double,
-    ) {
-        this.money.applyProfitRate(other.money, rate)
+    fun getName(): String = humanName.name
+
+    fun getMoneyAmount(): Int = money.amount
+
+    fun add(other: Money) {
+        money.add(other)
+    }
+
+    fun applyResultToDealerMoney(other: Int, rate: Double) {
+        money.add(other*rate.toInt())
     }
 
     companion object {
