feebc859acf6180b2c2a5701244ccee5c60827fe
Hogu59 <dbstjdguswkd@naver.com>
2024-03-19 08:45:05 +0900

    fix(Dealer): judge 로직 수정
    플레이어 블랙잭 판단 오류 수정

--- src/main/kotlin/model/human/Dealer.kt
+++ src/main/kotlin/model/human/Dealer.kt
@@ -40,38 +40,50 @@ class Dealer(hand: Hand, override val humanInfo: HumanInfo = HumanInfo(DEFAULT_N
 
     private fun getResultWhenNotBusted(other: Human): WinningResult {
         return when {
+            other.isBlackJack() -> WinningResult.LOSE
             other.isBusted() -> WinningResult.WIN
             !other.isBusted() && (this.hand.getPoint() > other.hand.getPoint()) -> WinningResult.WIN
             !other.isBusted() && (this.hand.getPoint() == other.hand.getPoint()) -> WinningResult.DRAW
             else -> WinningResult.LOSE
         }
     }
-/*
-    fun judge(player: Player) {
-        when (getCompareResult(player)) {
+
+    fun judge(player: Player): HumanInfo {
+        return when (getCompareResult(player)) {
             WinningResult.WIN -> {
-                humanInfo.applyResultToMoney(player.humanInfo, PROFIT_RATE_LOSE)
+                getJudgeResult(player, PROFIT_RATE_LOSE)
             }
 
             WinningResult.LOSE -> {
                 if (player.isBlackJack()) {
-                    humanInfo.applyResultToMoney(player.humanInfo, PROFIT_RATE_BLACKJACK)
+                    getJudgeResult(player, PROFIT_RATE_BLACKJACK)
                 } else {
-                    humanInfo.applyResultToMoney(player.humanInfo, PROFIT_RATE_WIN)
+                    getJudgeResult(player, PROFIT_RATE_WIN)
                 }
             }
 
             WinningResult.DRAW -> {
-                humanInfo.applyResultToMoney(player.humanInfo, PROFIT_RATE_DRAW)
+                getJudgeResult(player, PROFIT_RATE_DRAW)
             }
         }
     }
 
- */
+    private fun getJudgeResult(
+        player: Player,
+        rate: Double,
+    ): HumanInfo {
+        val result = HumanInfo(player.getName())
+        val rateAppliedAmount = (player.getMoneyAmount() * rate).toInt()
+        result.setMoney(rateAppliedAmount)
+        humanInfo.applyResultToDealerMoney(rateAppliedAmount, rate*DEALER_ASPECT)
+
+        return result
+    }
 
     companion object {
         private const val HIT_CONDITION = 17
         private const val DEFAULT_CARD_COUNT = 2
+        private const val DEALER_ASPECT = -1
 
         private const val PROFIT_RATE_LOSE = -1.0
         private const val PROFIT_RATE_DRAW = 0.0
