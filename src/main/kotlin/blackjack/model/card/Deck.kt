59319e898f6aa62016bcab70fd80bb21df9ac89e
ii2001 <iivks2001@gmail.com>
2024-03-18 13:28:08 +0900

    refect: Deck구조 방어적 복사로 변경

--- src/main/kotlin/blackjack/model/card/Deck.kt
+++ src/main/kotlin/blackjack/model/card/Deck.kt
@@ -1,28 +1,21 @@
 package blackjack.model.card
 
-class Deck {
-    // 내부적으로 수정 가능한 카드 리스트
-    private val _cards: MutableList<Card> = mutableListOf()
-
-    // 외부에 제공될 불변 리스트 뷰
+class Deck(cards: List<Card> = generateStandardDeck()) {
+    private val _cards = cards.toMutableList()
     val cards: List<Card> get() = _cards.toList()
 
     init {
-        if (_cards.isEmpty()) {
-            initializeDeck()
-        }
+        _cards.shuffle()
     }
 
-    private fun initializeDeck() {
-        _cards.clear()
-        _cards.addAll(
-            Suit.entries.flatMap { suit ->
+    companion object {
+        private fun generateStandardDeck(): MutableList<Card> {
+            return Suit.entries.flatMap { suit ->
                 Denomination.entries.map { denomination ->
                     Card(denomination, suit)
                 }
-            },
-        )
-        _cards.shuffle()
+            }.toMutableList()
+        }
     }
 
     fun doubleDealCard() = listOf(dealCard(), dealCard())
