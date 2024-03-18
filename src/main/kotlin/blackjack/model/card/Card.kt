d1b4ec039712aa4cda3bcd8fcc59621bacd8ada6
Namyunsuk <yunsuk0328@naver.com>
2024-03-18 09:35:41 +0900

    refactor: 불필요한 검증 로직 삭제

--- src/main/kotlin/blackjack/model/card/Card.kt
+++ src/main/kotlin/blackjack/model/card/Card.kt
@@ -1,25 +1,11 @@
 package blackjack.model.card
 
 data class Card private constructor(val denomination: Denomination, val suite: Suite) {
-    init {
-        validateCardInfo(denomination, suite)
-    }
-
     fun getScore(): Int {
         return denomination.score
     }
 
-    private fun validateCardInfo(
-        denomination: Denomination,
-        suite: Suite,
-    ) {
-        require(Denomination.entries.contains(denomination)) { INVALID_DENOMINATION }
-        require(Suite.entries.contains(suite)) { INVALID_SUITE }
-    }
-
     companion object {
-        private const val INVALID_DENOMINATION = "유효하지 않은 카드의 숫자 혹은 알파벳입니다."
-        private const val INVALID_SUITE = "유효하지 않은 카드의 모양입니다."
         val bundle: List<Card> = createCardBundle()
 
         private fun createCardBundle(): List<Card> {
