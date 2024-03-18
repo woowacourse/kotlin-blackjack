75ba3bd3774a81998914bc5a198c85ceffeff331
Junyoung-WON <wjyo60@gmail.com>
2024-03-18 09:54:50 +0900

    refactor: 수익 계산 메서드와 승패에 따른 게임 결과 생성 메서드 구현
    calculateProfitByOpponent: 수익 계산 메서드, 상대의 수익에 따라 자신의 수익을 변경하도록 profit에게 메시징
    
    makeOpponentLoser: 자신은 승리, 상대는 패배한 상황에서 결과를 생성하여 반환
    
    makeOpponentWinner: 자신은 패배, 상대는 승리한 상황에서 결과를 생성하여 반환
    
    push: 자신과 상대가 무승부인 상황에서 결과를 생성하여 반환

--- src/main/kotlin/blackjack/model/state/Finished.kt
+++ src/main/kotlin/blackjack/model/state/Finished.kt
@@ -1,6 +1,8 @@
 package blackjack.model.state
 
+import blackjack.base.BaseHolder
 import blackjack.model.Card
+import blackjack.model.GameResult
 import blackjack.model.Hand
 import blackjack.model.Profit
 
@@ -13,4 +15,28 @@ sealed class Finished(
     override fun updateState(totalPoint: Int): State = this
 
     override fun hitOrStay(isHit: Boolean): State = this
+
+    fun makeOpponentLoser(opponent: BaseHolder): GameResult {
+        opponent.state.calculateProfitByOpponent(profit.amount)
+        opponent.addResult(GameResult(defeat = 1))
+        return GameResult(win = 1)
+    }
+
+    fun push(opponent: BaseHolder): GameResult {
+        profit.giveBackBettingMoney()
+        opponent.state.calculateProfitByOpponent(profit.amount)
+        opponent.addResult(GameResult(push = 1))
+        return GameResult(push = 1)
+    }
+
+    fun makeOpponentWinner(opponent: BaseHolder): GameResult {
+        profit.lostAllBettingMoney()
+        opponent.state.calculateProfitByOpponent(profit.amount)
+        opponent.addResult(GameResult(win = 1))
+        return GameResult(defeat = 1)
+    }
+
+    override fun calculateProfitByOpponent(opponentProfit: Double) {
+        profit.getProfitFromPlayer(opponentProfit)
+    }
 }
