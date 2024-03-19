1340bb414eaab6dcb8a36b6c940244787456ef19
kkosang <eiwoqp77@gmail.com>
2024-03-20 00:47:41 +0900

    refactor: Fixture 추가 및 given-when-then 패턴 적용

--- src/test/kotlin/blackjack/model/CardDeckTest.kt
+++ src/test/kotlin/blackjack/model/CardDeckTest.kt
@@ -11,6 +11,10 @@ class CardDeckTest {
     private lateinit var cardDeck: CardDeck
     private lateinit var expectedCards: MutableList<Card>
 
+    private fun createCards(vararg cardPairs: Pair<Denomination, Suit>): List<Card> {
+        return cardPairs.map { Card(it.first, it.second) }
+    }
+
     @BeforeEach
     fun setUp() {
         cardDeck = CardDeck()
@@ -32,32 +36,36 @@ class CardDeckTest {
     @Test
     fun `생성된 카드 덱을 셔플하면 카드가 섞여야 한다`() {
         val shuffledCardDeck = cardDeck.cardShuffle()
+
         Assertions.assertNotEquals(cardDeck, shuffledCardDeck)
     }
 
     @Test
     fun `2스페이드, 3다이아, 4하트, 5클로버 구성의 카드덱을 설정했을 때 카드를 뽑으면 같은 순서로 나와야 한다`() {
-        val cards =
-            LinkedList<Card>().apply {
-                add(Card(Denomination.TWO, Suit.SPADE))
-                add(Card(Denomination.THREE, Suit.DIAMOND))
-                add(Card(Denomination.FOUR, Suit.HEART))
-                add(Card(Denomination.FIVE, Suit.CLOVER))
-            }
+        val cards = createCards(
+            Denomination.TWO to Suit.SPADE,
+            Denomination.THREE to Suit.DIAMOND,
+            Denomination.FOUR to Suit.HEART,
+            Denomination.FIVE to Suit.CLOVER
+        )
         val cardDeck = CardDeck(cards = LinkedList(cards))
 
-        for (i in 0 until 4) {
-            val drawnCard = cardDeck.draw()
-            assertThat(cards[i]).isEqualTo(drawnCard)
+        val handCard = mutableListOf<Card>()
+        repeat(cards.size) {
+            handCard.add(cardDeck.draw())
         }
+
+        assertThat(handCard).isEqualTo(cards)
     }
 
     @Test
     fun `카드 덱에서 뽑을 수 있는 카드가 없을 때, 예외를 던져야한다`() {
         val cardDeck = CardDeck()
+
         repeat(CardDeck.MAX_DRAW_COUNT) {
             cardDeck.draw()
         }
+
         assertThrows<IllegalArgumentException> { cardDeck.draw() }
     }
 }
