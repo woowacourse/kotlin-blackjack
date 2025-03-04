267d955f193130f98adf2b32be43ee46be8b5d7f
HamBeomJoon <hbj0802@naver.com>
2025-03-04 18:03:33 +0900

    feat: 덱 카드 한 장 뽑기 함수 추가

--- src/main/kotlin/blackjack/Deck.kt
+++ src/main/kotlin/blackjack/Deck.kt
@@ -4,6 +4,10 @@ class Deck {
     companion object {
         private val CARDS = generateCards()
 
+        fun poll(): Card {
+            return CARDS.removeFirst()
+        }
+
         private fun generateCards(): MutableList<Card> {
             val cards = mutableListOf<Card>()
 
