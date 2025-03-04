ec7c32925c05bb49bb68d9deb26b7785e7c03756
hwannow <vnfdlvgid061@naver.com>
2025-03-04 17:49:18 +0900

    test: 카드를 셔플해서 첫번째 장에 있는 카드를 나누어주는 테스트 추가

--- src/test/kotlin/CardTest.kt
+++ src/test/kotlin/CardTest.kt
@@ -3,19 +3,27 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class CardTest {
-    private lateinit var card: List<Card>
+    private lateinit var card: Deck
 
     @BeforeEach
     fun setup() {
-        val symbols = listOf("다이아몬드", "하트", "클로버", "스페이드")
+        val symbols = Shape.entries
         val cardNumbers = CardNumber.entries
 
-        card = symbols.flatMap { symbol -> cardNumbers.map { cardNumber -> Card(symbol, cardNumber) } }
+        card = Deck(symbols.flatMap { symbol -> cardNumbers.map { cardNumber -> Card(symbol, cardNumber) } })
     }
 
     @Test
     fun `심볼과 카드 숫자를 매핑하여 카드를 생성한다`() {
-        val deck = Card.deck
+        val deck = Card.initCard(FalseShuffle())
         assertThat(deck).isEqualTo(card)
+    }
+
+    @Test
+    fun `카드를 셔플해서 첫번째 장에 있는 카드를 나누어준다`() {
+        val deck = Card.initCard(FalseShuffle())
+        val card = deck.spreadCard()
+
+        assertThat(card).isEqualTo(Card(Shape.Diamond, CardNumber.Ace))
     }
 }
