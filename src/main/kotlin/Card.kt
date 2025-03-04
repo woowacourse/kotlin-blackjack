f26a64bd2e9bf238469a2258d7ec2007a30cf832
hwannow <vnfdlvgid061@naver.com>
2025-03-04 17:47:59 +0900

    refactor: 카드의 심볼 리스트를 래핑

--- src/main/kotlin/Card.kt
+++ src/main/kotlin/Card.kt
@@ -1,14 +1,9 @@
 data class Card(
-    val symbol: String,
+    val symbol: Shape,
     val number: CardNumber,
 ) {
     companion object {
-        private const val DIAMOND = "다이아몬드"
-        private const val HEART = "하트"
-        private const val CLOVER = "클로버"
-        private const val SPADE = "스페이드"
-
-        private val symbols = listOf(DIAMOND, HEART, CLOVER, SPADE)
+        private val symbols = Shape.entries
         private val cardNumbers = CardNumber.entries
 
         val deck = symbols.flatMap { symbol -> cardNumbers.map { cardNumber -> Card(symbol, cardNumber) } }
---
+++ src/main/kotlin/Shape.kt
@@ -0,0 +1,6 @@
+enum class Shape(val symbol: String) {
+    Diamond("다이아몬드"),
+    Heart("하트"),
+    Clover("클로버"),
+    Spade("스페이드"),
+}
