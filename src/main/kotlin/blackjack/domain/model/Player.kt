384bd1c5675225a5021919f3f629ef41508f1967
윤동현 <82762769+donghyun81@users.noreply.github.com>
2025-03-04 16:14:57 +0900

    feat(Player): 플레이어는 이름과 사용할 카드를 가진다

---
+++ src/main/kotlin/blackjack/domain/model/Player.kt
@@ -0,0 +1,3 @@
+package blackjack.domain.model
+
+data class Player(val name: String, val cards: List<Card>)
---
+++ src/test/kotlin/blackjack/domain/model/PlayerTest.kt
@@ -0,0 +1,25 @@
+package blackjack.domain.model
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class PlayerTest {
+
+    private lateinit var player: Player
+
+    @BeforeEach
+    fun `setUp`() {
+        player = Player("동전", cards = listOf(Card("하트", "A")))
+    }
+
+    @Test
+    fun `플레이어는 이름과 가진다`() {
+        assertThat(player.name).isEqualTo("동전")
+    }
+
+    @Test
+    fun `플레이어는 카드를 가진다`() {
+        assertThat(player.cards).isEqualTo(listOf(Card("하트", "A")))
+    }
+}
