397743864d382f5113a137e069d64bb67e2e7fb4
junseo511 <prime54@g.skku.edu>
2025-03-04 16:13:15 +0900

    feat: 카드 랭크 구현

---
+++ src/main/kotlin/blackjack/model/CardRank.kt
@@ -0,0 +1,35 @@
+package blackjack.model
+
+enum class CardRank(name: String) {
+    ACE("A"),
+    TWO("2"),
+    THREE("3"),
+    FOUR("4"),
+    FIVE("5"),
+    SIX("6"),
+    SEVEN("7"),
+    EIGHT("8"),
+    NINE("9"),
+    TEN("10"),
+    QUEEN("Q"),
+    KING("K"),
+    JACK("J"),
+    ;
+
+    companion object {
+        fun CardRank.score(isAceFlipped: Boolean = false):Int {
+            return when (this) {
+                ACE -> if (isAceFlipped) 1 else 11
+                TWO -> 2
+                THREE -> 3
+                FOUR -> 4
+                FIVE -> 5
+                SIX -> 6
+                SEVEN -> 7
+                EIGHT -> 8
+                NINE -> 9
+                else -> 10
+            }
+        }
+    }
+}
