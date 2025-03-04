bcd629fc74a6e9863ebcd513a7b1a60695ac33f1
HamBeomJoon <hbj0802@naver.com>
2025-03-04 18:03:09 +0900

    feat: 덱 카드 리스트 생성 함수 추가

---
+++ src/main/kotlin/blackjack/Deck.kt
@@ -0,0 +1,18 @@
+package blackjack
+
+class Deck {
+    companion object {
+        private val CARDS = generateCards()
+
+        private fun generateCards(): MutableList<Card> {
+            val cards = mutableListOf<Card>()
+
+            Shape.entries.forEach { shape ->
+                Number.entries.forEach { number ->
+                    cards.add(Card(shape, number))
+                }
+            }
+            return cards.shuffled().toMutableList()
+        }
+    }
+}
