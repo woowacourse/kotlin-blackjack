b932cb1dbaa95cf4f7f9b13b4388842028e34a40
medAndro <med4ndro@gmail.com>
2025-03-04 17:12:33 +0900

    test: Card 생성 테스트 코드 구현

---
+++ src/test/kotlin/blackjack/CardTest.kt
@@ -0,0 +1,32 @@
+package blackjack
+
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+
+class CardTest {
+    @ParameterizedTest
+    @CsvSource(
+        "1, 0",
+        "13, 3",
+    )
+    fun `유효한 카드 값을 받아 카드를 생성한다`(
+        cardNumber: Int,
+        suit: Int,
+    ) {
+        assertDoesNotThrow { Card(cardNumber, suit) }
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "14, 0",
+        "1, 4",
+    )
+    fun `유효하지 않은 카드 값을 받으면 카드가 생성되지 않는다`(
+        cardNumber: Int,
+        suit: Int,
+    ) {
+        assertThrows<IllegalArgumentException> { Card(cardNumber, suit) }
+    }
+}
