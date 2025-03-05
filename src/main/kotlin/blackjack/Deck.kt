fce0b8002d86d85d12c95ac0ff3b3178720ae91e
oungsi2000 <oungsi1000@gmail.com>
2025-03-05 11:22:37 +0900

    refactor: 테스트 코드 수정 및 덱 기능 추가

--- src/main/kotlin/blackjack/Deck.kt
+++ src/main/kotlin/blackjack/Deck.kt
@@ -1,19 +1,21 @@
 package blackjack
 
-import java.util.*
+import java.util.LinkedList
 
-class Deck(private val shuffledDeck: List<Card>) {
+class Deck(shuffledDeck: List<Card>) {
     private val deck = LinkedList(shuffledDeck)
-
 
     init {
         require(shuffledDeck.size == MAXIMUM_DECK_SIZE) { "덱의 사이즈는 52여야 합니다" }
         require(shuffledDeck.distinct().size == MAXIMUM_DECK_SIZE) { "카드는 중복될 수 없습니다" }
     }
 
-    fun getCard() : Card {
+    fun draw(): Card {
+        require(deck.isNotEmpty()) { "덱이 비어 있습니다" }
         return deck.poll()
     }
+
+    fun getSize() = deck.size
 
     companion object {
         const val MAXIMUM_DECK_SIZE = 52
--- src/test/kotlin/blackjack/DeckTest.kt
+++ src/test/kotlin/blackjack/DeckTest.kt
@@ -3,56 +3,47 @@ package blackjack
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
-import java.lang.IllegalArgumentException
-import java.lang.IllegalStateException
+import kotlin.IllegalArgumentException
 
 class DeckTest {
-    private val deck = Deck((0..51).toList())
+    private val testDeck = Deck(Card.getAllCard())
+
     @Test
     fun `카드를 뽑을 수 있다`() {
-        val card1 = deck.getCard()
-        assertThat(card1).isEqualTo(Card.SPADE_A)
-        val card2 = deck.getCard()
-        assertThat(card2).isEqualTo(Card.SPADE_2)
-
+        val original = testDeck.getSize()
+        testDeck.draw()
+        val change = testDeck.getSize()
+        assertThat(original - 1).isEqualTo(change)
     }
+
     @Test
-    fun `덱에서 카드는 최대 52번 뽑을 수 있다`(){
-        assertThrows<IllegalStateException> {
-            repeat(53) {deck.getCard()}
+    fun `덱에 카드가 없는 경우, 카드를 뽑을 수 없다`() {
+        assertThrows<IllegalArgumentException> {
+            repeat(testDeck.getSize() + 1) { testDeck.draw() }
         }
     }
 
     @Test
-    fun `뽑은 카드는 덱에 존재하지 않는다`(){
-        val card1 = deck.getCard()
-        assertThat(card1).isEqualTo(Card.SPADE_A)
+    fun `뽑은 카드는 덱에 존재하지 않는다`() {
+        val card1 = testDeck.draw()
+        assertThat(card1).isEqualTo(Card.of(Rank.ACE, Suit.SPADE))
         repeat(51) {
-            assertThat(deck.getCard()).isNotEqualTo(Card.SPADE_A)
+            assertThat(testDeck.draw()).isNotEqualTo(Card.of(Rank.ACE, Suit.SPADE))
         }
     }
 
     @Test
-    fun `덱의 사이즈는 52여야 합니다`(){
+    fun `덱의 사이즈가 52개가 아니면 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
-           Deck((0..30).toList())
+            Deck(Card.getAllCard().subList(0, 10))
         }
     }
 
     @Test
-    fun `카드는 중복될 수 없습니다`(){
+    fun `카드는 중복될 수 없습니다`() {
         assertThrows<IllegalArgumentException> {
-            val list = (0..51).toMutableList()
-            list[0] = 1
-            Deck(list)
-        }
-    }
-
-    @Test
-    fun `카드 번호는 0부터 51까지 입니다`(){
-        assertThrows<IllegalArgumentException> {
-            val list = (0..51).toMutableList()
-            list[0] = 100
+            val list = Card.getAllCard().toMutableList()
+            list[0] = Card.of(Rank.TWO, Suit.SPADE)
             Deck(list)
         }
     }
