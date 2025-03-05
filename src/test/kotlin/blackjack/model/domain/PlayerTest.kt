c463cabc8a0c0f955a1b9f148775e73139590642
hwannow <vnfdlvgid061@naver.com>
2025-03-05 20:01:48 +0900

    test: 플레이어의 받은 카드의 목록을 반환하는 테스트 추가

--- src/test/kotlin/blackjack/model/domain/PlayerTest.kt
+++ src/test/kotlin/blackjack/model/domain/PlayerTest.kt
@@ -11,4 +11,12 @@ class PlayerTest {
         player1.receiveCard(Card(Shape.Spade, CardNumber.Six))
         assertThat(player1.sumCardNumber).isEqualTo(7)
     }
+
+    @Test
+    fun `받은 카드의 목록을 반환한다`() {
+        val player1 = Player("제리")
+        player1.receiveCard(Card(Shape.Heart, CardNumber.Ace))
+        player1.receiveCard(Card(Shape.Spade, CardNumber.Six))
+        assertThat(player1.cardDeck).isEqualTo(listOf(Card(Shape.Heart, CardNumber.Ace), Card(Shape.Spade, CardNumber.Six)))
+    }
 }
