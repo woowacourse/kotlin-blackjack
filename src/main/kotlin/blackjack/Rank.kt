b04d9751038e33e0267ce614029e47ea747b43f5
oungsi2000 <oungsi1000@gmail.com>
2025-03-05 13:37:11 +0900

    feat: Rank에 점수 프로퍼티 추가

--- src/main/kotlin/blackjack/Rank.kt
+++ src/main/kotlin/blackjack/Rank.kt
@@ -1,17 +1,17 @@
 package blackjack
 
-enum class Rank {
-    ACE,
-    TWO,
-    THREE,
-    FOUR,
-    FIVE,
-    SIX,
-    SEVEN,
-    EIGHT,
-    NINE,
-    TEN,
-    JACK,
-    QUEEN,
-    KING,
+enum class Rank(val score: Int) {
+    ACE(11),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
+    TEN(10),
+    JACK(10),
+    QUEEN(10),
+    KING(10),
 }
