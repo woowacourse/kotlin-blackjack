14c3108be1d1ab29ab5fc5e7ec100ac0c811c96d
junseo511 <prime54@g.skku.edu>
2025-03-05 22:53:32 +0900

    refactor: 하드코딩된 숫자 상수화

--- src/main/kotlin/blackjack/model/CardDeck.kt
+++ src/main/kotlin/blackjack/model/CardDeck.kt
@@ -5,12 +5,13 @@ import java.util.LinkedList
 class CardDeck {
     private val cards = LinkedList(CACHE_CARDS.shuffled())
 
-    fun draw(count: Int = 1): List<Card> =
+    fun draw(count: Int = DEFAULT_DRAW_COUNT): List<Card> =
         List(count) {
             cards.poll() ?: throw IllegalArgumentException("[ERROR] 더 이상 카드를 뽑을 수 없습니다.")
         }
 
     companion object {
+        private const val DEFAULT_DRAW_COUNT = 1
         private val CACHE_CARDS =
             CardRank.entries.flatMap { cardRank ->
                 CardSuit.entries.map { cardSuit ->
