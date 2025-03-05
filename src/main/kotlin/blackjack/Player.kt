928c1255ec22d20cc8a09b48e6abca5b34890dbb
HamBeomJoon <hbj0802@naver.com>
2025-03-05 11:30:44 +0900

    feat: 플레이어 카드 받기 함수 추가

--- src/main/kotlin/blackjack/Player.kt
+++ src/main/kotlin/blackjack/Player.kt
@@ -5,4 +5,6 @@ class Player(
 ) {
     private val _cards: MutableList<Card> = mutableListOf()
     val cards get() = _cards.toList()
+
+    fun addCard(card: Card) = _cards.add(card)
 }
--- src/test/kotlin/blackjack/PlayerTest.kt
+++ src/test/kotlin/blackjack/PlayerTest.kt
@@ -1,12 +1,25 @@
 package blackjack
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class PlayerTest {
+    private lateinit var player: Player
+
+    @BeforeEach
+    fun setUp() {
+        player = Player("미플")
+    }
+
     @Test
     fun `플레이어는 이름을 가진다`() {
-        val player = Player("a")
-        assertThat(player.name).isEqualTo("a")
+        assertThat(player.name).isEqualTo("미플")
+    }
+
+    @Test
+    fun `플레이어는 카드 한 장을 받을 수 있다`() {
+        player.addCard(Card(Shape.SPADE, Number.NINE))
+        assertThat(player.cards.size).isEqualTo(1)
     }
 }
