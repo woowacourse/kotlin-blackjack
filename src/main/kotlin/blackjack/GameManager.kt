b9db3206949815fd81d8c0fc9e203199677ef4d8
HamBeomJoon <hbj0802@naver.com>
2025-03-05 13:10:17 +0900

    feat: 플레이어, 딜러 초기 세팅 함수 추가

--- src/main/kotlin/blackjack/GameManager.kt
+++ src/main/kotlin/blackjack/GameManager.kt
@@ -1,8 +1,14 @@
 package blackjack
 
 class GameManager(
-    private val players: List<Player>
+    private val players: List<Player>,
 ) {
+    fun setting(cnt: Int = 2) {
+        repeat(cnt) {
+            players.forEach { player -> handOutCard(player) }
+        }
+    }
+
     fun handOutCard(player: Player) {
         player.addCard(Deck.poll())
     }
--- src/test/kotlin/blackjack/GameManagerTest.kt
+++ src/test/kotlin/blackjack/GameManagerTest.kt
@@ -6,12 +6,29 @@ import org.junit.jupiter.api.Test
 class GameManagerTest {
     @Test
     fun `플레이어에게 카드를 1장 나눠준다`() {
+        // given
         val player1 = Player("a")
-        val player2 = Player("딜러")
-        val gameManager = GameManager(listOf(player1, player2))
+        val gameManager = GameManager(listOf(player1))
 
+        // when
         gameManager.handOutCard(player1)
 
+        // then
         assertThat(player1.cards.size).isEqualTo(1)
+    }
+
+    @Test
+    fun `초기 세팅은 플레이어와 딜러에게 카드 2장씩 나눠준다 `() {
+        // given
+        val player1 = Player("a")
+        val dealer = Player("딜러")
+        val gameManager = GameManager(listOf(player1, dealer))
+
+        // when
+        gameManager.setting()
+
+        // then
+        assertThat(player1.cards.size).isEqualTo(2)
+        assertThat(dealer.cards.size).isEqualTo(2)
     }
 }
