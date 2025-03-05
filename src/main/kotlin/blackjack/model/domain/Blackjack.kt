e60386bf3f0b69e9797865569ba9345c0b692062
hwannow <vnfdlvgid061@naver.com>
2025-03-05 20:50:26 +0900

    feat: 딜러의 카드 숫자 합이 16보다 작으면 카드를 계속 받는 기능 추가

--- src/main/kotlin/blackjack/model/domain/Blackjack.kt
+++ src/main/kotlin/blackjack/model/domain/Blackjack.kt
@@ -3,5 +3,6 @@ package blackjack.model.domain
 class Blackjack {
     companion object {
         const val BUST_STANDARD: Int = 21
+        const val THRESHOLD: Int = 16
     }
 }
--- src/main/kotlin/blackjack/model/domain/Dealer.kt
+++ src/main/kotlin/blackjack/model/domain/Dealer.kt
@@ -1,5 +1,13 @@
 package blackjack.model.domain
 
+import blackjack.model.domain.Blackjack.Companion.THRESHOLD
+
 class Dealer(val name: String = "딜러") : Participants {
     override val cards: MutableList<Card> = mutableListOf()
+
+    fun drawUntilThreshold(cards: Deck) {
+        while (sumCardNumber <= THRESHOLD) {
+            receiveCard(cards.spreadCard())
+        }
+    }
 }
