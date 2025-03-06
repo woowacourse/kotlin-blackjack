c51bb9cecf97682194f903a7e0388d59f872a4db
moondev03 <moondev03@gmail.com>
2025-03-06 13:49:43 +0900

    refactor: PlayerState BLACK_JACE 상태 제거

--- src/main/kotlin/blackjack/domain/state/PlayerState.kt
+++ src/main/kotlin/blackjack/domain/state/PlayerState.kt
@@ -6,7 +6,6 @@ import blackjack.domain.person.Player
 enum class PlayerState(override val isFinal: Boolean) : PersonState {
     FIRST_TURN(false),
     HIT(false),
-    BLACKJACK(false),
     BUST(true),
     STAY(true),
     ;
@@ -15,7 +14,6 @@ enum class PlayerState(override val isFinal: Boolean) : PersonState {
         fun from(player: Player): PlayerState =
             when {
                 player.cards().size < GameRule.FIRST_TURN_DRAW_AMOUNT -> FIRST_TURN
-                player.score() == GameRule.BLACKJACK_SCORE -> BLACKJACK
                 player.score() > GameRule.BLACKJACK_SCORE -> BUST
                 else -> HIT
             }
