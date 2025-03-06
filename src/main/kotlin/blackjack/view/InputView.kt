1a884bd448e337d829f4e78639e803f7322ee20e
hwannow <vnfdlvgid061@naver.com>
2025-03-06 13:37:31 +0900

    feat: InputView에 해당하는 기능 추가

---
+++ src/main/kotlin/blackjack/view/InputView.kt
@@ -0,0 +1,37 @@
+package blackjack.view
+
+import blackjack.model.domain.Player
+
+class InputView {
+    fun askForPlayersName(): List<String> {
+        println(INPUT_PLAYERS_NAME)
+        return validateBlankList(
+            readln()
+                .split(DELIMITER)
+                .map { it.trim() },
+        ) ?: askForPlayersName()
+    }
+
+    fun askForHitOrStay(player: Player): String {
+        println(INPUT_HIT_OR_STAY.format(player.name))
+        return validateBlank(readln()) ?: askForHitOrStay(player)
+    }
+
+    companion object {
+        private const val INPUT_PLAYERS_NAME = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)"
+        private const val DELIMITER = ","
+        private const val INPUT_HIT_OR_STAY = "%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)"
+
+        private fun validateBlank(input: String): String? {
+            if (input.isEmpty()) return null
+            return input
+        }
+
+        private fun validateBlankList(input: List<String>): List<String>? {
+            input.forEach {
+                validateBlank(it) ?: return null
+            }
+            return input
+        }
+    }
+}
