42ae4d35c8847212e347025eaa0b3b630cf4d889
medAndro <med4ndro@gmail.com>
2025-03-06 21:37:24 +0900

    feat: OutputView의 손패의 카드 정보를 출력하는 메서드 구현

--- src/main/kotlin/blackjack/view/OutputView.kt
+++ src/main/kotlin/blackjack/view/OutputView.kt
@@ -1,5 +1,6 @@
 package blackjack.view
 
+import blackjack.domain.model.GameParticipant
 import blackjack.domain.model.Player
 
 class OutputView {
@@ -8,7 +9,14 @@ class OutputView {
         println(DISTRIBUTE_CARD_MESSAGE.format(joinedNames))
     }
 
+    fun showCardsInfo(gameParticipant: GameParticipant) {
+        val name = gameParticipant.name
+        val cardsInfoText = gameParticipant.showCards().joinToString { it.getCardText() }
+        println(CARD_INFO_MESSAGE.format(name, cardsInfoText))
+    }
+
     companion object {
         private const val DISTRIBUTE_CARD_MESSAGE = "딜러와 %s에게 각각 2장의 카드를 나누었습니다."
+        private const val CARD_INFO_MESSAGE = "%s카드: %s"
     }
 }
