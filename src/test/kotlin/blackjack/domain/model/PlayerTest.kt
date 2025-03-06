07f861cf6b5eecc060dbaf7825d2ee5e8346941a
ijh1298 <imjh0417@gmail.com>
2025-03-06 20:04:49 +0900

    test: 플레이어가 카드를 뽑는 테스트 코드 구현

--- src/test/kotlin/blackjack/domain/model/PlayerTest.kt
+++ src/test/kotlin/blackjack/domain/model/PlayerTest.kt
@@ -10,4 +10,13 @@ class PlayerTest {
 
         assertThat(player.showCards()).hasSize(2)
     }
+
+    @Test
+    fun `플레이어는 덱에서 카드를 뽑을 수 있다`() {
+        val player = Player()
+
+        player.drawCard()
+
+        assertThat(player.showCards()).hasSize(3)
+    }
 }
