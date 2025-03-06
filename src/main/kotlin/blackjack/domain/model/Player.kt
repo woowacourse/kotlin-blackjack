dcca8f90ee12e4b7e79d62d9b73ac8a65a3d4cd3
ijh1298 <imjh0417@gmail.com>
2025-03-06 19:31:03 +0900

    refactor: Deck에서 카드를 주는 메서드명 변경

--- src/main/kotlin/blackjack/domain/model/Player.kt
+++ src/main/kotlin/blackjack/domain/model/Player.kt
@@ -6,6 +6,6 @@ class Player {
     fun showCards(): List<Card> = cards.toList()
 
     init {
-        repeat(2) { cards += Deck.drawCard() }
+        repeat(2) { cards += Deck.giveCard() }
     }
 }
