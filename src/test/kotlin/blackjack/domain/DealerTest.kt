6237de5d9711ae65a0c8fbae118dc7b3e266e485
moondev03 <moondev03@gmail.com>
2025-03-06 13:51:08 +0900

    test: PlayerTest & DealerTest 추가

--- src/test/kotlin/blackjack/domain/DealerTest.kt
+++ src/test/kotlin/blackjack/domain/DealerTest.kt
@@ -2,21 +2,62 @@ package blackjack.domain
 
 import blackjack.domain.card.Deck
 import blackjack.domain.person.Dealer
+import blackjack.domain.state.DealerState
 import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class DealerTest {
-    @Test
-    fun `이름과 패를 가지고 있다`() {
-        val dealer = Dealer("pobi")
-        dealer.name shouldBe "pobi"
+    private lateinit var deck: Deck
+    private lateinit var dealer: Dealer
+
+    @BeforeEach
+    fun setup() {
+        deck = Deck()
+        dealer = Dealer()
     }
 
     @Test
-    fun `요청한 수량에 맞게 카드를 덱에서 뽑아 패에 추가할수 있다`() {
-        val deck = Deck()
-        val dealer = Dealer("pobi")
-        dealer.draw(deck, 2)
-        dealer.hand.cards.size shouldBe 2
+    fun `딜러의 초기 상태는 FIRST_TURN이어야 한다`() {
+        dealer.gameState shouldBe DealerState.FIRST_TURN
+    }
+
+    @Test
+    fun `상태가 FIRST_TURN이면 2장을 드로우한다`() {
+        dealer.gameState shouldBe DealerState.FIRST_TURN
+        dealer.draw(deck)
+
+        dealer.cards().size shouldBe 2
+    }
+
+    @Test
+    fun `상태가 HIT이면 1장을 드로우한다`() {
+        setupPlayerWithState(DealerState.HIT)
+        dealer.draw(deck)
+
+        dealer.cards().size shouldBe 3
+    }
+
+    @Test
+    fun `상태가 FIRST_TURN이라면 true를 반환한다`() {
+        dealer.canDraw() shouldBe true
+    }
+
+    @Test
+    fun `상태가 FINISH라면 false를 반환한다`() {
+        setupPlayerWithState(DealerState.FINISH)
+        dealer.canDraw() shouldBe false
+    }
+
+    @Test
+    fun `상태가 HIT이라면 true를 반환한다`() {
+        setupPlayerWithState(DealerState.HIT)
+        dealer.canDraw() shouldBe true
+    }
+
+    private fun setupPlayerWithState(state: DealerState) {
+        while (dealer.gameState != state) {
+            dealer.draw(deck)
+        }
     }
 }
--- src/test/kotlin/blackjack/domain/PlayerTest.kt
+++ src/test/kotlin/blackjack/domain/PlayerTest.kt
@@ -2,22 +2,87 @@ package blackjack.domain
 
 import blackjack.domain.card.Deck
 import blackjack.domain.person.Player
+import blackjack.domain.state.PlayerState
 import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class PlayerTest {
-    @Test
-    fun `player는 이름, 상태를 가지고 있다`() {
-        val player = Player("pobi")
-        player.name shouldBe "pobi"
-        player.gameState shouldBe GameState.HIT
+    private lateinit var deck: Deck
+    private lateinit var player: Player
+
+    @BeforeEach
+    fun setup() {
+        deck = Deck()
+        player = Player("pobi")
     }
 
     @Test
-    fun `요청한 수량에 맞게 카드를 덱에서 뽑아 패에 추가할수 있다`() {
-        val deck = Deck()
-        val player = Player("pobi")
-        player.draw(deck, 2)
-        player.hand.cards.size shouldBe 2
+    fun `player는 이름을 가지고 있어야 한다`() {
+        player.name shouldBe "pobi"
+    }
+
+    @Test
+    fun `player의 초기 상태는 FIRST_TRUN이어야 한다`() {
+        player.gameState shouldBe PlayerState.FIRST_TURN
+    }
+
+    @Test
+    fun `isHit이 true면 카드를 덱에서 뽑아 패에 추가한다`() {
+        player.draw(deck, true)
+        player.cards().size shouldBe 2
+    }
+
+    @Test
+    fun `isHit이 false면 카드를 뽑지 않고 상태를 STAY로 변경한다`() {
+        player.draw(deck, false)
+
+        player.cards().size shouldBe 0
+        player.gameState shouldBe PlayerState.STAY
+    }
+
+    @Test
+    fun `상태가 FIRST_TURN이면 2장을 드로우한다`() {
+        player.gameState shouldBe PlayerState.FIRST_TURN
+        player.draw(deck, true)
+
+        player.cards().size shouldBe 2
+    }
+
+    @Test
+    fun `상태가 HIT이면 1장을 드로우한다`() {
+        setupPlayerWithState(PlayerState.HIT)
+        player.draw(deck, true)
+
+        player.cards().size shouldBe 3
+    }
+
+    @Test
+    fun `상태가 STAY라면 false를 반환한다`() {
+        player.draw(deck, false)
+        player.canDraw() shouldBe false
+    }
+
+    @Test
+    fun `상태가 BUST라면 false를 반환한다`() {
+        setupPlayerWithState(PlayerState.BUST)
+        player.canDraw() shouldBe false
+    }
+
+    @Test
+    fun `상태가 FIRST_TRUN이라면 true를 반환한다`() {
+        player.canDraw() shouldBe true
+    }
+
+    @Test
+    fun `상태가 HIT이라면 true를 반환한다`() {
+        setupPlayerWithState(PlayerState.HIT)
+        player.canDraw() shouldBe true
+    }
+
+    private fun setupPlayerWithState(state: PlayerState) {
+        while (player.gameState != state) {
+            player.draw(deck, true)
+        }
     }
 }
