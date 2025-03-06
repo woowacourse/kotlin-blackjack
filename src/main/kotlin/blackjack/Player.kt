39b47764657579bfece26c4733539f5617e7b23a
HamBeomJoon <hbj0802@naver.com>
2025-03-06 11:34:23 +0900

    feat: A카드 점수 조정

--- src/main/kotlin/blackjack/Player.kt
+++ src/main/kotlin/blackjack/Player.kt
@@ -10,7 +10,20 @@ class Player(
 
     fun addCards(cards: List<Card>) = _cards.addAll(cards)
 
+    fun adjustScore(): Int {
+        var sumScore = sumScore()
+        var countAce = countAce()
+        while (countAce-- > 0) {
+            if (sumScore > 21) {
+                sumScore -= 10
+            }
+        }
+        return sumScore
+    }
+
     fun sumScore() = cards.sumOf { card -> card.number.score }
 
-    fun isBurst() = sumScore() > 21
+    fun isBurst() = adjustScore() > 21
+
+    fun countAce() = _cards.count { it.number == Number.ACE }
 }
