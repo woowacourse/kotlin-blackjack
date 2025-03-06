d0cbfe0f258b7c00fcfa0e5644c56b55c1493caa
ijh1298 <imjh0417@gmail.com>
2025-03-06 20:11:15 +0900

    refactor: GameParticipant의 필드 가시성 변경자 수정

--- src/main/kotlin/blackjack/domain/model/GameParticipant.kt
+++ src/main/kotlin/blackjack/domain/model/GameParticipant.kt
@@ -1,7 +1,7 @@
 package blackjack.domain.model
 
 abstract class GameParticipant(val name: String) {
-    private val cards = mutableListOf<Card>()
+    val cards = mutableListOf<Card>()
 
     init {
         repeat(2) { cards += Deck.giveCard() }
