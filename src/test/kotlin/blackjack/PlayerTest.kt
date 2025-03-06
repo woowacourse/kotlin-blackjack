44b7c980a3cbd786955ad8a30a83bf47a1455a28
medAndro <med4ndro@gmail.com>
2025-03-06 11:13:48 +0900

    test: 플레이어가 카드를 2장 받는 테스트 코드 구현

---
+++ src/test/kotlin/blackjack/PlayerTest.kt
@@ -0,0 +1,13 @@
+package blackjack
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class PlayerTest {
+    @Test
+    fun `플레이어는 처음에 카드를 2장 받는다`() {
+        val player = Player()
+
+        assertThat(player.cards).hasSize(2)
+    }
+}
