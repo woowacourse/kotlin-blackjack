4997796e808458fd58788a59dd1422ca4e6ff442
HamBeomJoon <hbj0802@naver.com>
2025-03-06 11:08:56 +0900

    feat: 플레이어 카드 더 받을지 메세지 출력

--- src/main/kotlin/blackjack/view/InputView.kt
+++ src/main/kotlin/blackjack/view/InputView.kt
@@ -11,8 +11,16 @@ class InputView {
         return players
     }
 
+    fun readMoreCardCondition(player: Player): String {
+        println(PLAYER_MORE_CARD_MESSAGE_GUIDE.format(player.name))
+        val condition: String = readln().trim()
+        if (condition != "y" && condition != "n") return readMoreCardCondition(player)
+        return condition
+    }
+
     companion object {
         private const val PLAYER_NAME_MESSAGE_GUIDE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)"
+        private const val PLAYER_MORE_CARD_MESSAGE_GUIDE = "%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)"
         private const val PLAYER_NAME_DELIMITER = ","
     }
 }
