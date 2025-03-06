f46e5b718ae86147a1390d5b58d92c817adb85aa
ijh1298 <imjh0417@gmail.com>
2025-03-06 11:01:57 +0900

    refactor: 테스트 명과 기능 요구사항 수정

--- src/test/kotlin/blackjack/DeckTest.kt
+++ src/test/kotlin/blackjack/DeckTest.kt
@@ -1,10 +1,11 @@
 package blackjack
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class DeckTest {
     @Test
-    fun `무작위 카드를 뽑을 수 있다`() {
+    fun `카드를 뽑을 수 있다`() {
         val deck = Deck()
 
         val card = deck.drawCard()
