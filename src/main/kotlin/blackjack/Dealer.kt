abf3e768863827f81cd2d292a158209ed85239bf
HamBeomJoon <hbj0802@naver.com>
2025-03-06 11:42:01 +0900

    feat: 딜러 카드 합 계산, 더 받을지 여부 계산 함수 추가

--- src/main/kotlin/blackjack/Dealer.kt
+++ src/main/kotlin/blackjack/Dealer.kt
@@ -5,7 +5,13 @@ class Dealer {
     val cards get() = _cards.toList()
     val name = DEALER_NAME
 
+    fun addCard(card: Card) = _cards.add(card)
+
     fun addCards(cards: List<Card>) = _cards.addAll(cards)
+
+    fun sumScore() = cards.sumOf { card -> card.number.score }
+
+    fun isMoreCard() = sumScore() < 17
 
     companion object {
         private const val DEALER_NAME = "딜러"
