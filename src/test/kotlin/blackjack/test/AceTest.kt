4df1a92ec0e32df0603e30357b8085c02458a34f
eden <devfeijoa@gmail.com>
2025-03-06 16:12:46 +0900

    feat: implement Ace는 1 또는 11로 계산된다.

---
+++ src/test/kotlin/blackjack/test/AceTest.kt
@@ -0,0 +1,21 @@
+package blackjack.test
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class Ace {
+    val possibleValues: List<Int> = ACE_VALUES
+
+    companion object {
+        val ACE_VALUES = listOf(1, 11)
+    }
+}
+
+class AceTest {
+    @Test
+    fun `Ace는 1 또는 11로 계산된다`() {
+        val ace = Ace()
+        assertThat(ace.possibleValues).contains(1)
+        assertThat(ace.possibleValues).contains(11)
+    }
+}
