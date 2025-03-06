bc056661677c75b71e2dec15659e187068fa399d
eden <devfeijoa@gmail.com>
2025-03-06 16:04:40 +0900

    feat: implement "숫자는 2부터 10까지 존재하며, 그 숫자로 계산된다."

---
+++ src/test/kotlin/blackjack/test/NumberTest.kt
@@ -0,0 +1,33 @@
+package blackjack.test
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class Number(
+    val value: Int,
+) {
+    init {
+        require(value in RANGE)
+    }
+
+    companion object {
+        private const val MIN = 2
+        private const val MAX = 10
+        val RANGE = MIN..MAX
+    }
+}
+
+class NumberTest {
+    @Test
+    fun `숫자는 2부터 10까지 존재한다`() {
+        assertThrows<IllegalArgumentException> { Number(1) }
+        assertThrows<IllegalArgumentException> { Number(11) }
+    }
+
+    @Test
+    fun `숫자는 해당 숫자로 계산한다`() {
+        val number = Number(2)
+        assertThat(number.value).isEqualTo(2)
+    }
+}
