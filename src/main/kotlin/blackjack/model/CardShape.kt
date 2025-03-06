a90b02a01ec17912320daa43798060090f55258a
Hwnag ChaeWon <wondroid.world@gmail.com>
2025-03-06 15:11:57 +0900

    refactor: ktlint 수정

--- src/main/kotlin/blackjack/model/CardShape.kt
+++ src/main/kotlin/blackjack/model/CardShape.kt
@@ -1,6 +1,8 @@
 package blackjack.model
 
-enum class CardShape(val koreanName: String) {
+enum class CardShape(
+    val koreanName: String,
+) {
     DIAMOND("다이아몬드"),
     SPADE("스페이드"),
     HEART("하트"),
--- src/main/kotlin/blackjack/model/Dealer.kt
+++ src/main/kotlin/blackjack/model/Dealer.kt
@@ -1,8 +1,9 @@
 package blackjack.model
 
 class Dealer(
+    override val name: String = "딜러",
     cards: List<Card> = emptyList(),
-) : Player("딜러") {
+) : Player(name) {
     private val _cards: MutableList<Card> = cards.toMutableList()
     override val cards: List<Card> get() = _cards.map { it.copy() }
 
