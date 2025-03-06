ec97c613fac2172acdb9b17fbdc1f126ec78fa6b
HamBeomJoon <hbj0802@naver.com>
2025-03-06 16:12:00 +0900

    refactor: GameManagerTest 메서드 명 변경

--- src/test/kotlin/blackjack/GameManagerTest.kt
+++ src/test/kotlin/blackjack/GameManagerTest.kt
@@ -1,31 +1,22 @@
 package blackjack
 
+import blackjack.model.Dealer
+import blackjack.model.GameManager
+import blackjack.model.Player
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class GameManagerTest {
     @Test
-    fun `플레이어에게 카드를 1장 나눠준다`() {
+    fun `초기 세팅은 플레이어와 딜러에게 카드 2장씩 나눠준다`() {
         // given
         val player1 = Player("a")
-        val gameManager = GameManager(listOf(player1))
+        val player2 = Player("b")
+        val dealer = Dealer()
+        val gameManager = GameManager(dealer, listOf(player1, player2))
 
         // when
-        gameManager.handOutCard(player1)
-
-        // then
-        assertThat(player1.cards.size).isEqualTo(1)
-    }
-
-    @Test
-    fun `초기 세팅은 플레이어와 딜러에게 카드 2장씩 나눠준다 `() {
-        // given
-        val player1 = Player("a")
-        val dealer = Player("딜러")
-        val gameManager = GameManager(listOf(player1, dealer))
-
-        // when
-        gameManager.setting()
+        gameManager.dealInitialCardWithCount(2)
 
         // then
         assertThat(player1.cards.size).isEqualTo(2)
