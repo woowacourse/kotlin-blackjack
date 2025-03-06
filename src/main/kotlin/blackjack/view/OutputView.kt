812ff93993d6f14c0b28cef18fd1ace1204cc34a
hwannow <vnfdlvgid061@naver.com>
2025-03-06 16:42:48 +0900

    feat: 최종 게임 참여자들의 카드 현황 및 카드 숫자의 합 출력하는 기능 추가

--- src/main/kotlin/blackjack/view/OutputView.kt
+++ src/main/kotlin/blackjack/view/OutputView.kt
@@ -8,10 +8,15 @@ class OutputView {
         dealer: Participants,
         players: List<Participants>,
     ) {
-        val playerName = players.joinToString(", ") { it.name }
+        val playerName = players.joinToString(SEPARATOR) { it.name }
         println(OUTPUT_DISTRIBUTE_CARD.format(dealer.name, playerName))
         printDealerInitCard(dealer)
         printPlayerInitCard(players)
+    }
+
+    private fun printDealerInitCard(player: Participants) {
+        val firstCard = listOf(player.cardDeck.first())
+        println(PLAYER_STATUS.format(player.name, displayCard(firstCard)))
     }
 
     private fun printPlayerInitCard(players: List<Participants>) {
@@ -21,16 +26,15 @@ class OutputView {
     }
 
     fun printCardStatus(player: Participants) {
-        println(PLAYER_STATUS.format(player.name + CARD, displayCard(player.cardDeck)))
-    }
-
-    private fun printDealerInitCard(player: Participants) {
-        val firstCard = listOf(player.cardDeck.first())
-        println(PLAYER_STATUS.format(player.name, displayCard(firstCard)))
+        println(makeFormat(player))
     }
 
     private fun displayCard(cards: List<Card>): String {
         return cards.joinToString(SEPARATOR) { CARD_FORMAT.format(it.cardNumber.display, it.symbol.symbol) }
+    }
+
+    private fun makeFormat(player: Participants): String {
+        return PLAYER_STATUS.format(player.name + CARD, displayCard(player.cardDeck))
     }
 
     fun printDealerReceiveCard(count: Int) {
@@ -39,9 +43,16 @@ class OutputView {
         }
     }
 
+    fun participantsCardResult(participants: List<Participants>) {
+        participants.forEach { participant ->
+            println(makeFormat(participant) + OUTPUT_PARTICIPANTS_CARD_RESULT.format(participant.sumCardNumber))
+        }
+    }
+
     companion object {
         private const val OUTPUT_DISTRIBUTE_CARD: String = "%s와 %s에게 2장의 나누었습니다."
         private const val OUTPUT_DEALER_RECEIVE_CARD: String = "딜러는 16이하라 한장의 카드를 더 받았습니다."
+        private const val OUTPUT_PARTICIPANTS_CARD_RESULT = " - 결과: %d"
         private const val PLAYER_STATUS: String = "%s: %s"
         private const val CARD: String = "카드"
         private const val CARD_FORMAT: String = "%s%s"
