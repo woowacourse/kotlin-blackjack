7a217dd1e7e9b2a08982137f299adee11e4d825c
hwannow <vnfdlvgid061@naver.com>
2025-03-06 11:21:11 +0900

    test: 게임이 끝난 후 승패를 가리는 테스트 추가

--- src/test/kotlin/blackjack/model/service/BlackjackTest.kt
+++ src/test/kotlin/blackjack/model/service/BlackjackTest.kt
@@ -1,8 +1,11 @@
 package blackjack.model.service
 
+import blackjack.model.domain.Card
+import blackjack.model.domain.CardNumber
 import blackjack.model.domain.Dealer
 import blackjack.model.domain.Deck
 import blackjack.model.domain.Player
+import blackjack.model.domain.Shape
 import blackjack.model.strategy.FalseShuffle
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
@@ -21,5 +24,17 @@ class BlackjackTest {
         assertThat(dealer.cardDeck.size).isEqualTo(2)
         assertThat(player1.cardDeck.size).isEqualTo(2)
         assertThat(player2.cardDeck.size).isEqualTo(2)
+    }
+
+    @Test
+    fun `게임이 끝난 후 승패를 가린다`() {
+        player1.receiveCard(Card(Shape.Spade, CardNumber.Ace))
+        player2.receiveCard(Card(Shape.Spade, CardNumber.Six))
+        dealer.receiveCard(Card(Shape.Spade, CardNumber.Seven))
+
+        game.endGame(listOf(player1, player2), dealer)
+
+        assertThat(player1.alive).isTrue()
+        assertThat(player2.alive).isFalse()
     }
 }
