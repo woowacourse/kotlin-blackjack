03c03bb582ba6f3118b19cbc77c3e99057555d2e
DoableTuple <doabletuple@gmail.com>
2025-03-06 10:54:59 +0900

    feat(View): 참여자가 보유한 카드의 목록을 출력한다.

--- src/main/kotlin/blackjack/view/OutputView.kt
+++ src/main/kotlin/blackjack/view/OutputView.kt
@@ -1,5 +1,6 @@
 package blackjack.view
 
+import blackjack.domain.model.Card
 import blackjack.domain.model.Dealer
 import blackjack.domain.model.Player
 
@@ -22,11 +23,25 @@ class OutputView {
         )
     }
 
+    fun printPlayerStatus(player: Player) {
+        println(buildPlayerStatus(player))
+    }
+
+    private fun buildPlayerStatus(player: Player): String {
+        var status = ""
+        status += player.name + PLAYER_NAME_STATUS_SEPARATOR
+        status += player.cards.joinToString(PLAYER_CARDS_DELIMITER) { card: Card ->
+            card.rank.value + card.suit.value
+        }
+        return status
+    }
+
     companion object {
         private const val MESSAGE_ENTER_PLAYER_NAMES = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)"
         private const val MESSAGE_ENTER_PLAYER_YES_OR_NO = "%s은(는) 한 장의 카드를 더 받겠습니까? (예는 y, 아니오는 n)"
         private const val MESSAGE_INITIAL_HAND_DISTRIBUTED = "%s와(과) %s에게 %s장의 카드를 나누었습니다."
 
         private const val PLAYER_CARDS_DELIMITER = ", "
+        private const val PLAYER_NAME_STATUS_SEPARATOR = " 카드: "
     }
 }
