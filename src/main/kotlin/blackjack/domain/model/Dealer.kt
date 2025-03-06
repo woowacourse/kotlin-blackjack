e12691ea0babc721ef7050dc419d621e5cef5b33
DoableTuple <doabletuple@gmail.com>
2025-03-06 11:31:01 +0900

    refactor: Dealer가 Player를 상속하도록 변경

--- src/main/kotlin/blackjack/domain/model/Dealer.kt
+++ src/main/kotlin/blackjack/domain/model/Dealer.kt
@@ -1,22 +1,6 @@
 package blackjack.domain.model
 
-data class Dealer(val name: String = DEALER_NAME, val cards: List<Card> = emptyList()) {
-    fun accept(card: Card): Dealer {
-        return this.copy(cards = cards + card)
-    }
-
-    fun getScore(): Int {
-        val score = cards.sumOf { it.rank.score }
-        return score + getBonusScore(totalScore = score)
-    }
-
-    private fun getBonusScore(totalScore: Int): Int {
-        if (totalScore <= MAX_BONUS_SCORE && hasAce()) return BONUS_SCORE
-        return 0
-    }
-
-    private fun hasAce() = cards.any { it.rank == Rank.ACE }
-
+class Dealer(): Player(DEALER_NAME, mutableListOf()) {
     fun getPlayerVerdict(players: List<Player>): Map<Player, Verdict> {
         val dealerScore = getScore()
         return players.associateWith { player -> Verdict.determine(player.getScore(), dealerScore) }
@@ -29,7 +13,5 @@ data class Dealer(val name: String = DEALER_NAME, val cards: List<Card> = emptyL
 
     private companion object {
         const val DEALER_NAME = "딜러"
-        const val MAX_BONUS_SCORE = 11
-        const val BONUS_SCORE = 10
     }
 }
--- src/main/kotlin/blackjack/domain/model/Player.kt
+++ src/main/kotlin/blackjack/domain/model/Player.kt
@@ -1,8 +1,8 @@
 package blackjack.domain.model
 
-data class Player(val name: String, val cards: List<Card> = listOf()) {
-    fun accept(card: Card): Player {
-        return this.copy(cards = cards + card)
+open class Player(val name: String, val cards: MutableList<Card> = mutableListOf()) {
+    fun accept(cards: List<Card>) {
+        this.cards.addAll(cards)
     }
 
     fun getScore(): Int {
