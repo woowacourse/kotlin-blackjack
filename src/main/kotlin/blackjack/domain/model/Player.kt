2e568a2fce583879a43cdd7f0047a1c6bb9c8432
ijh1298 <imjh0417@gmail.com>
2025-03-06 20:13:04 +0900

    feat: Player가 GameParticipant를 받도록 구현

--- src/main/kotlin/blackjack/domain/model/Player.kt
+++ src/main/kotlin/blackjack/domain/model/Player.kt
@@ -1,11 +1,11 @@
 package blackjack.domain.model
 
-class Player {
-    private val cards = mutableListOf<Card>()
+class Player(name: String = DEFAULT_NAME) : GameParticipant(name = name) {
+    override fun drawCard() {
+        cards += Deck.giveCard()
+    }
 
-    fun showCards(): List<Card> = cards.toList()
-
-    init {
-        repeat(2) { cards += Deck.giveCard() }
+    companion object {
+        private const val DEFAULT_NAME = "이름 없음"
     }
 }
