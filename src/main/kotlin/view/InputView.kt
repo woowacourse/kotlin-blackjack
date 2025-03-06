d30cb7da72a6e2e42d2680af2e8f8b8df3e6a9a0
gahyunkim <mapledt001@naver.com>
2025-03-06 17:54:08 +0900

    refactor: 출력 방식, 반환 값 변경

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -6,7 +6,13 @@ class InputView {
         return readlnOrNull()?.split(",")?.map { it.trim() } ?: emptyList()
     }
 
-    fun readHitOrStand(): String {
-        return readlnOrNull()?.trim()?.lowercase() ?: ""
+    fun readHitOrStand(playerName: String): Boolean {
+        println("${playerName}는 한 장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)")
+        val answer = readln().lowercase()
+        return when (answer) {
+            "y" -> true
+            "n" -> false
+            else -> throw IllegalArgumentException("[ERROR] y나 n만 입력해주세요.")
+        }
     }
 }
