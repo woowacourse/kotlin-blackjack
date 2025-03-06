bb1979ecbb17b3bf1c437b19b63f6e10ace8709a
m6z1 <sonmyungj1zz@gmail.com>
2025-03-06 16:46:53 +0900

    feat: 카드 상태 반환 로직 구현

--- src/main/kotlin/blackjack/model/CardsStatus.kt
+++ src/main/kotlin/blackjack/model/CardsStatus.kt
@@ -1,7 +1,21 @@
 package blackjack.model
 
-enum class CardsStatus {
-    BLACKJACK,
-    BUST,
-    NONE,
+enum class CardsStatus(
+    val firstTurn: Boolean,
+) {
+    BLACKJACK(true),
+    BUST(false),
+    NONE(false),
+    ;
+
+    companion object {
+        fun from(
+            cardsScore: Int,
+            firstTurn: Boolean,
+        ): CardsStatus {
+            if (firstTurn && cardsScore == 21) return BLACKJACK
+            if (cardsScore > 21) return BUST
+            return NONE
+        }
+    }
 }
