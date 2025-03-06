b8411d64bae4bfde29e2ca0be6b7586e21eb0bac
DoableTuple <doabletuple@gmail.com>
2025-03-06 10:53:29 +0900

    feat(View): 참여자들의 이름과 나누어진 카드의 개수를 출력한다.

--- src/main/kotlin/blackjack/view/OutputView.kt
+++ src/main/kotlin/blackjack/view/OutputView.kt
@@ -1,5 +1,8 @@
 package blackjack.view
 
+import blackjack.domain.model.Dealer
+import blackjack.domain.model.Player
+
 class OutputView {
     fun requestPlayerNames() {
         println(MESSAGE_ENTER_PLAYER_NAMES)
@@ -9,8 +12,21 @@ class OutputView {
         println(MESSAGE_ENTER_PLAYER_YES_OR_NO)
     }
 
+    fun printInitialDeals(dealer: Dealer, players: List<Player>) {
+        println(
+            MESSAGE_INITIAL_HAND_DISTRIBUTED.format(
+                dealer.name,
+                players.map(Player::name).joinToString(PLAYER_CARDS_DELIMITER),
+                2
+            )
+        )
+    }
+
     companion object {
         private const val MESSAGE_ENTER_PLAYER_NAMES = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)"
         private const val MESSAGE_ENTER_PLAYER_YES_OR_NO = "%s은(는) 한 장의 카드를 더 받겠습니까? (예는 y, 아니오는 n)"
+        private const val MESSAGE_INITIAL_HAND_DISTRIBUTED = "%s와(과) %s에게 %s장의 카드를 나누었습니다."
+
+        private const val PLAYER_CARDS_DELIMITER = ", "
     }
 }
