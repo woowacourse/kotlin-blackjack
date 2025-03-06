77ad558aedbef8739810fe9b854f6a8c5480b187
DoableTuple <doabletuple@gmail.com>
2025-03-06 11:45:08 +0900

    refactor: 카드를 뽑을 때 뽑을 개수를 지정할 수 있도록 변경

--- src/main/kotlin/blackjack/domain/model/Cards.kt
+++ src/main/kotlin/blackjack/domain/model/Cards.kt
@@ -3,8 +3,8 @@ package blackjack.domain.model
 class Cards(initCards: List<Card> = deckCards.shuffled()) {
     private val cards: MutableList<Card> = initCards.toMutableList()
 
-    fun draw(): Card {
-        return cards.removeFirst()
+    fun draw(count: Int): List<Card> {
+        return List(count.coerceAtMost(count)) { cards.removeFirst() }
     }
 
     companion object {
