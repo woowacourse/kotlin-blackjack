e1917a82755176781be7a7822992f498d4a092fe
oungsi2000 <oungsi1000@gmail.com>
2025-03-06 16:55:46 +0900

    feat: Dealer 카드 계산 로직 추가

--- src/main/kotlin/blackjack/Dealer.kt
+++ src/main/kotlin/blackjack/Dealer.kt
@@ -1,11 +1,49 @@
 package blackjack
 
-class Dealer(val totalSum: Int) : Participant {
-    fun getPlayerResult(player: Player): Result {
-        return when {
-            totalSum > player.totalSum -> Result.WIN
-            player.totalSum > totalSum -> Result.LOSE
-            else -> Result.DRAW
+class Dealer : Participant() {
+    var totalSum: Int = 0
+        private set
+    val cards: MutableList<Card> = mutableListOf()
+
+    fun getCard(deck: Deck) {
+        while (canHit()) {
+            addCard(deck.draw())
         }
+    }
+
+    private fun addCard(card: Card) {
+        cards.add(card)
+        totalSum = calculateTotalSum()
+    }
+
+    fun canHit(): Boolean {
+        return totalSum < 17
+    }
+
+    fun isBust(): Boolean {
+        return totalSum > 21
+    }
+
+    fun getPlayerResult(player: Player): GameResultStatus {
+        return when {
+            totalSum > player.totalSum -> GameResultStatus.PLAYER_WIN
+            player.totalSum > totalSum -> GameResultStatus.PLAYER_LOSE
+            else -> GameResultStatus.DRAW
+        }
+    }
+
+    private fun calculateTotalSum(): Int {
+        val rawScore =
+            cards.fold(0) { accumulatedScore, card ->
+                accumulatedScore + card.getScore()
+            }
+        var editedScore = rawScore
+
+        for (ace in cards.filter { it.rank == Rank.ACE && rawScore > 21 }) {
+            editedScore -= 10
+            if (editedScore <= 21) break
+        }
+
+        return editedScore
     }
 }
---
+++ src/test/kotlin/blackjack/DealerTest.kt
@@ -0,0 +1,22 @@
+package blackjack
+
+class DealerTest
+//    @Test
+//    fun `딜러는 지급된 카드를 가진다`() {
+//        val card = Card.of(Rank.ACE, Suit.SPADE)
+//        val dealer = Dealer()
+//        dealer.addCard(card)
+//        assertThat(dealer.cards).contains(card)
+//    }
+//
+//    @Test
+//    fun `딜러의 카드의 총합이 17이상이면, 카드를 뽑을 수 없다`() {
+//        val dealer = Dealer()
+//
+//        val card1 = Card.of(Rank.ACE, Suit.SPADE)
+//        val card2 = Card.of(Rank.NINE, Suit.SPADE)
+//
+//        dealer.getCard()
+//        dealer.addCard(card2)
+//        assertThat(de}aler.canHit()).isFalse()
+//    }
