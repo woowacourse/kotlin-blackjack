387ef3aea2af132c7b5b7aab82292814ef03b799
gahyunkim <mapledt001@naver.com>
2025-03-06 17:49:41 +0900

    refactor: 함수 분리

--- src/main/kotlin/controller/BlackjackController.kt
+++ src/main/kotlin/controller/BlackjackController.kt
@@ -5,81 +5,79 @@ import model.CardsGenerator
 import model.Dealer
 import model.GameResultDecider
 import model.Player
-import model.ScoreCalculator
 import view.InputView
 import view.OutputView
 
 class BlackjackController(
     private val inputView: InputView,
     private val outputView: OutputView,
-    private val cardsGenerator: CardsGenerator
+    private val cardsGenerator: CardsGenerator,
 ) {
     fun run() {
         val playersNames = inputView.inputPlayers()
         val allCards = cardsGenerator.generateCards()
 
-        val players = playersNames.map { name ->
-            Player(name, allCards.drawCards(2))
-        }
+        val players =
+            playersNames.map { name ->
+                Player(name, allCards.drawCards(2))
+            }
 
         val playerCardsNames = players.map { it.playerCards.getCardNames() }
         val dealerInitialCards = allCards.drawCards(2)
         val dealerCardNames = dealerInitialCards.getCardNames()
 
-        val dealer = Dealer("딜러", dealerInitialCards)
+        val dealer = Dealer(dealerInitialCards)
         outputView.printDealerAndPlayers(playersNames)
         outputView.printInitialCards(dealerCardNames, playersNames, playerCardsNames)
 
         players.forEach { player ->
             while (true) {
-                val score = ScoreCalculator(player.playerCards).totalScore()
-                val isDraw = showHitOrStandQuestion(score, player, allCards)
-                if (isDraw) break
+                val score = player.getScore()
+                if (score > 21) break
+                val decision = inputView.readHitOrStand(player.name)
+                if (!decision) break
+                showHitOrStandQuestion(score, player, allCards)
             }
         }
 
-        val dealerTotalScore = ScoreCalculator(dealer.dealerCards).totalScore()
+        val dealerTotalScore = dealer.getScore()
         if (dealerTotalScore <= 16) {
             outputView.printDealerHit()
-            dealer.dealerTurn(allCards)
+            while (true) {
+                if (dealer.turn(allCards)) break
+            }
         }
+        val updatedDealerTotalScore = dealer.getScore()
 
-        val updatedDealerTotalScore = ScoreCalculator(dealer.dealerCards).totalScore()
-        val playersTotalScore = players.map { ScoreCalculator(it.playerCards).totalScore() }
-
+        val playersTotalScore = players.map { it.getScore() }
         outputView.printDealerResult(dealerInitialCards.getCardNames(), updatedDealerTotalScore)
 
         val updatedPlayerCardsNames = players.map { it.playerCards.getCardNames() }
         outputView.printPlayerResult(playersNames, updatedPlayerCardsNames, playersTotalScore)
 
-        // 아래 코드 합친 함수
         decideGameResult(dealerTotalScore, players)
     }
 
-    private fun showHitOrStandQuestion(score: Int, player: Player, allCards: Cards): Boolean {
+    private fun showHitOrStandQuestion(
+        score: Int,
+        player: Player,
+        allCards: Cards,
+    ) {
         if (score <= 21) {
-            outputView.printHitOrStandQuestion(player.name)
-            val decision = inputView.readHitOrStand()
-            when (decision) {
-                "y" -> {
-                    val playerTurn = player.playerTurn(allCards)
-                    outputView.printPlayerCards(player.name, player.playerCards.getCardNames())
-                    return playerTurn
-                }
-
-                "n" -> return true
-            }
+            player.turn(allCards)
         }
-        return true
+        outputView.printPlayerCards(player.name, player.playerCards.getCardNames())
     }
 
-    private fun decideGameResult(dealerTotalScore: Int, players: List<Player>) {
+    private fun decideGameResult(
+        dealerTotalScore: Int,
+        players: List<Player>,
+    ) {
         val gameResultOutput = GameResultDecider(dealerTotalScore, players).compareWinOrLose()
         outputView.printResult(
             gameResultOutput.dealerWins,
             gameResultOutput.dealerLosses,
-            gameResultOutput.playerResults
+            gameResultOutput.playerResults,
         )
     }
 }
-
