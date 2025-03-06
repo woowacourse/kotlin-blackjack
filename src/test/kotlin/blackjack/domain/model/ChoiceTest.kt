fef77e8abdbe3bc6f853da4dc3ffef490951e0e5
DoableTuple <doabletuple@gmail.com>
2025-03-06 17:28:13 +0900

    test: Choice 예외 처리 테스트

--- src/test/kotlin/blackjack/domain/model/ChoiceTest.kt
+++ src/test/kotlin/blackjack/domain/model/ChoiceTest.kt
@@ -1,7 +1,9 @@
 package blackjack.domain.model
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
+import java.lang.IllegalArgumentException
 
 class ChoiceTest {
     @Test
@@ -12,5 +14,10 @@ class ChoiceTest {
     @Test
     fun `n이 입력될 시 true를 반환한다`() {
         assertThat(Choice("n").isHit()).isEqualTo(false)
+    }
+
+    @Test
+    fun `y 또는 n이 아닌 것이 입력될 시 오류가 발생한다`() {
+        assertThatThrownBy { Choice("f") }.isInstanceOf(IllegalArgumentException::class.java).hasMessage("y 또는 n을 입력해주세요.")
     }
 }
