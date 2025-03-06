aab2c6c38a981b36e117612599f35a6d4c22bf82
gahyunkim <mapledt001@naver.com>
2025-03-06 17:55:01 +0900

    feat: 게임 진행 과정 출력 기능 구현

--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -1,27 +1,59 @@
 package view
 
-import model.Dealer
-import model.Player
-
 class OutputView {
     fun printDealerAndPlayers(players: List<String>) {
         val playerNames = players.joinToString(", ") { it }
         println("\n딜러와 ${playerNames}에게 2장의 나누었습니다.")
     }
 
-    fun printInitialCards(dealer: Dealer, players: List<Player>) {
-        println("딜러: ${dealer.cards.allCards[0]}")
-        players.forEach { player ->
-            println("${player.name}: ${player.cards.allCards.joinToString(", ") { it.toString() }}")
+    fun printInitialCards(
+        dealerCards: List<String>,
+        playerNames: List<String>,
+        playerCards: List<List<String>>,
+    ) {
+        println("딜러: ${dealerCards.first()}")
+        playerNames.forEachIndexed { index, playerName ->
+            println("$playerName: ${playerCards[index].joinToString(", ")}")
         }
     }
 
-    fun printHitOrStandPrompt(playerName: String) {
-        println("${playerName}는 한 장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)")
+    fun printPlayerCards(
+        playerName: String,
+        playerCards: List<String>,
+    ) {
+        println("${playerName}카드: ${playerCards.joinToString(", ")}")
     }
 
-    fun printPlayerCards(player: Player) {
-        println("${player.name}카드: ${player.cards.allCards.joinToString(", ") { it.toString() }}")
+    fun printDealerHit() {
+        println("\n딜러는 16이하라 한장의 카드를 더 받았습니다.")
+    }
+
+    fun printDealerResult(
+        dealerCards: List<String>,
+        dealerScore: Int,
+    ) {
+        println("\n딜러: ${dealerCards.joinToString(", ")} - 결과: $dealerScore")
+    }
+
+    fun printPlayerResult(
+        playerNames: List<String>,
+        playerCards: List<List<String>>,
+        playersScore: List<Int>,
+    ) {
+        playerNames.forEachIndexed { index, playerName ->
+            println("$playerName: ${playerCards[index].joinToString(", ")} - 결과: ${playersScore[index]}")
+        }
+    }
+
+    fun printResult(
+        dealerWins: Int,
+        dealerLosses: Int,
+        playerResults: List<Pair<String, String>>,
+    ) {
+        println("\n## 최종 승패")
+        println("딜러: ${dealerWins}승 ${dealerLosses}패")
+        playerResults.forEach { (name, result) ->
+            println("$name: $result")
+        }
     }
 }
-
