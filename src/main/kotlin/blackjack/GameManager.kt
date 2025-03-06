cfeb476895ac20d99ab3e624393530b89e04dfa9
HamBeomJoon <hbj0802@naver.com>
2025-03-06 10:50:49 +0900

    refactor: GameManager 초기 세팅, 카드 드로우 함수 수정

--- src/main/kotlin/blackjack/GameManager.kt
+++ src/main/kotlin/blackjack/GameManager.kt
@@ -1,15 +1,15 @@
 package blackjack
 
 class GameManager(
+    private val dealer: Dealer,
     private val players: List<Player>,
 ) {
-    fun setting(cnt: Int = 2) {
-        repeat(cnt) {
-            players.forEach { player -> handOutCard(player) }
-        }
+    fun dealInitialCardWithCount(count: Int) {
+        dealer.addCards(drawCards(count))
+        players.forEach { player -> player.addCards(drawCards(count)) }
     }
 
-    fun handOutCard(player: Player) {
-        player.addCard(Deck.poll())
+    private fun drawCards(count: Int): List<Card> {
+        return List(count) { Deck.draw() }
     }
 }
