fa7d4c41de79efd9fe94277be2e86b22c15a58a5
m6z1 <sonmyungj1zz@gmail.com>
2025-03-06 15:34:25 +0900

    feat: 카드 점수 계산 로직 구현

--- src/main/kotlin/blackjack/model/Cards.kt
+++ src/main/kotlin/blackjack/model/Cards.kt
@@ -1,7 +1,17 @@
 package blackjack.model
 
 class Cards(
-    val cards: List<Card>
+    val cards: List<Card>,
 ) {
-
+    fun calculateScore(): Int {
+        val aceCount: Int = cards.count { card -> card.isDenominationAce(card) }
+        val score: Int = cards.sumOf { card -> card.denomination.number }
+        return when (aceCount) {
+            1 -> if (score < 11) score + 11 else score + 1
+            2 -> if (score < 10) score + 11 + 1 else score + aceCount * 1
+            3 -> if (score < 9) score + 11 + 1 + 1 else score + aceCount * 1
+            4 -> if (score < 8) score + 11 + 1 + 1 + 1 else score + aceCount * 1
+            else -> score
+        }
+    }
 }
--- src/test/kotlin/blackjack/model/CardsTest.kt
+++ src/test/kotlin/blackjack/model/CardsTest.kt
@@ -1,6 +1,6 @@
 package blackjack.model
 
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class CardsTest {
@@ -9,6 +9,114 @@ class CardsTest {
         val cards = listOf((Card(CardShape.HEART, Denomination.FIVE)), Card(CardShape.CLOVER, Denomination.TWO))
         val actual = Cards(cards)
 
-        Assertions.assertThat(actual.cards).isEqualTo(cards)
+        assertThat(actual.cards).isEqualTo(cards)
+    }
+
+    @Test
+    fun `Ace 카드가 1~4개가 아닐 때 일때, 카드들의 점수를 반환한다`() {
+        val cards = listOf((Card(CardShape.HEART, Denomination.FIVE)), Card(CardShape.CLOVER, Denomination.TWO))
+        val actual = Cards(cards).calculateScore()
+
+        assertThat(actual).isEqualTo(7)
+    }
+
+    @Test
+    fun `Ace 카드가 1개이고 카드들의 점수가 11미만 일때, Ace의 점수를 11로 판단하고, 카드의 스코어에 Ace의 점수를 추가하여 반환한다`() {
+        val cards = listOf((Card(CardShape.HEART, Denomination.ACE)), Card(CardShape.CLOVER, Denomination.KING))
+        val actual = Cards(cards).calculateScore()
+
+        assertThat(actual).isEqualTo(21)
+    }
+
+    @Test
+    fun `Ace 카드가 1개이고 카드들의 점수가 11이상 일때, Ace의 점수를 1로 판단하고, 카드의 스코어에 Ace의 점수를 추가하여 반환한다`() {
+        val cards = listOf((Card(CardShape.HEART, Denomination.ACE)), Card(CardShape.CLOVER, Denomination.TWO))
+        val actual = Cards(cards).calculateScore()
+
+        assertThat(actual).isEqualTo(13)
+    }
+
+    @Test
+    fun `Ace 카드가 2개이고 카드들의 점수가 10미만 일때, Ace둘의 점수를 12로 판단하고, 카드의 스코어에 Ace들의 점수를 추가하여 반환한다`() {
+        val cards =
+            listOf(
+                Card(CardShape.HEART, Denomination.ACE),
+                Card(CardShape.CLOVER, Denomination.THREE),
+                Card(CardShape.DIAMOND, Denomination.ACE),
+            )
+        val actual = Cards(cards).calculateScore()
+
+        assertThat(actual).isEqualTo(15)
+    }
+
+    @Test
+    fun `Ace 카드가 2개이고 카드들의 점수가 10이상 일때, Ace들의 점수를 2로 판단하고, 카드의 스코어에 Ace들의 점수를 추가하여 반환한다`() {
+        val cards =
+            listOf(
+                Card(CardShape.HEART, Denomination.ACE),
+                Card(CardShape.CLOVER, Denomination.TEN),
+                Card(CardShape.DIAMOND, Denomination.ACE),
+            )
+        val actual = Cards(cards).calculateScore()
+
+        assertThat(actual).isEqualTo(12)
+    }
+
+    @Test
+    fun `Ace 카드가 3개이고 카드들의 점수가 9미만 일때, Ace둘의 점수를 13로 판단하고, 카드의 스코어에 Ace들의 점수를 추가하여 반환한다`() {
+        val cards =
+            listOf(
+                Card(CardShape.HEART, Denomination.ACE),
+                Card(CardShape.CLOVER, Denomination.THREE),
+                Card(CardShape.DIAMOND, Denomination.ACE),
+                Card(CardShape.CLOVER, Denomination.ACE),
+            )
+        val actual = Cards(cards).calculateScore()
+
+        assertThat(actual).isEqualTo(16)
+    }
+
+    @Test
+    fun `Ace 카드가 3개이고 카드들의 점수가 9이상 일때, Ace들의 점수를 3로 판단하고, 카드의 스코어에 Ace들의 점수를 추가하여 반환한다`() {
+        val cards =
+            listOf(
+                Card(CardShape.HEART, Denomination.ACE),
+                Card(CardShape.CLOVER, Denomination.NINE),
+                Card(CardShape.DIAMOND, Denomination.ACE),
+                Card(CardShape.CLOVER, Denomination.ACE),
+            )
+        val actual = Cards(cards).calculateScore()
+
+        assertThat(actual).isEqualTo(12)
+    }
+
+    @Test
+    fun `Ace 카드가 4개이고 카드들의 점수가 8미만 일때, Ace둘의 점수를 14로 판단하고, 카드의 스코어에 Ace들의 점수를 추가하여 반환한다`() {
+        val cards =
+            listOf(
+                Card(CardShape.HEART, Denomination.ACE),
+                Card(CardShape.CLOVER, Denomination.THREE),
+                Card(CardShape.DIAMOND, Denomination.ACE),
+                Card(CardShape.CLOVER, Denomination.ACE),
+                Card(CardShape.SPADE, Denomination.ACE),
+            )
+        val actual = Cards(cards).calculateScore()
+
+        assertThat(actual).isEqualTo(17)
+    }
+
+    @Test
+    fun `Ace 카드가 4개이고 카드들의 점수가 8이상 일때, Ace들의 점수를 4로 판단하고, 카드의 스코어에 Ace들의 점수를 추가하여 반환한다`() {
+        val cards =
+            listOf(
+                Card(CardShape.HEART, Denomination.ACE),
+                Card(CardShape.CLOVER, Denomination.NINE),
+                Card(CardShape.DIAMOND, Denomination.ACE),
+                Card(CardShape.CLOVER, Denomination.ACE),
+                Card(CardShape.SPADE, Denomination.ACE),
+            )
+        val actual = Cards(cards).calculateScore()
+
+        assertThat(actual).isEqualTo(13)
     }
 }
