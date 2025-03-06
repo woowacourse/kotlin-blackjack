a685667d9dd6a277d0a17ae5d49a0a1deafb1134
cucumber99 <dlsguq1@gmail.com>
2025-03-06 15:12:35 +0900

    refactor: 점수 계산 기능 분리

--- src/main/kotlin/blackjack/domain/person/Person.kt
+++ src/main/kotlin/blackjack/domain/person/Person.kt
@@ -1,11 +1,10 @@
 package blackjack.domain.person
 
-import blackjack.const.GameRule
+import blackjack.domain.ScoreCalculator
 import blackjack.domain.card.Card
-import blackjack.domain.card.CardNumber
 import blackjack.domain.state.PersonState
 
-abstract class Person {
+abstract class Person(private val scoreCalculator: ScoreCalculator = ScoreCalculator()) {
     protected val hand: Hand = Hand()
 
     private lateinit var _gameState: PersonState
@@ -13,17 +12,9 @@ abstract class Person {
 
     fun cards(): List<Card> = hand.cards
 
-    fun canDraw(): Boolean {
-        return !gameState.isFinal
-    }
+    fun canDraw(): Boolean = !gameState.isFinal
 
-    fun score(): Int {
-        val values = hand.cards.map { if (it.number == CardNumber.ACE) GameRule.ACE_OTHER_SCORE else it.number.value }
-        val sum = values.sum()
-        return values.fold(sum) { acc, number ->
-            if (acc > GameRule.BLACKJACK_SCORE && number == GameRule.ACE_OTHER_SCORE) acc - GameRule.ACE_BASE_SCORE else acc
-        }
-    }
+    fun score(): Int = scoreCalculator.calculate(cards())
 
     abstract fun updateGameState()
 
