33f86f954d8c690630b1f22a61435c1e96420ad0
medAndro <med4ndro@gmail.com>
2025-03-06 16:46:26 +0900

    feat: Rule에서 플레이어의 드로우 가능 여부를 반환하는 메서드 구현

--- src/main/kotlin/blackjack/domain/model/Rule.kt
+++ src/main/kotlin/blackjack/domain/model/Rule.kt
@@ -4,12 +4,22 @@ class Rule {
     companion object {
         fun calculateResultByCards(cards: Collection<Card>): Int {
             val cardValues = cards.map { it.cardNumber.cardNumber }
-            val minimumSum = cardValues.sum()
+            val minimumSum = calculateMinimumSumByCards(cards)
 
             if (1 in cardValues && (minimumSum + 10) <= BLACK_JACK_NUMBER) {
                 return minimumSum + 10
             }
             return minimumSum
+        }
+
+        fun calculateIsDrawableByCards(cards: Collection<Card>): Boolean {
+            val minimumSum = calculateMinimumSumByCards(cards)
+            return minimumSum <= BLACK_JACK_NUMBER
+        }
+
+        private fun calculateMinimumSumByCards(cards: Collection<Card>): Int {
+            val cardValues = cards.map { it.cardNumber.cardNumber }
+            return cardValues.sum()
         }
 
         private const val BLACK_JACK_NUMBER = 21
