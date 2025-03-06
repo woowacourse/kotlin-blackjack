b26bc833555fb3f312be1110cf445dd9f386867f
gahyunkim <mapledt001@naver.com>
2025-03-06 17:55:22 +0900

    chore: Players 클래스 삭제

--- src/main/kotlin/model/Players.kt
+++
@@ -1,8 +0,0 @@
-package model
-
-class Players(private val players: List<Player>) {
-    init {
-        require(players.isNotEmpty()) { "게임을 시작하기 위해서는 플레이어는 1명 이상이어야 합니다" }
-        require(players.map { it.name }.distinct().size == players.size) { "플레이어의 이름은 중복될 수 없습니다" }
-    }
-}
