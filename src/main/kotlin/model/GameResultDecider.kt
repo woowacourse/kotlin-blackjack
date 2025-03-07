caadd7783ca67a684fe7c9313a6220163a1a6ef7
gahyunkim <mapledt001@naver.com>
2025-03-07 13:40:04 +0900

    refactor: GameResultDecider 코드 간결화

--- src/main/kotlin/model/GameResultDecider.kt
+++ src/main/kotlin/model/GameResultDecider.kt
@@ -2,43 +2,39 @@ package model
 
 import kotlin.math.abs
 
-data class GameOutput(
-    val dealerWins: Int,
-    val dealerLosses: Int,
-    val playerResults: List<Pair<String, String>>,
-)
+data class PlayerResult(val name: String, val result: String)
 
-class GameResultDecider(private val dealerTotalScore: Int, private val players: List<Player>) {
+class GameResultDecider(private val dealer: Dealer, private val players: List<Player>) {
     fun compareWinOrLose(): GameOutput {
-        var dealerWins = 0
-        var dealerLosses = 0
-
-        val playerResults = mutableListOf<Pair<String, String>>()
-
-        players.forEach { player ->
-            val playerScore = ScoreCalculator(player.playerCards).totalCardScore()
-            val result =
-                when {
-                    dealerTotalScore > 21 -> "승"
-                    playerScore > 21 -> "패"
-                    else -> {
-                        val dealerDiff = abs(21 - dealerTotalScore)
-                        val playerDiff = abs(21 - playerScore)
-                        when {
-                            playerDiff < dealerDiff -> "승"
-                            playerDiff > dealerDiff -> "패"
-                            else -> "무"
-                        }
-                    }
-                }
-
-            when (result) {
-                "승" -> dealerLosses++
-                "패" -> dealerWins++
-            }
-            playerResults.add(player.name to result)
+        val playerResults: List<PlayerResult> = players.map { player ->
+            PlayerResult(player.name, comparePlayerResult(player.getScore()))
         }
 
+        val dealerWins = playerResults.count { it.result == LOSE }
+        val dealerLosses = playerResults.count { it.result == WIN }
         return GameOutput(dealerWins, dealerLosses, playerResults)
+    }
+
+    private fun comparePlayerResult(playerScore: Int): String = when {
+        dealer.getScore() > STANDARD_SCORE -> WIN
+        playerScore > STANDARD_SCORE -> LOSE
+        else -> compareScores(playerScore)
+    }
+
+    private fun compareScores(playerScore: Int): String {
+        val dealerDiff = abs(STANDARD_SCORE - dealer.getScore())
+        val playerDiff = abs(STANDARD_SCORE - playerScore)
+        return when {
+            playerDiff < dealerDiff -> WIN
+            playerDiff > dealerDiff -> LOSE
+            else -> DRAW
+        }
+    }
+
+    companion object {
+        private const val STANDARD_SCORE = 21
+        private const val WIN = "승"
+        private const val LOSE = "패"
+        private const val DRAW = "무"
     }
 }
