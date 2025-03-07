60ff2245b9e7cc4cb9a32b7b7673a3e239c5293a
m6z1 <sonmyungj1zz@gmail.com>
2025-03-07 15:15:42 +0900

    feat: 플레이어의 블랙잭 여부 및 결과 업데이트 로직 추가

--- src/main/kotlin/blackjack/model/Player.kt
+++ src/main/kotlin/blackjack/model/Player.kt
@@ -4,7 +4,15 @@ open class Player(
     open val name: String,
     open val cards: Cards = Cards(emptyList()),
 ) {
+    lateinit var result: GameResult
+
     open fun appendCard(card: Card) {
         cards.add(card)
+    }
+
+    open fun isBlackjack(firstTurn: Boolean): Boolean = cards.isBlackjack(firstTurn)
+
+    fun updateResult(dealerGameResult: GameResult) {
+        result = GameResult.reversed(dealerGameResult)
     }
 }
