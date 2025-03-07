d3a6cd97008e1b58d2aae02701ff0f53442d6dd9
rosemin928 <121722789+rosemin928@users.noreply.github.com>
2025-03-07 17:01:55 +0900

    feat: 승패 상태 클래스 추가

---
+++ src/main/kotlin/blackjack/domain/enums/GameResult.kt
@@ -0,0 +1,27 @@
+package blackjack.domain.enums
+
+import blackjack.domain.BlackJackGame.Companion.BUST_STANDARD
+
+enum class GameResult {
+    WIN,
+    LOSE,
+    ;
+
+    companion object {
+        fun from(
+            dealerSum: Int,
+            playerSum: Int,
+        ): GameResult =
+            if ((dealerSum > BUST_STANDARD) && (playerSum < BUST_STANDARD)) {
+                WIN
+            } else if (playerSum > BUST_STANDARD) {
+                LOSE
+            } else {
+                if (dealerSum > playerSum) {
+                    LOSE
+                } else {
+                    WIN
+                }
+            }
+    }
+}
