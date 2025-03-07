215a636f87969724cf49d35bebf2e7407ec1a253
윤동현 <82762769+donghyun81@users.noreply.github.com>
2025-03-07 16:08:28 +0900

    feat: 지정 타입 캐스트 처리 변경

--- src/main/kotlin/blackjack/domain/model/Participants.kt
+++ src/main/kotlin/blackjack/domain/model/Participants.kt
@@ -4,7 +4,7 @@ class Participants(val players: List<Player>) {
     constructor(dealer: Dealer, playersName: List<String>) : this(listOf(dealer) + playersName.map(::Player))
 
     fun findDealer(): Dealer {
-        return requireNotNull(players.find { it is Dealer }) as Dealer
+        return requireNotNull(players.find { it is Dealer } as? Dealer)
     }
 
     fun filterPlayers(): List<Player> {
