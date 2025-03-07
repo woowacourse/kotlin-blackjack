ccd0d3d9ebbc9713cbd12425a0497f824484bc71
rosemin928 <121722789+rosemin928@users.noreply.github.com>
2025-03-07 15:19:41 +0900

    feat: 플레이어 카드 추가 선택 입력 기능 구현

--- src/main/kotlin/blackjack/view/InputView.kt
+++ src/main/kotlin/blackjack/view/InputView.kt
@@ -13,6 +13,15 @@ class InputView {
         }
     }
 
+    fun readHitOrStay(name: String): String {
+        println(MESSAGE_ASK_PLAYER_HIT_OR_STAY.format(name))
+        val input = readln()
+        if (validEmpty(input).not()) readHitOrStay(name)
+        return input
+    }
+
+    private fun splitNames(input: String): List<String> = input.split(",").map { it.trim() }
+
     private fun validEmpty(input: String): Boolean {
         if (input.isBlank()) {
             println(MESSAGE_EMPTY_INPUT)
@@ -23,6 +32,9 @@ class InputView {
 
     companion object {
         const val MESSAGE_INPUT_PLAYER_NAME = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)"
-        const val MESSAGE_EMPTY_INPUT = "빈 값이 입력되었습니다."
+        const val MESSAGE_ASK_PLAYER_HIT_OR_STAY = "%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)"
+
+        const val MESSAGE_EMPTY_INPUT = "빈 값이 입력 되었습니다."
+        const val MESSAGE_EMPTY_NAME = "빈 이름이 포함되어 있습니다."
     }
 }
