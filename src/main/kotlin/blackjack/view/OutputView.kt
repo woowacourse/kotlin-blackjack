82e8d162eb0d52d44f1264ad72140382e3af03af
cucumber99 <dlsguq1@gmail.com>
2025-03-07 14:15:15 +0900

    refactor: 출력 로직 추가
    - 딜러의 첫 카드만 공개합니다

--- src/main/kotlin/blackjack/view/OutputView.kt
+++ src/main/kotlin/blackjack/view/OutputView.kt
@@ -13,7 +13,12 @@ class OutputView {
         val nameList = personUiModels.map { it.name }.joinToString(DELIMITER)
         println(FIRST_DRAW_MESSAGE.format(nameList))
         personUiModels.forEach { person ->
-            println(DRAW_STATUS_MESSAGE.format(person.name, person.cards.joinToString(DELIMITER)))
+            println(
+                DRAW_STATUS_MESSAGE.format(
+                    person.name,
+                    if (person.name == DEALER) person.cards.first() else person.cards.joinToString(DELIMITER),
+                ),
+            )
         }
         println()
     }
@@ -49,17 +54,18 @@ class OutputView {
     }
 
     companion object {
+        private const val DEALER = "딜러"
         private const val ENTER_PLAYER_NAMES_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)"
         private const val FIRST_DRAW_MESSAGE = "%s에게 2장을 나누었습니다."
         private const val ASK_DRAW_CARD_MESSAGE = "%s는 한장의 카드를 더 받겠습니까? (예는 y, 아니오는 n)"
-        private const val DEALER_DRAW_MESSAGE = "딜러는 16이하라 한장의 카드를 더 받았습니다."
+        private const val DEALER_DRAW_MESSAGE = "${DEALER}는 16이하라 한장의 카드를 더 받았습니다."
         private const val DRAW_STATUS_MESSAGE = "%s 카드: %s"
         private const val SCORE_RESULT_MESSAGE = " - 결과: %s"
         private const val RESULT_HEADLINE_MESSAGE = "## 최종 승패"
         private const val WIN = "승"
         private const val LOSE = "패"
         private const val DRAW = "무"
-        private const val DEALER_RESULT_MESSAGE = "딜러: %s$WIN %s$DRAW %s$LOSE"
+        private const val DEALER_RESULT_MESSAGE = "$DEALER: %s$WIN %s$DRAW %s$LOSE"
         private const val PLAYER_RESULT_MESSAGE = "%s: %s"
         private const val DELIMITER = ", "
     }
