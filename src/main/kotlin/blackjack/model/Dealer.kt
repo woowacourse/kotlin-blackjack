14d5ccbf1a7e0f8861627470bc7ab743d6b41df5
m6z1 <sonmyungj1zz@gmail.com>
2025-03-07 15:14:41 +0900

    feat: 딜러 결과 반환 및 업데이트 로직 추가

--- src/main/kotlin/blackjack/model/Dealer.kt
+++ src/main/kotlin/blackjack/model/Dealer.kt
@@ -4,7 +4,17 @@ class Dealer(
     override val name: String = "딜러",
     override val cards: Cards = Cards(mutableListOf()),
 ) : Player(name) {
+    private var _results: MutableMap<GameResult, Int> = mutableMapOf()
+    val results: Map<GameResult, Int> get() = _results.toMap()
+
     override fun appendCard(card: Card) {
         cards.add(card)
+    }
+
+    fun updateResult(playerScore: Int): GameResult {
+        val dealerScore: Int = cards.calculateScore()
+        val result: GameResult = GameResult.of(dealerScore, playerScore)
+        _results[result] = _results.getOrDefault(result, 0) + 1
+        return result
     }
 }
