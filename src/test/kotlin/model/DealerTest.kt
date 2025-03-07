32481297cdadd0156ccbc352d631d45ff0dd705b
gahyunkim <mapledt001@naver.com>
2025-03-07 13:44:26 +0900

    test: DealerTest 테스트 코드 추가

--- src/test/kotlin/model/DealerTest.kt
+++ src/test/kotlin/model/DealerTest.kt
@@ -1,26 +1,58 @@
 package model
 
+import model.CardsTest.Companion.cardOf
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 class DealerTest {
     @Test
     fun `딜러는 게임을 시작하면 2장의 카드를 갖는다`() {
-        val ace = Card(CardRank.ACE, Shape.CLUB)
-        val six = Card(CardRank.SIX, Shape.CLUB)
+        val cards = cardOf(
+            Card(CardRank.ACE, Shape.CLUB),
+            Card(CardRank.SIX, Shape.CLUB),
+        )
 
-        assertThat(Cards(mutableListOf(ace, six)).allCards.size).isEqualTo(2)
+        assertThat(cards.getCardsCount()).isEqualTo(2)
     }
 
     @Test
-    fun `딜러는 처음에 1장의 카드를 오픈할 수 있다`() {
-        val card1 = Card(CardRank.ACE, Shape.CLUB)
-        val card2 = Card(CardRank.SIX, Shape.SPADE)
+    fun `딜러는 보유한 카드가 16이하일 경우 게임을 진행할 수 있다`() {
+        val cards = cardOf(
+            Card(CardRank.FIVE, Shape.CLUB),
+            Card(CardRank.SIX, Shape.SPADE),
+        )
 
-        val dealer = Dealer(Cards(mutableListOf(card1, card2)))
-        val initialShowCount = 1
-        val showDealerCard = dealer.cards.showInitialCards(initialShowCount)
+        val dealer = Dealer(cards)
+        Assertions.assertTrue(dealer.isHit())
+    }
 
-        assertThat(showDealerCard).isEqualTo(card1)
+    @Test
+    fun `딜러는 보유한 카드가 16이하인 경우 카드를 뽑아야한다`() {
+        val cards = cardOf(
+            Card(CardRank.FIVE, Shape.CLUB),
+            Card(CardRank.SIX, Shape.SPADE),
+        )
+
+        val dealer = Dealer(cards)
+        Assertions.assertTrue(dealer.turn(cards))
+    }
+
+    @Test
+    fun `딜러는 보유한 카드가 16이하인 경우 카드를 1번 이상 뽑아야 한다`() {
+        val cards = cardOf(
+            Card(CardRank.TWO, Shape.CLUB),
+            Card(CardRank.THREE, Shape.SPADE),
+        )
+        val dealer = Dealer(cards)
+
+        val deckCards = mutableListOf(
+            Card(CardRank.TEN, Shape.CLUB),
+            Card(CardRank.NINE, Shape.SPADE),
+        )
+        val deck = Cards(deckCards)
+        val drawCount = dealer.getDrawCount(deck)
+
+        assertThat(drawCount).isEqualTo(2)
     }
 }
