c87c72c5a30b06e0fb4f4bc9c0ed4f9a6952e789
cucumber99 <dlsguq1@gmail.com>
2025-03-07 14:03:22 +0900

    style: 불필요한 개행 제거

--- src/main/kotlin/blackjack/domain/GameResult.kt
+++ src/main/kotlin/blackjack/domain/GameResult.kt
@@ -4,9 +4,7 @@ import blackjack.domain.person.Dealer
 import blackjack.domain.person.Player
 import blackjack.domain.state.ResultState
 
-class GameResult(
-    private val dealer: Dealer,
-) {
+class GameResult(private val dealer: Dealer) {
     fun calculateWin(players: List<Player>): Map<Player, ResultState> {
         return players.associateWith { player -> ResultState.calculateWin(player, dealer) }
     }
