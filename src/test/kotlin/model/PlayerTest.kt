8b008effd6206ccdd2f0d9092792606c6f9dcaef
gahyunkim <mapledt001@naver.com>
2025-03-07 14:22:02 +0900

    test: PlayerTest 테스트 코드 추가

--- src/test/kotlin/model/PlayerTest.kt
+++ src/test/kotlin/model/PlayerTest.kt
@@ -1,6 +1,8 @@
 package model
 
+import model.CardsTest.Companion.cardOf
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
@@ -12,9 +14,33 @@ class PlayerTest {
 
     @Test
     fun `플레이어는 게임을 시작하면 2장의 카드를 갖는다`() {
-        val king = Card(CardRank.KING, Shape.CLUB)
-        val queen = Card(CardRank.QUEEN, Shape.CLUB)
+        val cards = cardOf(
+            Card(CardRank.KING, Shape.CLUB),
+            Card(CardRank.QUEEN, Shape.SPADE),
+        )
 
-        assertThat(Cards(mutableListOf(king, queen)).allCards.size).isEqualTo(2)
+        assertThat(cards.getCardsCount()).isEqualTo(2)
+    }
+
+    @Test
+    fun `플레이어는 카드를 받을지 결정할 수 있다`() {
+        val cards = cardOf(
+            Card(CardRank.SIX, Shape.CLUB),
+            Card(CardRank.NINE, Shape.SPADE),
+        )
+
+        val player = Player("joy", cards)
+        Assertions.assertTrue(player.isHit())
+    }
+
+    @Test
+    fun `플레이어는 카드를 뽑을 수 있다`() {
+        val cards = cardOf(
+            Card(CardRank.FIVE, Shape.CLUB),
+            Card(CardRank.SIX, Shape.SPADE),
+        )
+
+        val player = Player("joy", cards)
+        Assertions.assertFalse(player.turn(cards))
     }
 }
