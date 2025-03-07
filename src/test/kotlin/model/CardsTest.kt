44ac5d027565c643143f0171044b47ab68e8edfe
gahyunkim <mapledt001@naver.com>
2025-03-07 13:43:31 +0900

    test: CardsTest 테스트 코드 추가

--- src/test/kotlin/model/CardsTest.kt
+++ src/test/kotlin/model/CardsTest.kt
@@ -9,25 +9,49 @@ class CardsTest {
     @Test
     fun `원하는 개수 만큼 카드를 뽑을 수 있다`() {
         val cards = CardsGenerator().generateCards().allCards
-        val drawCards = Cards(cards).drawCards(2)
+        val drawCards = Cards(cards).getInitialCards()
 
         assertThat(drawCards.getCardsCount()).isEqualTo(2)
     }
 
     @Test
+    fun `보유한 카드의 개수를 반환할 수 있다`() {
+        val cards = cardOf(
+            Card(CardRank.ACE, Shape.CLUB),
+            Card(CardRank.ACE, Shape.SPADE)
+        )
+
+        assertThat(cards.getCardsCount()).isEqualTo(2)
+    }
+
+    @Test
+    fun `보유한 카드의 점수들을 확인할 수 있다`() {
+        val cards = cardOf(
+            Card(CardRank.SIX, Shape.CLUB),
+            Card(CardRank.NINE, Shape.SPADE)
+        )
+
+        val scoreLists = listOf(6, 9)
+
+        assertThat(cards.getCardScores()).isEqualTo(scoreLists)
+    }
+
+    @Test
     fun `지정된 개수만큼 카드를 뽑은 후 남은 카드를 반환한다`() {
         val cards = CardsGenerator().generateCards()
-        cards.drawCards(2)
+        cards.getInitialCards()
+
         assertThat(cards.getCardsCount()).isEqualTo(50)
     }
 
     @Test
-    fun `카드는 중복될 수 없다`() {
-        val card1 = Card(CardRank.ACE, Shape.CLUB)
-        val card2 = Card(CardRank.ACE, Shape.CLUB)
+    fun `중복된 카드는 존재할 수 없다`() {
+        val cards = cardOf(
+            Card(CardRank.ACE, Shape.CLUB),
+            Card(CardRank.ACE, Shape.CLUB)
+        )
 
-        val cards = mutableListOf(card1, card2)
-        assertThrows<IllegalArgumentException> { Cards(cards) }
+        assertThrows<IllegalArgumentException> { cards }
     }
 
     @Test
@@ -42,4 +66,11 @@ class CardsTest {
 
         Assertions.assertEquals(expected, result)
     }
+
+    companion object {
+        fun cardOf(vararg card: Card): Cards {
+            return Cards(card.toList())
+        }
+    }
 }
+
