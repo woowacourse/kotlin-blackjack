94b11e4f872e7ba0637e54e3023710918d1f6e45
gahyunkim <mapledt001@naver.com>
2025-03-07 14:22:48 +0900

    test: ScoreCalculatorTest 테스트 코드 함수 매핑

--- src/test/kotlin/model/ScoreCalculatorTest.kt
+++ src/test/kotlin/model/ScoreCalculatorTest.kt
@@ -1,16 +1,16 @@
 package model
 
+import model.CardsTest.Companion.cardOf
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 class ScoreCalculatorTest {
     @Test
     fun `보유한 카드의 점수를 계산한다`() {
-
-        val card1 = Card(CardRank.SIX, Shape.CLUB)
-        val card2 = Card(CardRank.SEVEN, Shape.HEART)
-
-        val cards = Cards(mutableListOf(card1, card2))
+        val cards = cardOf(
+            Card(CardRank.SIX, Shape.CLUB),
+            Card(CardRank.SEVEN, Shape.HEART),
+        )
 
         val scoreCalculator = ScoreCalculator(cards)
         val exceptedTotalScore = scoreCalculator.calculateTotalCardScore()
@@ -21,11 +21,11 @@ class ScoreCalculatorTest {
 
     @Test
     fun `ACE가 포함된 카드를 계산할 수 있다`() {
-        val card1 = Card(CardRank.QUEEN, Shape.CLUB)
-        val card2 = Card(CardRank.NINE, Shape.HEART)
-        val card3 = Card(CardRank.ACE, Shape.HEART)
-
-        val cards = Cards(mutableListOf(card1, card2, card3))
+        val cards = cardOf(
+            Card(CardRank.QUEEN, Shape.CLUB),
+            Card(CardRank.NINE, Shape.HEART),
+            Card(CardRank.ACE, Shape.HEART)
+        )
 
         val scoreCalculator = ScoreCalculator(cards)
         val totalScore = scoreCalculator.calculateTotalCardScore()
@@ -35,13 +35,13 @@ class ScoreCalculatorTest {
 
     @Test
     fun `ACE가 여러개 포함되었을때의 값을 계산할 수 있다`() {
-        val card1 = Card(CardRank.ACE, Shape.CLUB)
-        val card2 = Card(CardRank.ACE, Shape.HEART)
-        val card3 = Card(CardRank.ACE, Shape.SPADE)
-        val card4 = Card(CardRank.TEN, Shape.SPADE)
-        val card5 = Card(CardRank.EIGHT, Shape.CLUB)
-
-        val cards = Cards(mutableListOf(card1, card2, card3, card4, card5))
+        val cards = cardOf(
+            Card(CardRank.ACE, Shape.CLUB),
+            Card(CardRank.ACE, Shape.HEART),
+            Card(CardRank.ACE, Shape.SPADE),
+            Card(CardRank.TEN, Shape.SPADE),
+            Card(CardRank.EIGHT, Shape.CLUB)
+        )
 
         val scoreCalculator = ScoreCalculator(cards)
         val totalScore = scoreCalculator.calculateTotalCardScore()
@@ -51,11 +51,11 @@ class ScoreCalculatorTest {
 
     @Test
     fun `ACE카드를 제외하고 보유한 카드의 합이 20일 때 ACE는 1이다`() {
-        val card1 = Card(CardRank.ACE, Shape.CLUB)
-        val card2 = Card(CardRank.TEN, Shape.SPADE)
-        val card3 = Card(CardRank.TEN, Shape.HEART)
-
-        val cards = Cards(mutableListOf(card1, card2, card3))
+        val cards = cardOf(
+            Card(CardRank.ACE, Shape.CLUB),
+            Card(CardRank.TEN, Shape.SPADE),
+            Card(CardRank.TEN, Shape.HEART)
+        )
 
         val scoreCalculator = ScoreCalculator(cards)
         val totalScore = scoreCalculator.calculateTotalCardScore()
@@ -65,11 +65,12 @@ class ScoreCalculatorTest {
 
     @Test
     fun `ACE카드를 제외하고 보유한 카드의 합이 10일 때 ACE는 11이다`() {
-        val card1 = Card(CardRank.ACE, Shape.CLUB)
-        val card2 = Card(CardRank.FIVE, Shape.SPADE)
-        val card3 = Card(CardRank.FIVE, Shape.HEART)
-
-        val cards = Cards(mutableListOf(card1, card2, card3))
+        val cards =
+            cardOf(
+                Card(CardRank.ACE, Shape.CLUB),
+                Card(CardRank.FIVE, Shape.SPADE),
+                Card(CardRank.FIVE, Shape.HEART)
+            )
 
         val scoreCalculator = ScoreCalculator(cards)
         val totalScore = scoreCalculator.calculateTotalCardScore()
