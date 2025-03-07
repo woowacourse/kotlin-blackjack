d78a3a27744401107780b352ca22ca4e07b418c6
HamBeomJoon <hbj0802@naver.com>
2025-03-07 15:44:30 +0900

    refactor: 출력 문구 수정

--- src/main/kotlin/blackjack/view/InputView.kt
+++ src/main/kotlin/blackjack/view/InputView.kt
@@ -21,7 +21,7 @@ class InputView {
 
     companion object {
         private const val PLAYER_NAME_MESSAGE_GUIDE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)"
-        private const val PLAYER_MORE_CARD_MESSAGE_GUIDE = "%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)"
+        private const val PLAYER_MORE_CARD_MESSAGE_GUIDE = "%s은(는) 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)"
         private const val PLAYER_NAME_DELIMITER = ","
     }
 }
