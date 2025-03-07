3697017a013c54564fbea30c4604ab07fa006f46
gahyunkim <mapledt001@naver.com>
2025-03-07 13:47:36 +0900

    feat: 오버라이딩 함수 구현

--- src/main/kotlin/model/Player.kt
+++ src/main/kotlin/model/Player.kt
@@ -5,15 +5,16 @@ class Player(val name: String, val playerCards: Cards) : Participant(playerCards
         require(name.isNotEmpty()) { PLAYER_BLANK_ERROR_MESSAGE }
     }
 
-    override fun turn(allCards: Cards): Boolean {
-        val score = getScore()
-        if (score <= 21) {
-            val drawnCard = allCards.drawCards(1)
-            playerCards.addCards(drawnCard)
+    override fun turn(cards: Cards): Boolean {
+        if (isHit()) {
+            val drawnCard = drawCard(cards.allCards)
+            addCard(drawnCard)
             return false
         }
         return true
     }
+
+    override fun isHit(): Boolean = getScore() <= 21
 
     companion object {
         private const val PLAYER_BLANK_ERROR_MESSAGE = "[ERROR] 이름은 빈 값일 수 없습니다."
