51f458cc1190cb979ab0de6eeccc7abb3dc523e4
yrsel <xxknu88@gmail.com>
2025-03-07 13:41:49 +0900

    refactor: 21을 초과할 경우의 로직 처리

--- src/main/kotlin/blackjack/model/ResultType.kt
+++ src/main/kotlin/blackjack/model/ResultType.kt
@@ -1,18 +1,17 @@
 package blackjack.model
 
 enum class ResultType(val value: Char) {
-    WIN('승'),
-    TIE('무'),
-    LOSS('패'),
-    ;
+    WIN('승'), TIE('무'), LOSS('패'), ;
 
     companion object {
         fun judgeScore(
             dealerScore: Int,
             playerScore: Int,
         ): ResultType {
-            if (dealerScore < playerScore) return WIN
-            if (dealerScore == playerScore) return TIE
+            val dealerFinalScore = if (dealerScore > 21) 0 else dealerScore
+            val playerFinalScore = if (playerScore > 21) 0 else playerScore
+            if (dealerFinalScore < playerFinalScore) return WIN
+            if (dealerFinalScore == playerFinalScore) return TIE
             return LOSS
         }
     }
