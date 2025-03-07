4249cd80fd8bfd1bac873e3bcafa3b0db37c5dc1
medAndro <med4ndro@gmail.com>
2025-03-07 17:12:00 +0900

    feat: DSL 연습을 파일 한개로 임시 구현 완료

--- src/test/kotlin/dslstudy/DslTest.kts
+++ src/test/kotlin/dslstudy/DslTest.kts
@@ -1,9 +1,7 @@
-package DslStudy
+package dslstudy
 
 
-fun introduce(block: PersonBuilder.() -> Unit): Person {
-    return PersonBuilder().apply(block).build()
-}
+fun introduce(block: PersonBuilder.() -> Unit): Person = PersonBuilder().apply(block).build()
 
 class PersonBuilder {
     private lateinit var name: String
@@ -29,30 +27,39 @@ class PersonBuilder {
         this.languages = languagesBuilder.getLanguages()
     }
 
-    fun build(): Person {
-        return Person(name, company, skill, languages)
-    }
+    fun build(): Person = Person(name, company, skill, languages)
 }
 
 class SkillsBuilder {
     private val softItems = mutableListOf<String>()
     private val hardItems = mutableListOf<String>()
+
     fun soft(value: String) = softItems.add(value)
+
     fun hard(value: String) = hardItems.add(value)
-    fun build(): Skill {
-        return Skill(softItems, hardItems)
-    }
+
+    fun build(): Skill = Skill(softItems, hardItems)
 }
 
 class LanguagesBuilder {
     private val languages = mutableMapOf<String, Int>()
+
     infix fun String.level(level: Int) = languages.put(this, level)
+
     fun getLanguages(): Map<String, Int> = languages
 }
 
-data class Person(val name: String, val company: String?, val skill: Skill, val languages: Map<String, Int>)
-data class Skill(val softSkill: List<String>, val hardSkill: List<String>)
+data class Person(
+    val name: String,
+    val company: String?,
+    val skill: Skill,
+    val languages: Map<String, Int>,
+)
 
+data class Skill(
+    val softSkill: List<String>,
+    val hardSkill: List<String>,
+)
 
 introduce {
     name("박재성")
