b29c81aac44e86d19ca9af89d4ee2b9a537fc8c1
gahyunkim <mapledt001@naver.com>
2025-03-07 16:05:02 +0900

    feat: Players 클래스 생성

---
+++ src/main/kotlin/model/Players.kt
@@ -0,0 +1,17 @@
+package model
+
+class Players(private val players: List<Player>) : Iterable<Player> {
+    init {
+        require(players.isNotEmpty()) { "플레이어의 수는 1명 이상이어야 합니다" }
+    }
+
+    override fun iterator(): Iterator<Player> = players.iterator()
+
+    fun getPlayerCardNames(): List<List<String>> {
+        return players.map { player ->
+            player.getPlayerCardNames()
+        }
+    }
+
+    fun getPlayersScores(): List<Int> = players.map { player -> player.getScore() }
+}
