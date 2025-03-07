8e410fffcaa559a5d928b3a1ebd8fc9e7ab449cd
gahyunkim <mapledt001@naver.com>
2025-03-07 13:43:17 +0900

    refactor: 초기 카드 생성 기능 변경

--- src/main/kotlin/model/Cards.kt
+++ src/main/kotlin/model/Cards.kt
@@ -1,14 +1,16 @@
 package model
 
-class Cards(val allCards: MutableList<Card>) : List<Card> by allCards {
+class Cards(allCards: List<Card>) {
+    val allCards: MutableList<Card> = allCards.toMutableList()
+
     init {
         require(allCards.distinct().size == getCardsCount()) { "[ERROR] 카드는 중복될 수 없습니다" }
     }
 
-    fun drawCards(count: Int): Cards {
-        val drawnCards = allCards.take(count)
-        allCards.removeAll(drawnCards)
-        return Cards(drawnCards.toMutableList())
+    fun getInitialCards(): Cards {
+        val initialCards = allCards.take(2)
+        allCards.removeAll(initialCards)
+        return Cards(initialCards)
     }
 
     fun getCardsCount() = allCards.size
@@ -18,10 +20,4 @@ class Cards(val allCards: MutableList<Card>) : List<Card> by allCards {
     fun getCardScores() = allCards.map { card -> card.cardScore }
 
     fun aceCount() = allCards.count { card -> card.isAceCard() }
-
-    fun addCards(cards: Cards) {
-        cards.forEach { card ->
-            allCards.add(card)
-        }
-    }
 }
