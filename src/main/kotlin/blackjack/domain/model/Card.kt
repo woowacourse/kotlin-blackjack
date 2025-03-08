937c2d4346be8048c68957626667d7d8c3d19e5a
ijh1298 <imjh0417@gmail.com>
2025-03-08 14:57:43 +0900

    feat: 인덱스로 카드를 생성하는 생성자 제거, 카드 생성 유효성 검사 추가

--- src/main/kotlin/blackjack/domain/model/Card.kt
+++ src/main/kotlin/blackjack/domain/model/Card.kt
@@ -1,26 +1,21 @@
 package blackjack.domain.model
 
-import blackjack.domain.model.CardNumber.Companion.MAX_CARD_NUMBER
-import blackjack.domain.model.Suit.Companion.MAX_SUIT_NUMBER
-
 data class Card(
     val cardNumber: CardNumber,
     private val suit: Suit,
 ) {
-    constructor(cardIndex: Int) : this(
-        cardNumber = CardNumber((cardIndex) % MAX_CARD_NUMBER + 1),
-        suit = Suit((cardIndex) / MAX_CARD_NUMBER),
-    ) {
-        require(cardIndex in CARD_INDEX_RANGE) { ERROR_OUT_OF_CARD_INDEX }
+    init {
+        require(cardNumber.toValue() in MINIMUM_CARD_NUMBER..MAXIMUM_CARD_NUMBER) { ERROR_OUT_OF_CARD_NUMBER }
+        require(suit in Suit.entries) { ERROR_INVALID_SUIT }
     }
 
-    fun getCardText(): String = cardNumber.getCardNumberName() + suit.getSuitName()
+    fun getCardText(): String = cardNumber.getCardNumberName() + suit.name
 
     companion object {
-        private const val MIN_CARD_INDEX = 0
-        private const val MAX_CARD_INDEX = (MAX_CARD_NUMBER * (MAX_SUIT_NUMBER + 1)) - 1
-        val CARD_INDEX_RANGE = MIN_CARD_INDEX..MAX_CARD_INDEX
+        private const val MINIMUM_CARD_NUMBER = 1
+        private const val MAXIMUM_CARD_NUMBER = 13
 
-        private val ERROR_OUT_OF_CARD_INDEX = "카드 인덱스는 $CARD_INDEX_RANGE 에 속하는 값이어야 합니다"
+        private const val ERROR_OUT_OF_CARD_NUMBER = "카드 번호는 ${MINIMUM_CARD_NUMBER}에서 $MAXIMUM_CARD_NUMBER 사이여야 합니다."
+        private const val ERROR_INVALID_SUIT = "유효하지 않은 카드 문양입니다."
     }
 }
