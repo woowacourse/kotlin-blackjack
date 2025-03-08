16b0dd6b387a9104222c0067d434b06ffccbc699
ijh1298 <imjh0417@gmail.com>
2025-03-08 16:02:51 +0900

    feat: Rule 클래스 제거 및 HandCards 클래스를 추가해서 손패의 카드 관리, 점수 계산의 역할 부여

---
+++ src/main/kotlin/blackjack/domain/model/HandCards.kt
@@ -0,0 +1,37 @@
+package blackjack.domain.model
+
+class HandCards {
+    private val _cards = mutableListOf<Card>()
+    private val cards
+        get() = _cards.toList()
+
+    fun add(card: Card) {
+        _cards += card
+    }
+
+    fun show(): List<Card> {
+        return cards
+    }
+
+    fun isBurst(): Boolean {
+        return getScore() > BLACK_JACK_NUMBER
+    }
+
+    fun isLessOrSameThan(score: Int): Boolean {
+        return getScore() <= score
+    }
+
+    private fun getScore(): Int {
+        val cardValues: Int = _cards.sumOf { it.cardNumber.value }
+
+        if (_cards.any { it.isAce() } && cardValues + ACE_EXTRA_SCORE <= BLACK_JACK_NUMBER) {
+            return cardValues + ACE_EXTRA_SCORE
+        }
+        return cardValues
+    }
+
+    companion object {
+        private const val ACE_EXTRA_SCORE = 10
+        private const val BLACK_JACK_NUMBER = 21
+    }
+}
