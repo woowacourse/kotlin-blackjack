3a60eeca1519c8425c1005f4a94fa9d1b4e3a90c
ijh1298 <imjh0417@gmail.com>
2025-03-08 20:55:36 +0900

    refactor: 딜러의 승패 결과를 구하는 클래스 재정의 및 테스트 코드 추가

--- src/main/kotlin/blackjack/domain/model/GameResultRecord.kt
+++ src/main/kotlin/blackjack/domain/model/GameResultRecord.kt
@@ -1,55 +1,15 @@
 package blackjack.domain.model
 
-import blackjack.domain.model.card.Card
+import blackjack.domain.model.participant.Dealer
+import blackjack.domain.model.participant.Player
 
-class GameResultRecord {
-    private val dealerResults = mutableMapOf(GameResult.WIN to 0, GameResult.LOSE to 0, GameResult.DRAW to 0)
-
-    fun calculatePlayerWinLoss(
-        dealerCards: List<Card>,
-        playerCards: List<Card>,
-    ): GameResult {
-        val dealerScore = Rule.calculateResultByCards(dealerCards)
-        val playerScore = Rule.calculateResultByCards(playerCards)
-
-        if (playerScore > 21) {
-            dealerResults[GameResult.WIN] = (dealerResults[GameResult.WIN] ?: 0) + 1
-            return GameResult.LOSE
-        }
-
-        if (dealerScore in (playerScore + 1)..21) {
-            dealerResults[GameResult.WIN] = (dealerResults[GameResult.WIN] ?: 0) + 1
-            return GameResult.LOSE
-        }
-
-        if (dealerScore == playerScore) {
-            dealerResults[GameResult.DRAW] = (dealerResults[GameResult.DRAW] ?: 0) + 1
-            return GameResult.DRAW
-        }
-        dealerResults[GameResult.LOSE] = (dealerResults[GameResult.LOSE] ?: 0) + 1
-        return GameResult.WIN
-    }
-
-    fun getDealerWinLossText(): String {
-        val dealerWinText =
-            if (dealerResults[GameResult.WIN] != 0) {
-                dealerResults[GameResult.WIN].toString() + "승 "
-            } else {
-                ""
-            }
-        val dealerDrawText =
-            if (dealerResults[GameResult.DRAW] != 0) {
-                dealerResults[GameResult.DRAW].toString() + "무 "
-            } else {
-                ""
-            }
-        val dealerLossText =
-            if (dealerResults[GameResult.LOSE] != 0) {
-                dealerResults[GameResult.LOSE].toString() + "패 "
-            } else {
-                ""
-            }
-
-        return (dealerWinText + dealerDrawText + dealerLossText).trim()
+class GameResultRecord(
+    private val dealer: Dealer,
+    private val players: List<Player>,
+) {
+    fun getDealerResult(): Map<GameResult, Int> {
+        val dealerResult: List<GameResult> = players.map { player -> dealer.compareTo(player) }
+        val initResult = GameResult.entries.associateWith { 0 }
+        return initResult + dealerResult.groupingBy { it }.eachCount()
     }
 }
