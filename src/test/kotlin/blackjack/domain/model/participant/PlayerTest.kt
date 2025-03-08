98a12934d3826acc2ce870235234cf44fb9f3cf7
ijh1298 <imjh0417@gmail.com>
2025-03-08 17:45:39 +0900

    feat: 플레이어가 딜러와 손패를 비교하는 메서드 및 테스트 코드 추가

--- src/test/kotlin/blackjack/domain/model/participant/PlayerTest.kt
+++ src/test/kotlin/blackjack/domain/model/participant/PlayerTest.kt
@@ -1,5 +1,6 @@
 package blackjack.domain.model.participant
 
+import blackjack.domain.model.GameResult
 import blackjack.domain.model.card.Card
 import blackjack.domain.model.card.CardNumber
 import blackjack.domain.model.card.Suit
@@ -7,6 +8,20 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class PlayerTest {
+    @Test
+    fun `딜러와 비교해서 승패 결과를 가져올 수 있다`() {
+        val player = Player()
+        player.handCards.add(Card(CardNumber.KING, Suit.SPADE))
+        player.handCards.add(Card(CardNumber.ACE, Suit.HEART))
+
+        val dealer = Dealer()
+        dealer.handCards.add(Card(CardNumber.JACK, Suit.SPADE))
+        dealer.handCards.add(Card(CardNumber.EIGHT, Suit.CLUB))
+
+        val result = player.compareTo(dealer)
+        assertThat(result).isEqualTo(GameResult.WIN)
+    }
+
     @Test
     fun `플레이어가 드로우를 더 할 수 있는지 여부를 알 수 있다`() {
         val player = Player()
