486bfaf6ec24915bb41c5e4bbc19b2662944061f
ijh1298 <imjh0417@gmail.com>
2025-03-08 20:59:18 +0900

    feat: 컨트롤러 생성 시 ShuffledGenerator 주입

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,10 +1,13 @@
 import blackjack.controller.Casino
+import blackjack.domain.generator.ShuffledCardsGenerator
 import blackjack.view.InputView
 import blackjack.view.OutputView
 
 fun main() {
     val inputView = InputView()
     val outputView = OutputView()
-    val casino = Casino(inputView, outputView)
-    casino.gameStart()
+    val cardsGenerator = ShuffledCardsGenerator()
+    val casino = Casino(inputView, outputView, cardsGenerator)
+
+    casino.run()
 }
