c209ca095fa39a085163af6c675a23a354234dbc
chanho0908 <uni070@naver.com>
2025-03-08 17:47:35 +0900

    refactor : 게임 결과 상태 클래스 팩토리 함수 수정

--- src/main/kotlin/blackjack/domain/GameResult.kt
+++ src/main/kotlin/blackjack/domain/GameResult.kt
@@ -8,20 +8,17 @@ enum class GameResult {
     ;
 
     companion object {
-        fun from(
-            dealerSum: Int,
-            playerSum: Int,
-        ): GameResult =
-            if ((dealerSum > BUST_STANDARD) && (playerSum < BUST_STANDARD)) {
-                WIN
-            } else if (playerSum > BUST_STANDARD) {
-                LOSE
-            } else {
-                if (dealerSum > playerSum) {
-                    LOSE
-                } else {
-                    WIN
-                }
-            }
+        fun from(dealerSum: Int, playerSum: Int): GameResult {
+            if (isPlayerSumBust(playerSum)) return LOSE
+            if (isDealerSumBust(dealerSum)) return WIN
+            return compareScores(dealerSum, playerSum)
+        }
+
+        private fun isPlayerSumBust(playerSum: Int): Boolean = playerSum > BUST_STANDARD
+
+        private fun isDealerSumBust(dealerSum: Int): Boolean = dealerSum > BUST_STANDARD
+
+        private fun compareScores(dealerSum: Int, playerSum: Int): GameResult =
+            if (dealerSum > playerSum) LOSE else WIN
     }
 }
