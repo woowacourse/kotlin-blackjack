c2d610fc45c305ce46a0b7a2dcff4917d92c9e33
ijh1298 <imjh0417@gmail.com>
2025-03-08 15:27:15 +0900

    test: Card 클래스 수정으로 인해 사용하지 않는 테스트 제거, DeckTest 카드 뽑는 테스트 코드 수정

--- src/test/kotlin/blackjack/domain/model/CardTest.kt
+++ src/test/kotlin/blackjack/domain/model/CardTest.kt
@@ -1,63 +1,16 @@
 package blackjack.domain.model
 
-import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.assertDoesNotThrow
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.CsvSource
+import kotlin.test.Test
 
 class CardTest {
-    @ParameterizedTest
-    @CsvSource(
-        "1, 0",
-        "13, 3",
-    )
-    fun `유효한 카드 값을 받아 카드를 생성한다`(
-        rawCardNumber: Int,
-        rawSuit: Int,
-    ) {
+    @Test
+    fun `유효한 카드 값을 받아 카드를 생성한다`() {
         // given
-        val cardNumber = CardNumber(rawCardNumber)
-        val suit = Suit(rawSuit)
+        val cardNumber = CardNumber.ACE
+        val suit = Suit.SPADE
 
         // when then
         assertDoesNotThrow { Card(cardNumber, suit) }
-    }
-
-    @ParameterizedTest
-    @CsvSource("0,0,1", "13,1,1", "51,3,13")
-    fun `0부터 51까지의 카드 인덱스로 카드를 만들 수 있다`(
-        index: Int,
-        rawSuit: Int,
-        rawCardNumber: Int,
-    ) {
-        // given
-        val actualCard = Card(index)
-        val cardNumber = CardNumber(rawCardNumber)
-        val suit = Suit(rawSuit)
-
-        // when
-        val expectedCard = Card(cardNumber, suit)
-
-        // then
-        assertThat(actualCard).isEqualTo(expectedCard)
-    }
-
-    @ParameterizedTest
-    @CsvSource(
-        "0, A스페이드",
-        "13, A하트",
-        "32, 7다이아몬드",
-        "51, K클로버",
-    )
-    fun `카드의 정보를 문자열로 만들 수 있다`(
-        rawCardNumber: Int,
-        expectedCardText: String,
-    ) {
-        // given
-        val card = Card(rawCardNumber)
-        val actualCardText = card.getCardText()
-
-        // when then
-        assertThat(actualCardText).isEqualTo(expectedCardText)
     }
 }
--- src/test/kotlin/blackjack/domain/model/DeckTest.kt
+++ src/test/kotlin/blackjack/domain/model/DeckTest.kt
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.Test
 class DeckTest {
     @Test
     fun `카드를 뽑을 수 있다`() {
-        val card = Deck.pop()
+        val card: Card = Card(CardNumber.ACE, Suit.CLUB)
+
+        val deck = Deck { listOf(card) }.pop()
 
         assertThat(card).isInstanceOf(Card::class.java)
     }
