877f724ba16131cd25f67d5c632baea4567cdc7d
박지민 <pjm5489@naver.com>
2025-03-09 16:29:15 +0900

    feat: 상수 정의

--- src/main/kotlin/model/Dealer.kt
+++ src/main/kotlin/model/Dealer.kt
@@ -14,7 +14,7 @@ class Dealer(dealerCards: Cards) : Participant(dealerCards) {
     }
 
     fun drawCount(drawnCard: Card): Int {
-        var drawCount = 0
+        var drawCount = DEFAULT_DRAW_COUNT
         while (canHit()) {
             if (turn(drawnCard)) drawCount++
         }
@@ -24,6 +24,7 @@ class Dealer(dealerCards: Cards) : Participant(dealerCards) {
     override fun canHit(): Boolean = currentScore() <= DEALER_HIT_LIMIT
 
     companion object {
+        private const val DEFAULT_DRAW_COUNT = 0
         private const val DEALER_INITIAL_CARD_ERROR_MESSAGE = "[ERROR] 딜러는 2장의 카드를 가져야합니다."
         private const val DEALER_HIT_LIMIT = 16
     }
