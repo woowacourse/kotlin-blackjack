6d46d10d273cea52bc7d0483d292604ce9f472ed
chanho0908 <uni070@naver.com>
2025-03-09 21:06:02 +0900

    test : 딜러가 최소 점수까지 카드를 뽑는 테스트 추가

--- src/test/kotlin/blackjack/domain/BlackJackGameTest.kt
+++ src/test/kotlin/blackjack/domain/BlackJackGameTest.kt
@@ -1,14 +1,19 @@
 package blackjack.domain
 
 import blackjack.domain.card.CardFactoryImpl
+import blackjack.domain.card.FakeCardFactory
 import blackjack.domain.card.Shape
 import blackjack.domain.card.Tier
 import blackjack.domain.card.TrumpCard
 import blackjack.domain.participant.Participants
 import blackjack.fixture.participantsFixture
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 
 class BlackJackGameTest {
     private lateinit var game: BlackJackGame
@@ -50,5 +55,42 @@ class BlackJackGameTest {
             onPlayerStateUpdated = {},
         )
         assertThat(participants.players.first().cards.size()).isEqualTo(0)
+    }
+
+    @MethodSource("dealerCardDrawTestSet")
+    @ParameterizedTest
+    fun `딜러는 카드 점수의 최소합이 16이될 때 까지 카드를 뽑는다`(cards: List<TrumpCard>) {
+        val deck = Deck(FakeCardFactory(cards))
+        val game = BlackJackGame(participants, deck)
+
+        game.handOutInitializedCards()
+        val result = game.processDealerTurn()
+
+        assertEquals(result, 2)
+    }
+
+    companion object {
+        @JvmStatic
+        fun dealerCardDrawTestSet() =
+            listOf(
+                Arguments.of(
+                    listOf(
+                        // 플레이어 카드
+                        TrumpCard(Tier.KING, Shape.HEART),
+                        TrumpCard(Tier.JACK, Shape.HEART),
+                        TrumpCard(Tier.TEN, Shape.DIA),
+                        TrumpCard(Tier.NINE, Shape.DIA),
+                        // 딜러 초기 카드 1
+                        TrumpCard(Tier.TWO, Shape.DIA),
+                        // 딜러 초기 카드 2
+                        TrumpCard(Tier.THREE, Shape.DIA),
+                        // 추가 카드 1 (점수 합이 16 미만일 경우)
+                        TrumpCard(Tier.SIX, Shape.DIA),
+                        // 추가 카드 2 (점수 합이 16 미만일 경우)
+                        TrumpCard(Tier.SEVEN, Shape.DIA),
+                        TrumpCard(Tier.SEVEN, Shape.HEART),
+                    ),
+                ),
+            )
     }
 }
