836b5558353412c058210be57fcb5a31680b00eb
yrsel <xxknu88@gmail.com>
2025-03-10 00:59:29 +0900

    refactor: 최종 결과를 위한 점수 계산 로직 구현

--- src/main/kotlin/blackjack/model/ResultType.kt
+++ src/main/kotlin/blackjack/model/ResultType.kt
@@ -10,13 +10,11 @@ enum class ResultType(val value: Char) {
             dealer: Dealer,
             player: Player,
         ): ResultType {
-            val dealerFinalScore = if (dealer.isBust()) 0 else dealer.calculateTotalScore()
-            val playerFinalScore = if (player.isBust()) 0 else player.calculateTotalScore()
+            val dealerFinalScore = ScoreCalculator.calculateFinalScore(dealer.cards)
+            val playerFinalScore = ScoreCalculator.calculateFinalScore(player.cards)
             if (dealerFinalScore < playerFinalScore) return WIN
             if (dealerFinalScore == playerFinalScore) return TIE
             return LOSS
         }
-
-        const val BUST_NUMBER = 21
     }
 }
--- src/main/kotlin/blackjack/model/ScoreCalculator.kt
+++ src/main/kotlin/blackjack/model/ScoreCalculator.kt
@@ -1,11 +1,12 @@
 package blackjack.model
 
-import blackjack.model.ResultType.Companion.BUST_NUMBER
 import blackjack.model.card.Card
 import blackjack.model.card.Number
 
 object ScoreCalculator {
     private const val ADJUST_ACE_NUMBER = 10
+    const val BUST_NUMBER = 21
+    private const val ALTERNATIVE_NUMBER_FOR_BUSTED_NUMBER = 0
 
     fun sum(cards: List<Card>) = cards.sumOf { card -> card.number.score }
 
@@ -16,6 +17,12 @@ object ScoreCalculator {
             if (totalScore > BUST_NUMBER) totalScore -= ADJUST_ACE_NUMBER
         }
         return totalScore
+    }
+
+    fun calculateFinalScore(cards: List<Card>): Int {
+        val sum = calculateOptimalSum(cards)
+        if (sum > BUST_NUMBER) return ALTERNATIVE_NUMBER_FOR_BUSTED_NUMBER
+        return sum
     }
 
     private fun countNumber(
