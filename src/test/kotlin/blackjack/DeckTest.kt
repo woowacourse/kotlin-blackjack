a190432b80f5326e95b85ef72c6886af8ed87527
yrsel <xxknu88@gmail.com>
2025-03-09 23:59:46 +0900

    test: 덱 구조변경에 따른 테스트 코드 수정

--- src/test/kotlin/blackjack/DeckTest.kt
+++ src/test/kotlin/blackjack/DeckTest.kt
@@ -1,16 +1,18 @@
 package blackjack
 
-import blackjack.model.Deck
+import blackjack.model.card.Deck
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
-import org.junit.jupiter.api.assertThrows
 
 class DeckTest {
     @Test
-    fun `덱에서 카드를 뽑을 수 없다면 예외를 발생시킨다`() {
-        val totalCardCount = 52
+    fun `덱에 존재하는 52장 카드를 모두 사용하면 새로운 카드 뭉치의 카드를 사용한다`() {
+        val deck = Deck.create()
 
-        assertDoesNotThrow { Deck.drawWithCount(totalCardCount) }
-        assertThrows<IllegalStateException> { Deck.draw() }
+        assertDoesNotThrow {
+            repeat(53) {
+                deck.draw()
+            }
+        }
     }
 }
