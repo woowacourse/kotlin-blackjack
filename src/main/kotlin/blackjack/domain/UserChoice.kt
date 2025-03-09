fe6542638c2a2448875cc176cd73a542a40f6c03
chanho0908 <uni070@naver.com>
2025-03-09 18:45:00 +0900

    refactor : 유저 카드 추가 선택 상태 클래스 캐싱 제거

--- src/main/kotlin/blackjack/domain/UserChoice.kt
+++ src/main/kotlin/blackjack/domain/UserChoice.kt
@@ -10,15 +10,11 @@ enum class UserChoice {
         private const val INPUT_STAY = "n"
         private const val INVALID_VALUE = "잘못된 값을 입력하였습니다."
 
-        private val CHOICE = mutableMapOf<String, UserChoice>()
-
         fun from(value: String): UserChoice =
-            CHOICE.computeIfAbsent(value) {
-                when (value) {
-                    INPUT_HIT -> HIT
-                    INPUT_STAY -> STAY
-                    else -> throw IllegalArgumentException(INVALID_VALUE)
-                }
+            when (value) {
+                INPUT_HIT -> HIT
+                INPUT_STAY -> STAY
+                else -> throw IllegalArgumentException(INVALID_VALUE)
             }
     }
 }
