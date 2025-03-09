dc4eb6e44f3efdc798651c0860a90a8f9a5d3610
chanho0908 <uni070@naver.com>
2025-03-09 13:46:33 +0900

    feat : 플레이어 인원 제한 추가

--- src/main/kotlin/blackjack/domain/participant/Participants.kt
+++ src/main/kotlin/blackjack/domain/participant/Participants.kt
@@ -7,12 +7,20 @@ class Participants(
         require(players.size == players.map { it.name }.distinct().size) {
             ERROR_DUPLICATED_PLAYERS
         }
+
+        require(players.size in MIN_PARTICIPANTS..MAX_PARTICIPANTS) {
+            INVALID_PLAYERS_SIZE
+        }
     }
 
     val dealer: Dealer get() = value.filterIsInstance<Dealer>().first()
     val players: List<Player> get() = value.filterIsInstance<Player>()
 
     companion object {
-        const val ERROR_DUPLICATED_PLAYERS = "플레이어 이름은 중복될 수 없습니다."
+        private const val MIN_PARTICIPANTS = 2
+        private const val MAX_PARTICIPANTS = 8
+
+        private const val INVALID_PLAYERS_SIZE = "플레이어는 최소 2명부터 최대 8명입니다."
+        private const val ERROR_DUPLICATED_PLAYERS = "플레이어 이름은 중복될 수 없습니다."
     }
 }
--- src/test/kotlin/blackjack/domain/ParticipantsTest.kt
+++ src/test/kotlin/blackjack/domain/ParticipantsTest.kt
@@ -4,6 +4,9 @@ import blackjack.domain.participant.Participants
 import blackjack.domain.participant.Player
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 
 class ParticipantsTest {
     @Test
@@ -14,5 +17,32 @@ class ParticipantsTest {
         ) {
             Participants(players)
         }
+    }
+
+    @MethodSource("invalidPlayerCounts")
+    @ParameterizedTest
+    fun `플레이어는 최소 2명이다`() {
+        val players = listOf(Player("peto"))
+
+        assertThrows<IllegalArgumentException>(
+            message = "플레이어는 최소 2명부터 최대 8명입니다.",
+        ) {
+            Participants(players)
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun invalidPlayerCounts() =
+            listOf(
+                Arguments.of(Player("peto")),
+                Arguments.of(
+                    listOf(
+                        Player("peto"), Player("bibi"), Player("hwano"), Player("por"),
+                        Player("kream"), Player("eden"), Player("tama"), Player("jerry"),
+                        Player("Lea"),
+                    ),
+                ),
+            )
     }
 }
