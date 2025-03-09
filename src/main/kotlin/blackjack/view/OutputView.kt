ed36eb7b136f26cd2da14e56f902044e15837e4e
DoableTuple <doabletuple@gmail.com>
2025-03-09 21:43:23 +0900

    refactor: 카드를 문자열로 표현하는 로직을 View로 이동

--- src/main/kotlin/blackjack/view/OutputView.kt
+++ src/main/kotlin/blackjack/view/OutputView.kt
@@ -1,10 +1,12 @@
 package blackjack.view
 
 import blackjack.domain.model.Dealer
-import blackjack.domain.model.Deck.Companion.START_CARD_COUNT
+import blackjack.domain.model.Deck
 import blackjack.domain.model.Participant
 import blackjack.domain.model.Player
+import blackjack.domain.model.Rank
 import blackjack.domain.model.Result
+import blackjack.domain.model.Suit
 
 class OutputView {
     fun printInitialDeals(
@@ -15,7 +17,7 @@ class OutputView {
             MESSAGE_INITIAL_HAND_DISTRIBUTED.format(
                 dealer.name,
                 players.map(Player::name).joinToString(PLAYER_CARDS_DELIMITER),
-                START_CARD_COUNT,
+                Deck.INITIAL_DRAW_COUNT,
             ),
         )
     }
@@ -42,13 +44,13 @@ class OutputView {
     private fun renderDealerStatus(dealer: Dealer): String {
         return dealer.name + PLAYER_NAME_STATUS_DELIMITER +
             dealer.showHand(DEALER_VISIBLE_CARD_COUNT)
-                .joinToString { it.rank.value + it.suit.value }
+                .joinToString { it.rank.stringRepresentation() + it.suit.stringRepresentation() }
     }
 
     private fun renderParticipantStatus(participant: Participant): String {
         return participant.name + PLAYER_NAME_STATUS_DELIMITER +
             participant.showHand()
-                .joinToString { it.rank.value + it.suit.value }
+                .joinToString { it.rank.stringRepresentation() + it.suit.stringRepresentation() }
     }
 
     fun printDealerHitsState() {
@@ -65,7 +67,7 @@ class OutputView {
     ) {
         print(dealer.name + NAME_RESULT_DELIMITER)
         results.filter { it.value > 0 }.forEach { (result, count) ->
-            print("${count}${resultToString(result)} ")
+            print("${count}${result.stringRepresentation()} ")
         }
         println()
     }
@@ -74,22 +76,41 @@ class OutputView {
         player: Player,
         result: Result,
     ) {
-        println(player.name + NAME_RESULT_DELIMITER + resultToString(result))
+        println(player.name + NAME_RESULT_DELIMITER + result.stringRepresentation())
     }
 
     fun printErrorMessage(message: String) {
         println(message)
     }
 
-    private fun resultToString(result: Result): String {
-        return when (result) {
+    private fun Result.stringRepresentation(): String {
+        return when (this) {
             Result.WIN -> RESULT_WIN
             Result.LOSE -> RESULT_LOSE
             Result.DRAW -> RESULT_DRAW
         }
     }
 
-    companion object {
+    private fun Suit.stringRepresentation(): String {
+        return when (this) {
+            Suit.HEART -> SUIT_HEART
+            Suit.DIAMOND -> SUIT_DIAMOND
+            Suit.CLUB -> SUIT_CLUB
+            Suit.SPADE -> SUIT_SPADE
+        }
+    }
+
+    private fun Rank.stringRepresentation(): String {
+        return when (this) {
+            Rank.ACE -> RANK_ACE
+            Rank.JACK -> RANK_JACK
+            Rank.QUEEN -> RANK_QUEEN
+            Rank.KING -> RANK_KING
+            else -> this.point.toString()
+        }
+    }
+
+    private companion object {
         private const val MESSAGE_INITIAL_HAND_DISTRIBUTED = "%s와(과) %s에게 %s장의 카드를 나누었습니다."
         private const val MESSAGE_DEALER_HITS_STATE = "딜러는 16이하라 한장의 카드를 더 받았습니다."
         private const val MESSAGE_RESULTS_HEADER = "## 최종 승패"
@@ -98,8 +119,19 @@ class OutputView {
         private const val PLAYER_NAME_STATUS_DELIMITER = " 카드: "
         private const val PLAYER_RESULT_DELIMITER = " - 결과: "
         private const val NAME_RESULT_DELIMITER = ": "
+
         private const val RESULT_WIN = "승"
         private const val RESULT_LOSE = "패"
         private const val RESULT_DRAW = "무"
+
+        private const val SUIT_HEART = "하트"
+        private const val SUIT_DIAMOND = "다이아몬드"
+        private const val SUIT_SPADE = "스페이드"
+        private const val SUIT_CLUB = "클로버"
+
+        private const val RANK_ACE = "A"
+        private const val RANK_JACK = "J"
+        private const val RANK_QUEEN = "Q"
+        private const val RANK_KING = "K"
     }
 }
