028375b199e56bdf8d963504e9e5c4f6db9a1974
cucumber99 <dlsguq1@gmail.com>
2025-03-11 01:42:11 +0900

    refactor: 입력 로직 개선

--- src/main/kotlin/blackjack/view/InputView.kt
+++ src/main/kotlin/blackjack/view/InputView.kt
@@ -2,21 +2,24 @@ package blackjack.view
 
 class InputView {
     fun getNames(): List<String> {
-        val input = readlnOrNull() ?: ""
-        validateInput(input)
-        return input.split(DELIMITER).map { it.trim() }
+        println(ENTER_PLAYER_NAMES_MESSAGE)
+        return readUserInput().split(DELIMITER).map { it.trim() }
     }
 
-    fun getFlag(): Boolean {
-        val input = readlnOrNull() ?: ""
-        validateInput(input)
-        require(input == YES || input == NO) { INVALID_FLAG_MESSAGE }
-        return input == YES
+    fun getFlag(name: String): Boolean {
+        println(ASK_DRAW_CARD_MESSAGE.format(name))
+        return readUserInput().toBooleanFlag()
     }
 
-    private fun validateInput(input: String) {
-        require(input.isNotBlank()) {
-            INVALID_INPUT_MESSAGE
+    private fun readUserInput(): String {
+        return readlnOrNull()?.takeIf { it.isNotBlank() } ?: throw IllegalArgumentException(INVALID_INPUT_MESSAGE)
+    }
+
+    private fun String.toBooleanFlag(): Boolean {
+        return when (this) {
+            YES -> true
+            NO -> false
+            else -> throw IllegalArgumentException(INVALID_FLAG_MESSAGE)
         }
     }
 
@@ -24,7 +27,9 @@ class InputView {
         private const val DELIMITER = ","
         private const val YES = "y"
         private const val NO = "n"
-        private const val INVALID_FLAG_MESSAGE = "$YES 혹은 ${NO}을 입력해주세요."
+        private const val INVALID_FLAG_MESSAGE = "$YES 혹은 $NO 을 입력해주세요."
         private const val INVALID_INPUT_MESSAGE = "입력이 비어있습니다."
+        private const val ENTER_PLAYER_NAMES_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)"
+        private const val ASK_DRAW_CARD_MESSAGE = "%s는 한장의 카드를 더 받겠습니까? (예는 y, 아니오는 n)"
     }
 }
