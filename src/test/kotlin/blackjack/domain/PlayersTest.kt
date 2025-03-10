02a3d72545916ef050b55c04a1fb50a23c3fccc4
Leeyerin0210 <lyl8661@naver.com>
2025-03-10 14:48:16 +0900

    fix : 테스트 수정

--- src/test/kotlin/blackjack/domain/PlayersTest.kt
+++ src/test/kotlin/blackjack/domain/PlayersTest.kt
@@ -26,15 +26,15 @@ class PlayersTest {
         // given
         val playerName = "시아"
         val playerCards = listOf(
-            Card.getCard(TWO, CLUB),
-            Card.getCard(NINE, CLUB)
+            Card(TWO, CLUB),
+            Card(NINE, CLUB)
         )
         val player = Player(playerName, playerCards)
         val players = Players(listOf(player))
 
         val dealerCards = listOf(
-            Card.getCard(TWO, DIAMOND),
-            Card.getCard(NINE, DIAMOND)
+            Card(TWO, DIAMOND),
+            Card(NINE, DIAMOND)
         )
         val dealer = Dealer(dealerCards)
 
@@ -50,15 +50,15 @@ class PlayersTest {
     fun `플레이어의 점수가 딜러의 점수보다 높으면 우승을 반환한다`() {
         val playerName = "시아"
         val playerCards = listOf(
-            Card.getCard(ACE, CLUB),
-            Card.getCard(KING, CLUB)
+            Card(ACE, CLUB),
+            Card(KING, CLUB)
         )
         val player = Player(playerName, playerCards)
         val players = Players(listOf(player))
 
         val dealerCards = listOf(
-            Card.getCard(TWO, DIAMOND),
-            Card.getCard(NINE, DIAMOND)
+            Card(TWO, DIAMOND),
+            Card(NINE, DIAMOND)
         )
         val dealer = Dealer(dealerCards)
 
@@ -75,15 +75,15 @@ class PlayersTest {
 
         val playerName = "시아"
         val playerCards = listOf(
-            Card.getCard(TWO, CLUB),
-            Card.getCard(NINE, CLUB)
+            Card(TWO, CLUB),
+            Card(NINE, CLUB)
         )
         val player = Player(playerName, playerCards)
         val players = Players(listOf(player))
 
         val dealerCards = listOf(
-            Card.getCard(KING, DIAMOND),
-            Card.getCard(QUEEN, DIAMOND)
+            Card(KING, DIAMOND),
+            Card(QUEEN, DIAMOND)
         )
         val dealer = Dealer(dealerCards)
 
