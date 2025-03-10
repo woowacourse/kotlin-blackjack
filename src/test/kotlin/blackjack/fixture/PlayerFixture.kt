8e0a348435bf36e6174880ce6ae81537ee168ef5
rosemin928 <121722789+rosemin928@users.noreply.github.com>
2025-03-10 23:20:45 +0900

    refactor: Player 수정에 따른 fixture 수정

--- src/test/kotlin/blackjack/fixture/PlayerFixture.kt
+++ src/test/kotlin/blackjack/fixture/PlayerFixture.kt
@@ -1,6 +1,7 @@
 package blackjack.fixture
 
+import blackjack.domain.ParticipantCards
 import blackjack.domain.participant.Dealer
 import blackjack.domain.participant.Player
 
-fun playersFixture() = listOf(Player("peto"), Player("bibi")) + Dealer()
+fun playersFixture() = listOf(Player("peto", ParticipantCards()), Player("bibi", ParticipantCards())) + Dealer(ParticipantCards())
