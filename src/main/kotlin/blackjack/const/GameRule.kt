335e4c474a7cf2ad15329cdf6e35808c5b1ab047
cucumber99 <dlsguq1@gmail.com>
2025-03-11 01:40:54 +0900

    feat: 상수 추가

--- src/main/kotlin/blackjack/const/GameRule.kt
+++ src/main/kotlin/blackjack/const/GameRule.kt
@@ -9,4 +9,14 @@ object GameRule {
     const val BLACKJACK_SCORE = 21
 
     const val DEALER_ADDITIONAL_DRAW_BASE_SCORE = 16
+
+    const val ACE = "A"
+    const val JACK = "J"
+    const val QUEEN = "Q"
+    const val KING = "K"
+
+    const val HEART = "하트"
+    const val SPADE = "스페이드"
+    const val DIAMOND = "다이아몬드"
+    const val CLOVER = "클로버"
 }
