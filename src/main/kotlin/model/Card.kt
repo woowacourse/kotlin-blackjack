8f079c09ab575e3d386433dabff944fa647130a9
박지민 <pjm5489@naver.com>
2025-03-10 22:42:51 +0900

    refactor: 함수에서 필드로 변경

--- src/main/kotlin/model/Card.kt
+++ src/main/kotlin/model/Card.kt
@@ -3,6 +3,5 @@ package model
 data class Card(private val cardRank: CardRank, private val shape: Shape) {
     val cardScore: Int = cardRank.score
     val cardName: Pair<String, String> = cardRank.name to shape.name
-
-    fun isAceCard(): Boolean = cardRank == CardRank.ACE
+    val isAceCard: Boolean = cardRank == CardRank.ACE
 }
