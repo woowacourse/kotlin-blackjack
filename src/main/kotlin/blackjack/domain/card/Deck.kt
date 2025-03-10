c69d087c0b532610717c938375a9312f47cfef3d
cucumber99 <dlsguq1@gmail.com>
2025-03-11 00:37:05 +0900

    refactor: Deck 객체 리팩터링
    - 생성자로 카드 목록을 받습니다.
    - 카드 개수에 따른 유효성 검증을 하지 않습니다.

--- src/main/kotlin/blackjack/domain/card/Deck.kt
+++ src/main/kotlin/blackjack/domain/card/Deck.kt
@@ -1,12 +1,11 @@
 package blackjack.domain.card
 
-class Deck {
-    private val _cards = mutableListOf<Card>()
-    val cards: List<Card> get() = _cards.toList()
+class Deck(private val _cards: MutableList<Card> = mutableListOf()) {
+    val cards: List<Card>
+        get() = _cards.toList()
 
     init {
-        _cards.addAll(generateDeck())
-        require(cards.size == DECK_SIZE) { INVALID_DECK_SIZE_ERROR_MESSAGE }
+        if (_cards.isEmpty()) _cards.addAll(generateDeck())
     }
 
     fun draw(): Card {
@@ -21,8 +20,6 @@ class Deck {
     }
 
     companion object {
-        private const val DECK_SIZE = 52
-        private const val INVALID_DECK_SIZE_ERROR_MESSAGE = "덱은 52장의 카드로 구성되어야 합니다."
         private const val NO_SUCH_ELEMENT_ERROR_MESSAGE = "남은 카드가 없습니다."
     }
 }
--- src/test/kotlin/blackjack/domain/card/DeckTest.kt
+++ src/test/kotlin/blackjack/domain/card/DeckTest.kt
@@ -6,25 +6,28 @@ import org.junit.jupiter.api.Test
 
 class DeckTest {
     @Test
-    fun `고유한 52장의 카드를 가지고 있어야 한다`() {
-        val deck = Deck()
+    fun `카드를 한 장 뽑는다`() {
+        // Given
+        val cards = MutableList(3) { Card.create(CardNumber.ACE, CardPattern.HEART) }
+        val deck = Deck(cards)
 
-        deck.cards.size shouldBe 52
-    }
-
-    @Test
-    fun `덱에서 카드를 뽑을 수 있다`() {
-        val deck = Deck()
-
+        // When
         deck.draw()
-        deck.cards.size shouldBe 51
+
+        // Then
+        deck.cards.size shouldBe 2
     }
 
     @Test
-    fun `덱에 남은 카드가 없을 때 드로우를 할 수 없다`() {
-        shouldThrowExactly<IllegalArgumentException> {
-            val deck = Deck()
-            repeat(53) { deck.draw() }
-        }
+    fun `카드가 존재하지 않을 경우 카드를 뽑을 수 없다`() {
+        // Given
+        val cards = mutableListOf(Card.create(CardNumber.ACE, CardPattern.HEART))
+        val deck = Deck(cards)
+
+        // When
+        deck.draw()
+
+        // Then
+        shouldThrowExactly<IllegalArgumentException> { deck.draw() }
     }
 }
