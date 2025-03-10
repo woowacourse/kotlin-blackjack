6f1d3433b14a2c171836c5bbca4bc9850aec4c62
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-03-10 15:29:32 +0900

    test: refactor CharacterTests

--- src/test/kotlin/blackjack/domain/CharacterTest.kt
+++ src/test/kotlin/blackjack/domain/CharacterTest.kt
@@ -6,24 +6,24 @@ import org.junit.jupiter.api.Test
 class CharacterTest {
     @Test
     fun `캐릭터 카드는 Jack, Queen, King이 존재한다`() {
-        assertThat(Face.entries).hasSameElementsAs(listOf(Face.JACK, Face.QUEEN, Face.KING))
+        assertThat(Face.entries).hasSameElementsAs(setOf(Face.JACK, Face.QUEEN, Face.KING))
     }
 
     @Test
     fun `캐릭터 카드 Jack은 숫자 10으로 계산한다`() {
         val faceCard = Face.JACK
-        assertThat(faceCard.possibleValues).contains(10)
+        assertThat(faceCard.possibleValues).hasSameElementsAs(setOf(10))
     }
 
     @Test
     fun `캐릭터 카드 Queen은 숫자 10으로 계산한다`() {
         val faceCard = Face.QUEEN
-        assertThat(faceCard.possibleValues).contains(10)
+        assertThat(faceCard.possibleValues).hasSameElementsAs(setOf(10))
     }
 
     @Test
     fun `캐릭터 카드 King은 숫자 10으로 계산한다`() {
         val faceCard = Face.KING
-        assertThat(faceCard.possibleValues).contains(10)
+        assertThat(faceCard.possibleValues).hasSameElementsAs(setOf(10))
     }
 }
