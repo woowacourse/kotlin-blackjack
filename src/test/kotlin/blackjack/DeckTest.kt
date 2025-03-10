be642353b2ea7d47ff4ef8baad434028c33830f6
HamBeomJoon <hbj0802@naver.com>
2025-03-10 16:39:03 +0900

    test: Deck 테스트 코드 수정

--- src/test/kotlin/blackjack/DeckTest.kt
+++ src/test/kotlin/blackjack/DeckTest.kt
@@ -1,16 +1,27 @@
 package blackjack
 
 import blackjack.model.Deck
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 
 class DeckTest {
+    private val deck = Deck()
+
+    @Test
+    fun `덱 하나는 52장의 카드를 가진다`() {
+        assertThat(deck.cards.size).isEqualTo(CARD_DECK_SIZE)
+    }
+
     @Test
     fun `덱에서 카드를 뽑을 수 없다면 예외를 발생시킨다`() {
-        val totalCardCount = 52
+        repeat(CARD_DECK_SIZE) {
+            deck.draw()
+        }
+        assertThrows<IllegalStateException> { deck.draw() }
+    }
 
-        assertDoesNotThrow { Deck.drawWithCount(totalCardCount) }
-        assertThrows<IllegalStateException> { Deck.draw() }
+    companion object {
+        const val CARD_DECK_SIZE = 52
     }
 }
