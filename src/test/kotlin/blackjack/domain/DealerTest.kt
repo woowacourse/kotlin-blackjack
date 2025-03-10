301f3ba07e848aa88120bed1a2fe5e6edfeedbcb
junseo511 <prime54@g.skku.edu>
2025-03-10 14:28:50 +0900

    refactor: 중복되는 변수 분리

--- src/test/kotlin/blackjack/domain/DealerTest.kt
+++ src/test/kotlin/blackjack/domain/DealerTest.kt
@@ -4,16 +4,22 @@ import blackjack.model.CardDeck
 import blackjack.model.Dealer
 import blackjack.model.ScoreCalculator
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class DealerTest {
+    private lateinit var cardDeck: CardDeck
+    private lateinit var dealer: Dealer
+
+    @BeforeEach
+    fun setup() {
+        cardDeck = CardDeck()
+        dealer = Dealer(ScoreCalculator())
+    }
+
     @Test
     fun `딜러가 카드를 다 뽑고나면 점수는 16점을 초과하거나 버스트이다`() {
-        // given
-        val cardDeck = CardDeck()
-        val dealer = Dealer(ScoreCalculator())
-
-        // when
+        // given & when
         dealer.drawUntilFinished(cardDeck)
 
         // then
@@ -23,8 +29,6 @@ class DealerTest {
     @Test
     fun `딜러 점수가 16 이전까지 뽑은 카드의 장수를 반환한다`() {
         // given
-        val cardDeck = CardDeck()
-        val dealer = Dealer(ScoreCalculator())
         val initialDrawCount = 2
         dealer.draw(cardDeck)
 
--- src/test/kotlin/blackjack/domain/ScoreCalculatorTest.kt
+++ src/test/kotlin/blackjack/domain/ScoreCalculatorTest.kt
@@ -10,14 +10,21 @@ import blackjack.model.CardRank.TWO
 import blackjack.model.CardSuit.CLUB
 import blackjack.model.ScoreCalculator
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class ScoreCalculatorTest {
+    private lateinit var scoreCalculator: ScoreCalculator
+
+    @BeforeEach
+    fun setup() {
+        scoreCalculator = ScoreCalculator()
+    }
+
     @Test
     fun `핸드에 2클로버 한 장을 가지고 있으면 2점을 반환한다`() {
         // given
         val expectedScore = 2
-        val scoreCalculator = ScoreCalculator()
         val card = Card(TWO, CLUB)
 
         // when
@@ -31,7 +38,6 @@ class ScoreCalculatorTest {
     fun `핸드에 에이스를 포함한 값이 21이 넘으면 에이스를 1로 바꾼다`() {
         // given
         val expectedScore = 2
-        val scoreCalculator = ScoreCalculator()
         val card = Card(TWO, CLUB)
 
         // when
@@ -44,7 +50,6 @@ class ScoreCalculatorTest {
     @Test
     fun `21점이 초과하면 Bust를 반환한다`() {
         // given
-        val scoreCalculator = ScoreCalculator()
         val card1 = Card(QUEEN, CLUB)
         val card2 = Card(KING, CLUB)
         val card3 = Card(JACK, CLUB)
@@ -57,7 +62,6 @@ class ScoreCalculatorTest {
     @Test
     fun `ACE 1장, Q 1장, 9 1장을 가지고 있으면 20점을 반환한다`() {
         // given
-        val scoreCalculator = ScoreCalculator()
         val card1 = Card(ACE, CLUB)
         val card2 = Card(QUEEN, CLUB)
         val card3 = Card(NINE, CLUB)
