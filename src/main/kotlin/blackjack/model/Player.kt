b91a70bb71a69f0e8cb281c26fa96b49607b1013
HamBeomJoon <hbj0802@naver.com>
2025-03-10 15:41:50 +0900

    refactor: 점수 조정 역할 ResultCalculator로 이동

--- src/main/kotlin/blackjack/model/Player.kt
+++ src/main/kotlin/blackjack/model/Player.kt
@@ -1,26 +1,10 @@
 package blackjack.model
 
-import blackjack.model.ResultCalculator.Companion.BUST_NUMBER
+import blackjack.model.ResultCalculator.BUST_NUMBER
+import blackjack.model.ResultCalculator.adjustScore
 
 class Player(
     name: String,
 ) : Person(name) {
-    fun adjustScore(): Int {
-        var sumScore = calculateTotalScore()
-        var countAce = countAce()
-        while (countAce-- > 0) {
-            if (sumScore > BUST_NUMBER) {
-                sumScore -= ADJUST_ACE_NUMBER
-            }
-        }
-        return sumScore
-    }
-
-    private fun countAce() = cards.count { it.number == Number.ACE }
-
-    override fun isBust() = adjustScore() > BUST_NUMBER
-
-    companion object {
-        private const val ADJUST_ACE_NUMBER = 10
-    }
+    override fun isBust() = adjustScore(cards) > BUST_NUMBER
 }
--- src/main/kotlin/blackjack/model/ResultCalculator.kt
+++ src/main/kotlin/blackjack/model/ResultCalculator.kt
@@ -4,7 +4,21 @@ import blackjack.model.ResultType.LOSS
 import blackjack.model.ResultType.TIE
 import blackjack.model.ResultType.WIN
 
-class ResultCalculator {
+object ResultCalculator {
+    const val BUST_NUMBER = 21
+    private const val ADJUST_ACE_NUMBER = 10
+
+    fun adjustScore(cards: List<Card>): Int {
+        var sumScore = calculateTotalScore(cards)
+        var countAce = countAce(cards)
+        while (countAce-- > 0) {
+            if (sumScore > BUST_NUMBER) {
+                sumScore -= ADJUST_ACE_NUMBER
+            }
+        }
+        return sumScore
+    }
+
     fun judgeScore(
         dealer: Dealer,
         player: Player,
