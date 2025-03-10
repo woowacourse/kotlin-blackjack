49db58a96824e019afe7638ef79a596917f50414
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-03-10 22:55:57 +0900

    test: test startPlayerTurn()

--- src/test/kotlin/blackjack/domain/DealerTest.kt
+++ src/test/kotlin/blackjack/domain/DealerTest.kt
@@ -22,4 +22,14 @@ class DealerTest {
         dealer.giveCard(player)
         assertThat(player.cards.size).isEqualTo(1)
     }
+
+    @Test
+    fun `플레이어는 모든 카드의 합이 21 미만이 될 수 있을 경우 계속해서 카드를 뽑을 수 있다`() {
+        val player = Player("Player")
+        val dealer = Dealer(listOf(player), FakeShuffler)
+        dealer.startPlayerTurn {
+            player.draw(Card(Rank.AceRank, Suit.DIAMOND))
+        }
+        assertThat(player.score).isEqualTo(11)
+    }
 }
