ad47a3c2fbf5d1705724df67ced7434d3bcd72d9
ijh1298 <imjh0417@gmail.com>
2025-03-10 15:29:25 +0900

    test: HandTest에 팩토리 메서드로 생성하도록 테스트를 수정 및 점수 계산 테스트 추가

---
+++ src/test/kotlin/blackjack/domain/model/card/HandTest.kt
@@ -0,0 +1,92 @@
+package blackjack.domain.model.card
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class HandTest {
+    @Test
+    fun `손패에 카드를 추가할 수 있다`() {
+        val hand: Hand =
+            Hand.of(
+                Card(CardNumber.ACE, Suit.CLUB),
+            )
+        val actualCardsList = hand.toList()
+
+        val expectedSize = 1
+
+        assertThat(actualCardsList).hasSize(expectedSize)
+    }
+
+    @Test
+    fun `손패에 카드 목록을 확인할 수 있다`() {
+        val queenHeart = Card(CardNumber.QUEEN, Suit.HEART)
+        val aceSpade = Card(CardNumber.ACE, Suit.SPADE)
+        val hand: Hand =
+            Hand.of(
+                queenHeart,
+                aceSpade,
+            )
+        val actualCardList = hand.toList()
+
+        val expectedCardList = listOf(queenHeart, aceSpade)
+
+        assertThat(actualCardList).isEqualTo(expectedCardList)
+    }
+
+    @Test
+    fun `손패에 카드 값이 21이 넘으면 버스트인지 알 수 있다`() {
+        val hand: Hand =
+            Hand.of(
+                Card(CardNumber.TEN, Suit.HEART),
+                Card(CardNumber.KING, Suit.SPADE),
+                Card(CardNumber.TWO, Suit.CLUB),
+            )
+        val actualIsBust = hand.isBust()
+
+        val expected = true
+
+        assertThat(actualIsBust).isEqualTo(expected)
+    }
+
+    @Test
+    fun `손패에 카드 값이 21이 넘지 않으면 버스트가 아닌지 알 수 있다`() {
+        val hand: Hand =
+            Hand.of(
+                Card(CardNumber.TEN, Suit.HEART),
+                Card(CardNumber.TWO, Suit.CLUB),
+            )
+        val actualIsBust = hand.isBust()
+
+        val expected = false
+
+        assertThat(actualIsBust).isEqualTo(expected)
+    }
+
+    @Test
+    fun `손패에 카드 값이 특정한 값보다 이하인지 알 수 있다`() {
+        val hand: Hand =
+            Hand.of(
+                Card(CardNumber.TEN, Suit.SPADE),
+                Card(CardNumber.SIX, Suit.CLUB),
+            )
+        val actualIsLessOrSameThanSixteen = hand.isLessOrSameThan(16)
+
+        val expected = true
+
+        assertThat(actualIsLessOrSameThanSixteen).isEqualTo(expected)
+    }
+
+    @Test
+    fun `손패를 계산해 줄 수 있다`() {
+        val hand: Hand =
+            Hand.of(
+                Card(CardNumber.ACE, Suit.SPADE),
+                Card(CardNumber.KING, Suit.CLUB),
+            )
+        val actualScore = hand.getScore()
+
+        val expectedScore = 21
+
+        assertThat(actualScore).isEqualTo(expectedScore)
+    }
+}
