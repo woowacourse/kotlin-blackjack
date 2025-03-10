ef3785c53f5165836cf062eaeb34be0baf0f4c0e
ijh1298 <imjh0417@gmail.com>
2025-03-10 22:12:21 +0900

    refactor: Deck에서 카드를 뽑을 때 카드가 비어있으면 채워주도록 수정

--- src/main/kotlin/blackjack/domain/model/card/Deck.kt
+++ src/main/kotlin/blackjack/domain/model/card/Deck.kt
@@ -3,11 +3,14 @@ package blackjack.domain.model.card
 import blackjack.domain.generator.CardsGenerator
 
 class Deck(
-    cardGenerator: CardsGenerator,
+    private val cardGenerator: CardsGenerator,
 ) {
     private val cards: ArrayDeque<Card> = ArrayDeque(cardGenerator.createCards())
 
     fun pop(): Card {
+        if (cards.isEmpty()) {
+            cards.addAll(cardGenerator.createCards())
+        }
         return cards.removeFirst()
     }
 }
