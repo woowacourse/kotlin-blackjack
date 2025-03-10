011cfff67ed5f613803cd04845055aa905f2f784
Leeyerin0210 <lyl8661@naver.com>
2025-03-10 15:06:36 +0900

    refactor : linkedList -> mutableList 변경

--- src/main/kotlin/blackjack/model/CardDeck.kt
+++ src/main/kotlin/blackjack/model/CardDeck.kt
@@ -1,14 +1,14 @@
 package blackjack.model
 
 import blackjack.model.Card.Companion.SINGLE_DECK
-import java.util.LinkedList
+
 
 interface ShuffleStrategy {
-    fun shuffle(cardList: List<Card>): LinkedList<Card>
+    fun shuffle(cardList: List<Card>): MutableList<Card>
 }
 
 class RandomShuffle : ShuffleStrategy {
-    override fun shuffle(cardList: List<Card>) = LinkedList(cardList.shuffled())
+    override fun shuffle(cardList: List<Card>) = cardList.shuffled().toMutableList()
 }
 
 
@@ -16,9 +16,9 @@ class CardDeck(val shuffleStrategy: ShuffleStrategy = RandomShuffle()) {
     private var cards = shuffleStrategy.shuffle(SINGLE_DECK)
 
     fun draw(): Card {
-        return cards.poll() ?: run {
+        return cards.removeFirstOrNull() ?: run {
             cards = shuffleStrategy.shuffle(SINGLE_DECK)
-            return cards.poll()
+            return cards.removeFirst()
         }
     }
 }
