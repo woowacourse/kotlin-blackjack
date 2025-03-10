5a4aa7f2a7da1d42b10b820bcd2b161fd1559e81
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-03-10 15:34:32 +0900

    test: refactor SuitTests

--- src/test/kotlin/blackjack/domain/SuitTest.kt
+++ src/test/kotlin/blackjack/domain/SuitTest.kt
@@ -6,6 +6,6 @@ import org.junit.jupiter.api.Test
 class SuitTest {
     @Test
     fun `수트의 종류는 스페이드(♤, Spade), 하트(♡, Heart), 다이아몬드(◇, Diamond), 클로버(♧, Clover)가 있다`() {
-        assertThat(Suit.entries).isEqualTo(listOf(Suit.SPADE, Suit.HEART, Suit.DIAMOND, Suit.CLOVER))
+        assertThat(Suit.entries).hasSameElementsAs(setOf(Suit.SPADE, Suit.HEART, Suit.DIAMOND, Suit.CLOVER))
     }
 }
