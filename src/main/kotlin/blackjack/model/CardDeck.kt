13e5774b7811be79234bcb06372f45c8e06a1785
Leeyerin0210 <lyl8661@naver.com>
2025-03-10 14:59:05 +0900

    refactor : 덱에서 뽑을 카드가 없을 경우에 새 덱을 가져오는 식으로 변경

--- src/main/kotlin/blackjack/model/CardDeck.kt
+++ src/main/kotlin/blackjack/model/CardDeck.kt
@@ -3,23 +3,22 @@ package blackjack.model
 import blackjack.model.Card.Companion.SINGLE_DECK
 import java.util.LinkedList
 
-interface ShuffleStrategy{
-    fun shuffle(cardList: List<Card>) : LinkedList<Card>
+interface ShuffleStrategy {
+    fun shuffle(cardList: List<Card>): LinkedList<Card>
 }
 
-class RandomShuffle: ShuffleStrategy{
-    override fun shuffle(cardList : List<Card>) = LinkedList(cardList.shuffled())
+class RandomShuffle : ShuffleStrategy {
+    override fun shuffle(cardList: List<Card>) = LinkedList(cardList.shuffled())
 }
 
 
-class CardDeck(val shuffleStrategy :ShuffleStrategy = RandomShuffle()) {
-    private val cards = shuffleStrategy.shuffle(CACHE_CARDS)
+class CardDeck(val shuffleStrategy: ShuffleStrategy = RandomShuffle()) {
+    private var cards = shuffleStrategy.shuffle(SINGLE_DECK)
 
-    fun draw(): Card =
-        cards.poll() ?: throw IllegalArgumentException("[ERROR] 더 이상 카드를 뽑을 수 없습니다.")
-
-    companion object {
-        private const val DECK_COUNT = 6
-        private val CACHE_CARDS = (1..DECK_COUNT).flatMap { SINGLE_DECK }
+    fun draw(): Card {
+        return cards.poll() ?: run {
+            cards = shuffleStrategy.shuffle(SINGLE_DECK)
+            return cards.poll()
+        }
     }
 }
