4bb0b354c78dac1e95d44992660e1a1dee40d769
chanho0908 <uni070@naver.com>
2025-03-11 01:35:33 +0900

    refactor : 딜러가 충족해야하는 카드 최소 점수 변수명 수정

--- src/main/kotlin/blackjack/domain/participant/Dealer.kt
+++ src/main/kotlin/blackjack/domain/participant/Dealer.kt
@@ -3,8 +3,8 @@ package blackjack.domain.participant
 class Dealer : Participant() {
     fun isOverMaxScore(): Boolean {
         if (hasAce() && !isBust()) {
-            return sumOfCards() + ACE_EXTRACT_SCORE > DEALER_MAX_SCORE
+            return sumOfCards() + ACE_EXTRACT_SCORE > DEALER_MUST_REACH_SCORE
         }
-        return sumOfCards() > DEALER_MAX_SCORE
+        return sumOfCards() > DEALER_MUST_REACH_SCORE
     }
 }
--- src/main/kotlin/blackjack/domain/participant/Participant.kt
+++ src/main/kotlin/blackjack/domain/participant/Participant.kt
@@ -27,7 +27,7 @@ abstract class Participant {
     protected fun sumOfCards(): Int = cards.sumOfCards()
 
     companion object {
-        const val DEALER_MAX_SCORE = 16
+        const val DEALER_MUST_REACH_SCORE = 16
         const val ACE_EXTRACT_SCORE = 10
     }
 }
