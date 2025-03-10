e5f3ace39f985376cd3231c56a32f0c1cae6099f
박지민 <pjm5489@naver.com>
2025-03-10 15:42:59 +0900

    refactor: title 프로퍼티 제거

--- src/main/kotlin/model/CardRank.kt
+++ src/main/kotlin/model/CardRank.kt
@@ -1,17 +1,17 @@
 package model
 
-enum class CardRank(val score: Int, val title: String) {
-    ACE(11, "A"),
-    KING(10, "K"),
-    QUEEN(10, "Q"),
-    JACK(10, "J"),
-    TWO(2, "2"),
-    THREE(3, "3"),
-    FOUR(4, "4"),
-    FIVE(5, "5"),
-    SIX(6, "6"),
-    SEVEN(7, "7"),
-    EIGHT(8, "8"),
-    NINE(9, "9"),
-    TEN(10, "10"),
+enum class CardRank(val score: Int) {
+    ACE(11),
+    KING(10),
+    QUEEN(10),
+    JACK(10),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
+    TEN(10),
 }
