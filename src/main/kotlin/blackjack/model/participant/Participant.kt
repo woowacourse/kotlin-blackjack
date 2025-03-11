f1da4b623d54a8236f3bbcb14883311a8446da92
HamBeomJoon <hbj0802@naver.com>
2025-03-11 15:38:05 +0900

    feat: 참여자 Blackjack 여부 판단 함수 추가

--- src/main/kotlin/blackjack/model/participant/Participant.kt
+++ src/main/kotlin/blackjack/model/participant/Participant.kt
@@ -1,10 +1,27 @@
 package blackjack.model.participant
 
+import blackjack.model.GameManager.Companion.INITIAL_HAND_OUT_CARD_COUNT
 import blackjack.model.card.Card
+import blackjack.model.card.CardNumber
 
 abstract class Participant(val name: String) {
     private val _cards: MutableList<Card> = mutableListOf()
     val cards get() = _cards.toList()
+
+    fun isBlackjack(): Boolean {
+        if (_cards.size != INITIAL_HAND_OUT_CARD_COUNT) return false
+        val numbers = _cards.map { it.number }
+        return CardNumber.ACE in numbers &&
+            numbers.any {
+                it in
+                    listOf(
+                        CardNumber.TEN,
+                        CardNumber.JACK,
+                        CardNumber.QUEEN,
+                        CardNumber.KING,
+                    )
+            }
+    }
 
     fun addCard(card: Card) = _cards.add(card)
 
