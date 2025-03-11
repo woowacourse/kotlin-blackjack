dd41418524bb36c01de307df87863036d727af71
medAndro <med4ndro@gmail.com>
2025-03-11 23:55:49 +0900

    refactor: Deck을 object로 변경

--- src/main/kotlin/blackjack/domain/model/card/Deck.kt
+++ src/main/kotlin/blackjack/domain/model/card/Deck.kt
@@ -1,14 +1,12 @@
 package blackjack.domain.model.card
 
-class Deck {
-    companion object {
-        private val cards = ArrayDeque(Card.CARD_INDEX_RANGE.shuffled().map { Card(it) })
+object Deck {
+    private val cards = ArrayDeque(Card.CARD_INDEX_RANGE.shuffled().map { Card(it) })
 
-        fun giveCard(): Card {
-            require(!cards.isEmpty()) { ERROR_EMPTY_DECK }
-            return cards.removeLast()
-        }
-
-        private const val ERROR_EMPTY_DECK = "모든 카드가 소진되어 덱이 비었습니다"
+    fun giveCard(): Card {
+        require(!cards.isEmpty()) { ERROR_EMPTY_DECK }
+        return cards.removeLast()
     }
+
+    private const val ERROR_EMPTY_DECK = "모든 카드가 소진되어 덱이 비었습니다"
 }
