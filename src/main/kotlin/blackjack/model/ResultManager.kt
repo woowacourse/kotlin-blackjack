856c173f9fc237d3491bc166001aab0e13465062
junseo511 <prime54@g.skku.edu>
2025-03-12 02:12:54 +0900

    fix: 누락된 딜러 결과 추가

--- src/main/kotlin/blackjack/model/ResultManager.kt
+++ src/main/kotlin/blackjack/model/ResultManager.kt
@@ -10,13 +10,16 @@ class ResultManager(
                 WinningResult.from(player.score(), dealer.score(), player.isBust(), dealer.isBust())
         }
 
-    fun dealerResult(): Map<WinningResult, Int> =
-        players.value
-            .map { player ->
-                WinningResult.from(dealer.score(), player.score(), dealer.isBust(), player.isBust())
-            }.groupingBy { it }
-            .eachCount()
-            .withDefault { INITIAL_SCORE }
+    fun dealerResult(): Map<WinningResult, Int> {
+        val resultCounts =
+            players.value
+                .map { player ->
+                    WinningResult.from(dealer.score(), player.score(), dealer.isBust(), player.isBust())
+                }.groupingBy { it }
+                .eachCount()
+
+        return WinningResult.entries.associateWith { resultCounts[it] ?: INITIAL_SCORE }
+    }
 
     companion object {
         private const val INITIAL_SCORE = 0
