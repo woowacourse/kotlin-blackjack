b26e59c10d84cb17dd1d2c7b4027302ef0deed26
yrsel <xxknu88@gmail.com>
2025-03-11 17:43:51 +0900

    refactor: 블랙잭 게임 객체를 사용하는 방식으로 수정

--- src/main/kotlin/blackjack/controller/BlackjackController.kt
+++ src/main/kotlin/blackjack/controller/BlackjackController.kt
@@ -1,10 +1,8 @@
 package blackjack.controller
 
-import blackjack.model.CardDrawDecision
+import blackjack.model.BlackjackGame
 import blackjack.model.Dealer
-import blackjack.model.GameManager
 import blackjack.model.Player
-import blackjack.model.card.Deck.Companion.INITIAL_HAND_OUT_CARD_COUNT
 import blackjack.view.InputView
 import blackjack.view.OutputView
 
@@ -14,54 +12,49 @@ class BlackjackController(
 ) {
     fun play(dealer: Dealer) {
         val players: List<Player> = inputView.readPlayerNames()
-        outputView.printInitialHandOutCardMessage(players)
+        val blackjackGame = BlackjackGame(dealer, players)
 
-        val gameManager = GameManager(dealer, players)
-        gameManager.distributeInitialCardWithCount(INITIAL_HAND_OUT_CARD_COUNT)
-        outputView.printAllPlayerHands(dealer, players)
-
-        playersDrawCards(gameManager, players)
-        dealerDrawCards(gameManager, dealer)
-
-        outputView.printFinalHandStatus(dealer, players)
-        resultSummary(gameManager)
+        allParticipantsInitialCards(blackjackGame)
+        playersDrawCards(blackjackGame)
+        dealerDrawCard(blackjackGame)
+        allParticipantsCardStatus(blackjackGame)
+        resultSummary(blackjackGame)
     }
 
-    private fun playersDrawCards(
-        gameManager: GameManager,
-        players: List<Player>,
-    ) {
-        players.forEach { player -> playerDrawOrStay(gameManager, player) }
+    private fun allParticipantsInitialCards(blackjackGame: BlackjackGame) {
+        blackjackGame.processDistributeInitialCards(
+            showDistributeGuideMessage = { players -> outputView.printInitialHandOutCardMessage(players) },
+            showDistributedCardStatus = { dealer, players -> outputView.printAllPlayerHands(dealer, players) },
+        )
     }
 
-    private fun playerDrawOrStay(
-        gameManager: GameManager,
-        player: Player,
-    ) {
-        while (true) {
-            val decision: CardDrawDecision = inputView.readCardDrawChoice(player)
-            if (gameManager.distributeCardWithChoice(decision, player)) {
-                outputView.printPlayerHands(player)
-                if (player.isBust()) break
-                continue
-            }
+    private fun playersDrawCards(blackjackGame: BlackjackGame) {
+        blackjackGame.processPlayerDrawCards(playerDrawDecision = { player: Player ->
+            inputView.readCardDrawChoice(player)
+        }, showPlayerCardStatus = { player: Player ->
             outputView.printPlayerHands(player)
-            break
-        }
+        })
     }
 
-    private fun dealerDrawCards(
-        gameManager: GameManager,
-        dealer: Dealer,
-    ) {
-        val isDraw = dealer.isAvailDrawCard()
-        if (isDraw) gameManager.distributeCard(dealer)
-        outputView.printDealerHandStatus(isDraw)
+    private fun dealerDrawCard(blackjackGame: BlackjackGame) {
+        blackjackGame.processDealerDrawCard(
+            showDealerCardStatus = { isDraw -> outputView.printDealerHandStatus(isDraw) },
+        )
     }
 
-    private fun resultSummary(gameManager: GameManager) {
-        val playersSummary = gameManager.calculatePlayersSummary()
-        val dealerSummary = gameManager.calculateDealerSummary()
-        outputView.printFinalResult(playersSummary, dealerSummary)
+    private fun allParticipantsCardStatus(blackjackGame: BlackjackGame) {
+        blackjackGame.processAllParticipantsCardStatus(
+            showParticipantsCardStatus = { dealer, players ->
+                outputView.printFinalHandStatus(dealer, players)
+            },
+        )
+    }
+
+    private fun resultSummary(blackjackGame: BlackjackGame) {
+        blackjackGame.processResultSummary(
+            showResultSummary = { playerResultSummary, dealerResultSummary ->
+                outputView.printFinalResult(playerResultSummary, dealerResultSummary)
+            },
+        )
     }
 }
