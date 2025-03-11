289022f46b34e517e7de06a231e79c6b6e18755b
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-03-11 16:40:21 +0900

    refactor: change code order

--- src/main/kotlin/blackjack/domain/Deck.kt
+++ src/main/kotlin/blackjack/domain/Deck.kt
@@ -7,6 +7,12 @@ import blackjack.domain.Rank.NumberRank
 class Deck(
     shuffler: Shuffler,
 ) {
+    fun draw(): Card {
+        val card = cards.first()
+        cards = cards.minus(card)
+        return card
+    }
+
     private val aceCards: List<Card> = Suit.entries.map { suit -> Card(AceRank, suit) }
     private val numberCards: List<Card> =
         Suit.entries.flatMap { suit ->
@@ -20,10 +26,4 @@ class Deck(
         }
 
     private var cards: List<Card> = shuffler.shuffle(aceCards + numberCards + faceCards)
-
-    fun draw(): Card {
-        val card = cards.first()
-        cards = cards.minus(card)
-        return card
-    }
 }
