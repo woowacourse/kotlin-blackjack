afe2ed89baf893c1425c1400c4dba9e9afe6e221
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-03-11 16:00:50 +0900

    refactor: use functional interface

--- src/main/kotlin/blackjack/domain/Shuffler.kt
+++ src/main/kotlin/blackjack/domain/Shuffler.kt
@@ -1,13 +1,9 @@
 package blackjack.domain
 
-interface Shuffler {
+fun interface Shuffler {
     fun shuffle(cards: List<Card>): List<Card>
 }
 
 object RandomShuffler : Shuffler {
     override fun shuffle(cards: List<Card>): List<Card> = cards.shuffled()
-}
-
-object FakeShuffler : Shuffler {
-    override fun shuffle(cards: List<Card>): List<Card> = cards
 }
--- src/test/kotlin/blackjack/domain/DealerTest.kt
+++ src/test/kotlin/blackjack/domain/DealerTest.kt
@@ -18,7 +18,7 @@ class DealerTest {
     @Test
     fun `딜러는 플레이어가 카드를 더 뽑도록 할 수 있다`() {
         val player = Player("Player")
-        val dealer = Dealer(listOf(player), FakeShuffler)
+        val dealer = Dealer(listOf(player), { it })
         dealer.giveCard(player)
         assertThat(player.cards.size).isEqualTo(1)
     }
@@ -26,7 +26,7 @@ class DealerTest {
     @Test
     fun `플레이어는 모든 카드의 합이 21 미만이 될 수 있을 경우 계속해서 카드를 뽑을 수 있다`() {
         val player = Player("Player")
-        val dealer = Dealer(listOf(player), FakeShuffler)
+        val dealer = Dealer(listOf(player), { it })
         dealer.startPlayerTurn {
             player.draw(Card(Rank.AceRank, Suit.DIAMOND))
         }
