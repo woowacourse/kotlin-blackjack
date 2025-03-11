3f7ad88b9ee6e1cf78c555e4f472d669c032d434
박지민 <pjm5489@naver.com>
2025-03-11 18:34:54 +0900

    refactor: drawCount() 로직 분리

--- src/main/kotlin/model/Dealer.kt
+++ src/main/kotlin/model/Dealer.kt
@@ -15,10 +15,15 @@ class Dealer(dealerCards: Cards) : Participant(dealerCards) {
 
     fun drawCount(drawnCard: Card): Int {
         var drawCount = DEFAULT_DRAW_COUNT
-        while (canHit()) {
-            if (turn(drawnCard)) drawCount++
-        }
+        drawCount += addDraws(drawnCard)
         return drawCount
+    }
+
+    private fun addDraws(drawnCard: Card): Int {
+        var count = DEFAULT_DRAW_COUNT
+        while (canHit() && turn(drawnCard)) count++
+
+        return count
     }
 
     override fun canHit(): Boolean = currentScore <= DEALER_HIT_LIMIT
