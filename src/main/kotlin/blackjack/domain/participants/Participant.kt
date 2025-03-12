497d24342b18171f0cd9a261dce91b9953b383b7
cucumber99 <dlsguq1@gmail.com>
2025-03-12 18:52:47 +0900

    refactor: Participant 클래스 리팩터링

--- src/main/kotlin/blackjack/domain/participants/Participant.kt
+++ src/main/kotlin/blackjack/domain/participants/Participant.kt
@@ -3,40 +3,36 @@ package blackjack.domain.participants
 import blackjack.const.GameRule
 import blackjack.domain.ScoreCalculator
 import blackjack.domain.card.Card
-import blackjack.domain.state.GameState
 
 abstract class Participant(
     private val _hand: MutableList<Card> = mutableListOf(),
-    private val calculator: ScoreCalculator = ScoreCalculator(),
 ) {
     val hand: List<Card>
         get() = _hand.toList()
 
-    var gameState: GameState = GameState.FIRST_TURN
-        private set
-
-    abstract fun shouldHit(): Boolean
+    abstract fun canHit(): Boolean
 
     fun addCard(card: Card) {
         _hand.add(card)
-        updateState()
     }
 
-    fun score(): Int = calculator.calculate(hand)
+    fun isBlackjack(): Boolean {
+        return score() == GameRule.BLACKJACK_SCORE && _hand.size == GameRule.FIRST_TURN_DRAW_AMOUNT
+    }
+
+    fun isBust(): Boolean = score() > GameRule.BLACKJACK_SCORE
+
+    fun score(): Int = ScoreCalculator.calculate(hand)
 
     fun getDrawAmount(): Int {
-        if (gameState == GameState.FIRST_TURN) {
+        if (hand.isEmpty()) {
             return GameRule.FIRST_TURN_DRAW_AMOUNT
         }
 
-        if (gameState == GameState.HIT) {
+        if (canHit()) {
             return GameRule.HIT_DRAW_AMOUNT
         }
 
         throw IllegalArgumentException("[ERROR] 카드를 뽑을 수 없습니다.")
-    }
-
-    private fun updateState() {
-        gameState = GameState.from(this)
     }
 }
