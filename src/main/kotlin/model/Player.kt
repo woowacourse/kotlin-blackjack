9f93f983fb4818b696bf11312d613e6c0dea3862
gahyunkim <mapledt001@naver.com>
2025-03-13 00:14:12 +0900

    feat: Player의 게임 진행 조건 실행 코드 구현

--- src/main/kotlin/model/Player.kt
+++ src/main/kotlin/model/Player.kt
@@ -2,4 +2,15 @@ package model
 
 class Player(val name: String, private val hand: Hand) : Participant(hand) {
     override fun decideToHit(): Boolean = getScore() <= GameResultDecider.BLACKJACK_SCORE
+
+    fun playTurn(
+        shouldHit: (Player) -> Boolean,
+        getCard: () -> List<Card>,
+        showCards: () -> Unit,
+    ) {
+        while (decideToHit() && shouldHit(this)) {
+            receiveCards { getCard() }
+            showCards()
+        }
+    }
 }
