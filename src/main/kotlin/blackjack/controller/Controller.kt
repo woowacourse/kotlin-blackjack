0fb3f9401dc6b8394df4cc34fd36599a151c7105
eden <devfeijoa@gmail.com>
2025-03-13 02:23:21 +0900

    feat: implement controller

--- src/main/kotlin/blackjack/controller/Controller.kt
+++ src/main/kotlin/blackjack/controller/Controller.kt
@@ -1,51 +1,34 @@
 package blackjack.controller
 
-import blackjack.domain.Ace
-import blackjack.domain.Card
-import blackjack.domain.Character
+import blackjack.domain.Blackjack
 import blackjack.domain.Dealer
-import blackjack.domain.Number
-import blackjack.domain.Player
-import blackjack.domain.Rank
-import blackjack.domain.Suit
+import blackjack.view.InputView
+import blackjack.view.OutputView
 
-class Controller {
-    fun readMoreCards(
-        dealer: Dealer,
-        players: List<Player>,
-    ) {
+class Controller(
+    private val inputView: InputView,
+    private val outputView: OutputView,
+) {
+    fun gameStart() {
+        val players = inputView.readPlayers()
+        val dealer = Dealer(players)
+        val blackjack = Blackjack(dealer, players)
+
+        outputView.printInitialCardsState(dealer, players)
+
         players.forEach { player ->
-            println("${player.name}는 한 장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)")
-            var input = readln()
-            while (input == "y") {
-                dealer.giveCard(player)
-                println("${player.name}카드: ${player.cards.joinToString { card -> card.prettyString }}")
-                println("${player.name}는 한 장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)")
-                input = readln()
+            while (player.canGetCard()) {
+                if (inputView.askMoreCards(player)) {
+                    dealer.giveCard(player)
+                    println("${player.name}카드: ${player.cards.joinToString { card -> card.prettyString }}")
+                } else {
+                    break
+                }
             }
-            println("${player.name}카드: ${player.cards.joinToString { card -> card.prettyString }}")
         }
-        println()
+
+        outputView.printFinalCardsScores(dealer, players)
+        blackjack.finish()
+        outputView.printFinalResults(dealer, players)
     }
-
-    val Card.prettyString: String
-        get() = rank.prettyString + suit.prettyString
-
-    val Rank.prettyString: String
-        get() =
-            when (this) {
-                is Ace -> "A"
-                Character.JACK -> "J"
-                Character.QUEEN -> "Q"
-                Character.KING -> "K"
-                is Number -> value.toString()
-            }
-    val Suit.prettyString: String
-        get() =
-            when (this) {
-                Suit.SPADE -> "스페이드"
-                Suit.HEART -> "하트"
-                Suit.DIAMOND -> "다이아몬드"
-                Suit.CLOVER -> "클로버"
-            }
 }
