037ef68ad7f5d792be8b10cb134a8cd2038b5802
yrsel <xxknu88@gmail.com>
2025-03-12 23:18:22 +0900

    feat: 참여자의 게임 진행 상태 구현

---
+++ src/main/kotlin/blackjack/model/GameStatus.kt
@@ -0,0 +1,21 @@
+package blackjack.model
+
+import blackjack.model.ScoreCalculator.BUST_NUMBER
+import blackjack.model.card.Deck.Companion.INITIAL_HAND_OUT_CARD_COUNT
+
+enum class GameStatus {
+    BLACKJACK,
+    BUST,
+    IN_PROGRESS, ;
+
+    companion object {
+        fun of(
+            score: Int,
+            cardCount: Int,
+        ): GameStatus {
+            if (score == BUST_NUMBER && cardCount == INITIAL_HAND_OUT_CARD_COUNT) return BLACKJACK
+            if (score > BUST_NUMBER) return BUST
+            return IN_PROGRESS
+        }
+    }
+}
