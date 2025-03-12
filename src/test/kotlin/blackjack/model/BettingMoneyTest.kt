3f741588a3740cb5b7ede45442ca86360ccf7051
m6z1 <sonmyungj1zz@gmail.com>
2025-03-12 16:12:58 +0900

    feat: 베팅 금액이 베팅한 돈을 가지도록 객체 수정

--- src/test/kotlin/blackjack/model/BettingMoneyTest.kt
+++ src/test/kotlin/blackjack/model/BettingMoneyTest.kt
@@ -1,5 +1,7 @@
 package blackjack.model
 
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
@@ -9,5 +11,16 @@ class BettingMoneyTest {
     @ValueSource(ints = [0, 300_000_001])
     fun `베팅 금액이 1원 이상 최대 3억원이 아닐 경우 예외를 발생시킨다`(money: Int) {
         assertThrows<IllegalArgumentException> { BettingMoney(money) }
+    }
+
+    @Test
+    fun `베팅 금액은 베팅한 금액을 가진다`() {
+        val bettingMoney = BettingMoney(100_000)
+
+        val actual = bettingMoney.money
+
+        val expected = 100_000
+
+        assertThat(actual).isEqualTo(expected)
     }
 }
