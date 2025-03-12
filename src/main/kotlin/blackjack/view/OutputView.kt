966cd27b5796665df4ff7bba023269799040e49c
moondev03 <moondev03@gmail.com>
2025-03-12 15:14:43 +0900

    refactor: View 관련 확장 함수 파일 분리

--- src/main/kotlin/blackjack/view/OutputView.kt
+++ src/main/kotlin/blackjack/view/OutputView.kt
@@ -2,12 +2,9 @@ package blackjack.view
 
 import blackjack.domain.GameResult
 import blackjack.domain.card.Card
-import blackjack.domain.card.CardNumber
-import blackjack.domain.card.CardPattern
 import blackjack.domain.person.Dealer
 import blackjack.domain.person.Person
 import blackjack.domain.person.Player
-import blackjack.domain.state.ResultState
 
 class OutputView {
     fun printFirstDrawMessage(players: List<Player>) {
@@ -40,13 +37,13 @@ class OutputView {
     fun printPersonResult(person: Person) {
         val name = (person as? Player)?.name ?: DEALER
         if (name == DEALER) println()
-        printGameResult(name, person.cards, person.score)
+        printGameResult(name, person.cards, person.isBlackJackString())
     }
 
     private fun printGameResult(
         name: String,
         cards: List<Card>,
-        score: Int,
+        score: String,
     ) {
         printDrawStatus(name, cards.toUiString())
         println(SCORE_RESULT_MESSAGE.format(score))
@@ -54,20 +51,8 @@ class OutputView {
 
     fun printGameResults(result: GameResult) {
         println("\n" + RESULT_HEADLINE_MESSAGE)
-        printDealerWinInfo(result.countByResultState())
-        result.winStatus.forEach {
-            println(PLAYER_RESULT_MESSAGE.format(it.key.name, it.value.toUiString()))
-        }
-    }
-
-    private fun printDealerWinInfo(countByResultState: Map<ResultState, Int>) {
-        println(
-            DEALER_RESULT_MESSAGE.format(
-                countByResultState[ResultState.LOSE] ?: 0,
-                countByResultState[ResultState.DRAW] ?: 0,
-                countByResultState[ResultState.WIN] ?: 0,
-            ),
-        )
+        println(PLAYER_RESULT_MESSAGE.format(DEALER, result.dealerProfit.formatAmount()))
+        result.playerPayouts.forEach { (player, payout) -> println(PLAYER_RESULT_MESSAGE.format(player.name, payout.formatAmount())) }
     }
 
     private fun printDrawStatus(
@@ -77,34 +62,6 @@ class OutputView {
         print(DRAW_STATUS_MESSAGE.format(name, cards))
     }
 
-    private fun ResultState.toUiString() =
-        when (this) {
-            ResultState.WIN -> WIN
-            ResultState.DRAW -> DRAW
-            else -> LOSE
-        }
-
-    private fun List<Card>.toUiString(): String = joinToString(DELIMITER) { it.toUiString() }
-
-    private fun Card.toUiString(): String = "${number.toUiString()}${pattern.toUiString()}"
-
-    private fun CardNumber.toUiString(): String =
-        when (this) {
-            CardNumber.ACE -> "A"
-            CardNumber.KING -> "K"
-            CardNumber.QUEEN -> "Q"
-            CardNumber.JACK -> "J"
-            else -> this.value.toString()
-        }
-
-    private fun CardPattern.toUiString(): String =
-        when (this) {
-            CardPattern.HEART -> "하트"
-            CardPattern.SPADE -> "스페이드"
-            CardPattern.DIAMOND -> "다이아몬드"
-            CardPattern.CLOVER -> "클로버"
-        }
-
     companion object {
         private const val DEALER = "딜러"
         private const val FIRST_DRAW_MESSAGE = "${DEALER}와 %s에게 2장을 나누었습니다."
@@ -112,10 +69,6 @@ class OutputView {
         private const val DRAW_STATUS_MESSAGE = "%s 카드: %s"
         private const val SCORE_RESULT_MESSAGE = " - 결과: %s"
         private const val RESULT_HEADLINE_MESSAGE = "## 최종 승패"
-        private const val WIN = "승"
-        private const val LOSE = "패"
-        private const val DRAW = "무"
-        private const val DEALER_RESULT_MESSAGE = "$DEALER: %s$WIN %s$DRAW %s$LOSE"
         private const val PLAYER_RESULT_MESSAGE = "%s: %s"
         private const val DELIMITER = ", "
     }
---
+++ src/main/kotlin/blackjack/view/ViewExtension.kt
@@ -0,0 +1,36 @@
+package blackjack.view
+
+import blackjack.domain.card.Card
+import blackjack.domain.card.CardNumber
+import blackjack.domain.card.CardPattern
+import blackjack.domain.person.Person
+import blackjack.domain.state.PersonState
+import java.text.DecimalFormat
+
+fun Double.formatAmount(): String {
+    val formatter = DecimalFormat("#.##")
+    return formatter.format(this)
+}
+
+fun Person.isBlackJackString(): String = if (this.gameState == PersonState.BLACKJACK) "BLACKJACK!" else "${this.score}"
+
+fun List<Card>.toUiString(): String = joinToString(", ") { it.toUiString() }
+
+fun Card.toUiString(): String = "${number.toUiString()}${pattern.toUiString()}"
+
+fun CardNumber.toUiString(): String =
+    when (this) {
+        CardNumber.ACE -> "A"
+        CardNumber.KING -> "K"
+        CardNumber.QUEEN -> "Q"
+        CardNumber.JACK -> "J"
+        else -> this.value.toString()
+    }
+
+fun CardPattern.toUiString(): String =
+    when (this) {
+        CardPattern.HEART -> "하트"
+        CardPattern.SPADE -> "스페이드"
+        CardPattern.DIAMOND -> "다이아몬드"
+        CardPattern.CLOVER -> "클로버"
+    }
