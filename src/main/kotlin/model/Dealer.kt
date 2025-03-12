63dbe9bad06ba44177a0272a711b3d07295be4f3
gahyunkim <mapledt001@naver.com>
2025-03-13 00:14:02 +0900

    feat: Dealer의 게임 진행 조건 실행 코드 구현

--- src/main/kotlin/model/Dealer.kt
+++ src/main/kotlin/model/Dealer.kt
@@ -3,6 +3,12 @@ package model
 class Dealer(private val hand: Hand) : Participant(hand) {
     override fun decideToHit(): Boolean = getScore() <= STANDARD_DEALER_HIT
 
+    fun playTurn(getCard: () -> List<Card>) {
+        while (decideToHit()) {
+            receiveCards { getCard() }
+        }
+    }
+
     companion object {
         private const val STANDARD_DEALER_HIT = 16
     }
