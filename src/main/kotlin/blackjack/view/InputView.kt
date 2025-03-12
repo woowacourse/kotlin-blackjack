6ab68f4344c4fb5381986c7583f8567b439d5e0d
ijh1298 <imjh0417@gmail.com>
2025-03-12 19:19:25 +0900

    refactor: InputView 베팅 금액을 입력받는 readBetAmount 메서드 추가

--- src/main/kotlin/blackjack/view/InputView.kt
+++ src/main/kotlin/blackjack/view/InputView.kt
@@ -16,11 +16,22 @@ class InputView {
         }
     }
 
+    fun readBetAmount(name: String): Double {
+        println(ALERT_HOW_MUCH_BET_AMOUNT.format(name))
+        val rawInput: String = readln()
+        return rawInput.toDoubleOrNull() ?: run {
+            println(ERROR_WRONG_BET_AMOUNT)
+            readBetAmount(name)
+        }
+    }
+
     companion object {
         private const val ALERT_PLAYER_NAME_INPUT = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)"
         private const val ALERT_READ_WANT_EXTRA_CARD = "%s은(는) 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)"
+        private const val ALERT_HOW_MUCH_BET_AMOUNT = "%s의 배팅 금액은?"
 
         private const val ERROR_WRONG_WANT_EXTRA_CARD_INPUT = "잘못된 입력입니다 Y 또는 N만 입력해 주세요"
+        private const val ERROR_WRONG_BET_AMOUNT = "잘못된 베팅 금액 입력입니다"
 
         private fun String.toBooleanOrNull(): Boolean? =
             when (this.uppercase()) {
