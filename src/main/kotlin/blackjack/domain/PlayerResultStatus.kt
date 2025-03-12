b1a5aefd793469182a183cfbb0f8ca73fbacc3ab
DongjooSEO <readyjoo24@gmail.com>
2025-03-13 02:30:28 +0900

    refactor: Player Result상태 가지도록 변경

--- src/main/kotlin/blackjack/domain/PlayerResultStatus.kt
+++ src/main/kotlin/blackjack/domain/PlayerResultStatus.kt
@@ -1,6 +1,6 @@
 package blackjack.domain
 
-enum class GameResultStatus {
+enum class PlayerResultStatus {
     PLAYER_WIN,
     PLAYER_LOSE,
     DRAW,
--- src/main/kotlin/blackjack/domain/participant/Player.kt
+++ src/main/kotlin/blackjack/domain/participant/Player.kt
@@ -1,8 +1,39 @@
 package blackjack.domain.participant
 
+import blackjack.domain.PlayerResultStatus
+
 class Player(val name: String) : Participant() {
+    private var gameStatus: PlayerResultStatus = PlayerResultStatus.DRAW
+
     override val hitThreshold: Int
         get() = PLAYER_HIT_THRESHOLD
+
+    fun setPlayerStatus(dealer: Dealer): PlayerResultStatus {
+        val playerBlackJack = hand.isBlackJack()
+        val dealerBlackJack = dealer.hand.isBlackJack()
+        val playerBust = hand.isBust()
+        val dealerBust = dealer.hand.isBust()
+        val playerTotalSum = hand.getTotalSum()
+        val dealerTotalSum = dealer.hand.getTotalSum()
+
+        gameStatus =
+            when {
+                // 블랙잭 판별
+                playerBlackJack && !dealerBlackJack -> PlayerResultStatus.PLAYER_WIN
+                playerBlackJack && dealerBlackJack -> PlayerResultStatus.DRAW
+                dealerBlackJack && !playerBlackJack -> PlayerResultStatus.PLAYER_LOSE
+
+                // 버스트 판별
+                playerBust -> PlayerResultStatus.PLAYER_LOSE
+                dealerBust -> PlayerResultStatus.PLAYER_WIN
+
+                // 점수 비교
+                playerTotalSum > dealerTotalSum -> PlayerResultStatus.PLAYER_WIN
+                playerTotalSum < dealerTotalSum -> PlayerResultStatus.PLAYER_LOSE
+                else -> PlayerResultStatus.DRAW
+            }
+        return gameStatus
+    }
 
     companion object {
         const val PLAYER_HIT_THRESHOLD = 21
