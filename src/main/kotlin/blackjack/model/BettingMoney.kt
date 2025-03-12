7a81115b308f6b30a1cf6375cd75aeecce71c324
m6z1 <sonmyungj1zz@gmail.com>
2025-03-12 16:12:46 +0900

    feat: 베팅 금액 범위 예외 구현

---
+++ src/main/kotlin/blackjack/model/BettingMoney.kt
@@ -0,0 +1,9 @@
+package blackjack.model
+
+class BettingMoney(
+    private val money: Int,
+) {
+    init {
+        require(money in 1..300_000_000) { "베팅 금액은 최소 1원 이상 최대 3억원까지 가능합니다. $money 는 범위에 해당되지 않습니다." }
+    }
+}
