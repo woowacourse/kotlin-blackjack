69678a5245fc385cd629c4dece47cb6ce4f32050
박지민 <pjm5489@naver.com>
2025-03-13 00:44:36 +0900

    test:  플레이어만 초기 카드 점수가 블랙잭일 때 베팅 금액 반환 테스트 코드 추가

--- src/test/kotlin/model/GameResultDeciderTest.kt
+++ src/test/kotlin/model/GameResultDeciderTest.kt
@@ -1,6 +1,7 @@
 package model
 
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
 import java.util.stream.Stream
@@ -24,6 +25,31 @@ class GameResultDeciderTest {
 
         Assertions.assertEquals(-10000f, winOrLose.playerResults[0].profit)
     }
+
+    @Test
+    fun `딜러의 처음 두 장의 카드 합이 21이 아니며 플레이어의 처음 두 장의 카드 합이 21일 경우 플레이어는 베팅 금액의 1_5배를 받는다`() {
+        val dealer = Dealer(
+            Cards(
+                listOf(
+                    Card(CardRank.QUEEN, Shape.CLUB),
+                    Card(CardRank.JACK, Shape.SPADE),
+                )
+            )
+        )
+        val playerCards = Cards(
+            listOf(
+                Card(CardRank.QUEEN, Shape.CLUB),
+                Card(CardRank.ACE, Shape.SPADE),
+            )
+        )
+        val winOrLose = GameResultDecider(
+            dealer, Players(listOf(Player("jay", playerCards, 10000f)))
+        ).compareWinOrLose()
+
+        Assertions.assertEquals(15000f, winOrLose.playerResults[0].profit)
+
+    }
+
 
     companion object {
         @JvmStatic
