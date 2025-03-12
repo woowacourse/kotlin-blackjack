281ea7ea4851d67f6fffcca846d1b372cf32e707
gahyunkim <mapledt001@naver.com>
2025-03-12 23:01:52 +0900

    feat: receiveCards 함수 구현
    - 원하는 개수만큼 카드를 받고, 저장하도록 하는 코드 구현

--- src/main/kotlin/model/Participant.kt
+++ src/main/kotlin/model/Participant.kt
@@ -1,15 +1,21 @@
 package model
 
 abstract class Participant(private val hand: Hand) {
-    abstract fun performTurn(cardDistributor: CardDistributor): Boolean
+    val cards: List<Card> get() = hand.handCards
 
     abstract fun decideToHit(): Boolean
 
     fun getScore(): Int = hand.getScore()
 
-    fun addCard(card: Card) {
-        hand.addCard(card)
+    fun receiveCards(getCards: (Int) -> List<Card>) {
+        val count = if (cards.isEmpty()) INITIAL_DRAW_COUNT else DEFAULT_DRAW_COUNT
+        hand.addCards(getCards(count))
     }
 
     fun getHand(): Hand = hand
+
+    companion object {
+        private const val INITIAL_DRAW_COUNT = 2
+        private const val DEFAULT_DRAW_COUNT = 1
+    }
 }
