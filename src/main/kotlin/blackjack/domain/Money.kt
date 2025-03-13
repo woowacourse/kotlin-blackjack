05513101bed408c1dd32a9aa7534101339da9a2a
rosemin928 <121722789+rosemin928@users.noreply.github.com>
2025-03-13 16:43:35 +0900

    feat: Money 클래스 생성

---
+++ src/main/kotlin/blackjack/domain/Money.kt
@@ -0,0 +1,14 @@
+package blackjack.domain
+
+class Money(
+    val value: Int,
+) {
+    init {
+        require(value >= MINIMUM_BETTING_MONEY) { ERROR_UNDER_MINIMUM_BETTING_MONEY_MESSAGE }
+    }
+
+    companion object {
+        const val ERROR_UNDER_MINIMUM_BETTING_MONEY_MESSAGE = "[ERROR] 최소 금액은 1000원입니다."
+        const val MINIMUM_BETTING_MONEY = 1000
+    }
+}
