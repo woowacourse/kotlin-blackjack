1b2378ac930beeaca19c3dca56281cd03013185e
medAndro <med4ndro@gmail.com>
2025-03-14 00:50:20 +0900

    refactor: HandCards를 한번에 초기화 하도록 테스트 코드 반영

--- src/test/kotlin/blackjack/domain/model/card/HandCardsTest.kt
+++ src/test/kotlin/blackjack/domain/model/card/HandCardsTest.kt
@@ -15,11 +15,7 @@ class HandCardsTest {
 
     @Test
     fun `특정 인덱스의 카드를 반환할 수 있다`() {
-        val handCards = HandCards()
-
-        handCards.addCard(Card(Number.ACE))
-        handCards.addCard(Card(Number.QUEEN))
-        handCards.addCard(Card(Number.SEVEN))
+        val handCards = HandCards(mutableListOf(Card(Number.ACE), Card(Number.QUEEN), Card(Number.SEVEN)))
 
         assertThat(handCards.getCardByIndex(1)).isEqualTo(Card(Number.QUEEN))
     }
@@ -28,7 +24,7 @@ class HandCardsTest {
     fun `카드가 만들 수 있는 최선의 값을 반환할 수 있다1`() {
         val handCards = HandCards()
 
-        handCards.addCard(Card(Number.ACE)) // 11
+        handCards.addCard(Card(Number.ACE)) // 11 (1취급시 최선의 값x)
         handCards.addCard(Card(Number.QUEEN)) // 10
 
         assertThat(handCards.calculateBestCardValue()).isEqualTo(21)
