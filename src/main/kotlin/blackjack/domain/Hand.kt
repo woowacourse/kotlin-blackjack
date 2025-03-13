b461cb8b2491b42d99a7207c4667bff222832ec0
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-03-13 17:09:43 +0900

    refactor: make constant

--- src/main/kotlin/blackjack/domain/Hand.kt
+++ src/main/kotlin/blackjack/domain/Hand.kt
@@ -17,7 +17,11 @@ class Hand(
     val canHit: Boolean get() = score is Score.Hittable
 
     fun draw(card: Card) {
-        require(canHit) { "모든 카드의 합이 21 미만이 될 수 있을 경우에만 카드를 얻을 수 있습니다." }
+        require(canHit) { MESSAGE_CAN_HIT_WHEN_SCORE_UNDER_MAX_SCORE }
         cards = cards.plus(card)
+    }
+
+    companion object {
+        private const val MESSAGE_CAN_HIT_WHEN_SCORE_UNDER_MAX_SCORE = "모든 카드의 합이 21 미만이 될 수 있을 경우에만 카드를 얻을 수 있습니다."
     }
 }
