952d6d21f063b8fc22960756e742e5d599953239
HamBeomJoon <hbj0802@naver.com>
2025-03-13 17:11:01 +0900

    test: BetAmount 테스트 추가

---
+++ src/test/kotlin/blackjack/model/BetAmountTest.kt
@@ -0,0 +1,28 @@
+package blackjack.model
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+import org.assertj.core.api.Assertions.assertThat
+
+
+class BetAmountTest {
+    @ParameterizedTest
+    @ValueSource(ints = [0, -1, -999])
+    fun `배팅 금액이 0원 이하면 예외를 던진다`(amount: Int) {
+        assertThrows<IllegalArgumentException> {
+            BetAmount(amount)
+        }
+    }
+
+    @Test
+    fun `배팅금액은 Double을 반환할 수 있다`() {
+        val amount = BetAmount(100)
+        val expect = 100.0
+
+        val actual = amount.toDouble()
+
+        assertThat(actual).isEqualTo(expect)
+    }
+}
