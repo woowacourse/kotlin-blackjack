25a8c43d8090141ba31a6be48356cf88a91e0bb6
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-03-13 15:03:29 +0900

    refactor: merge Cards into Hand

--- src/main/kotlin/blackjack/domain/Hand.kt
+++ src/main/kotlin/blackjack/domain/Hand.kt
@@ -1,17 +1,26 @@
 package blackjack.domain
 
-class Hand {
-    private val _cards = Cards()
-    val cards: List<Card> = _cards.value
+class Hand(
+    private val onBusted: () -> Unit,
+) {
+    var cards: List<Card> = listOf()
+        private set
 
-    fun getScore(onBusted: () -> Unit): Score {
-        if (_cards.score is Score.Bust) {
-            onBusted()
+    val score: Score
+        get() {
+            val score: Score = Score(cards)
+            if (score is Score.Bust) {
+                onBusted()
+            }
+            return score
         }
-        return _cards.score
-    }
 
     fun draw(card: Card) {
-        _cards.add(card)
+        cards = cards.plus(card)
+    }
+
+    fun add(card: Card) {
+        require(score is Score.Hittable && score.value < 21) { "모든 카드의 합이 21 미만이 될 수 있을 경우에만 카드를 얻을 수 있습니다." }
+        cards = cards.plus(card)
     }
 }
--- src/main/kotlin/blackjack/domain/Participant.kt
+++ src/main/kotlin/blackjack/domain/Participant.kt
@@ -2,9 +2,9 @@ package blackjack.domain
 
 abstract class Participant {
     protected abstract val onBusted: () -> Unit
-    protected val hand: Hand = Hand()
+    protected val hand: Hand = Hand(onBusted)
     val cards: List<Card> get() = hand.cards
-    val score: Score get() = hand.getScore(onBusted)
+    val score: Score get() = hand.score
 
     fun draw(card: Card) {
         hand.draw(card)
