63f38febf693dcbf0439e8c58fe5c219b3ac9995
medAndro <med4ndro@gmail.com>
2025-03-13 22:21:20 +0900

    refactor: Number의 orderNumber 프로퍼티 삭제

--- src/main/kotlin/blackjack/domain/model/card/Number.kt
+++ src/main/kotlin/blackjack/domain/model/card/Number.kt
@@ -1,30 +1,31 @@
 package blackjack.domain.model.card
 
+import kotlin.runCatching
+
 enum class Number(
     val value: List<Int>,
-    val orderNumber: Int,
     val initial: String,
 ) {
-    ACE(listOf(1, 11), 1, "A"),
-    TWO(listOf(2), 2, "2"),
-    THREE(listOf(3), 3, "3"),
-    FOUR(listOf(4), 4, "4"),
-    FIVE(listOf(5), 5, "5"),
-    SIX(listOf(6), 6, "6"),
-    SEVEN(listOf(7), 7, "7"),
-    EIGHT(listOf(8), 8, "8"),
-    NINE(listOf(9), 9, "9"),
-    TEN(listOf(10), 10, "10"),
-    JACK(listOf(10), 11, "J"),
-    QUEEN(listOf(10), 12, "Q"),
-    KING(listOf(10), 13, "K"),
+    ACE(listOf(1, 11), "A"),
+    TWO(listOf(2), "2"),
+    THREE(listOf(3), "3"),
+    FOUR(listOf(4), "4"),
+    FIVE(listOf(5), "5"),
+    SIX(listOf(6), "6"),
+    SEVEN(listOf(7), "7"),
+    EIGHT(listOf(8), "8"),
+    NINE(listOf(9), "9"),
+    TEN(listOf(10), "10"),
+    JACK(listOf(10), "J"),
+    QUEEN(listOf(10), "Q"),
+    KING(listOf(10), "K"),
     ;
 
     companion object {
         fun getByOrderNumber(orderNumber: Int): Number =
-            entries.find { it.orderNumber == orderNumber } ?: throw IllegalArgumentException(
-                ERROR_UNKNOWN_CARD_ORDER_NUMBER,
-            )
+            runCatching {
+                entries[orderNumber - 1]
+            }.getOrElse { throw IllegalArgumentException(ERROR_UNKNOWN_CARD_ORDER_NUMBER) }
 
         const val MAX_ORDER_NUMBER = 13
 
