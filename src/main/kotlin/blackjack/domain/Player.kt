602c108436e0d191469a2c0aec46fb833117a45e
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-03-13 19:36:37 +0900

    refactor: player has betting and can calculate profit

--- src/main/kotlin/blackjack/domain/Player.kt
+++ src/main/kotlin/blackjack/domain/Player.kt
@@ -2,14 +2,15 @@ package blackjack.domain
 
 class Player(
     val name: String,
+    private val betting: Betting,
 ) : Playable {
+    private val hand: Hand = Hand(onBusted = { state = ParticipantState.LOSE })
     override val cards: List<Card> get() = hand.cards
     override val score: Score get() = hand.score
     var state: ParticipantState = ParticipantState.PLAYING
         private set
     val canHit: Boolean get() = hand.canHit
-
-    private val hand: Hand = Hand(onBusted = { state = ParticipantState.LOSE })
+    val profit: Int get() = betting.toProfit(state, score)
 
     fun setResult(dealerScore: Score) {
         this.state =
