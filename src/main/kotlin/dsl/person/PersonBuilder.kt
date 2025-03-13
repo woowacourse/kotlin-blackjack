a5223832c7047a422e4e2d02e2ec8a268d337ca6
chanho0908 <uni070@naver.com>
2025-03-13 12:45:25 +0900

    refactor : DSL 빈 입력값에 대한 에러처리 추가

--- src/main/kotlin/dsl/person/PersonBuilder.kt
+++ src/main/kotlin/dsl/person/PersonBuilder.kt
@@ -5,14 +5,21 @@ class PersonBuilder {
     private lateinit var company: String
 
     fun name(value: String) {
+        require(value.isNotEmpty()) { NAME_EMPTY }
         name = value
     }
 
     fun company(value: String) {
+        require(value.isNotEmpty()) { NAME_COMPANY }
         company = value
     }
 
     fun build(): Person {
         return Person(name, company)
+    }
+
+    companion object {
+        private const val NAME_EMPTY = "빈 이름이 입력 되었습니다. 이름을 입력해 주세요."
+        private const val NAME_COMPANY = "빈 회사명이 입력 되었습니다. 회사명을 입력해 주세요."
     }
 }
--- src/main/kotlin/dsl/skill/SkillBuilder.kt
+++ src/main/kotlin/dsl/skill/SkillBuilder.kt
@@ -5,14 +5,21 @@ class SkillBuilder {
     private lateinit var hard: String
 
     fun soft(value: String) {
+        require(value.isNotEmpty()) { SOFT_SKILL_EMPTY }
         soft = value
     }
 
     fun hard(value: String) {
+        require(value.isNotEmpty()) { HARD_SKILL_EMPTY }
         hard = value
     }
 
     fun build(): Skills {
         return Skills(soft, hard)
+    }
+
+    companion object {
+        private const val SOFT_SKILL_EMPTY = "빈 소프트 스킬 이름이 입력 되었습니다. 소프트 스킬을 입력해 주세요."
+        private const val HARD_SKILL_EMPTY = "빈 하드 스킬 이름이 입력 되었습니다. 하드 스킬을 입력해 주세요."
     }
 }
