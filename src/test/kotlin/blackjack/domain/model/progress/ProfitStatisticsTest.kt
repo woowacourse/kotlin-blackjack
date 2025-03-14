a92c1762f4280c60e9bac537facf67b119a547ba
medAndro <med4ndro@gmail.com>
2025-03-14 19:04:19 +0900

    test: 딜러의 수익률 테스트 코드 추가

--- src/test/kotlin/blackjack/domain/model/progress/ProfitStatisticsTest.kt
+++ src/test/kotlin/blackjack/domain/model/progress/ProfitStatisticsTest.kt
@@ -7,24 +7,30 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class ProfitStatisticsTest {
+    val meda = Player("메다", listOf(Card(Number.TEN), Card(Number.NINE))) // NORMAL
+    val cream = Player("크림", List(3) { Card(Number.TEN) }) // BUST
+    val peto = Player("페토", listOf(Card(Number.QUEEN), Card(Number.ACE))) // BLACKJACK
+
+    val betHistory = BetHistory(mutableMapOf(meda to 1000, cream to 2000, peto to 3000))
+    val winLossStatistics =
+        WinLossStatistics(
+            _playerWinLoseInfo =
+                mutableMapOf(
+                    meda to WinLoss.WIN,
+                    cream to WinLoss.LOSE,
+                    peto to WinLoss.WIN,
+                ),
+        )
+
     @Test
     fun `참가자별 수익을 계산한다`() {
-        val meda = Player("메다", listOf(Card(Number.TEN), Card(Number.NINE))) // NORMAL
-        val cream = Player("크림", List(3) { Card(Number.TEN) }) // BUST
-        val peto = Player("페토", listOf(Card(Number.QUEEN), Card(Number.ACE))) // BLACKJACK
-
-        val betHistory = BetHistory(mutableMapOf(meda to 1000, cream to 2000, peto to 3000))
-        val winLossStatistics =
-            WinLossStatistics(
-                _playerWinLoseInfo =
-                    mutableMapOf(
-                        meda to WinLoss.WIN,
-                        cream to WinLoss.LOSE,
-                        peto to WinLoss.WIN,
-                    ),
-            )
-
         val profitStatistics = ProfitStatistics(betHistory, winLossStatistics)
-        assertThat(profitStatistics.profits).isEqualTo(mapOf(meda to 1000f, cream to -2000f, peto to 4500f))
+        assertThat(profitStatistics.playerProfits).isEqualTo(mapOf(meda to 1000f, cream to -2000f, peto to 4500f))
+    }
+
+    @Test
+    fun `딜러의 수익을 계산한다`() {
+        val profitStatistics = ProfitStatistics(betHistory, winLossStatistics)
+        assertThat(profitStatistics.dealerProfits).isEqualTo(-3500f)
     }
 }
