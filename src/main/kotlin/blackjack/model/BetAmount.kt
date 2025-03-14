b4763a12586d2cdc3b15f808fe5679f767ef4b89
HamBeomJoon <hbj0802@naver.com>
2025-03-14 09:47:20 +0900

    refactor: BetAmount Value class로 변경

--- src/main/kotlin/blackjack/model/BetAmount.kt
+++ src/main/kotlin/blackjack/model/BetAmount.kt
@@ -1,13 +1,10 @@
 package blackjack.model
 
-data class BetAmount(private val amount: Int) {
+@JvmInline
+value class BetAmount(val amount: Int) {
     init {
         require(amount > 0) { ERROR_BET_AMOUNT_VALUE }
     }
-
-    fun toDouble(): Double = amount.toDouble()
-
-    operator fun times(multiplier: Double): Double = amount * multiplier
 
     companion object {
         private const val ERROR_BET_AMOUNT_VALUE = "배팅 금액은 1원 이상이어야 합니다."
--- src/test/kotlin/blackjack/model/BetAmountTest.kt
+++ src/test/kotlin/blackjack/model/BetAmountTest.kt
@@ -1,7 +1,5 @@
 package blackjack.model
 
-import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
@@ -13,25 +11,5 @@ class BetAmountTest {
         assertThrows<IllegalArgumentException> {
             BetAmount(amount)
         }
-    }
-
-    @Test
-    fun `배팅금액은 Double을 반환할 수 있다`() {
-        val amount = BetAmount(100)
-        val expect = 100.0
-
-        val actual = amount.toDouble()
-
-        assertThat(actual).isEqualTo(expect)
-    }
-
-    @Test
-    fun `배팅금액에 *로 곱셈연산을 할 수 있다`() {
-        val amount = BetAmount(100)
-        val expect = 150.0
-
-        val actual = amount * 1.5
-
-        assertThat(actual).isEqualTo(expect)
     }
 }
