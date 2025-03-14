a8280f5751bbca50defb996deaf4edd068dbd9ac
gahyunkim <mapledt001@naver.com>
2025-03-14 16:35:42 +0900

    refactor: BettingManager 코드 조건 추가 및 수정

--- src/main/kotlin/model/BettingManager.kt
+++ src/main/kotlin/model/BettingManager.kt
@@ -7,10 +7,15 @@ class BettingManager {
         player: Player,
         amount: Int,
     ) {
+        require(amount > 0) { INITIAL_BETTING_AMOUNT_MESSAGE }
         bets[player] = amount
     }
 
-    fun getProfit(player: Player): Int  {
+    fun getProfit(player: Player): Int {
         return bets[player] ?: 0
+    }
+
+    companion object {
+        private const val INITIAL_BETTING_AMOUNT_MESSAGE = "베팅 금액은 0보다 커야합니다."
     }
 }
