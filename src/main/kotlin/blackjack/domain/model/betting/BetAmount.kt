06f7f34f3396320fe1cbd411e6149a00add12a48
ijh1298 <imjh0417@gmail.com>
2025-03-15 23:20:10 +0900

    refactor: BetAmount에서 Profit을 구하는 메서드를 BetRecord에서 분리하여 BetAmount로 이전

--- src/main/kotlin/blackjack/domain/model/betting/BetAmount.kt
+++ src/main/kotlin/blackjack/domain/model/betting/BetAmount.kt
@@ -1,14 +1,30 @@
 package blackjack.domain.model.betting
 
+import blackjack.domain.model.GameResult
+import blackjack.domain.model.GameResult.BLACKJACK_WIN
+import blackjack.domain.model.GameResult.DRAW
+import blackjack.domain.model.GameResult.LOSE
+import blackjack.domain.model.GameResult.WIN
+
 @JvmInline
 value class BetAmount(
-    val value: Double,
+    private val value: Double,
 ) {
     init {
         require(value >= 0) { ERROR_SHOULD_BE_MORE_THAN_ZERO }
     }
 
+    fun toProfit(gameResult: GameResult): Double =
+        when (gameResult) {
+            BLACKJACK_WIN -> value * BLACKJACK_RATE
+            WIN -> value
+            DRAW -> DRAW_RATE
+            LOSE -> -value
+        }
+
     companion object {
+        private const val BLACKJACK_RATE = 1.5
+        private const val DRAW_RATE = 0.0
         const val ERROR_SHOULD_BE_MORE_THAN_ZERO = "베팅 금액은 0원 이상이어야 합니다."
     }
 }
--- src/main/kotlin/blackjack/domain/model/betting/BetRecord.kt
+++ src/main/kotlin/blackjack/domain/model/betting/BetRecord.kt
@@ -1,9 +1,6 @@
 package blackjack.domain.model.betting
 
-import blackjack.domain.model.GameResult.BLACKJACK_WIN
-import blackjack.domain.model.GameResult.DRAW
-import blackjack.domain.model.GameResult.LOSE
-import blackjack.domain.model.GameResult.WIN
+import blackjack.domain.model.GameResult
 import blackjack.domain.model.participant.Dealer
 import blackjack.domain.model.participant.Player
 
@@ -12,19 +9,8 @@ class BetRecord(
     private val betAmount: BetAmount,
 ) {
     fun profit(dealer: Dealer): Map<Player, Double> {
-        val betAmount: Double = betAmount.value
-        val profit: Double =
-            when (player.compareTo(dealer)) {
-                BLACKJACK_WIN -> betAmount * BLACKJACK_RATE
-                WIN -> betAmount
-                DRAW -> DRAW_RATE
-                LOSE -> -betAmount
-            }
+        val gameResult: GameResult = player.compareTo(dealer)
+        val profit: Double = betAmount.toProfit(gameResult)
         return mapOf(player to profit)
-    }
-
-    companion object {
-        private const val BLACKJACK_RATE = 1.5
-        private const val DRAW_RATE = 0.0
     }
 }
