6a1a86585fce15f3086a6cfb2a93c7df1a63c61c
jerry8282 <ka06196@naver.com>
2025-03-15 20:30:13 +0900

    feat: BlackJack인지 여부에 따라 승패 판단 기준 추가

--- src/main/kotlin/blackjack/model/domain/participant/Player.kt
+++ src/main/kotlin/blackjack/model/domain/participant/Player.kt
@@ -2,21 +2,25 @@ package blackjack.model.domain.participant
 
 import blackjack.model.domain.GameResult
 import blackjack.model.domain.card.Hand
+import blackjack.model.domain.card.Status
 
 data class Player(override val name: String) : Participants() {
     override val hand: Hand = Hand(mutableListOf())
 
     override fun canHit(): Boolean {
-        return hand.isBust()
+        return hand.status == Status.BUST
     }
 
     fun compareScores(
-        isDealerBust: Boolean,
+        status: Status,
         number: Int,
     ): GameResult =
         when {
-            hand.isBust() -> GameResult.Lose
-            isDealerBust -> GameResult.Win
+            hand.status == Status.BUST -> GameResult.Lose
+            status == Status.BUST -> GameResult.Win
+            status == Status.BLACKJACK && hand.status == Status.BLACKJACK -> GameResult.Draw
+            status == Status.BLACKJACK -> GameResult.Lose
+            hand.status == Status.BLACKJACK -> GameResult.Win
             else -> GameResult.compare(sumCardNumber, number)
         }
 }
