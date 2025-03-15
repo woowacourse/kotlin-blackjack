435c588683c07dfcd9c5c728c4dec6f1b7fa2f64
yrsel <xxknu88@gmail.com>
2025-03-15 09:54:11 +0900

    feat: 결과 맞는 금액 반환

--- src/main/kotlin/blackjack/model/BetMoney.kt
+++ src/main/kotlin/blackjack/model/BetMoney.kt
@@ -3,4 +3,12 @@ package blackjack.model
 @JvmInline
 value class BetMoney(
     val amount: Long,
-)
+) {
+    fun toBlackjackMoney(): BetMoney = BetMoney((amount * 1.5).toLong())
+
+    fun toWinMoney(): BetMoney = BetMoney(amount)
+
+    fun toDrawMoney(): BetMoney = BetMoney(0L)
+
+    fun toLoseMoney(): BetMoney = BetMoney(amount * -1)
+}
---
+++ src/test/kotlin/blackjack/model/BetMoneyTest.kt
@@ -0,0 +1,46 @@
+package blackjack.model
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class BetMoneyTest {
+    @Test
+    fun `블랙잭은 배팅금액의 150% 금액을 얻는다`() {
+        val betMoney = BetMoney(1_000L)
+        val expected = BetMoney(1_500L)
+
+        val actual = betMoney.toBlackjackMoney()
+
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `승리시 배팅금액의 100% 금액을 얻는다`() {
+        val betMoney = BetMoney(1_000L)
+        val expected = BetMoney(1_000L)
+
+        val actual = betMoney.toWinMoney()
+
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `비길 경우 배팅금액의 0% 금액을 얻는다`() {
+        val betMoney = BetMoney(1_000L)
+        val expected = BetMoney(0L)
+
+        val actual = betMoney.toDrawMoney()
+
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `Bust 또는 질 경우 배팅금액의 100% 금액을 잃는다`() {
+        val betMoney = BetMoney(1_000L)
+        val expected = BetMoney(-1_000L)
+
+        val actual = betMoney.toLoseMoney()
+
+        assertThat(actual).isEqualTo(expected)
+    }
+}
