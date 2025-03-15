8cdc5e3dd700dad54941f9a0903220a16d263e2c
DoableTuple <doabletuple@gmail.com>
2025-03-15 22:59:51 +0900

    refactor: 최소 베팅 금액 상수 처리

--- src/main/kotlin/blackjack/domain/model/Bet.kt
+++ src/main/kotlin/blackjack/domain/model/Bet.kt
@@ -2,10 +2,11 @@ package blackjack.domain.model
 
 class Bet(val amount: Int) {
     init {
-        require(amount >= 0) { ERROR_MESSAGE_BET_NOT_POSITIVE }
+        require(amount >= MIN_BET_AMOUNT) { ERROR_MESSAGE_BET_NOT_POSITIVE }
     }
 
     companion object {
-        private const val ERROR_MESSAGE_BET_NOT_POSITIVE = "베팅 금액은 0보다 작을 수 없습니다."
+        private const val MIN_BET_AMOUNT = 0
+        private const val ERROR_MESSAGE_BET_NOT_POSITIVE = "베팅 금액은 ${MIN_BET_AMOUNT}보다 작을 수 없습니다."
     }
 }
