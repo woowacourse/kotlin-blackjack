227acc13ad064ab6957820c01a98f5e639b4924a
junseo511 <prime54@g.skku.edu>
2025-03-16 03:17:00 +0900

    test: 이름 객체 관련 테스트 구현

--- src/test/kotlin/blackjack/domain/participant/NameTest.kt
+++ src/test/kotlin/blackjack/domain/participant/NameTest.kt
@@ -1,3 +1,25 @@
 package blackjack.domain.participant
 
-class NameTest
+import blackjack.model.participant.Name
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class NameTest {
+    @Test
+    fun `닉네임이 공백이면 오류를 반환한다`() {
+        // given & when & then
+        assertThrows<IllegalArgumentException> {
+            Name("")
+        }
+    }
+
+    @Test
+    fun `닉네임이 타마면 타마라는 이름를 가진 이름 객체를 반환한다`() {
+        // given & when
+        val name = Name("타마")
+
+        // then
+        assertThat(name.value).isEqualTo("타마")
+    }
+}
