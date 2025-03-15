b694736ff34e03000204a4e7b708e413d8825f8f
yrsel <xxknu88@gmail.com>
2025-03-15 23:35:28 +0900

    refactor: 테스트 전용 카드 주입 메서드 생성

--- src/test/kotlin/blackjack/ProfitCalculatorTest.kt
+++ src/test/kotlin/blackjack/ProfitCalculatorTest.kt
@@ -9,7 +9,9 @@ import blackjack.CardFixture.Companion.HEART_KING
 import blackjack.CardFixture.Companion.HEART_QUEEN
 import blackjack.model.Money
 import blackjack.model.ProfitCalculator
+import blackjack.model.card.Card
 import blackjack.model.user.Dealer
+import blackjack.model.user.Participant
 import blackjack.model.user.Player
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
@@ -27,9 +29,7 @@ class ProfitCalculatorTest {
 
     @Test
     fun `플레이어가 Bust 일 경우 원금을 잃는다`() {
-        player.addCard(CLOVER_JACK)
-        player.addCard(HEART_QUEEN)
-        player.addCard(HEART_KING)
+        player.drawCards(CLOVER_JACK, HEART_QUEEN, HEART_KING)
         val expected = Money(-1_000L)
 
         val actual = ProfitCalculator.calculateProfit(dealer, player)
@@ -39,8 +39,7 @@ class ProfitCalculatorTest {
 
     @Test
     fun `플레이어만 Blackjack 이면 원금의 150%를 얻는다`() {
-        player.addCard(CLOVER_JACK)
-        player.addCard(CLOVER_ACE)
+        player.drawCards(CLOVER_JACK, CLOVER_ACE)
         val expected = Money(1_500L)
 
         val actual = ProfitCalculator.calculateProfit(dealer, player)
@@ -50,10 +49,8 @@ class ProfitCalculatorTest {
 
     @Test
     fun `플레이어와 딜러가 Blackjack 이면 0원을 얻는다`() {
-        player.addCard(CLOVER_JACK)
-        player.addCard(CLOVER_ACE)
-        dealer.addCard(HEART_QUEEN)
-        dealer.addCard(HEART_ACE)
+        player.drawCards(CLOVER_JACK, CLOVER_ACE)
+        dealer.drawCards(HEART_QUEEN, HEART_ACE)
         val expected = Money(0L)
 
         val actual = ProfitCalculator.calculateProfit(dealer, player)
@@ -63,12 +60,8 @@ class ProfitCalculatorTest {
 
     @Test
     fun `딜러가 bust 이고 player가 bust가 아니면 원금을 얻는다`() {
-        player.addCard(CLOVER_ACE)
-        player.addCard(DIAMOND_ACE)
-        player.addCard(HEART_ACE)
-        dealer.addCard(HEART_QUEEN)
-        dealer.addCard(HEART_KING)
-        dealer.addCard(HEART_JACK)
+        player.drawCards(CLOVER_ACE, DIAMOND_ACE, HEART_ACE)
+        dealer.drawCards(HEART_QUEEN, HEART_KING, HEART_JACK)
         val expected = Money(1_000L)
 
         val actual = ProfitCalculator.calculateProfit(dealer, player)
@@ -78,14 +71,16 @@ class ProfitCalculatorTest {
 
     @Test
     fun `딜러와 Player 모두 bust, blackjack이 아니면 21에 가까운 점수를 기준으로 금액을 반환한다`() {
-        player.addCard(CLOVER_ACE)
-        player.addCard(DIAMOND_ACE)
-        dealer.addCard(HEART_KING)
-        dealer.addCard(HEART_JACK)
+        player.drawCards(CLOVER_ACE, DIAMOND_ACE)
+        dealer.drawCards(HEART_KING, HEART_JACK)
         val expected = Money(-1_000L)
 
         val actual = ProfitCalculator.calculateProfit(dealer, player)
 
         assertEquals(expected, actual)
+    }
+
+    private fun Participant.drawCards(vararg cards: Card) {
+        cards.forEach { card -> this.addCard(card) }
     }
 }
