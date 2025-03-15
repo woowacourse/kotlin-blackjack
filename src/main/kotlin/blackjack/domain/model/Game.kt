a059b841a212d198808a99de435c159a61555942
DoableTuple <doabletuple@gmail.com>
2025-03-15 23:59:38 +0900

    refactor: 변수 이름 변경

--- src/main/kotlin/blackjack/domain/model/Game.kt
+++ src/main/kotlin/blackjack/domain/model/Game.kt
@@ -12,9 +12,9 @@ class Game(
 ) {
     init {
         val playerNames: List<String> = players.map { player -> player.name }
-        require(playerNames == playerNames.distinct()) { MESSAGE_ERROR_PLAYER_NAMES_NOT_UNIQUE }
+        require(playerNames == playerNames.distinct()) { ERROR_MESSAGE_PLAYER_NAMES_NOT_UNIQUE }
         require(playerNames.none { playerName -> playerName == dealer.name }) {
-            MESSAGE_ERROR_PLAYER_NAME_SAME_AS_DEALER.format(dealer.name)
+            ERROR_MESSAGE_PLAYER_NAME_SAME_AS_DEALER.format(dealer.name)
         }
     }
 
@@ -39,7 +39,7 @@ class Game(
     }
 
     companion object {
-        private const val MESSAGE_ERROR_PLAYER_NAMES_NOT_UNIQUE = "플레이어들의 이름은 서로 중복될 수 없습니다."
-        private const val MESSAGE_ERROR_PLAYER_NAME_SAME_AS_DEALER = "플레이어의 이름은 딜러의 이름(\"%s\")과 같을 수 없습니다."
+        private const val ERROR_MESSAGE_PLAYER_NAMES_NOT_UNIQUE = "플레이어들의 이름은 서로 중복될 수 없습니다."
+        private const val ERROR_MESSAGE_PLAYER_NAME_SAME_AS_DEALER = "플레이어의 이름은 딜러의 이름(\"%s\")과 같을 수 없습니다."
     }
 }
