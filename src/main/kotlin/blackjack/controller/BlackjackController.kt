7d88744328a219c83e8552c63abfbf0d4a43022e
HamBeomJoon <hbj0802@naver.com>
2025-03-15 23:24:23 +0900

    refactor: 컨트롤러 비즈니스 로직 이동

--- src/main/kotlin/blackjack/controller/BlackjackController.kt
+++ src/main/kotlin/blackjack/controller/BlackjackController.kt
@@ -1,5 +1,6 @@
 package blackjack.controller
 
+import blackjack.model.BlackjackGame
 import blackjack.model.DrawChoice
 import blackjack.model.GameManager
 import blackjack.model.amount.BetAmount
@@ -12,15 +13,29 @@ class BlackjackController(
     private val inputView: InputView,
     private val outputView: OutputView,
 ) {
-    private lateinit var gameManager: GameManager
-
     fun play(dealer: Dealer) {
         val players = playerSetting()
+        val blackjackGame = BlackjackGame(dealer, players)
 
-        playBlackjack(dealer, players)
+        outputView.printInitialHandOutCardMessage(players)
+        startBlackjackGame(blackjackGame, dealer, players)
 
         outputView.printFinalHandStatus(dealer, players)
-        resultSummary(gameManager)
+        val result = blackjackGame.calculateResults()
+        outputView.printFinalResult(result)
+    }
+
+    private fun startBlackjackGame(
+        blackjackGame: BlackjackGame,
+        dealer: Dealer,
+        players: List<Player>
+    ) {
+        blackjackGame.startGame(
+            wantsToDraw = { player -> getValidDrawChoice(player) },
+            printPlayerHands = { player -> outputView.printPlayerHands(player) },
+            printDealerHandStatus = { status -> outputView.printDealerHandStatus(status) },
+            printAllHands = { _, _ -> outputView.printAllPlayerHands(dealer, players) }
+        )
     }
 
     private fun playerSetting(): List<Player> {
@@ -38,55 +53,13 @@ class BlackjackController(
         }
     }
 
-    private fun playBlackjack(
-        dealer: Dealer,
-        players: List<Player>,
-    ) {
-        outputView.printInitialHandOutCardMessage(players)
-        gameManager = GameManager(dealer, players)
-        gameManager.dealInitialCards()
-        outputView.printAllPlayerHands(dealer, players)
-
-        if (!dealer.isBlackjack()) {
-            playersDrawCards(players)
-            dealerDrawCards(dealer)
-        }
-    }
-
-    private fun playersDrawCards(players: List<Player>) {
-        players.forEach { player -> playerDrawOrStay(player) }
-    }
-
-    private fun playerDrawOrStay(player: Player) {
-        while (!player.isBust()) {
-            val playerChoice = inputView.readMoreCardCondition(player)
-            when (DrawChoice.from(playerChoice)) {
-                DrawChoice.YES -> {
-                    gameManager.drawCard(player)
-                    outputView.printPlayerHands(player)
-                }
-
-                DrawChoice.NO -> {
-                    outputView.printPlayerHands(player)
-                    break
-                }
-
-                null -> continue
+    private fun getValidDrawChoice(player: Player): DrawChoice {
+        while (true) {
+            val input = inputView.readMoreCardCondition(player)
+            val choice = DrawChoice.from(input)
+            if (choice != null) {
+                return choice
             }
         }
-    }
-
-    private fun dealerDrawCards(dealer: Dealer) {
-        val moreCard = dealer.isMoreCard()
-        if (moreCard) {
-            gameManager.drawCard(dealer)
-        }
-        outputView.printDealerHandStatus(moreCard)
-    }
-
-    private fun resultSummary(gameManager: GameManager) {
-        val result = gameManager.calculateResultMap()
-        val profitResult = gameManager.gameResult(result)
-        outputView.printFinalResult(profitResult)
     }
 }
---
+++ src/main/kotlin/blackjack/model/BlackjackGame.kt
@@ -0,0 +1,57 @@
+package blackjack.model
+
+import blackjack.model.participant.Dealer
+import blackjack.model.participant.Player
+
+class BlackjackGame(
+    private val dealer: Dealer,
+    private val players: List<Player>
+) {
+    private val gameManager = GameManager(dealer, players)
+
+    fun startGame(
+        wantsToDraw: (Player) -> DrawChoice,
+        printPlayerHands: (Player) -> Unit,
+        printDealerHandStatus: (Boolean) -> Unit,
+        printAllHands: (Dealer, List<Player>) -> Unit
+    ) {
+        gameManager.dealInitialCards()
+        printAllHands(dealer, players)
+
+        if (!dealer.isBlackjack()) {
+            players.forEach { drawCards(it, wantsToDraw, printPlayerHands) }
+            println()
+            dealerDrawCards(printDealerHandStatus)
+            println()
+        }
+    }
+
+    fun drawCards(
+        player: Player,
+        wantsToDraw: (Player) -> DrawChoice,
+        printPlayerHands: (Player) -> Unit
+    ) {
+        while (!player.isBust() && wantsToDraw(player) == DrawChoice.YES) {
+            gameManager.drawCard(player)
+            printPlayerHands(player)
+        }
+        printPlayerHands(player)
+    }
+
+    fun dealerDrawCards(printDealerHandStatus: (Boolean) -> Unit) {
+        val condition = dealer.isMoreCard()
+        if (!condition) {
+            printDealerHandStatus(false)
+        }
+
+        while (dealer.isMoreCard()) {
+            printDealerHandStatus(true)
+            gameManager.drawCard(dealer)
+        }
+    }
+
+    fun calculateResults(): List<Profit> {
+        return gameManager.gameResult(gameManager.calculateResultMap())
+    }
+
+}
