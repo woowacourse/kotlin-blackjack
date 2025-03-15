da2dad9e118aae50bcf78e7d00607915de433f85
gahyunkim <mapledt001@naver.com>
2025-03-15 16:12:23 +0900

    refactor: GameResultDecider 내용을 GameResult로 이동
    - 내부에서 GameResult에 따른 결과 계산하도록 구현
    - profit을 추가하여 승패에 맞은 profitRate를 반환하도록 구현

--- src/main/kotlin/model/GameResult.kt
+++ src/main/kotlin/model/GameResult.kt
@@ -1,8 +1,58 @@
 package model
 
-enum class GameResult {
-    WIN,
-    LOSE,
-    PUSH,
-    BLACKJACK,
+import kotlin.math.abs
+
+data class PlayerResult(val player: Player, val result: GameResult)
+
+
+enum class GameResult(val profit: Float) {
+    WIN(1f),
+    LOSE(-1f),
+    PUSH(0f),
+    BLACKJACK(1.5f),
+    ;
+
+    companion object {
+        const val BLACKJACK_SCORE = 21
+
+        fun compareWinOrLose(dealer: Dealer, players: Players): List<PlayerResult> {
+            val playerResults: List<PlayerResult> =
+                players.map { player ->
+                    PlayerResult(player, decideResult(dealer, player))
+                }
+            return playerResults
+        }
+
+        private fun decideResult(dealer: Dealer, player: Player): GameResult {
+            return when {
+                dealer.isBust() -> WIN
+                player.isBust() -> LOSE
+                dealer.isBlackJack() && player.isBlackJack() -> PUSH
+                player.isBlackJack() -> BLACKJACK
+                dealer.getTotalScore() == player.getTotalScore() -> PUSH
+                else -> compareScore(dealer, player)
+            }
+        }
+
+        private fun compareScore(dealer: Dealer, player: Player): GameResult {
+            val dealerDiff = abs(BLACKJACK_SCORE - dealer.getTotalScore())
+            val playerDiff = abs(BLACKJACK_SCORE - player.getTotalScore())
+            return when {
+                playerDiff < dealerDiff -> WIN
+                playerDiff > dealerDiff -> LOSE
+                else -> PUSH
+            }
+        }
+
+        fun decideProfitRates(playerResults: List<PlayerResult>): Map<Player, Float> {
+            return playerResults.associate { playerResult ->
+                playerResult.player to when (playerResult.result) {
+                    WIN -> WIN.profit
+                    LOSE -> LOSE.profit
+                    BLACKJACK -> BLACKJACK.profit
+                    PUSH -> PUSH.profit
+                }
+            }
+        }
+    }
 }
