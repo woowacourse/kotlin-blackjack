2e9a0d3a1f749259dda3de84ddf00f3da5da5480
gahyunkim <mapledt001@naver.com>
2025-03-15 17:32:53 +0900

    feat: ProfitCalculator 클래스 구현

---
+++ src/main/kotlin/model/ProfitCalculator.kt
@@ -0,0 +1,31 @@
+package model
+
+class ProfitCalculator {
+    private fun getProfitRate(playerResult: PlayerResult): Float {
+        return when (playerResult.result) {
+            GameResult.WIN -> GameResult.WIN.profitRate
+            GameResult.LOSE -> GameResult.LOSE.profitRate
+            GameResult.PUSH -> GameResult.PUSH.profitRate
+            GameResult.BLACKJACK -> GameResult.BLACKJACK.profitRate
+        }
+    }
+
+    private fun calculateProfitRates(playerResults: List<PlayerResult>): Map<Player, Float> {
+        return playerResults.associate { playerResult ->
+            playerResult.player to getProfitRate(playerResult)
+        }
+    }
+
+    fun calculateFinalProfits(
+        playerResults: List<PlayerResult>,
+        bettingManager: BettingManager,
+        players: Players
+    ): Map<Player, Int> {
+        val profitRates = calculateProfitRates(playerResults)
+        return players.associateWith { player ->
+            val baseBet = bettingManager.getProfit(player)
+            val multiplier = profitRates[player] ?: 0f
+            (baseBet * multiplier).toInt()
+        }
+    }
+}
