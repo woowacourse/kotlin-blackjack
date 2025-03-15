55276a17390ed6ec564d173b8c3c363348f2de99
yrsel <xxknu88@gmail.com>
2025-03-15 23:35:07 +0900

    feat: 화면 출력용 함수 분리

--- src/main/kotlin/blackjack/view/OutputView.kt
+++ src/main/kotlin/blackjack/view/OutputView.kt
@@ -2,27 +2,9 @@ package blackjack.view
 
 import blackjack.model.ScoreCalculator
 import blackjack.model.card.Card
-import blackjack.model.card.CardNumber
-import blackjack.model.card.CardNumber.ACE
-import blackjack.model.card.CardNumber.EIGHT
-import blackjack.model.card.CardNumber.FIVE
-import blackjack.model.card.CardNumber.FOUR
-import blackjack.model.card.CardNumber.JACK
-import blackjack.model.card.CardNumber.KING
-import blackjack.model.card.CardNumber.NINE
-import blackjack.model.card.CardNumber.QUEEN
-import blackjack.model.card.CardNumber.SEVEN
-import blackjack.model.card.CardNumber.SIX
-import blackjack.model.card.CardNumber.TEN
-import blackjack.model.card.CardNumber.THREE
-import blackjack.model.card.CardNumber.TWO
 import blackjack.model.card.Deck.Companion.INITIAL_HAND_OUT_CARD_COUNT
-import blackjack.model.card.Shape
-import blackjack.model.card.Shape.CLOVER
-import blackjack.model.card.Shape.DIAMOND
-import blackjack.model.card.Shape.HEART
-import blackjack.model.card.Shape.SPADE
 import blackjack.model.dto.ParticipantProfitInfo
+import blackjack.model.toUi
 import blackjack.model.user.Dealer
 import blackjack.model.user.Player
 
@@ -98,33 +80,6 @@ class OutputView : BlackjackOutput {
     private fun printContentSeparator() {
         println()
     }
-
-    private fun Card.toUi(): String = "${this.number.toUi()}${this.shape.toUi()}"
-
-    private fun CardNumber.toUi(): String =
-        when (this) {
-            ACE -> "A"
-            TWO -> "2"
-            THREE -> "3"
-            FOUR -> "4"
-            FIVE -> "5"
-            SIX -> "6"
-            SEVEN -> "7"
-            EIGHT -> "8"
-            NINE -> "9"
-            TEN -> "10"
-            JACK -> "J"
-            QUEEN -> "Q"
-            KING -> "K"
-        }
-
-    private fun Shape.toUi(): String =
-        when (this) {
-            SPADE -> "스페이드"
-            DIAMOND -> "다이아몬드"
-            HEART -> "하트"
-            CLOVER -> "클로버"
-        }
 
     companion object {
         private const val INITIAL_HAND_OUT_CARD_MESSAGE_FORMAT = "\n딜러와 %s에게 %d장의 카드를 나누어 주었습니다."
