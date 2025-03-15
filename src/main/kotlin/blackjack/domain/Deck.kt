6cdc98117d2b23f246247afc868425ad33d89a26
지원 <cheerfulboys@naver.com>
2025-03-15 23:18:49 +0900

    feat: 카드 덱 기능 구현

---
+++ src/main/kotlin/blackjack/domain/Deck.kt
@@ -0,0 +1,22 @@
+package blackjack.domain
+
+import blackjack.domain.card.Card
+import blackjack.domain.card.Denomination
+import blackjack.domain.card.Suit
+
+class Deck {
+    private var _cards: ArrayDeque<Card> = create()
+    val cards: List<Card> get() = _cards
+
+    private fun create(): ArrayDeque<Card> {
+        val cards = Suit.entries.flatMap<Suit, Card> { suit -> Denomination.entries.map{ denomination -> Card( suit = suit, denomination = denomination) } }.shuffled().toCollection(ArrayDeque())
+        return cards
+    }
+
+    fun pick(): Card {
+        if (_cards.isEmpty()) {
+            _cards = create()
+        }
+        return _cards.removeLast()
+    }
+}
---
+++ src/test/kotlin/blackjack/domain/DeckTest.kt
@@ -0,0 +1,36 @@
+package blackjack.domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class DeckTest {
+    @Test
+    fun `덱은 카드 한 장을 뽑을 수 있다`() {
+        val deck = Deck()
+        val drawnCard = deck.pick()
+        assertThat(deck.cards.contains(drawnCard)).isFalse()
+    }
+
+    @Test
+    fun `덱은 52장을 모두 뽑은 후 카드를 뽑으면 새로운 덱을 사용한다`() {
+        val deck = Deck()
+        repeat(53) {
+            deck.pick()
+        }
+        assertThat(deck.cards.size).isEqualTo(51)
+    }
+
+    // 리뷰 받은 부분 확인용
+//    @Test
+//    fun `덱 clear`() {
+//        val deck = Deck()
+//        deck.cards.remove()
+//        deck.cards.remove()
+//        deck.cards.clear()
+//        deck.pick()
+//        deck.cards.remove()
+//        deck.pick()
+//        deck.cards.remove()
+//        assertThat(deck.cards.size).isEqualTo(51)
+//    }
+}
