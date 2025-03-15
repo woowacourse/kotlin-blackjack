ae5220243b83ee4cee45466cac2b981f67030df9
yrsel <xxknu88@gmail.com>
2025-03-15 11:04:33 +0900

    feat: 수익 계산 객체 로직 구현

---
+++ src/main/kotlin/blackjack/model/ProfitCalculator.kt
@@ -0,0 +1,26 @@
+package blackjack.model
+
+import blackjack.model.state.GameStatus.BLACKJACK
+import blackjack.model.state.GameStatus.BUST
+import blackjack.model.user.Dealer
+import blackjack.model.user.Player
+
+object ProfitCalculator {
+    fun calculateProfit(
+        dealer: Dealer,
+        player: Player,
+    ): BetMoney {
+        val dealerGameStatus = dealer.gameStatus
+        val playerGameStatus = player.gameStatus
+        if (playerGameStatus == BUST) return player.money.toLoseMoney()
+        if (dealerGameStatus == BUST) return player.money.toWinMoney()
+        if (dealerGameStatus == BLACKJACK && playerGameStatus == BLACKJACK) return player.money.toDrawMoney()
+        if (playerGameStatus == BLACKJACK) return player.money.toBlackjackMoney()
+
+        val dealerFinalScore = ScoreCalculator.calculateFinalScore(dealer.cards)
+        val playerFinalScore = ScoreCalculator.calculateFinalScore(player.cards)
+        if (dealerFinalScore < playerFinalScore) return player.money.toWinMoney()
+        if (dealerFinalScore == playerFinalScore) return player.money.toDrawMoney()
+        return player.money.toLoseMoney()
+    }
+}
---
+++ src/test/kotlin/blackjack/model/ProfitCalculatorTest.kt
@@ -0,0 +1,89 @@
+package blackjack.model
+
+import blackjack.CardFixture.Companion.CLOVER_ACE
+import blackjack.CardFixture.Companion.CLOVER_JACK
+import blackjack.CardFixture.Companion.DIAMOND_ACE
+import blackjack.CardFixture.Companion.HEART_ACE
+import blackjack.CardFixture.Companion.HEART_JACK
+import blackjack.CardFixture.Companion.HEART_KING
+import blackjack.CardFixture.Companion.HEART_QUEEN
+import blackjack.model.user.Dealer
+import blackjack.model.user.Player
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class ProfitCalculatorTest {
+    private lateinit var player: Player
+    private lateinit var dealer: Dealer
+
+    @BeforeEach
+    fun setUp() {
+        player = Player("플레이어", BetMoney(1_000L))
+        dealer = Dealer()
+    }
+
+    @Test
+    fun `플레이어가 Bust 일 경우 원금을 잃는다`() {
+        player.addCard(CLOVER_JACK)
+        player.addCard(HEART_QUEEN)
+        player.addCard(HEART_KING)
+        val expected = BetMoney(-1_000L)
+
+        val actual = ProfitCalculator.calculateProfit(dealer, player)
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `플레이어만 Blackjack 이면 원금의 150%를 얻는다`() {
+        player.addCard(CLOVER_JACK)
+        player.addCard(CLOVER_ACE)
+        val expected = BetMoney(1_500L)
+
+        val actual = ProfitCalculator.calculateProfit(dealer, player)
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `플레이어와 딜러가 Blackjack 이면 0원을 얻는다`() {
+        player.addCard(CLOVER_JACK)
+        player.addCard(CLOVER_ACE)
+        dealer.addCard(HEART_QUEEN)
+        dealer.addCard(HEART_ACE)
+        val expected = BetMoney(0L)
+
+        val actual = ProfitCalculator.calculateProfit(dealer, player)
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `딜러가 bust 이고 player가 bust가 아니면 원금을 얻는다`() {
+        player.addCard(CLOVER_ACE)
+        player.addCard(DIAMOND_ACE)
+        player.addCard(HEART_ACE)
+        dealer.addCard(HEART_QUEEN)
+        dealer.addCard(HEART_KING)
+        dealer.addCard(HEART_JACK)
+        val expected = BetMoney(1_000L)
+
+        val actual = ProfitCalculator.calculateProfit(dealer, player)
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `딜러와 Player 모두 bust, blackjack이 아니면 21에 가까운 점수를 기준으로 금액을 반환한다`() {
+        player.addCard(CLOVER_ACE)
+        player.addCard(DIAMOND_ACE)
+        dealer.addCard(HEART_KING)
+        dealer.addCard(HEART_JACK)
+        val expected = BetMoney(-1_000L)
+
+        val actual = ProfitCalculator.calculateProfit(dealer, player)
+
+        assertEquals(expected, actual)
+    }
+}
