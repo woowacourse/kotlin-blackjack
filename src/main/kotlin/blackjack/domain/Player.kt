954eaeee830748bb48d87bdccd91db24aa1f8d42
devfeijoa <devfeijoa@gmail.com>
2025-03-17 02:50:04 +0900

    feat: Player 클래스 구현

--- src/main/kotlin/blackjack/domain/Player.kt
+++ src/main/kotlin/blackjack/domain/Player.kt
@@ -1,5 +1,26 @@
 package blackjack.domain
 
+import blackjack.domain.state.Hit
+import blackjack.domain.state.State
+
 class Player(
     val name: String,
-) : Participant
+    override var state: State,
+    val getMoreCard: (String) -> Boolean,
+) : Participant {
+    override fun drawCard(giveCard: () -> Card) {
+        repeat(2) {
+            state = state.draw(giveCard())
+        }
+    }
+
+    override fun drawMoreCard(
+        giveCards: () -> Card,
+        printCards: (Participant) -> Unit,
+    ) {
+        while (state == Hit(state.hand) && getMoreCard(name)) {
+            state = state.draw(giveCards())
+            printCards(this)
+        }
+    }
+}
