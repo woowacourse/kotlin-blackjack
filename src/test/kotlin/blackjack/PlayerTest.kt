5cd968655ec70ee25260dfa5353ce04f8e185271
DongjooSEO <readyjoo24@gmail.com>
2025-03-16 16:28:16 +0900

    test: PlayerTest 테스트 코드 추가

--- src/test/kotlin/blackjack/PlayerTest.kt
+++ src/test/kotlin/blackjack/PlayerTest.kt
@@ -23,4 +23,19 @@ class PlayerTest {
 
         assertThat(player.canHit()).isEqualTo(false)
     }
+
+    @Test
+    fun `카드의 총합이 21을 넘지 않으면, 카드를 뽑을 수 있다`() {
+        val player = Player("player", BetAmount(10000))
+
+        val card1 = Card.of(rank = Rank.TEN, suit = Suit.SPADE)
+        val card2 = Card.of(rank = Rank.NINE, suit = Suit.SPADE)
+        val card3 = Card.of(rank = Rank.TWO, suit = Suit.HEART)
+
+        player.addCard(card1)
+        player.addCard(card2)
+        player.addCard(card3)
+
+        assertThat(player.canHit()).isEqualTo(true)
+    }
 }
