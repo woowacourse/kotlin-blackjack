0217343bfbafd9240f9c95ce636b79de36b28879
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-03-17 03:29:16 +0900

    refactor: implement TrumpCard

--- src/main/kotlin/blackjack/domain/Deck.kt
+++ src/main/kotlin/blackjack/domain/Deck.kt
@@ -1,28 +1,11 @@
 package blackjack.domain
 
 import blackjack.domain.card.Card
-import blackjack.domain.card.Rank.AceRank
-import blackjack.domain.card.Rank.FaceRank
-import blackjack.domain.card.Rank.NumberRank
-import blackjack.domain.card.Suit
+import blackjack.domain.card.TrumpCard
 
 class Deck(
     private val shuffler: Shuffler,
 ) {
-    private val aceCards: List<Card> = Suit.entries.map { suit -> Card(AceRank, suit) }
-    private val numberCards: List<Card> =
-        Suit.entries.flatMap { suit ->
-            NumberRank.entries.map { number: NumberRank ->
-                Card(number, suit)
-            }
-        }
-    private val faceCards: List<Card> =
-        Suit.entries.flatMap { suit ->
-            FaceRank.entries.map { face: FaceRank -> Card(face, suit) }
-        }
-
-    private val cardPack = aceCards + numberCards + faceCards
-
     private var cards: List<Card> = emptyList()
 
     init {
@@ -40,6 +23,6 @@ class Deck(
     }
 
     private fun refillDeck() {
-        cards = shuffler.shuffle(cardPack)
+        cards = shuffler.shuffle(TrumpCard.getNewCardPack())
     }
 }
