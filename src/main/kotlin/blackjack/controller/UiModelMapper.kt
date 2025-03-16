93dc0c3f32245a19ae111bac7bc4b0ca181bd9bc
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-03-17 02:42:41 +0900

    refactor: update UiModelMapper

--- src/main/kotlin/blackjack/controller/UiModelMapper.kt
+++ src/main/kotlin/blackjack/controller/UiModelMapper.kt
@@ -1,15 +1,24 @@
 package blackjack.controller
 
-import blackjack.domain.Betting
 import blackjack.domain.Card
-import blackjack.domain.Dealer
-import blackjack.domain.Player
+import blackjack.domain.Deck
 import blackjack.domain.Rank
 import blackjack.domain.Suit
+import blackjack.domain.update.NewBetting
+import blackjack.domain.update.participant.NewDealer
+import blackjack.domain.update.participant.NewPlayer
+import blackjack.domain.update.participant.Participant
+import blackjack.domain.update.state.NewParticipantState
 import blackjack.view.model.DealerSummary
 import blackjack.view.model.PlayerConfig
 import blackjack.view.model.PlayerResult
 import blackjack.view.model.PlayerSummary
+
+fun List<PlayerConfig>.toNewPlayers(deck: Deck): List<NewPlayer> = map { playerConfig -> playerConfig.toNewPlayer(deck) }
+
+private fun PlayerConfig.toNewPlayer(deck: Deck): NewPlayer = NewPlayer(name, NewBetting(bettingAmount), deck)
+
+val List<NewPlayer>.names: List<String> get() = map { player -> player.name }
 
 private val Rank.prettyString: String
     get() =
@@ -38,8 +47,10 @@ private val Suit.prettyString: String
             Suit.CLOVER -> "♣"
         }
 
-val List<Player>.names: List<String>
-    get() = map { player -> player.name }
+val NewDealer.cardsPrettyString: List<String> get() = cards.prettyString
+
+val List<Participant>.cardsPrettyStrings: List<List<String>>
+    get() = map { participant -> participant.cards.prettyString }
 
 val List<Card>.prettyString: List<String>
     get() = map { card: Card -> card.prettyString }
@@ -47,27 +58,18 @@ val List<Card>.prettyString: List<String>
 private val Card.prettyString: String
     get() = rank.prettyString + suit.prettyString
 
-val List<Player>.playersCards: List<List<String>>
-    get() = map { player -> player.cards.prettyString }
-
-val List<PlayerConfig>.toPlayers: List<Player>
-    get() = map { playerConfig -> Player(playerConfig.name, Betting(playerConfig.bettingAmount)) }
-
-val Dealer.playersResult: List<PlayerResult>
-    get() =
-        playersName.zip(playersProfit).map { (name, profit) ->
-            PlayerResult(name, profit)
-        }
-
-val Dealer.summary: DealerSummary
+val Participant.summary: DealerSummary
     get() =
         DealerSummary(
-            cards.map { card -> card.prettyString },
+            cards.prettyString,
             score.value,
         )
 
-private val Player.summary: PlayerSummary
+private val NewPlayer.summary: PlayerSummary
     get() = PlayerSummary(name, cards.prettyString, score.value)
 
-val List<Player>.summaries: List<PlayerSummary>
+val List<NewPlayer>.summaries: List<PlayerSummary>
     get() = map { player -> player.summary }
+
+fun List<NewPlayer>.toResults(dealerState: NewParticipantState): List<PlayerResult> =
+    map { player -> PlayerResult(player.name, player.calculateProfit(dealerState).toInt()) }
