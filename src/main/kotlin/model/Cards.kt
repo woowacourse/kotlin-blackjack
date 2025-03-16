0a5233ee5d3a64e246f39c694c14b43a90b185d4
gahyunkim <mapledt001@naver.com>
2025-03-16 16:55:56 +0900

    refactor: 랜덤 셔플링 분리 및 카드 부족 예외 처리 추가

--- src/main/kotlin/model/Cards.kt
+++ src/main/kotlin/model/Cards.kt
@@ -1,6 +1,9 @@
 package model
 
-class Cards(allCards: List<Card>) {
+import util.RandomShuffler
+import util.Shuffler
+
+class Cards(allCards: List<Card>,private val shuffler: Shuffler = RandomShuffler() ) {
     private val _allCards: MutableList<Card> = allCards.toMutableList()
     val allCards: List<Card> get() = _allCards.toList()
 
@@ -9,16 +12,16 @@ class Cards(allCards: List<Card>) {
     }
 
     fun drawCards(count: Int): List<Card> {
-        if (_allCards.isEmpty()) {
-            _allCards.addAll(CardsGenerator().generateCards().allCards)
-        }
+        require(_allCards.size >= count) { REMAINING_CARD_ERROR_MESSAGE }
 
         val drawnCards = _allCards.take(count)
         _allCards.removeAll(drawnCards.toSet())
+
         return drawnCards
     }
 
     companion object {
         private const val DUPLICATE_CARD_ERROR_MESSAGE = "[ERROR] 카드는 중복될 수 없습니다"
+        private const val REMAINING_CARD_ERROR_MESSAGE = "남아있는 카드의 수가 부족합니다"
     }
 }
--- src/test/kotlin/model/CardsTest.kt
+++ src/test/kotlin/model/CardsTest.kt
@@ -1,8 +1,10 @@
 package model
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import util.TestCards
+import util.TestShuffler
 
 class CardsTest {
     @Test
@@ -16,5 +18,24 @@ class CardsTest {
         assertThrows<IllegalArgumentException> {
             Cards(duplicateCards)
         }
+    }
+
+    @Test
+    fun `남아있는 카드보다 더 많은 카드를 뽑으면 예외가 발생한다`() {
+        val cards = Cards(listOf(TestCards.CLUB_ACE), TestShuffler())
+
+        assertThrows<IllegalArgumentException> {cards.drawCards(2) }
+    }
+
+    @Test
+    fun `셔플 없이 카드를 뽑으면 순서가 유지된다`() {
+        val cards = Cards(
+            listOf(TestCards.CLUB_ACE, TestCards.SPADE_KING, TestCards.HEART_QUEEN),
+            TestShuffler()
+        )
+
+        val drawnCards = cards.drawCards(2)
+
+        assertThat(drawnCards).containsExactly(TestCards.CLUB_ACE, TestCards.SPADE_KING)
     }
 }
