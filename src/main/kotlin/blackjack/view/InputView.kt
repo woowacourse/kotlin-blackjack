7302335a9dfd491edb8fa5d5a5495171480c12ad
devfeijoa <devfeijoa@gmail.com>
2025-03-17 02:50:56 +0900

    feat: InputView 구현

--- src/main/kotlin/blackjack/view/InputView.kt
+++ src/main/kotlin/blackjack/view/InputView.kt
@@ -1,11 +1,20 @@
 package blackjack.view
 
-import blackjack.domain.Player
-
 class InputView {
-    fun readPlayersName(): List<Player> {
+    fun readPlayersName(): List<String> {
         println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)")
-        val players = readln().split(",").map { name: String -> Player(name.trim()) }
+        val players = readln().split(",").map { name: String -> name.trim() }
         return players
+    }
+
+    fun readPlayersBattingAmount(playerName: String): Int {
+        println("\n${playerName}의 배팅 금액은?")
+        return readln().toInt()
+    }
+
+    fun askDrawCard(playerName: String): String {
+        println("%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)".format(playerName))
+        return readln().trim()
+        // y이면 true로 바꿔주는 로직 어디에....?
     }
 }
