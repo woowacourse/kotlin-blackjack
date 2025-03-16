52c98f9a2110f83798f434c9d6dcbef4281a97fa
yrsel <xxknu88@gmail.com>
2025-03-17 08:05:16 +0900

    refactor: 이미 생성된 카드가 존재하면 캐싱된 카드 사용

--- src/main/kotlin/blackjack/model/card/DeckFactory.kt
+++ src/main/kotlin/blackjack/model/card/DeckFactory.kt
@@ -2,17 +2,18 @@ package blackjack.model.card
 
 object DeckFactory {
     fun createDeck(): ArrayDeque<Card> {
-        val cards = generateCards().shuffled()
+        val cards = CARDS.shuffled()
         return ArrayDeque<Card>().create(cards)
     }
 
-    private fun generateCards(): MutableList<Card> =
+    private val CARDS: List<Card> by lazy {
         Shape.entries
             .flatMap { shape ->
                 CardNumber.entries.map { number ->
                     Card(shape, number)
                 }
-            }.toMutableList()
+            }.toList()
+    }
 
     private fun <T> ArrayDeque<T>.create(elements: List<T>): ArrayDeque<T> {
         this.addAll(elements)
