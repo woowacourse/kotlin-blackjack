a56f62c8bd993080f9f797cd7239f8a61a00297d
gahyunkim <mapledt001@naver.com>
2025-03-17 14:32:08 +0900

    test: HandTest에 점수 계산 테스트 코드 추가

--- src/test/kotlin/model/HandTest.kt
+++ src/test/kotlin/model/HandTest.kt
@@ -1,6 +1,7 @@
 package model
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import util.TestCards
 
@@ -52,5 +53,78 @@ class HandTest {
         val updatedHandCount = Hand(initialCards).getCardsCount()
 
         assertThat(updatedHandCount).isEqualTo(2)
+    }
+
+    @Test
+    fun `보유한 카드의 점수를 계산한다`() {
+        val cards = listOf(TestCards.CLUB_SIX, TestCards.HEART_SEVEN)
+
+        val totalScore = Hand(cards).getTotalScore()
+
+        assertEquals(totalScore, 13)
+    }
+
+    @Test
+    fun `ACE가 포함된 카드를 계산할 수 있다`() {
+        val cards = listOf(TestCards.CLUB_QUEEN, TestCards.HEART_NINE, TestCards.HEART_ACE)
+
+        val totalScore = Hand(cards).getTotalScore()
+
+        assertEquals(totalScore, 20)
+    }
+
+    @Test
+    fun `ACE가 여러개 포함되었을때의 값을 계산할 수 있다`() {
+        val cards =
+            listOf(
+                TestCards.CLUB_ACE,
+                TestCards.HEART_ACE,
+                TestCards.SPADE_ACE,
+                TestCards.SPADE_TEN,
+                TestCards.SPADE_EIGHT,
+            )
+
+        val totalScore = Hand(cards).getTotalScore()
+
+        assertEquals(totalScore, 21)
+    }
+
+    @Test
+    fun `ACE카드를 제외하고 보유한 카드의 합이 20일 때 ACE는 1이다`() {
+        val cards = listOf(TestCards.CLUB_ACE, TestCards.SPADE_TEN, TestCards.HEART_TEN)
+
+        val totalScore = Hand(cards).getTotalScore()
+
+        assertEquals(totalScore, 21)
+    }
+
+    @Test
+    fun `ACE카드를 제외하고 보유한 카드의 합이 10일 때 ACE는 11이다`() {
+        val cards = listOf(TestCards.CLUB_ACE, TestCards.SPADE_FIVE, TestCards.HEART_FIVE)
+
+        val totalScore = Hand(cards).getTotalScore()
+
+        assertEquals(totalScore, 21)
+    }
+
+    @Test
+    fun `ACE가 포함되었을 때 18임을 확인할 수 있다`() {
+        val cards = listOf(TestCards.CLUB_ACE, TestCards.SPADE_ACE, TestCards.SPADE_ACE, TestCards.HEART_FIVE)
+
+        val totalScore = Hand(cards).getTotalScore()
+
+        assertEquals(totalScore, 18)
+    }
+
+    @Test
+    fun `ACE가 포함되었을 때 21임을 확인할 수 있다`() {
+        val cards1 = listOf(TestCards.CLUB_ACE, TestCards.SPADE_ACE, TestCards.HEART_NINE)
+        val cards2 = listOf(TestCards.CLUB_ACE, TestCards.HEART_KING)
+
+        val totalScore1 = Hand(cards1).getTotalScore()
+        val totalScore2 = Hand(cards2).getTotalScore()
+
+        assertEquals(totalScore1, 21)
+        assertEquals(totalScore2, 21)
     }
 }
