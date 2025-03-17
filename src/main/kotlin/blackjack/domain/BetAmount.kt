b7db99d408a9ec287f3ffcef0d7fdd9216e5e048
DongjooSEO <readyjoo24@gmail.com>
2025-03-17 14:20:02 +0900

    feat: BetAmount 예외 처리 로직 구현

--- src/main/kotlin/blackjack/domain/BetAmount.kt
+++ src/main/kotlin/blackjack/domain/BetAmount.kt
@@ -1,6 +1,10 @@
 package blackjack.domain
 
 class BetAmount(private var amount: Int) {
+    init {
+        require(amount > 0) { "배팅 금액은 0원 이상 입력해주세요." }
+    }
+
     fun getAmount(): Int = amount
 
     fun update(result: PlayerResultStatus) {
--- src/test/kotlin/blackjack/BetAmountTest.kt
+++ src/test/kotlin/blackjack/BetAmountTest.kt
@@ -3,9 +3,19 @@ package blackjack
 import blackjack.domain.BetAmount
 import blackjack.domain.PlayerResultStatus
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
 
 class BetAmountTest {
+    @Test
+    fun `배팅 금액에 음수가 입력되면 예외가 발생한다`() {
+        assertThatThrownBy {
+            BetAmount(-1)
+        }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("배팅 금액은 0원 이상 입력해주세요.")
+    }
+
     @Test
     fun `BLACKJACK_WIN 일때, 배팅 금액의 1․5배를 받는다`() {
         val betAmount = BetAmount(10000)
