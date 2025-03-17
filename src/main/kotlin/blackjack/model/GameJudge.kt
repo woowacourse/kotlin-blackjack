e6d09e1ad36e6f6835eb6e37e56360401a0e76b5
yrsel <xxknu88@gmail.com>
2025-03-17 16:18:57 +0900

    chore: 불필요한 변수 선언 제거

--- src/main/kotlin/blackjack/model/GameJudge.kt
+++ src/main/kotlin/blackjack/model/GameJudge.kt
@@ -3,8 +3,5 @@ package blackjack.model
 import blackjack.model.state.GameStatus
 
 object GameJudge {
-    fun judge(hand: Hand): GameStatus {
-        val score = hand.score
-        return GameStatus.of(score, hand.cards.size)
-    }
+    fun judge(hand: Hand): GameStatus = GameStatus.of(hand.score, hand.cards.size)
 }
