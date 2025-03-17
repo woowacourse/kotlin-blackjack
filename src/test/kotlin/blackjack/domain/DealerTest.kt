e62f8ad7df65650ed7fd92958b8d6d779d979a3f
leeyerin0210 <lyl8661@gmail.com>
2025-03-17 17:07:03 +0900

    test : 사용하지 않는 기능 관련 테스트 함수 제거

--- src/test/kotlin/blackjack/domain/DealerTest.kt
+++ src/test/kotlin/blackjack/domain/DealerTest.kt
@@ -1,19 +1,10 @@
 package blackjack.domain
 
 import blackjack.model.BlackjackEngine
-import blackjack.model.Card
-import blackjack.model.CardRank
-import blackjack.model.CardSuit
-import blackjack.model.Dealer
-import blackjack.model.Hand
-import blackjack.model.Player
-import blackjack.model.WinningResult
 import blackjack.view.InputView
 import blackjack.view.OutputView
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertAll
 
 class DealerTest {
     @Test
@@ -29,48 +20,5 @@ class DealerTest {
 
         // then
         assertThat(dealer.items.hand.score() > 16 || dealer.items.hand.isBust()).isTrue()
-    }
-
-    @Test
-    fun `딜러 점수와 플레이어 점수 리스트를 비교하여 승패 결과를 반환한다`() {
-        // given
-        val dealer =
-            Dealer.makeDealer(
-                Hand(
-                    listOf(
-                        Card.getCashed(CardRank.TWO, CardSuit.CLUB),
-                        Card.getCashed(CardRank.THREE, CardSuit.CLUB),
-                    ),
-                ),
-            )
-
-        // when
-        val losePlayer =
-            Player.makePlayer(
-                "패배",
-                Hand(listOf(Card.getCashed(CardRank.TWO, CardSuit.CLUB), Card.getCashed(CardRank.TWO, CardSuit.CLUB))),
-            )
-        val pushPlayer =
-            Player.makePlayer(
-                "동점",
-                Hand(
-                    listOf(
-                        Card.getCashed(CardRank.TWO, CardSuit.CLUB),
-                        Card.getCashed(CardRank.THREE, CardSuit.CLUB),
-                    ),
-                ),
-            )
-        val winningPlayer =
-            Player.makePlayer(
-                "승리",
-                Hand(listOf(Card.getCashed(CardRank.TWO, CardSuit.CLUB), Card.getCashed(CardRank.ACE, CardSuit.CLUB))),
-            )
-
-        // then
-        assertAll(
-            { assertEquals(dealer.compareHand(winningPlayer), WinningResult.LOSE) },
-            { assertEquals(dealer.compareHand(pushPlayer), WinningResult.PUSH) },
-            { assertEquals(dealer.compareHand(losePlayer), WinningResult.WIN) },
-        )
     }
 }
