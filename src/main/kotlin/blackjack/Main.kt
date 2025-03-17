28139cdb7e4ef363d7923a15020de13ae4a3a919
devfeijoa <devfeijoa@gmail.com>
2025-03-17 17:02:36 +0900

    main 함수 구현

--- src/main/kotlin/blackjack/Main.kt
+++ src/main/kotlin/blackjack/Main.kt
@@ -1,3 +1,13 @@
 package blackjack
 
-fun main() {}
+import blackjack.controller.Controller
+import blackjack.view.InputView
+import blackjack.view.OutputView
+
+fun main() {
+    val inputView = InputView()
+    val outputView = OutputView()
+
+    val controller = Controller(inputView, outputView)
+    controller.gameStart()
+}
