221d9687f373fcd25edc0d3ed2adf705891ce6c3
cucumber99 <dlsguq1@gmail.com>
2025-03-17 16:13:58 +0900

    refactor: ResultState 개선
    - 더 이상 승패를 계산하지 않습니다.

--- src/main/kotlin/blackjack/domain/state/ResultState.kt
+++ src/main/kotlin/blackjack/domain/state/ResultState.kt
@@ -1,31 +1,8 @@
 package blackjack.domain.state
 
-import blackjack.domain.participants.Dealer
-import blackjack.domain.participants.Player
-
-enum class ResultState {
-    WIN,
-    BLACKJACK_WIN,
-    LOSE,
-    DRAW,
-    ;
-
-    companion object {
-        fun from(
-            player: Player,
-            dealer: Dealer,
-        ): ResultState {
-            val playerScore = player.score()
-            val dealerScore = dealer.score()
-
-            return when {
-                player.isBlackjack() -> BLACKJACK_WIN
-                player.isBust() -> LOSE
-                dealer.isBust() -> WIN
-                playerScore > dealerScore -> WIN
-                playerScore < dealerScore -> LOSE
-                else -> DRAW
-            }
-        }
-    }
+enum class ResultState(val profitRate: Double) {
+    WIN(1.0),
+    BLACKJACK_WIN(1.5),
+    LOSE(-1.0),
+    DRAW(0.0),
 }
