3253738fec9916faf293e9db84aeae417602122f
junseo511 <prime54@g.skku.edu>
2025-03-17 16:25:58 +0900

    test: 우승 결과 반환 테스트 추가

--- src/test/kotlin/blackjack/domain/participant/PlayerTest.kt
+++ src/test/kotlin/blackjack/domain/participant/PlayerTest.kt
@@ -1,10 +1,13 @@
 package blackjack.domain.participant
 
 import blackjack.domain.ACE_HEART
+import blackjack.domain.SIX_HEART
 import blackjack.domain.TEN_HEART
 import blackjack.model.hand.Hand
+import blackjack.model.participant.Dealer
 import blackjack.model.participant.Name
 import blackjack.model.participant.Player
+import blackjack.model.winning.WinningState
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
@@ -60,5 +63,125 @@ class PlayerTest {
         // when & then
         assertTrue(alivePlayer.isDrawable)
         assertFalse(bustPlayer.isDrawable)
+    }
+
+    @Test
+    fun `플레이어가 블랙잭이고 딜러가 블랙잭이 아니면 승리한다`() {
+        // given
+        val player =
+            Player.create(
+                Name("플레이어"),
+                hand = Hand(listOf(ACE_HEART, TEN_HEART)),
+            )
+        val dealer =
+            Dealer.create(
+                Name("딜러"),
+                hand = Hand(listOf(SIX_HEART, TEN_HEART)),
+            )
+
+        // when
+        val result = player.winningState(dealer)
+
+        // then
+        assertEquals(WinningState.WIN_BY_BLACKJACK, result)
+    }
+
+    @Test
+    fun `플레이어와 딜러가 블랙잭이면 무승부한다`() {
+        // given
+        val player =
+            Player.create(
+                Name("플레이어"),
+                hand = Hand(listOf(ACE_HEART, TEN_HEART)),
+            )
+        val dealer =
+            Dealer.create(
+                Name("딜러"),
+                hand = Hand(listOf(ACE_HEART, TEN_HEART)),
+            )
+
+        // when
+        val result = player.winningState(dealer)
+
+        // then
+        assertEquals(WinningState.PUSH, result)
+    }
+
+    @Test
+    fun `딜러가 블랙잭이면 플레이어는 패배한다`() {
+        // given
+        val player =
+            Player.create(
+                Name("플레이어"),
+                hand = Hand(listOf(TEN_HEART)),
+            )
+        val dealer =
+            Dealer.create(
+                Name("딜러"),
+                hand = Hand(listOf(ACE_HEART, TEN_HEART)),
+            )
+
+        // when
+        val result = player.winningState(dealer)
+
+        // then
+        assertEquals(WinningState.LOSE, result)
+    }
+
+    @Test
+    fun `플레이어가 버스트면 패배한다`() {
+        // given
+        val player =
+            Player.create(
+                Name("플레이어"),
+                hand = Hand(listOf(TEN_HEART, TEN_HEART, TEN_HEART)),
+            )
+        val dealer =
+            Dealer.create(Name("딜러"))
+
+        // when
+        val result = player.winningState(dealer)
+
+        // then
+        assertEquals(WinningState.LOSE, result)
+    }
+
+    @Test
+    fun `플레이어가 생존하고 딜러가 버스트면 승리한다`() {
+        // given
+        val player =
+            Player.create(Name("플레이어"))
+        val dealer =
+            Dealer.create(
+                Name("딜러"),
+                hand = Hand(listOf(TEN_HEART, TEN_HEART, TEN_HEART)),
+            )
+
+        // when
+        val result = player.winningState(dealer)
+
+        // then
+        assertEquals(WinningState.WIN_DEFAULT, result)
+    }
+
+    @Test
+    fun `플레이어와 딜러가 버스트면 플레이어가 패배한다`() {
+        // given
+        val player =
+            Player.create(
+                Name("플레이어"),
+                hand = Hand(listOf(TEN_HEART, TEN_HEART, TEN_HEART)),
+            )
+        val dealer =
+            Dealer.create(
+                Name("딜러"),
+                hand = Hand(listOf(TEN_HEART, TEN_HEART, TEN_HEART)),
+            )
+
+        // when
+        val result = player.winningState(dealer)
+
+        // then
+        assertEquals(WinningState.LOSE, result)
     }
 }
