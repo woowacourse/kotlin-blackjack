b73b505cf1d91aeabc49f5bad2919425f538e49f
cucumber99 <dlsguq1@gmail.com>
2025-03-17 16:14:39 +0900

    refactor: Dealer 클래스 개선
    - 승패 여부, 수익금을 계산합니다.

--- src/main/kotlin/blackjack/domain/participants/Dealer.kt
+++ src/main/kotlin/blackjack/domain/participants/Dealer.kt
@@ -1,17 +1,45 @@
 package blackjack.domain.participants
 
+import blackjack.domain.betting.BettingAmount
+import blackjack.domain.betting.BettingInfo
+import blackjack.domain.betting.ProfitAmount
 import blackjack.domain.card.Card
 import blackjack.domain.card.Deck
+import blackjack.domain.state.ResultState
 
 class Dealer(
-    private val deck: Deck,
+    private val deck: Deck = Deck.createDefaultDeck(),
     initialHand: List<Card> = emptyList(),
 ) : Participant(initialHand) {
-    override fun canHit(): Boolean = score() <= DRAW_SCORE
+    override fun canHit(): Boolean = score <= DRAW_SCORE
 
     fun handOut(participant: Participant) {
         val card = drawFromDeck()
         participant.addCard(card)
+    }
+
+    fun profitFromGame(bettingInfo: BettingInfo): ProfitAmount {
+        val result = determineResult(bettingInfo.player)
+        return calculateProfit(bettingInfo.bettingAmount, result)
+    }
+
+    private fun determineResult(player: Player): ResultState {
+        return when {
+            player.isBlackjack() -> ResultState.BLACKJACK_WIN
+            player.isBust() -> ResultState.LOSE
+            this.isBust() -> ResultState.WIN
+            player.score > this.score -> ResultState.WIN
+            player.score < this.score -> ResultState.LOSE
+            else -> ResultState.DRAW
+        }
+    }
+
+    private fun calculateProfit(
+        bettingAmount: BettingAmount,
+        resultState: ResultState,
+    ): ProfitAmount {
+        val profit = bettingAmount.value * resultState.profitRate
+        return ProfitAmount(profit)
     }
 
     private fun drawFromDeck(): Card = deck.draw()
--- src/test/kotlin/blackjack/domain/participants/DealerTest.kt
+++ src/test/kotlin/blackjack/domain/participants/DealerTest.kt
@@ -1,53 +1,17 @@
 package blackjack.domain.participants
 
+import blackjack.domain.betting.BettingAmount
+import blackjack.domain.betting.BettingInfo
 import blackjack.domain.card.Card
 import blackjack.domain.card.CardNumber
 import blackjack.domain.card.CardPattern
 import blackjack.domain.card.Deck
 import io.kotest.assertions.assertSoftly
-import io.kotest.matchers.collections.shouldContain
 import io.kotest.matchers.ints.shouldBeLessThanOrEqual
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 
 class DealerTest {
-    @Test
-    fun `카드를 받을 수 있다`() {
-        // Given
-        val deck = Deck.createCustomDeck(mutableListOf(Card(CardNumber.ACE, CardPattern.HEART)))
-        val dealer = Dealer(deck)
-        val card = deck.draw()
-
-        // When
-        dealer.addCard(card)
-
-        // Then
-        dealer.hand shouldContain card
-    }
-
-    @Test
-    fun `덱에서 카드를 한 장 나눠줄 수 있다`() {
-        // Given
-        val deck = Deck.createCustomDeck(mutableListOf(Card(CardNumber.ACE, CardPattern.HEART)))
-        val dealer = Dealer(deck)
-
-        // When
-        dealer.handOut(dealer)
-
-        // Then
-        deck.cards.size shouldBe 0
-    }
-
-    @Test
-    fun `가지고 있는 패의 총 합을 계산한다`() {
-        // Given
-        val cards = List(2) { Card(CardNumber.KING, CardPattern.HEART) }
-        val dealer = Dealer(Deck.createDefaultDeck(), cards)
-
-        // Then
-        dealer.score() shouldBe 20
-    }
-
     @Test
     fun `초기 상태일 경우 카드를 2장 받는다`() {
         val dealer = Dealer(Deck.createDefaultDeck())
@@ -66,7 +30,7 @@ class DealerTest {
 
         // Then
         assertSoftly(dealer) {
-            score() shouldBeLessThanOrEqual 16
+            score shouldBeLessThanOrEqual 16
             canHit() shouldBe true
             getDrawAmount() shouldBe 1
         }
@@ -83,5 +47,61 @@ class DealerTest {
             isBust() shouldBe true
             canHit() shouldBe false
         }
+    }
+
+    @Test
+    fun `플레이어의 점수가 더 큰 경우 플레이어는 베팅 금액만큼 받는다`() {
+        // Given
+        val player = Player("test", listOf(Card(CardNumber.ACE, CardPattern.CLOVER)))
+        val bettingInfo = BettingInfo(player, BettingAmount(10000))
+        val dealer = Dealer(initialHand = listOf(Card(CardNumber.KING, CardPattern.CLOVER)))
+
+        // When
+        val result = dealer.profitFromGame(bettingInfo)
+
+        // Then
+        result.value shouldBe 10000.0
+    }
+
+    @Test
+    fun `플레이어의 점수가 더 작은 경우 플레이어는 베팅 금액만큼 잃는다`() {
+        // Given
+        val player = Player("test", listOf(Card(CardNumber.KING, CardPattern.CLOVER)))
+        val bettingInfo = BettingInfo(player, BettingAmount(10000))
+        val dealer = Dealer(initialHand = listOf(Card(CardNumber.ACE, CardPattern.CLOVER)))
+
+        // When
+        val result = dealer.profitFromGame(bettingInfo)
+
+        // Then
+        result.value shouldBe -10000.0
+    }
+
+    @Test
+    fun `점수가 같은 경우 플레이어는 0원을 받는다`() {
+        // Given
+        val player = Player("test", listOf(Card(CardNumber.ACE, CardPattern.CLOVER)))
+        val bettingInfo = BettingInfo(player, BettingAmount(10000))
+        val dealer = Dealer(initialHand = listOf(Card(CardNumber.ACE, CardPattern.SPADE)))
+
+        // When
+        val result = dealer.profitFromGame(bettingInfo)
+
+        // Then
+        result.value shouldBe 0.0
+    }
+
+    @Test
+    fun `플레이어가 블랙잭인 경우 플레이어는 베팅 금액의 1․5배만큼 받는다`() {
+        // Given
+        val player = Player("test", listOf(Card(CardNumber.ACE, CardPattern.CLOVER), Card(CardNumber.KING, CardPattern.CLOVER)))
+        val bettingInfo = BettingInfo(player, BettingAmount(10000))
+        val dealer = Dealer(initialHand = listOf(Card(CardNumber.KING, CardPattern.SPADE)))
+
+        // When
+        val result = dealer.profitFromGame(bettingInfo)
+
+        // Then
+        result.value shouldBe 15000.0
     }
 }
