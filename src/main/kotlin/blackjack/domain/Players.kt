9597fa06e86294a42bd1d5b2437421fa5b12d4fe
devfeijoa <devfeijoa@gmail.com>
2025-03-17 17:01:18 +0900

    feat: 플레이어 수 제한하는 기능

--- src/main/kotlin/blackjack/domain/Players.kt
+++ src/main/kotlin/blackjack/domain/Players.kt
@@ -5,6 +5,10 @@ import blackjack.domain.state.Ready
 class Players(
     private val _players: List<Player>,
 ) {
+    init {
+        require(_players.size in 1..8) { "참가자는 최소 1명에서 최대 8명까지만 가능합니다." }
+    }
+
     val players: List<Player>
         get() = _players
 
