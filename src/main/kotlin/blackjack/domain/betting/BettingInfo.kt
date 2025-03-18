6dc38eef01ab342f4769b578f96840a2f307e947
cucumber99 <dlsguq1@gmail.com>
2025-03-19 01:16:27 +0900

    feat: 수익금 계산 로직 추가

--- src/main/kotlin/blackjack/domain/betting/BettingInfo.kt
+++ src/main/kotlin/blackjack/domain/betting/BettingInfo.kt
@@ -1,8 +1,13 @@
 package blackjack.domain.betting
 
 import blackjack.domain.participants.Player
+import blackjack.domain.state.ResultState
 
 data class BettingInfo(
     val player: Player,
     val bettingAmount: BettingAmount,
-)
+) {
+    fun calculateProfitAmount(resultState: ResultState): ProfitAmount {
+        return ProfitAmount(bettingAmount.value * resultState.profitRate)
+    }
+}
