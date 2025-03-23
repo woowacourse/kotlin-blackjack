5d0017d542949a678bd53877aa62df75409c7d62
devfeijoa <devfeijoa@gmail.com>
2025-03-24 02:30:24 +0900

    refactor: Hand에서 blackjack과 bust 확인 가능하게 구현

--- src/main/kotlin/blackjack/domain/Hand.kt
+++ src/main/kotlin/blackjack/domain/Hand.kt
@@ -1,20 +1,36 @@
 package blackjack.domain
 
 class Hand(
-    val cards: List<Card>,
+    cards: List<Card>,
     val money: Int,
 ) {
+    private val _cards: MutableList<Card> = cards.toMutableList()
+
+    val cards: List<Card>
+        get() = _cards
+
     val size = cards.size
 
+    fun addCard(card: Card) {
+        _cards.add(card)
+    }
+
     fun getTotalScore(): Int {
-        val score = cards.sumOf { it.getScore() }
-        if (cards.any { it.hasAce() && score + 10 <= 21 }) {
+        val score = _cards.sumOf { it.getScore() }
+        if (_cards.any { it.hasAce() && score + 10 <= 21 }) {
             return score + 10
         }
         return score
     }
 
-    fun getProfitMoney(rate: Double): Double = rate * money
+    fun hasBlackjack(): Boolean = _cards.size == BLACKJACK_SIZE && getTotalScore() == BLACKJACK_SCORE && hasAce()
 
-    fun findWinner(dealerTotal: Int): Int = this.getTotalScore().compareTo(dealerTotal)
+    fun hasBust(): Boolean = getTotalScore() > BLACKJACK_SCORE
+
+    private fun hasAce(): Boolean = _cards.any { it.hasAce() }
+
+    companion object {
+        const val BLACKJACK_SIZE = 2
+        const val BLACKJACK_SCORE = 21
+    }
 }
