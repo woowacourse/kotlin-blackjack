7c78af7cc845ec98ecdf5ef335e34aab8049a800
devfeijoa <devfeijoa@gmail.com>
2025-03-24 20:10:43 +0900

    refactor: Hit 상태 세부 구현 수정

--- src/main/kotlin/blackjack/domain/state/Hit.kt
+++ src/main/kotlin/blackjack/domain/state/Hit.kt
@@ -7,24 +7,21 @@ class Hit(
     override val hand: Hand,
 ) : State {
     override fun draw(card: Card): State {
-        val hand = Hand(hand.cards + card, hand.money)
-        if (hand.cards.size == BLACKJACK_SIZE && hand.getTotalScore() == BLACKJACK_SCORE) {
-            return Blackjack(hand)
+        hand.addCard(card)
+        return when {
+            hand.hasBust() -> Bust(hand)
+            hand.hasBlackjack() -> Blackjack(hand)
+            else -> this
         }
-        if (hand.getTotalScore() > BLACKJACK_SCORE) {
-            return Bust(hand)
-        }
-
-        return Hit(hand)
     }
 
     override fun canDrawCard(): Boolean = hand.getTotalScore() <= DEALER_DRAW_CARD_STANDARD
 
-    override fun profit(dealerTotal: Int): Int = hand.getProfitMoney(hand.findWinner(dealerTotal).toDouble()).toInt()
+    override fun profit(state: State): Double = 0.0
+
+    fun changeStay(): State = Stay(hand)
 
     companion object {
-        const val BLACKJACK_SIZE = 2
-        const val BLACKJACK_SCORE = 21
         const val DEALER_DRAW_CARD_STANDARD = 16
     }
 }
