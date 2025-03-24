7650210bfa82cfb4c04382290f32026cb055c21f
devfeijoa <devfeijoa@gmail.com>
2025-03-24 20:21:20 +0900

    refactor: Players 수정

--- src/main/kotlin/blackjack/domain/Players.kt
+++ src/main/kotlin/blackjack/domain/Players.kt
@@ -1,7 +1,5 @@
 package blackjack.domain
 
-import blackjack.domain.state.Ready
-
 class Players(
     private val _players: List<Player>,
 ) {
@@ -12,27 +10,11 @@ class Players(
     val players: List<Player>
         get() = _players
 
-    companion object {
-        fun from(
-            names: List<String>,
-            getMoreCard: (String) -> Boolean,
-            battingAmountProvider: (String) -> Int,
-        ): Players =
-            Players(
-                names.map { name ->
-                    Player(name, Ready(Hand(emptyList(), battingAmountProvider(name))), getMoreCard)
-                },
-            )
-    }
-
-    fun initializeCards(giveCard: () -> Card) {
-        _players.forEach { it.drawInitialCards(giveCard) }
-    }
-
-    fun drawMoreCards(
-        giveCard: () -> Card,
-        printCards: (String, Hand) -> Unit,
-    ) {
-        _players.forEach { it.drawAdditionalCards(giveCard, printCards) }
+    fun from(names: List<String>): Players {
+        val players =
+            names.map { name ->
+                Player(name, bettingAmount)
+            }
+        return Players(players)
     }
 }
