29be6a2c8c806ca22885daad5480a4a7774be587
devfeijoa <devfeijoa@gmail.com>
2025-03-24 20:02:52 +0900

    refactor: Dealer가 뽑은 카드가 한 장만 보이게 수정

--- src/main/kotlin/blackjack/domain/Dealer.kt
+++ src/main/kotlin/blackjack/domain/Dealer.kt
@@ -1,24 +1,29 @@
 package blackjack.domain
 
-import blackjack.domain.state.Ready
-import blackjack.domain.state.State
+import blackjack.domain.state.Hit
 
-class Dealer(
-    override var state: State = Ready(Hand(emptyList(), 0)),
-) : Participant {
-    override fun drawCard(giveCard: () -> Card) {
-        repeat(2) {
-            state = state.draw(giveCard())
+class Dealer : Participant() {
+    var profit: Double = 0.0
+        private set
+
+    fun drawInitialCards(deck: Deck) {
+        repeat(2) { drawCard(deck.draw()) }
+    }
+
+    fun getVisibleCard(): Card = state.hand.cards.first()
+
+    override fun drawMoreCard(): Boolean = state.hand.getTotalScore() < 17
+
+    override fun stay() {
+        if (state is Hit) {
+            state = (state as Hit).changeStay()
         }
     }
 
-    override fun drawMoreCard(
-        giveCard: () -> Card,
-        printCards: (Participant) -> Unit,
-    ) {
-        while (state.canDrawCard()) {
-            state = state.draw(giveCard())
-            printCards(this)
+    fun playTurn(deck: Deck) {
+        while (drawMoreCard()) {
+            drawCard(deck.draw())
         }
+        stay()
     }
 }
